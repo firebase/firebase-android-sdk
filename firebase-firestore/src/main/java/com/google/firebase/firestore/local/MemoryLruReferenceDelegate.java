cdeda33e7aafc3e26d4bc8e950495ca1a86d8ee0
Greg Soltis <gsoltis@google.com>
2018-09-17 16:18:26 -0700

    removeQueries -> removeTargets (#23)
    Requested rename from firebase/firebase-js-sdk#1224, targets is a better name than queries for internal portions of the SDK.

--- firebase-firestore/src/main/java/com/google/firebase/firestore/local/MemoryLruReferenceDelegate.java
+++ firebase-firestore/src/main/java/com/google/firebase/firestore/local/MemoryLruReferenceDelegate.java
@@ -93,7 +93,7 @@ class MemoryLruReferenceDelegate implements ReferenceDelegate, LruDelegate {
   }
 
   @Override
-  public int removeQueries(long upperBound, Set<Integer> activeTargetIds) {
+  public int removeTargets(long upperBound, Set<Integer> activeTargetIds) {
     return persistence.getQueryCache().removeQueries(upperBound, activeTargetIds);
   }
 
