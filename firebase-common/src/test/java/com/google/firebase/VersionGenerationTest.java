b63e847793097f676641bd9ddf144a9fba412816
vinay <vguthal@google.com>
2019-01-22 11:41:29 -0500

    add test

---
+++ firebase-common/src/test/java/com/google/firebase/VersionGenerationTest.java
@@ -0,0 +1,14 @@
+package com.google.firebase;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.Test;
+
+public class VersionGenerationTest {
+
+  @Test
+  public void isVersionGenerated_shouldNotBeEmpty() {
+    assertThat(BuildConfig.VERSION_NAME).isNotEmpty();
+  }
+
+}
