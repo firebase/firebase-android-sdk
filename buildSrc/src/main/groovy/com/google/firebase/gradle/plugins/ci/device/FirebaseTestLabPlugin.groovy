2317ed555737d66ef44f5d72add81ebc2f74f012
Vladimir Kryachko <vkryachko@google.com>
2019-03-21 14:21:33 -0400

    Import cleanup.

--- buildSrc/src/main/groovy/com/google/firebase/gradle/plugins/ci/device/FirebaseTestLabPlugin.groovy
+++ buildSrc/src/main/groovy/com/google/firebase/gradle/plugins/ci/device/FirebaseTestLabPlugin.groovy
@@ -14,9 +14,8 @@
 
 package com.google.firebase.gradle.plugins.ci.device
 
-import com.android.build.gradle.LibraryExtension
 import org.gradle.api.Plugin
-import org.gradle.api.Project;
+import org.gradle.api.Project
 
 class FirebaseTestLabPlugin implements Plugin<Project>{
     @Override
