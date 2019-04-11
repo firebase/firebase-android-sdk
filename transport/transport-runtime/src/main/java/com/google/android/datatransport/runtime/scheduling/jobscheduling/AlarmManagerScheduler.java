f4196eac610cdf7361e3a92bea6482f22c7fee72
VinayGuthal <vinayg@qhode.com>
2019-04-11 10:24:09 -0400

    Add alarmManager Scheduler (#344)
    * add javadocs and tests
    
    * alarm manager
    
    * fix copyright test
    
    * use alarm manager broadcast receiver

--- transport/transport-runtime/src/main/java/com/google/android/datatransport/runtime/scheduling/jobscheduling/AlarmManagerScheduler.java
+++ transport/transport-runtime/src/main/java/com/google/android/datatransport/runtime/scheduling/jobscheduling/AlarmManagerScheduler.java
@@ -1,4 +1,4 @@
-// Copyright 2018 Google LLC
+// Copyright 2019 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,14 +14,22 @@
 
 package com.google.android.datatransport.runtime.scheduling.jobscheduling;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.RestrictTo;
+import android.support.annotation.VisibleForTesting;
+import com.google.android.datatransport.runtime.BuildConfig;
 import com.google.android.datatransport.runtime.TransportContext;
 import com.google.android.datatransport.runtime.scheduling.persistence.EventStore;
 import com.google.android.datatransport.runtime.time.Clock;
 import javax.inject.Inject;
 
 /**
- * Schedules the AlarmManager service based on the backendname. Used for Api levels 20 and below.
+ * Schedules the service {@link AlarmManagerSchedulerBroadcastReceiver} based on the backendname.
+ * Used for Apis 20 and below.
  */
 public class AlarmManagerScheduler implements WorkScheduler {
 
@@ -31,13 +39,65 @@ public class AlarmManagerScheduler implements WorkScheduler {
 
   private final Clock clock;
 
+  private AlarmManager alarmManager;
+
+  private final int DELTA = 30000; // 30 seconds delta
+
   @Inject
   public AlarmManagerScheduler(Context applicationContext, EventStore eventStore, Clock clock) {
     this.context = applicationContext;
     this.eventStore = eventStore;
     this.clock = clock;
+    this.alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+    ;
   }
 
+  @RestrictTo(RestrictTo.Scope.TESTS)
+  @VisibleForTesting
+  AlarmManagerScheduler(
+      Context applicationContext, EventStore eventStore, Clock clock, AlarmManager alarmManager) {
+    this.context = applicationContext;
+    this.eventStore = eventStore;
+    this.clock = clock;
+    this.alarmManager = alarmManager;
+  }
+
+  @VisibleForTesting
+  boolean isJobServiceOn(Intent intent) {
+    return (PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_NO_CREATE) != null);
+  }
+
+  /**
+   * Schedules the AlarmManager service.
+   *
+   * @param backendName The backend to where the events are logged.
+   * @param attemptNumber Number of times the AlarmManager has tried to log for this backend.
+   */
   @Override
-  public void schedule(TransportContext transportContext, int attemptNumber) {}
+  public void schedule(TransportContext transportContext, int attemptNumber) {
+    // Create Intent
+    String backendName = transportContext.getBackendName();
+    Uri.Builder intentDataBuilder = new Uri.Builder();
+    intentDataBuilder.appendQueryParameter(SchedulerUtil.BACKEND_NAME, backendName);
+    intentDataBuilder.appendQueryParameter(
+        SchedulerUtil.APPLICATION_BUNDLE_ID, BuildConfig.APPLICATION_ID);
+    Intent intent = new Intent(context, AlarmManagerSchedulerBroadcastReceiver.class);
+    intent.setData(intentDataBuilder.build());
+    intent.putExtra(SchedulerUtil.ATTEMPT_NUMBER, attemptNumber);
+
+    if (isJobServiceOn(intent)) return;
+
+    Long backendTime = eventStore.getNextCallTime(transportContext);
+
+    long timeDiff = 0;
+    if (backendTime != null) {
+      timeDiff = backendTime - clock.getTime();
+    }
+
+    PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
+    this.alarmManager.set(
+        AlarmManager.ELAPSED_REALTIME,
+        SchedulerUtil.getScheduleDelay(timeDiff, DELTA, attemptNumber),
+        pendingIntent);
+  }
 }
---
+++ transport/transport-runtime/src/main/java/com/google/android/datatransport/runtime/scheduling/jobscheduling/AlarmManagerSchedulerBroadcastReceiver.java
@@ -0,0 +1,32 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.android.datatransport.runtime.scheduling.jobscheduling;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import com.google.android.datatransport.runtime.TransportRuntime;
+
+/** The service responsible for uploading information to the backend. */
+public class AlarmManagerSchedulerBroadcastReceiver extends BroadcastReceiver {
+
+  @Override
+  public void onReceive(Context context, Intent intent) {
+    String backendName = intent.getData().getQueryParameter(SchedulerUtil.BACKEND_NAME);
+    int attemptNumber = intent.getExtras().getInt(SchedulerUtil.ATTEMPT_NUMBER);
+    TransportRuntime.initialize(context);
+    TransportRuntime.getInstance().getUploader().upload(backendName, attemptNumber, () -> {});
+  }
+}
