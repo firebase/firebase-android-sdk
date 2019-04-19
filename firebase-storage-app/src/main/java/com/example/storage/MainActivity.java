0f49e608c95008ef630b1027d51fd15361841d44
Sebastian Schmidt <mrschmidt@google.com>
2019-04-19 16:03:30 -0700

    Adding google-services plugin

--- firebase-storage-app/src/main/java/com/example/storage/MainActivity.java
+++ firebase-storage-app/src/main/java/com/example/storage/MainActivity.java
@@ -105,8 +105,6 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    FirebaseApp.initializeApp(this);
-
     FirebaseAuth.getInstance().signInAnonymously();
 
     Button clickButton = findViewById(R.id.streamDownload);
