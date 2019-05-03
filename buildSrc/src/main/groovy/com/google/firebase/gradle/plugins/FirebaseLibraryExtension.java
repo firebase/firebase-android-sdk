f81e00a8ba73dae3e9f0a3cdeb5b3e058085270b
Vladimir Kryachko <vkryachko@google.com>
2019-05-03 13:17:22 -0400

    Fixes

--- buildSrc/src/main/groovy/com/google/firebase/gradle/plugins/FirebaseLibraryExtension.java
+++ buildSrc/src/main/groovy/com/google/firebase/gradle/plugins/FirebaseLibraryExtension.java
@@ -1,3 +1,17 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package com.google.firebase.gradle.plugins;
 
 import com.google.firebase.gradle.plugins.ci.device.FirebaseTestLabExtension;
@@ -9,6 +23,7 @@ import org.gradle.api.provider.Property;
 import javax.inject.Inject;
 
 public class FirebaseLibraryExtension {
+  /** True by default */
   public final Property<Boolean> publishJavadoc;
   public final Property<Boolean> publishSources;
   public final FirebaseTestLabExtension testLab;
@@ -18,6 +33,7 @@ public class FirebaseLibraryExtension {
     this.publishJavadoc = objectFactory.property(Boolean.class);
     this.publishSources = objectFactory.property(Boolean.class);
     this.testLab = new FirebaseTestLabExtension(objectFactory);
+    this.publishJavadoc.set(true);
   }
 
   public void setPublishJavadoc(boolean value) {
