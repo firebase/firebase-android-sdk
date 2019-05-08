fe381e3d30446ab76b115cfb07a1da4ca14eb981
Vladimir Kryachko <vkryachko@google.com>
2019-05-08 19:19:50 -0400

    Automate KTX release process and version tracking (#409)
    * Libraries can implicitly release with other libraries.
    
    * Log publish info, add tests
    
    * Add javadoc
    
    * Make ktx libraries child projects of their respective java library.
    
    * Improve uniqueness of kotlin module name.
    
    * Address review comments.

--- buildSrc/src/main/groovy/com/google/firebase/gradle/plugins/FirebaseLibraryExtension.java
+++ buildSrc/src/main/groovy/com/google/firebase/gradle/plugins/FirebaseLibraryExtension.java
@@ -14,22 +14,72 @@
 
 package com.google.firebase.gradle.plugins;
 
+import com.google.common.collect.ImmutableSet;
 import com.google.firebase.gradle.plugins.ci.device.FirebaseTestLabExtension;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 import javax.inject.Inject;
 import org.gradle.api.Action;
-import org.gradle.api.model.ObjectFactory;
+import org.gradle.api.GradleException;
+import org.gradle.api.Project;
+import org.gradle.api.UnknownDomainObjectException;
 
 public class FirebaseLibraryExtension {
+  private final Project project;
+  private final Set<FirebaseLibraryExtension> librariesToCoRelease = new HashSet<>();
+
+  /** Indicates whether the library has public javadoc. */
   public boolean publishJavadoc = true;
+
+  /** Indicates whether sources are published alongside the library. */
   public boolean publishSources;
+
+  /** Firebase Test Lab configuration/ */
   public final FirebaseTestLabExtension testLab;
 
   @Inject
-  public FirebaseLibraryExtension(ObjectFactory objectFactory) {
-    this.testLab = new FirebaseTestLabExtension(objectFactory);
+  public FirebaseLibraryExtension(Project project) {
+    this.project = project;
+    this.testLab = new FirebaseTestLabExtension(project.getObjects());
   }
 
-  void testLab(Action<FirebaseTestLabExtension> action) {
+  /** Configure Firebase Test Lab. */
+  public void testLab(Action<FirebaseTestLabExtension> action) {
     action.execute(testLab);
+  }
+
+  /**
+   * Register to be released alongside another Firebase Library project.
+   *
+   * <p>This will force the released version of the current project to match the one it's released
+   * with.
+   */
+  public void releaseWith(Project releaseWithProject) {
+    try {
+      FirebaseLibraryExtension releaseWithLibrary =
+          releaseWithProject.getExtensions().getByType(FirebaseLibraryExtension.class);
+      releaseWithLibrary.librariesToCoRelease.add(this);
+      this.project.setVersion(releaseWithProject.getVersion());
+
+      String latestRelease = "latestReleasedVersion";
+      if (releaseWithProject.getExtensions().getExtraProperties().has(latestRelease)) {
+        this.project
+            .getExtensions()
+            .getExtraProperties()
+            .set(latestRelease, releaseWithProject.getProperties().get(latestRelease));
+      }
+
+    } catch (UnknownDomainObjectException ex) {
+      throw new GradleException(
+          "Library cannot be released with a project that is not a Firebase Library itself");
+    }
+  }
+
+  public Set<Project> getProjectsToRelease() {
+    return ImmutableSet.<Project>builder()
+        .add(project)
+        .addAll(librariesToCoRelease.stream().map(l -> l.project).collect(Collectors.toSet()))
+        .build();
   }
 }
