ab057f2fa2c75755d5156bdfc82a00c8bd48f688
Brian Chen <chenbrian@google.com>
2019-05-22 10:06:09 -0700

    add publicAPI

--- firebase-firestore/src/main/java/com/google/firebase/firestore/FirebaseFirestore.java
+++ firebase-firestore/src/main/java/com/google/firebase/firestore/FirebaseFirestore.java
@@ -405,6 +405,7 @@ public class FirebaseFirestore {
    * @return A Task that is resolved once the persistent storage has been cleared. Otherwise, the
    *     Task is rejected with an error.
    */
+  @PublicApi
   public Task<Void> clearPersistence() {
     final TaskCompletionSource<Void> source = new TaskCompletionSource<>();
     asyncQueue.enqueueAndForget(
