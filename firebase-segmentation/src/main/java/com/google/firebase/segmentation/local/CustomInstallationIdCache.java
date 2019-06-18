d9520039c24708e4ee90d401dca8b02e16930529
Di Wu <49409954+diwu-arete@users.noreply.github.com>
2019-06-18 16:06:15 -0700

    package refactor (#534)
    * Implement Firebase segmentation SDK device local cache
    
    * [Firebase Segmentation] Add custom installation id cache layer and tests for it.
    
    * Add test for updating cache
    
    * Switch to use SQLiteOpenHelper
    
    * Switch to use SharedPreferences from SQLite.
    
    * Change the cache class to be singleton
    
    * Wrap shared pref commit in a async task.
    
    * Address comments
    
    * Google format fix
    
    * Replace some deprecated code.
    
    * Package refactor
    
    * nit
    
    * nit

--- firebase-segmentation/src/main/java/com/google/firebase/segmentation/local/CustomInstallationIdCache.java
+++ firebase-segmentation/src/main/java/com/google/firebase/segmentation/local/CustomInstallationIdCache.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.google.firebase.segmentation;
+package com.google.firebase.segmentation.local;
 
 import android.content.Context;
 import android.content.SharedPreferences;
