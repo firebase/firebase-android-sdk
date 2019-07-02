5c204a6240ae072ecb43557340f49abf6d83ea26
Vladimir Kryachko <vkryachko@google.com>
2019-07-02 11:24:44 -0400

    Gradle 5

---
+++ firebase-common/data-collection-tests/src/test/java/com/google/firebase/DataCollectionDefaultChangeRegistrar.java
@@ -0,0 +1,44 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.firebase;
+
+import com.google.firebase.components.Component;
+import com.google.firebase.components.ComponentRegistrar;
+import com.google.firebase.components.Dependency;
+import com.google.firebase.events.Subscriber;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class DataCollectionDefaultChangeRegistrar implements ComponentRegistrar {
+  @Override
+  public List<Component<?>> getComponents() {
+    return Collections.singletonList(
+        Component.builder(ChangeListener.class)
+            .add(Dependency.required(Subscriber.class))
+            .alwaysEager()
+            .factory(c -> new ChangeListener(c.get(Subscriber.class)))
+            .build());
+  }
+
+  public static class ChangeListener {
+    public List<Boolean> changes = new ArrayList<>();
+
+    ChangeListener(Subscriber subscriber) {
+      subscriber.subscribe(
+          DataCollectionDefaultChange.class, change -> changes.add(change.getPayload().enabled));
+    }
+  }
+}
