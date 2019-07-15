2bf12c20c3f7b0237cda154da67cde059b8b0fb7
Sebastian Schmidt <mrschmidt@google.com>
2019-07-15 16:29:36 +0700

    Update ktx's test-util

--- firebase-firestore/ktx/src/test/java/com/google/firebase/firestore/TestUtil.java
+++ firebase-firestore/ktx/src/test/java/com/google/firebase/firestore/TestUtil.java
@@ -138,36 +138,6 @@ public class TestUtil {
     return new QuerySnapshot(query(path), viewSnapshot, FIRESTORE);
   }
 
-  /**
-   * An implementation of TargetMetadataProvider that provides controlled access to the
-   * `TargetMetadataProvider` callbacks. Any target accessed via these callbacks must be registered
-   * beforehand via `setSyncedKeys()`.
-   */
-  public static class TestTargetMetadataProvider
-      implements WatchChangeAggregator.TargetMetadataProvider {
-    final Map<Integer, ImmutableSortedSet<DocumentKey>> syncedKeys = new HashMap<>();
-    final Map<Integer, QueryData> queryData = new HashMap<>();
-
-    @Override
-    public ImmutableSortedSet<DocumentKey> getRemoteKeysForTarget(int targetId) {
-      return syncedKeys.get(targetId) != null
-          ? syncedKeys.get(targetId)
-          : DocumentKey.emptyKeySet();
-    }
-
-    @Nullable
-    @Override
-    public QueryData getQueryDataForTarget(int targetId) {
-      return queryData.get(targetId);
-    }
-
-    /** Sets or replaces the local state for the provided query data. */
-    public void setSyncedKeys(QueryData queryData, ImmutableSortedSet<DocumentKey> keys) {
-      this.queryData.put(queryData.getTargetId(), queryData);
-      this.syncedKeys.put(queryData.getTargetId(), keys);
-    }
-  }
-
   public static <T> T waitFor(Task<T> task) {
     if (!task.isComplete()) {
       Robolectric.flushBackgroundThreadScheduler();
