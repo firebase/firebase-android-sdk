7904e168d16827fa6050fd7bf2ff9d84b1dc6d42
Marek Gilbert <mcg@google.com>
2019-07-23 09:57:46 -0700

    Only disallow __foo__ on write

--- firebase-firestore/src/main/java/com/google/firebase/firestore/core/UserData.java
+++ firebase-firestore/src/main/java/com/google/firebase/firestore/core/UserData.java
@@ -304,11 +304,11 @@ public class UserData {
     private static final String RESERVED = "__";
 
     private void validatePathSegment(String segment) {
-      if (!isWrite() && segment.isEmpty()) {
+      if (segment.isEmpty()) {
         throw this.createError("Document fields must not be empty");
       }
 
-      if (segment.startsWith(RESERVED) && segment.endsWith(RESERVED)) {
+      if (isWrite() && segment.startsWith(RESERVED) && segment.endsWith(RESERVED)) {
         throw this.createError("Document fields cannot begin or end with \"__\"");
       }
     }
