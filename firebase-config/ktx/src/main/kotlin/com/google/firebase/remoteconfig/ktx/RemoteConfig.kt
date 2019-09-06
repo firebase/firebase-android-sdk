6e66027f50b6436d8c5019aafd0e72d6b719d03b
Rodrigo Lazo <rlazo@users.noreply.github.com>
2019-09-06 16:27:24 -0400

    Implement firebase-config-ktx Kotlin library. (#702)
    * Implement firebase-config ktx module.

---
+++ firebase-config/ktx/src/main/kotlin/com/google/firebase/remoteconfig/ktx/RemoteConfig.kt
@@ -0,0 +1,46 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.firebase.remoteconfig.ktx
+
+import androidx.annotation.Keep
+import com.google.firebase.FirebaseApp
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigValue
+import com.google.firebase.components.Component
+import com.google.firebase.components.ComponentRegistrar
+
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.platforminfo.LibraryVersionComponent
+
+/** Returns the [FirebaseRemoteConfig] instance of the default [FirebaseApp]. */
+val Firebase.remoteConfig: FirebaseRemoteConfig
+    get() = FirebaseRemoteConfig.getInstance()
+
+/** Returns the [FirebaseRemoteConfig] instance of a given [FirebaseApp]. */
+fun Firebase.remoteConfig(app: FirebaseApp): FirebaseRemoteConfig = FirebaseRemoteConfig.getInstance(app)
+
+/** See [FirebaseRemoteConfig#getValue] */
+operator fun FirebaseRemoteConfig.get(key: String): FirebaseRemoteConfigValue {
+    return this.getValue(key)
+}
+
+internal const val LIBRARY_NAME: String = "fire-cfg-ktx"
+
+/** @suppress */
+@Keep
+class FirebaseRemoteConfigKtxRegistrar : ComponentRegistrar {
+    override fun getComponents(): List<Component<*>> =
+            listOf(LibraryVersionComponent.create(LIBRARY_NAME, BuildConfig.VERSION_NAME))
+}
