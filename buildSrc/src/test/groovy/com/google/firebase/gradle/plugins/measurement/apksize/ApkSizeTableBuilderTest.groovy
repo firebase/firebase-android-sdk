78db0f5927c98062630bfd97f67dd4435ba95681
Rodrigo Lazo Paz <rlazo@google.com>
2019-10-09 11:00:21 -0400

    Fix tests.

--- buildSrc/src/test/groovy/com/google/firebase/gradle/plugins/measurement/apksize/ApkSizeTableBuilderTest.groovy
+++ buildSrc/src/test/groovy/com/google/firebase/gradle/plugins/measurement/apksize/ApkSizeTableBuilderTest.groovy
@@ -25,13 +25,14 @@ import org.junit.runners.JUnit4
 public class ApkSizeTableBuilderTest {
 
   private static final String HEADER =
-      "|--------------------        APK Sizes        ------------------|\n" +
-      "|---    project    ---|--  build type   --|--  size in bytes  --|\n"
+      "|-------------------------        APK Sizes        -----------------------|\n" +
+      "|--------    project    --------|--  build type   --|--  size in bytes  --|\n"
 
   @Test(expected = IllegalStateException.class)
   public void toTableString_throwsWhenZeroAdded() {
     def builder = new ApkSizeTableBuilder()
     builder.toTableString()
+    assertEquals("", builder.toTableString())
   }
 
   @Test
@@ -48,9 +49,9 @@ public class ApkSizeTableBuilderTest {
   @Test
   public void toTableString_withThreeMeasurements() {
     def expected = HEADER +
-        "|firebase foo         |debug              |              255,000|\n" +
-        "|google loo           |release            |                4,000|\n" +
-        "|Appy Snap App        |Snappy             |              781,000|"
+        "|firebase foo                   |debug              |              255,000|\n" +
+        "|google loo                     |release            |                4,000|\n" +
+        "|Appy Snap App                  |Snappy             |              781,000|"
 
     def builder = new ApkSizeTableBuilder()
     builder.addApkSize("firebase foo", "debug", 255000)
