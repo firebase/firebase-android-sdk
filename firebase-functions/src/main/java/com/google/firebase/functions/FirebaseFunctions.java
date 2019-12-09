da2feb422c27eabcfd14456b872a48be18dfb38e
Yifan Yang <yifayan@gmail.com>
2019-12-09 10:02:37 -0800

    Test, test, test.

--- firebase-functions/src/main/java/com/google/firebase/functions/FirebaseFunctions.java
+++ firebase-functions/src/main/java/com/google/firebase/functions/FirebaseFunctions.java
@@ -83,6 +83,8 @@ public class FirebaseFunctions {
     this.region = Preconditions.checkNotNull(region);
 
     maybeInstallProviders(context);
+
+    System.out.println('Test, test, test.');
   }
 
   /**
