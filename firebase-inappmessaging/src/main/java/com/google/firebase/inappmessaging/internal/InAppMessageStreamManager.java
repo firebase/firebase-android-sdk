f03383ae35fc54b2d8371e5bb507c6e7187bcf70
Jason Heron <jheron@google.com>
2020-04-01 10:44:42 -0700

    Remove impressions for Campaigns delivered by backend service (#1402)
    Changes impression storage client to allow for clearing impressed campaigns based on a fetch response. Then adds this new method to the fetch response flowable chain

--- firebase-inappmessaging/src/main/java/com/google/firebase/inappmessaging/internal/InAppMessageStreamManager.java
+++ firebase-inappmessaging/src/main/java/com/google/firebase/inappmessaging/internal/InAppMessageStreamManager.java
@@ -242,6 +242,8 @@ public class InAppMessageStreamManager {
                                           Locale.US,
                                           "Successfully fetched %d messages from backend",
                                           resp.getMessagesList().size())))
+                          .doOnSuccess(
+                              resp -> impressionStorageClient.clearImpressions(resp).subscribe())
                           .doOnSuccess(analyticsEventsManager::updateContextualTriggers)
                           .doOnSuccess(testDeviceHelper::processCampaignFetch)
                           .doOnError(e -> Logging.logw("Service fetch error: " + e.getMessage()))
