c4cfc6a06ffabcce6ab22b8d48d31aa43033ec79
Vinay Guthal <vguthal@google.com>
2020-04-09 11:50:24 -0400

    remove crash

--- firebase-crashlytics/src/androidTest/java/com/google/firebase/crashlytics/internal/common/CrashlyticsControllerTest.java
+++ firebase-crashlytics/src/androidTest/java/com/google/firebase/crashlytics/internal/common/CrashlyticsControllerTest.java
@@ -842,7 +842,6 @@ public class CrashlyticsControllerTest extends CrashlyticsTestCase {
     Task<Void> task = controller.submitAllReports(1.0f, testSettingsDataProvider.getAppSettings());
 
     arbiter.setCrashlyticsDataCollectionEnabled(true);
-    
     await(task);
     verify(mockReportManager).areReportsAvailable();
     verify(mockReportManager).findReports();
