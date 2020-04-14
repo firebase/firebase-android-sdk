cb446ddc40bc7f034b777c80eee852baaab0403a
Ashwin Raghav <ashwinraghav@google.com>
2020-04-13 18:09:27 -0700

    Configure SDK  version

--- firebase-firestore/src/test/java/com/google/firebase/firestore/local/SQLiteLocalStoreTest.java
+++ firebase-firestore/src/test/java/com/google/firebase/firestore/local/SQLiteLocalStoreTest.java
@@ -20,8 +20,10 @@ import org.junit.runner.RunWith;
 import org.robolectric.ParameterizedRobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
+import android.os.Build;
+
 @RunWith(ParameterizedRobolectricTestRunner.class)
-@Config(manifest = Config.NONE)
+@Config(manifest = Config.NONE, sdk = Build.VERSION_CODES.O_MR1)
 public class SQLiteLocalStoreTest extends LocalStoreTestCase {
 
   private QueryEngine queryEngine;
