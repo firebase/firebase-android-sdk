fb6c5bae9a48ca723213ec80e78154789bdaab1d
Jason Heron <jheron@google.com>
2020-04-21 13:28:12 -0700

    format

--- firebase-inappmessaging/src/main/java/com/google/firebase/inappmessaging/internal/injection/modules/ApiClientModule.java
+++ firebase-inappmessaging/src/main/java/com/google/firebase/inappmessaging/internal/injection/modules/ApiClientModule.java
@@ -77,9 +77,7 @@ public class ApiClientModule {
   @Provides
   @FirebaseAppScope
   ApiClient providesApiClient(
-      Lazy<GrpcClient> grpcClient,
-      Application application,
-      ProviderInstaller providerInstaller) {
+      Lazy<GrpcClient> grpcClient, Application application, ProviderInstaller providerInstaller) {
     return new ApiClient(grpcClient, firebaseApp, application, clock, providerInstaller);
   }
 }
