58bc8f6077a01c08dcdbb3af8366a181a9583273
ChaoqunCHEN <cqchen93@gmail.com>
2020-04-27 20:42:57 -0700

    Revert "Move FIS precondition format check to installation constructor"
    This reverts commit 6d0938bfe72a1f6a7129a11d06cda990fd8d3694.

--- firebase-installations/src/main/java/com/google/firebase/installations/FirebaseInstallations.java
+++ firebase-installations/src/main/java/com/google/firebase/installations/FirebaseInstallations.java
@@ -142,17 +142,17 @@ public class FirebaseInstallations implements FirebaseInstallationsApi {
    * FirebaseOptions#getApplicationId()} , and ({@link FirebaseOptions#getProjectId()} are not null
    * or empty.
    */
-  private static void preConditionChecks(@NonNull FirebaseApp app) {
-    Preconditions.checkNotEmpty(app.getOptions().getApplicationId());
-    Preconditions.checkNotEmpty(app.getOptions().getProjectId());
-    Preconditions.checkNotEmpty(app.getOptions().getApiKey());
+  private void preConditionChecks() {
+    Preconditions.checkNotEmpty(getApplicationId());
+    Preconditions.checkNotEmpty(getProjectIdentifier());
+    Preconditions.checkNotEmpty(getApiKey());
     Preconditions.checkArgument(
-        Utils.isValidAppIdFormat(app.getOptions().getApplicationId()),
+        Utils.isValidAppIdFormat(getApplicationId()),
         "Please set your Application ID. A valid Firebase App ID is required to communicate "
             + "with Firebase server APIs: It identifies your application with Firebase."
             + "Please refer to https://firebase.google.com/support/privacy/init-options.");
     Preconditions.checkArgument(
-        Utils.isValidApiKeyFormat(app.getOptions().getApiKey()),
+        Utils.isValidApiKeyFormat(getApiKey()),
         "Please set a valid API key. A Firebase API key is required to communicate with "
             + "Firebase server APIs: It authenticates your project with Google."
             + "Please refer to https://firebase.google.com/support/privacy/init-options.");
@@ -184,7 +184,6 @@ public class FirebaseInstallations implements FirebaseInstallationsApi {
   @NonNull
   public static FirebaseInstallations getInstance(@NonNull FirebaseApp app) {
     Preconditions.checkArgument(app != null, "Null is not a valid value of FirebaseApp.");
-    preConditionChecks(app);
     return (FirebaseInstallations) app.get(FirebaseInstallationsApi.class);
   }
 
@@ -213,6 +212,7 @@ public class FirebaseInstallations implements FirebaseInstallationsApi {
   @NonNull
   @Override
   public Task<String> getId() {
+    preConditionChecks();
     Task<String> task = addGetIdListener();
     backgroundExecutor.execute(this::doGetId);
     return task;
@@ -229,6 +229,7 @@ public class FirebaseInstallations implements FirebaseInstallationsApi {
   @NonNull
   @Override
   public Task<InstallationTokenResult> getToken(boolean forceRefresh) {
+    preConditionChecks();
     Task<InstallationTokenResult> task = addGetAuthTokenListener();
     if (forceRefresh) {
       backgroundExecutor.execute(this::doGetAuthTokenForceRefresh);
