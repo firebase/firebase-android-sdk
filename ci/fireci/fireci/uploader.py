a34efd27a00bfe102a920b219395a5a326fafbf1
Yifan Yang <yifayan@gmail.com>
2020-05-01 08:42:49 -0700

    reformat commit info

--- ci/fireci/fireci/uploader.py
+++ ci/fireci/fireci/uploader.py
@@ -68,7 +68,7 @@ def _get_commit_hash(revision):
 
 def _get_commit_info(revision):
   result = subprocess.run(
-    ['git', 'show', revision, '--format=oneline', '--abbrev-commit', '-s'],
+    ['git', 'show', revision, '--format="%C(auto)(%h) - %s"', '--abbrev-commit', '-s'],
     stdout=subprocess.PIPE,
     check=True,
   )
