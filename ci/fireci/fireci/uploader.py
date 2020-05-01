ecc8b7b13ee1bcff4095e81da15fa15b690882ec
Yifan Yang <yifayan@gmail.com>
2020-05-01 11:08:03 -0700

    Display prow merge commit info in metric reports. (#1520)

--- ci/fireci/fireci/uploader.py
+++ ci/fireci/fireci/uploader.py
@@ -17,14 +17,15 @@ import logging
 import os
 import requests
 import subprocess
+import urllib.parse
 
 _logger = logging.getLogger('fireci.uploader')
 
 
-def post_report(test_report, metrics_service_url, access_token):
+def post_report(test_report, metrics_service_url, access_token, note=''):
   """Post a report to the metrics service backend."""
 
-  endpoint = _construct_request_endpoint()
+  endpoint = _construct_request_endpoint(note)
   headers = {'Authorization': f'Bearer {access_token}', 'Content-Type': 'application/json'}
   data = json.dumps(test_report)
 
@@ -38,7 +39,7 @@ def post_report(test_report, metrics_service_url, access_token):
   _logger.info(f'Response: {result.text}')
 
 
-def _construct_request_endpoint():
+def _construct_request_endpoint(note):
   repo_owner = os.getenv('REPO_OWNER')
   repo_name = os.getenv('REPO_NAME')
   branch = os.getenv('PULL_BASE_REF')
@@ -50,6 +51,10 @@ def _construct_request_endpoint():
   if pull_request:
     base_commit = _get_commit_hash('HEAD@{1}')
     endpoint += f'?pull_request={pull_request}&base_commit={base_commit}'
+
+    commit_note = _get_prow_commit_note('HEAD@{0}')
+    note += f'\n{commit_note}\n'
+    endpoint += f'&note={urllib.parse.quote(note)}'
   else:
     endpoint += f'?branch={branch}'
 
@@ -58,4 +63,14 @@ def _construct_request_endpoint():
 
 def _get_commit_hash(revision):
   result = subprocess.run(['git', 'rev-parse', revision], stdout=subprocess.PIPE, check=True)
+  return result.stdout.decode('utf-8').strip()
+
+
+def _get_prow_commit_note(revision):
+  template = 'Head commit (%h) is created by Prow via merging commits: %p.'
+  result = subprocess.run(
+    ['git', 'show', revision, f'--format={template}', '-s'],
+    stdout=subprocess.PIPE,
+    check=True,
+  )
   return result.stdout.decode('utf-8').strip()
