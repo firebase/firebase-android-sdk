2087dff8d41bdce1ee23c8a5600201074660ce05
Rosário Pereira Fernandes <rosariofernandes51@gmail.com>
2020-05-11 21:52:18 +0200

    FirebaseCrashlyticsRegistrar --> FirebaseCrashlyticsKtxRegistrar

--- firebase-crashlytics/ktx/src/main/kotlin/com/google/firebase/crashlytics/ktx/FirebaseCrashlytics.kt
+++ firebase-crashlytics/ktx/src/main/kotlin/com/google/firebase/crashlytics/ktx/FirebaseCrashlytics.kt
@@ -38,7 +38,7 @@ internal const val LIBRARY_NAME: String = "fire-cls-ktx"
 
 /** @suppress */
 @Keep
-class FirebaseCrashlyticsRegistrar : ComponentRegistrar {
+class FirebaseCrashlyticsKtxRegistrar : ComponentRegistrar {
     override fun getComponents(): List<Component<*>> =
             listOf(LibraryVersionComponent.create(LIBRARY_NAME, BuildConfig.VERSION_NAME))
 }
