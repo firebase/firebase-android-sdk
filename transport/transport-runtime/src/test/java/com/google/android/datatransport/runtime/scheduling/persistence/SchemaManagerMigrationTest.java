c0e647c2e67db2619dd6fac3cfb2923e759589fa
ashwinraghav <ashwinraghav@google.com>
2020-05-18 15:26:05 -0700

    Added state simulations

--- transport/transport-runtime/src/test/java/com/google/android/datatransport/runtime/scheduling/persistence/SchemaManagerMigrationTest.java
+++ transport/transport-runtime/src/test/java/com/google/android/datatransport/runtime/scheduling/persistence/SchemaManagerMigrationTest.java
@@ -13,73 +13,128 @@
 // limitations under the License.
 package com.google.android.datatransport.runtime.scheduling.persistence;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+
 import static com.google.android.datatransport.runtime.scheduling.persistence.SchemaManager.DB_NAME;
 import static com.google.android.datatransport.runtime.scheduling.persistence.SchemaManager.SCHEMA_VERSION;
 import static com.google.common.truth.Truth.assertThat;
 
-import android.content.ContentValues;
-import android.database.sqlite.SQLiteDatabase;
 import androidx.test.core.app.ApplicationProvider;
-import com.google.android.datatransport.Encoding;
-import com.google.android.datatransport.Priority;
-import com.google.android.datatransport.runtime.EncodedPayload;
+
 import com.google.android.datatransport.runtime.EventInternal;
 import com.google.android.datatransport.runtime.TransportContext;
-import com.google.android.datatransport.runtime.time.TestClock;
-import com.google.android.datatransport.runtime.time.UptimeClock;
 import com.google.android.datatransport.runtime.util.PriorityMapping;
-import java.nio.charset.Charset;
+
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import org.junit.Assert;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.ParameterizedRobolectricTestRunner;
-import org.robolectric.RobolectricTestRunner;
 
 @RunWith(ParameterizedRobolectricTestRunner.class)
 public class SchemaManagerMigrationTest {
-    private final int toVersion;
-    private final int fromVersion;
+    private final int highVersion;
+    private final int lowVersion;
 
+    private static Map<Integer, StateSimulations.StateSimulator> simulatorMap = new HashMap<>();
+    static {
+        simulatorMap.put(1, new StateSimulations.V1());
+        simulatorMap.put(2, new StateSimulations.V2());
+        simulatorMap.put(3, new StateSimulations.V3());
+        simulatorMap.put(4, new StateSimulations.V4());
+    }
     @ParameterizedRobolectricTestRunner.Parameters(name = "lowVersion = {0}, highVersion = {1}")
     public static Collection<Object[]> data() {
         Collection<Object[]> params = new ArrayList<>();
-        for(int fromVersion  = 1 ; fromVersion < SCHEMA_VERSION; fromVersion ++) {
-            for (int toVersion = fromVersion + 1; toVersion <= SCHEMA_VERSION; toVersion++) {
-                params.add(new Object[]{fromVersion, toVersion});
+        for(int lowVersion  = 1 ; lowVersion < SCHEMA_VERSION; lowVersion ++) {
+            for (int highVersion = lowVersion + 1; highVersion <= SCHEMA_VERSION; highVersion++) {
+                params.add(new Object[]{lowVersion, highVersion});
             }
         }
         return params;
     }
 
-    public SchemaManagerMigrationTest(int fromVersion, int toVersion) {
-        this.fromVersion = fromVersion;
-        this.toVersion = toVersion;
+    public SchemaManagerMigrationTest(int lowVersion, int highVersion) {
+        this.lowVersion = lowVersion;
+        this.highVersion = highVersion;
     }
 
     @Test
     public void upgrade_migratesSuccessfully() {
         SchemaManager schemaManager =
-                new SchemaManager(ApplicationProvider.getApplicationContext(), DB_NAME, fromVersion);
-        schemaManager.onUpgrade(schemaManager.getWritableDatabase(), fromVersion, toVersion);
+                new SchemaManager(ApplicationProvider.getApplicationContext(), DB_NAME, lowVersion);
+        schemaManager.onUpgrade(schemaManager.getWritableDatabase(), lowVersion, highVersion);
+        simulatorMap.get(highVersion).simulate(schemaManager);
     }
 
     @Test
     public void downgrade_migratesSuccessfully() {
         SchemaManager schemaManager =
-                new SchemaManager(ApplicationProvider.getApplicationContext(), DB_NAME, toVersion);
-        schemaManager.onDowngrade(schemaManager.getWritableDatabase(), toVersion, fromVersion);
+                new SchemaManager(ApplicationProvider.getApplicationContext(), DB_NAME, highVersion);
+        schemaManager.onDowngrade(schemaManager.getWritableDatabase(), highVersion, lowVersion);
+        simulatorMap.get(lowVersion).simulate(schemaManager);
     }
 
     @Test
     public void downgrade_upgrade_migratesSuccessfully() {
         SchemaManager schemaManager =
-                new SchemaManager(ApplicationProvider.getApplicationContext(), DB_NAME, toVersion);
+                new SchemaManager(ApplicationProvider.getApplicationContext(), DB_NAME, highVersion);
 
-        schemaManager.onDowngrade(schemaManager.getWritableDatabase(), toVersion, fromVersion);
-        schemaManager.onUpgrade(schemaManager.getWritableDatabase(), fromVersion, toVersion);
+        schemaManager.onDowngrade(schemaManager.getWritableDatabase(), highVersion, lowVersion);
+        simulatorMap.get(lowVersion).simulate(schemaManager);
+
+        schemaManager.onUpgrade(schemaManager.getWritableDatabase(), lowVersion, highVersion);
+        simulatorMap.get(highVersion).simulate(schemaManager);
+    }
+
+    @Test
+    public void upgrade_downgrade_upgrade_migratesSuccessfully() {
+        SchemaManager schemaManager =
+                new SchemaManager(ApplicationProvider.getApplicationContext(), DB_NAME, lowVersion);
+
+        schemaManager.onUpgrade(schemaManager.getWritableDatabase(), lowVersion, highVersion);
+        simulatorMap.get(highVersion).simulate(schemaManager);
+
+        schemaManager.onDowngrade(schemaManager.getWritableDatabase(), highVersion, lowVersion);
+        simulatorMap.get(lowVersion).simulate(schemaManager);
+
+        schemaManager.onUpgrade(schemaManager.getWritableDatabase(), lowVersion, highVersion);
+        simulatorMap.get(highVersion).simulate(schemaManager);
+    }
+
+    private PersistedEvent simulatedPersistOnV1Database(
+            SchemaManager schemaManager, TransportContext transportContext, EventInternal eventInternal) {
+        SQLiteDatabase db = schemaManager.getWritableDatabase();
+
+        ContentValues record = new ContentValues();
+        record.put("backend_name", transportContext.getBackendName());
+        record.put("priority", PriorityMapping.toInt(transportContext.getPriority()));
+        record.put("next_request_ms", 0);
+        long contextId = db.insert("transport_contexts", null, record);
+
+        ContentValues values = new ContentValues();
+        values.put("context_id", contextId);
+        values.put("transport_name", eventInternal.getTransportName());
+        values.put("timestamp_ms", eventInternal.getEventMillis());
+        values.put("uptime_ms", eventInternal.getUptimeMillis());
+        values.put("payload", eventInternal.getPayload());
+        values.put("code", eventInternal.getCode());
+        values.put("num_attempts", 0);
+        long newEventId = db.insert("events", null, values);
+
+        for (Map.Entry<String, String> entry : eventInternal.getMetadata().entrySet()) {
+            ContentValues metadata = new ContentValues();
+            metadata.put("event_id", newEventId);
+            metadata.put("name", entry.getKey());
+            metadata.put("value", entry.getValue());
+            db.insert("event_metadata", null, metadata);
+        }
+
+        return PersistedEvent.create(newEventId, transportContext, eventInternal);
     }
 }
