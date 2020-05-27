fc280ade9fd853cfc33bb8e23625a9d6a58a74ee
Dana Silver <danasilver@google.com>
2020-05-27 16:28:06 -0700

    Update Kotlin tests for logger.

--- firebase-config/ktx/src/test/kotlin/com/google/firebase/remoteconfig/TestConstructorUtil.kt
+++ firebase-config/ktx/src/test/kotlin/com/google/firebase/remoteconfig/TestConstructorUtil.kt
@@ -16,13 +16,10 @@ package com.google.firebase.remoteconfig
 
 import android.content.Context
 import com.google.firebase.FirebaseApp
-import com.google.firebase.remoteconfig.internal.ConfigCacheClient
-import com.google.firebase.remoteconfig.internal.ConfigFetchHandler
-import com.google.firebase.remoteconfig.internal.ConfigGetParameterHandler
-import com.google.firebase.remoteconfig.internal.ConfigMetadataClient
 import java.util.concurrent.Executor
 import com.google.firebase.abt.FirebaseABTesting
 import com.google.firebase.iid.FirebaseInstanceId
+import com.google.firebase.remoteconfig.internal.*
 
 // This method is a workaround for testing. It enable us to create a FirebaseRemoteConfig object
 // with mocks using the package-private constructor.
@@ -37,7 +34,8 @@ fun createRemoteConfig(
     defaultConfigsCache: ConfigCacheClient,
     fetchHandler: ConfigFetchHandler,
     getHandler: ConfigGetParameterHandler,
-    frcMetadata: ConfigMetadataClient
+    frcMetadata: ConfigMetadataClient,
+    configLogger: ConfigLogger
 ): FirebaseRemoteConfig {
         return FirebaseRemoteConfig(
             context,
@@ -50,6 +48,7 @@ fun createRemoteConfig(
             defaultConfigsCache,
             fetchHandler,
             getHandler,
-            frcMetadata
+            frcMetadata,
+            configLogger
     )
 }
