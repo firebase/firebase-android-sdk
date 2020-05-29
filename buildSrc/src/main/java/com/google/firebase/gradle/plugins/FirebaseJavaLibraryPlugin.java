2ee55fe874aabc89cfa0c829315e3d317adc4623
Vladimir Kryachko <vkryachko@google.com>
2020-05-28 22:10:54 -0400

    Add license to file.

--- buildSrc/src/main/java/com/google/firebase/gradle/plugins/FirebaseJavaLibraryPlugin.java
+++ buildSrc/src/main/java/com/google/firebase/gradle/plugins/FirebaseJavaLibraryPlugin.java
@@ -1,3 +1,17 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package com.google.firebase.gradle.plugins;
 
 import com.google.common.collect.ImmutableList;
@@ -18,6 +32,7 @@ import org.gradle.api.tasks.SourceSet;
 import org.gradle.api.tasks.TaskProvider;
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile;
 
+// TODO(vkryachko): extract functionality common across Firebase{,Java}LibraryPlugin plugins.
 public class FirebaseJavaLibraryPlugin implements Plugin<Project> {
 
   @Override
