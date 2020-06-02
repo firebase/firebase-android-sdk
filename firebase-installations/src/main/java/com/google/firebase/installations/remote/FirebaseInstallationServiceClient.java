c92e9b35a4bf2bf7596a980a0eb8a2e9f2ee8e39
Ankita <ankitaj224@gmail.com>
2020-06-02 15:54:00 -0700

    Avoid throwing explcit IOExceptions. (#1609)
    * Dropping guava and appcompat dependency from FIS SDK.
    
    * Fix OverlappingFileLockException.
    
    Access the data shared by multiple threads only after acquiring cross-process and multi-thread safe locks.
    
    * Avoid throwing explcit IOExceptions.
    
    Also, handle slow network connection issues by retrying and back off.
    
    * Addressing rayo's comments
    
    * Fixing Delete Api
    
    * Fixing method name

--- firebase-installations/src/main/java/com/google/firebase/installations/remote/FirebaseInstallationServiceClient.java
+++ firebase-installations/src/main/java/com/google/firebase/installations/remote/FirebaseInstallationServiceClient.java
@@ -28,7 +28,6 @@ import androidx.annotation.Nullable;
 import com.google.android.gms.common.util.AndroidUtilsLight;
 import com.google.android.gms.common.util.Hex;
 import com.google.android.gms.common.util.VisibleForTesting;
-import com.google.firebase.FirebaseException;
 import com.google.firebase.heartbeatinfo.HeartBeatInfo;
 import com.google.firebase.heartbeatinfo.HeartBeatInfo.HeartBeat;
 import com.google.firebase.installations.FirebaseInstallationsException;
@@ -41,6 +40,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.charset.Charset;
@@ -127,8 +127,8 @@ public class FirebaseInstallationServiceClient {
    *       <li>400: return response with status BAD_CONFIG
    *       <li>403: return response with status BAD_CONFIG
    *       <li>403: return response with status BAD_CONFIG
-   *       <li>429: throw IOException
-   *       <li>500: throw IOException
+   *       <li>429: throw FirebaseInstallationsException
+   *       <li>500: throw FirebaseInstallationsException
    *     </ul>
    */
   @NonNull
@@ -138,16 +138,10 @@ public class FirebaseInstallationServiceClient {
       @NonNull String projectID,
       @NonNull String appId,
       @Nullable String iidToken)
-      throws IOException {
+      throws FirebaseInstallationsException {
     String resourceName = String.format(CREATE_REQUEST_RESOURCE_NAME_FORMAT, projectID);
     int retryCount = 0;
-    URL url =
-        new URL(
-            String.format(
-                "https://%s/%s/%s",
-                FIREBASE_INSTALLATIONS_API_DOMAIN,
-                FIREBASE_INSTALLATIONS_API_VERSION,
-                resourceName));
+    URL url = getFullyQualifiedRequestUri(resourceName);
     while (retryCount <= MAX_RETRIES) {
       HttpURLConnection httpURLConnection = openHttpURLConnection(url, apiKey);
 
@@ -179,12 +173,16 @@ public class FirebaseInstallationServiceClient {
 
         // Return empty installation response with BAD_CONFIG response code after max retries
         return InstallationResponse.builder().setResponseCode(ResponseCode.BAD_CONFIG).build();
+      } catch (IOException ignored) {
+        retryCount++;
       } finally {
         httpURLConnection.disconnect();
       }
     }
 
-    throw new IOException();
+    throw new FirebaseInstallationsException(
+        "Firebase Installations Service is unavailable. Please try again later.",
+        Status.UNAVAILABLE);
   }
 
   private void writeFIDCreateRequestBodyToOutputStream(
@@ -287,15 +285,9 @@ public class FirebaseInstallationServiceClient {
       @NonNull String fid,
       @NonNull String projectID,
       @NonNull String refreshToken)
-      throws FirebaseException, IOException {
+      throws FirebaseInstallationsException {
     String resourceName = String.format(DELETE_REQUEST_RESOURCE_NAME_FORMAT, projectID, fid);
-    URL url =
-        new URL(
-            String.format(
-                "https://%s/%s/%s",
-                FIREBASE_INSTALLATIONS_API_DOMAIN,
-                FIREBASE_INSTALLATIONS_API_VERSION,
-                resourceName));
+    URL url = getFullyQualifiedRequestUri(resourceName);
 
     int retryCount = 0;
     while (retryCount <= MAX_RETRIES) {
@@ -321,12 +313,28 @@ public class FirebaseInstallationServiceClient {
 
         throw new FirebaseInstallationsException(
             "Bad config while trying to delete FID", Status.BAD_CONFIG);
+      } catch (IOException ignored) {
+        retryCount++;
       } finally {
         httpURLConnection.disconnect();
       }
     }
 
-    throw new IOException();
+    throw new FirebaseInstallationsException(
+        "Firebase Installations Service is unavailable. Please try again later.",
+        Status.UNAVAILABLE);
+  }
+
+  private URL getFullyQualifiedRequestUri(String resourceName)
+      throws FirebaseInstallationsException {
+    try {
+      return new URL(
+          String.format(
+              "https://%s/%s/%s",
+              FIREBASE_INSTALLATIONS_API_DOMAIN, FIREBASE_INSTALLATIONS_API_VERSION, resourceName));
+    } catch (MalformedURLException e) {
+      throw new FirebaseInstallationsException(e.getMessage(), Status.UNAVAILABLE);
+    }
   }
 
   /**
@@ -342,8 +350,8 @@ public class FirebaseInstallationServiceClient {
    *       <li>401: return response with status INVALID_AUTH
    *       <li>403: return response with status BAD_CONFIG
    *       <li>404: return response with status INVALID_AUTH
-   *       <li>429: throw IOException
-   *       <li>500: throw IOException
+   *       <li>429: throw FirebaseInstallationsException
+   *       <li>500: throw FirebaseInstallationsException
    *     </ul>
    */
   @NonNull
@@ -352,17 +360,11 @@ public class FirebaseInstallationServiceClient {
       @NonNull String fid,
       @NonNull String projectID,
       @NonNull String refreshToken)
-      throws IOException {
+      throws FirebaseInstallationsException {
     String resourceName =
         String.format(GENERATE_AUTH_TOKEN_REQUEST_RESOURCE_NAME_FORMAT, projectID, fid);
     int retryCount = 0;
-    URL url =
-        new URL(
-            String.format(
-                "https://%s/%s/%s",
-                FIREBASE_INSTALLATIONS_API_DOMAIN,
-                FIREBASE_INSTALLATIONS_API_VERSION,
-                resourceName));
+    URL url = getFullyQualifiedRequestUri(resourceName);
     while (retryCount <= MAX_RETRIES) {
       HttpURLConnection httpURLConnection = openHttpURLConnection(url, apiKey);
       try {
@@ -391,11 +393,15 @@ public class FirebaseInstallationServiceClient {
         logBadConfigError();
 
         return TokenResult.builder().setResponseCode(TokenResult.ResponseCode.BAD_CONFIG).build();
+      } catch (IOException ignored) {
+        retryCount++;
       } finally {
         httpURLConnection.disconnect();
       }
     }
-    throw new IOException();
+    throw new FirebaseInstallationsException(
+        "Firebase Installations Service is unavailable. Please try again later.",
+        Status.UNAVAILABLE);
   }
 
   private static void logBadConfigError() {
@@ -407,8 +413,16 @@ public class FirebaseInstallationServiceClient {
             + " initializing Firebase.");
   }
 
-  private HttpURLConnection openHttpURLConnection(URL url, String apiKey) throws IOException {
-    HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+  private HttpURLConnection openHttpURLConnection(URL url, String apiKey)
+      throws FirebaseInstallationsException {
+    HttpURLConnection httpURLConnection;
+    try {
+      httpURLConnection = (HttpURLConnection) url.openConnection();
+    } catch (IOException ignored) {
+      throw new FirebaseInstallationsException(
+          "Firebase Installations Service is unavailable. Please try again later.",
+          Status.UNAVAILABLE);
+    }
     httpURLConnection.setConnectTimeout(NETWORK_TIMEOUT_MILLIS);
     httpURLConnection.setUseCaches(false);
     httpURLConnection.setReadTimeout(NETWORK_TIMEOUT_MILLIS);
