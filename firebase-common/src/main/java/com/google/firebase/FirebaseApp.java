dbc40aa27303d8668af97de6e4381eb653d69106
Vinay Guthal <vguthal@google.com>
2020-06-04 16:17:09 -0400

    add deprecation

--- firebase-common/src/main/java/com/google/firebase/FirebaseApp.java
+++ firebase-common/src/main/java/com/google/firebase/FirebaseApp.java
@@ -395,15 +395,12 @@ public class FirebaseApp {
    * <p>Note: this value is respected by all SDKs unless overridden by the developer via SDK
    * specific mechanisms.
    *
-   * <p>This api has been deprecated. Please use the api which takes in a Java boolean
-   *
    * @deprecated Use {@link #setDataCollectionDefaultEnabled(Boolean)} instead.
    * @hide
    */
   @KeepForSdk
   @Deprecated
   public void setDataCollectionDefaultEnabled(boolean enabled) {
-    checkNotDeleted();
     setDataCollectionDefaultEnabled(Boolean.valueOf(enabled));
   }
 
