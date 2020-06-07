a2a77fecb6ca097918fb861d1a8fe0a58421c360
Rodrigo Lazo Paz <rlazo@google.com>
2020-06-07 08:22:59 -0400

    Remove unwanted change.

--- smoke-tests/src/main/java/com/google/firebase/testing/TestSuite.java
+++ smoke-tests/src/main/java/com/google/firebase/testing/TestSuite.java
@@ -27,7 +27,6 @@ import org.junit.runners.Suite;
   BuildOnlyTest.class,
   DatabaseTest.class,
   DynamicLinksTest.class,
-  FiamTest.class,
   FirestoreTest.class,
   FunctionsTest.class,
   RemoteConfigTest.class,
