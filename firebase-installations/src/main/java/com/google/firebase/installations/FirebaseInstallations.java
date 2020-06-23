8181416e8e757216471089d536fb61e3f51ab3bf
Andreas Rayo Kniep <andirayo@gmail.com>
2020-06-23 21:57:26 +0200

    Revert "Move Preconditions check to FirebaseInstallations constructor. (#1619)" (#1707)
    This reverts commit cbc422e009690f818563a0aea80982230b1db351.

--- firebase-installations/src/main/java/com/google/firebase/installations/FirebaseInstallations.java
+++ firebase-installations/src/main/java/com/google/firebase/installations/FirebaseInstallations.java
@@ -139,7 +139,6 @@ public class FirebaseInstallations implements FirebaseInstallationsApi {
             TimeUnit.SECONDS,
             new LinkedBlockingQueue<>(),
             THREAD_FACTORY);
-    preConditionChecks();
   }
 
   /**
@@ -217,6 +216,7 @@ public class FirebaseInstallations implements FirebaseInstallationsApi {
   @NonNull
   @Override
   public Task<String> getId() {
+    preConditionChecks();
     return Tasks.forResult(doGetId());
   }
 
@@ -231,6 +231,7 @@ public class FirebaseInstallations implements FirebaseInstallationsApi {
   @NonNull
   @Override
   public Task<InstallationTokenResult> getToken(boolean forceRefresh) {
+    preConditionChecks();
     Task<InstallationTokenResult> task = addGetAuthTokenListener();
     backgroundExecutor.execute(() -> doGetAuthToken(forceRefresh));
     return task;
