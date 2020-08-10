77b48091484c3ad1001e7144b3bc27e583501d11
Rosário Pereira Fernandes <rosariofernandes51@gmail.com>
2020-08-10 21:41:23 +0200

    style: google java format

--- firebase-storage/src/testUtil/java/com/google/firebase/storage/TestUtil.java
+++ firebase-storage/src/testUtil/java/com/google/firebase/storage/TestUtil.java
@@ -18,8 +18,8 @@ import androidx.annotation.Nullable;
 import java.util.List;
 
 public class TestUtil {
-    public static ListResult listResult(
-            List<StorageReference> prefixes, List<StorageReference> items, @Nullable String pageToken) {
-        return new ListResult(prefixes, items, pageToken);
-    }
+  public static ListResult listResult(
+      List<StorageReference> prefixes, List<StorageReference> items, @Nullable String pageToken) {
+    return new ListResult(prefixes, items, pageToken);
+  }
 }
