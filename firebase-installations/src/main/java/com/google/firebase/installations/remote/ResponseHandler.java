c75a1a704f220d0f28585de1ac816d61cdbe7b50
Ankita Jhawar <ankitaj224@gmail.com>
2020-09-10 13:09:23 -0700

    Addressing Maksym's comments.

--- firebase-installations/src/main/java/com/google/firebase/installations/remote/ResponseHandler.java
+++ firebase-installations/src/main/java/com/google/firebase/installations/remote/ResponseHandler.java
@@ -20,42 +20,50 @@ import java.util.concurrent.TimeUnit;
 
 /**
  * The {@link ResponseHandler} class calculates the next allowed request time. Also, decides whether
- * the given request is allowed to execute.
+ * a network request to FIS servers is allowed to execute.
  *
  * @hide
  */
 class ResponseHandler {
   private static final long BACKOFF_TIME_24H_IN_MILLIS = TimeUnit.HOURS.toMillis(24);
+  private static final long BACKOFF_TIME_30_MINS_IN_MILLIS = TimeUnit.MINUTES.toMillis(30);
   private final Utils utils;
 
   @GuardedBy("this")
   private long nextAllowedRequestTime = Long.MAX_VALUE;
 
   @GuardedBy("this")
-  private int retryCount = 0;
+  private int attemptCount = 0;
 
   ResponseHandler(Utils utils) {
     this.utils = utils;
   }
 
   public synchronized void setNextAllowedRequestTime(int responseCode) {
-    retryCount++;
+    if (responseCode == 200) {
+      resetAllowedRequestTime();
+      return;
+    }
+    attemptCount++;
     long backOffTime = getBackoffTime(responseCode);
     nextAllowedRequestTime = utils.currentTimeInMillis() + backOffTime;
-    if (backOffTime == BACKOFF_TIME_24H_IN_MILLIS) {
-      retryCount = 0;
-    }
+  }
+
+  private synchronized void resetAllowedRequestTime() {
+    nextAllowedRequestTime = Long.MAX_VALUE;
+    attemptCount = 0;
   }
 
   private synchronized long getBackoffTime(int responseCode) {
+    // Fixed 24 hours silence period for non-retryable errors. Read more: b/160751425.
+    if (isNonRetryableError(responseCode)) {
+      return BACKOFF_TIME_24H_IN_MILLIS;
+    }
     // Quickly increasing dynamically configured back-off strategy for Retryable errors. Read more:
     // https://cloud.google.com/storage/docs/exponential-backoff.
-    if (!isNonRetryableError(responseCode)) {
-      return (long)
-          Math.min(Math.pow(2, retryCount) + utils.getRandomMillis(), BACKOFF_TIME_24H_IN_MILLIS);
-    }
-    // Fixed 24 hours silence period for non-retryable errors. Read more: b/160751425.
-    return BACKOFF_TIME_24H_IN_MILLIS;
+    return (long)
+        Math.min(
+            Math.pow(2, attemptCount) + utils.getRandomMillis(), BACKOFF_TIME_30_MINS_IN_MILLIS);
   }
 
   // Response codes classified as non-retryable for FIS API, all other response codes will be
@@ -65,8 +73,11 @@ class ResponseHandler {
   }
 
   public boolean isRequestAllowed() {
-    boolean isRequestAllowed = utils.currentTimeInMillis() > nextAllowedRequestTime;
-    if (isRequestAllowed) {
+    long currentTime = utils.currentTimeInMillis();
+    boolean isRequestAllowed =
+        attemptCount == 0 || currentTime > nextAllowedRequestTime;
+
+    if (currentTime > nextAllowedRequestTime) {
       synchronized (this) {
         nextAllowedRequestTime = Long.MAX_VALUE;
       }
