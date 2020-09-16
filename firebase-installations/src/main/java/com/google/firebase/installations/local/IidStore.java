9ba52937273a9592812ae78a0389974fce7109ba
Ankita Jhawar <ankitaj224@gmail.com>
2020-09-16 14:53:29 -0700

    Addressing comments.

--- firebase-installations/src/main/java/com/google/firebase/installations/local/IidStore.java
+++ firebase-installations/src/main/java/com/google/firebase/installations/local/IidStore.java
@@ -167,13 +167,6 @@ public class IidStore {
     }
   }
 
-  /**
-   * FirebaseInstallations SDK uses the SHA1 hash for backwards compatibility with the legacy
-   * InstanceID SDK. The SHA1 hash is used to access Instance IDs stored on the device and not for
-   * any security relevant process. This is a one-time step that allows migration of old client
-   * identifiers. Cryptographic security is not needed here so potential hash collisions are not a
-   * problem.
-   */
   @Nullable
   private static String getIdFromPublicKey(@NonNull PublicKey publicKey) {
     // The ID is the sha of the public key truncated to 60 bit, with first 4 bits switched to
@@ -183,6 +176,11 @@ public class IidStore {
 
     byte[] derPub = publicKey.getEncoded();
     try {
+      // FirebaseInstallations SDK uses the SHA1 hash for backwards compatibility with the legacy
+      // InstanceID SDK. The SHA1 hash is used to access Instance IDs stored on the device and not
+      // for any security relevant process. This is a one-time step that allows migration of old
+      // client identifiers. Cryptographic security is not needed here so potential hash collisions
+      // are not a problem.
       MessageDigest md = MessageDigest.getInstance("SHA1");
 
       byte[] digest = md.digest(derPub);
