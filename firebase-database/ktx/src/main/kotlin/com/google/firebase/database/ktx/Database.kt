5cba5bbfefcc6bbe8e4706861f8cb0ed40844367
Rosário Pereira Fernandes <rosariofernandes51@gmail.com>
2020-10-01 11:49:15 +0200

    feat(database-ktx): add KTX functions for event listeners

--- firebase-database/ktx/src/main/kotlin/com/google/firebase/database/ktx/Database.kt
+++ firebase-database/ktx/src/main/kotlin/com/google/firebase/database/ktx/Database.kt
@@ -18,10 +18,14 @@ import androidx.annotation.Keep
 import com.google.firebase.FirebaseApp
 import com.google.firebase.components.Component
 import com.google.firebase.components.ComponentRegistrar
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.GenericTypeIndicator
 import com.google.firebase.database.MutableData
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.platforminfo.LibraryVersionComponent
 
@@ -57,6 +61,55 @@ inline fun <reified T> DataSnapshot.getValue(): T? {
  */
 inline fun <reified T> MutableData.getValue(): T? {
     return getValue(object : GenericTypeIndicator<T>() {})
+}
+
+inline fun DatabaseReference.addValueEventListener(
+        crossinline onDataChange: (snapshot: DataSnapshot) -> Unit,
+        crossinline onCancelled: (error: DatabaseError) -> Unit
+): ValueEventListener {
+    val listener = object : ValueEventListener {
+        override fun onDataChange(snapshot: DataSnapshot) = onDataChange(snapshot)
+        override fun onCancelled(error: DatabaseError) = onCancelled(error)
+    }
+    addValueEventListener(listener)
+    return listener
+}
+
+inline fun DatabaseReference.addListenerForSingleValueEvent(
+        crossinline onDataChange: (snapshot: DataSnapshot) -> Unit,
+        crossinline onCancelled: (error: DatabaseError) -> Unit
+): ValueEventListener {
+    val listener = object : ValueEventListener {
+        override fun onDataChange(snapshot: DataSnapshot) = onDataChange(snapshot)
+        override fun onCancelled(error: DatabaseError) = onCancelled(error)
+    }
+    addListenerForSingleValueEvent(listener)
+    return listener
+}
+
+inline fun DatabaseReference.addChildEventListener(
+        crossinline onChildAdded: (snapshot: DataSnapshot, previousChildName: String?) -> Unit,
+        crossinline onChildChanged: (snapshot: DataSnapshot, previousChildName: String?) -> Unit,
+        crossinline onChildRemoved: (snapshot: DataSnapshot) -> Unit,
+        crossinline onChildMoved: (snapshot: DataSnapshot, previousChildName: String?) -> Unit,
+        crossinline onCancelled: (error: DatabaseError) -> Unit
+): ChildEventListener {
+    val listener = object : ChildEventListener {
+        override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) =
+                onChildAdded(snapshot, previousChildName)
+
+        override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) =
+                onChildChanged(snapshot, previousChildName)
+
+        override fun onChildRemoved(snapshot: DataSnapshot) = onChildRemoved(snapshot)
+
+        override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) =
+                onChildMoved(snapshot, previousChildName)
+
+        override fun onCancelled(error: DatabaseError) = onCancelled(error)
+    }
+    addChildEventListener(listener)
+    return listener
 }
 
 internal const val LIBRARY_NAME: String = "fire-db-ktx"
