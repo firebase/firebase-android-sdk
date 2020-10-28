229ea5b9bf5f61d75654d9d90ba5929b9ff20c7e
Tejas Deshpande <tdeshpande@google.com>
2020-10-28 11:51:35 -0400

    Add comment that the private field in firebase-datatransport is used by reflection

--- transport/transport-backend-cct/src/main/java/com/google/android/datatransport/cct/CCTDestination.java
+++ transport/transport-backend-cct/src/main/java/com/google/android/datatransport/cct/CCTDestination.java
@@ -40,6 +40,7 @@ public final class CCTDestination implements EncodedDestination {
   private static final String EXTRAS_VERSION_MARKER = "1$";
   private static final String EXTRAS_DELIMITER = "\\";
 
+  // This is private, but is used in reflection by firebase messaging, so changing it will break it.
   private static final Set<Encoding> SUPPORTED_ENCODINGS =
       Collections.unmodifiableSet(
           new HashSet<>(Arrays.asList(Encoding.of("proto"), Encoding.of("json"))));
