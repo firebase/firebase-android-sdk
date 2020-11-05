70dabcac04485fb0577352d572df2e9170b46ed5
annzimmer <annz@google.com>
2020-11-05 13:46:58 -0500

    Add tests for download service calls

--- firebase-ml-modeldownloader/src/main/java/com/google/firebase/ml/modeldownloader/internal/CustomModelDownloadService.java
+++ firebase-ml-modeldownloader/src/main/java/com/google/firebase/ml/modeldownloader/internal/CustomModelDownloadService.java
@@ -14,15 +14,17 @@
 
 package com.google.firebase.ml.modeldownloader.internal;
 
-import static com.google.android.gms.common.internal.Preconditions.checkArgument;
-
 import android.os.Build.VERSION_CODES;
 import android.util.JsonReader;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
-import androidx.annotation.WorkerThread;
 import com.google.android.gms.common.util.VisibleForTesting;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.installations.FirebaseInstallationsApi;
+import com.google.firebase.installations.InstallationTokenResult;
 import com.google.firebase.ml.modeldownloader.CustomModel;
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -32,32 +34,59 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Locale;
-import java.util.regex.Pattern;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import javax.net.ssl.HttpsURLConnection;
 
 /**
  * Calls the Download Service API and returns the information related to the current status of a
  * custom model. If new model available returns 200 and new model details such as the download url;
  * If same model is available returns 304 (not modified) Otherwise returns an error.
+ *
+ * @hide
  */
 @RequiresApi(api = VERSION_CODES.KITKAT)
-@WorkerThread
-class CustomModelDownloadService {
+final class CustomModelDownloadService {
+
+  private static final String TAG = "CustomModelDownloadSer";
+  private final ExecutorService executorService;
 
   private static final int CONNECTION_TIME_OUT_MS = 2000; // 2 seconds.
   private static final Charset UTF_8 = StandardCharsets.UTF_8;
-  private static final Pattern EXPIRATION_TIMESTAMP_PATTERN = Pattern.compile("[0-9]+s");
+
   private static final String ETAG_HEADER = "ETag";
 
-  private final String DOWNLOAD_MODEL_REGEX =
+  @VisibleForTesting
+  static final String INSTALLATIONS_AUTH_TOKEN_HEADER = "X-Goog-Firebase-Installations-Auth";
+
+  @VisibleForTesting
+  static final String DOWNLOAD_MODEL_REGEX =
       "https://firebaseml.googleapis.com/Model/v1beta2/projects/%s/models/%s:download";
+
+  private FirebaseInstallationsApi firebaseInstallations;
 
   @VisibleForTesting
   static final String PARSING_EXPIRATION_TIME_ERROR_MESSAGE = "Invalid Expiration Timestamp.";
 
+  CustomModelDownloadService() {
+    firebaseInstallations = FirebaseApp.getInstance().get(FirebaseInstallationsApi.class);
+    // what should this be?
+    executorService = Executors.newCachedThreadPool();
+  }
+
+  @VisibleForTesting
+  CustomModelDownloadService(
+      FirebaseInstallationsApi firebaseInstallations, ExecutorService executorService) {
+    this.firebaseInstallations = firebaseInstallations;
+    this.executorService = executorService;
+  }
+
   @Nullable
-  public CustomModel getNewDownloadUrlWithExpiry(String projectNumber, String modelName)
+  public Task<CustomModel> getNewDownloadUrlWithExpiry(String projectNumber, String modelName)
       throws Exception {
     return getCustomModelDetails(projectNumber, modelName, "");
   }
@@ -75,37 +104,66 @@ class CustomModelDownloadService {
    * @throws Exception -errors when call to API fails.
    */
   @Nullable
-  public CustomModel getCustomModelDetails(String projectNumber, String modelName, String modelHash)
-      throws Exception {
+  public Task<CustomModel> getCustomModelDetails(
+      String projectNumber, String modelName, String modelHash) throws Exception {
     try {
       URL url = new URL(String.format(DOWNLOAD_MODEL_REGEX, projectNumber, modelName));
+      System.out.println("Set this url : " + url);
+
       HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
       connection.setConnectTimeout(CONNECTION_TIME_OUT_MS);
       if (modelHash != null && !modelHash.isEmpty()) {
         connection.setRequestProperty("If-None-Match", modelHash);
       }
-      connection.connect();
-      int httpResponseCode = connection.getResponseCode();
 
-      if ((httpResponseCode != HttpURLConnection.HTTP_OK)
-          && (httpResponseCode != HttpURLConnection.HTTP_NOT_MODIFIED)) {
-        String errorMessage = getErrorStream(connection);
-        throw new Exception(
-            String.format(
-                Locale.getDefault(),
-                "Failed to connect to Firebase ML download server with HTTP status code: %d"
-                    + " and error message: %s",
-                connection.getResponseCode(),
-                errorMessage));
-      } else if (httpResponseCode == HttpURLConnection.HTTP_NOT_MODIFIED) {
-        return null;
-      }
+      Task<InstallationTokenResult> installationAuthTokenTask =
+          firebaseInstallations.getToken(false);
+      installationAuthTokenTask.continueWithTask(
+          executorService,
+          (CustomModelTask) -> {
+            if (!installationAuthTokenTask.isSuccessful()) {
+              // TODO(annz) update to better error handling (use FirebaseMLExceptions)
+              return Tasks.forException(
+                  new Exception(
+                      "Firebase Installations failed to get installation auth token for fetch.",
+                      installationAuthTokenTask.getException()));
+            }
+            connection.setRequestProperty(
+                INSTALLATIONS_AUTH_TOKEN_HEADER, installationAuthTokenTask.getResult().getToken());
 
-      return readCustomModelResponse(modelName, connection);
+            return fetchDownloadDetails(modelName, connection);
+          });
+
     } catch (Exception e) {
       // TODO(annz) update to better error handling (use FirebaseMLExceptions)
       throw new Exception("Error reading custom model from download service: ", e);
     }
+    throw new Exception("FAILED");
+  }
+
+  private Task<CustomModel> fetchDownloadDetails(String modelName, HttpsURLConnection connection)
+      throws Exception {
+    System.out.println("connection details: " + connection.getRequestMethod());
+    System.out.println("connection details: " + connection.getContent());
+
+    connection.connect();
+    int httpResponseCode = connection.getResponseCode();
+
+    if ((httpResponseCode != HttpURLConnection.HTTP_OK)
+        && (httpResponseCode != HttpURLConnection.HTTP_NOT_MODIFIED)) {
+      String errorMessage = getErrorStream(connection);
+      throw new Exception(
+          String.format(
+              Locale.getDefault(),
+              "Failed to connect to Firebase ML download server with HTTP status code: %d"
+                  + " and error message: %s",
+              connection.getResponseCode(),
+              errorMessage));
+    } else if (httpResponseCode == HttpURLConnection.HTTP_NOT_MODIFIED) {
+      return null;
+    }
+
+    return Tasks.forResult(readCustomModelResponse(modelName, connection));
   }
 
   private CustomModel readCustomModelResponse(
@@ -151,12 +209,19 @@ class CustomModelDownloadService {
    */
   @VisibleForTesting
   static long parseTokenExpirationTimestamp(String expiresIn) {
-    checkArgument(
-        EXPIRATION_TIMESTAMP_PATTERN.matcher(expiresIn).matches(),
-        PARSING_EXPIRATION_TIME_ERROR_MESSAGE);
-    return (expiresIn == null || expiresIn.length() == 0)
-        ? 0L
-        : Long.parseLong(expiresIn.substring(0, expiresIn.length() - 1));
+    if (expiresIn == null || expiresIn.length() == 0) {
+      return 0;
+    }
+
+    try {
+      String isoDatePattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
+      SimpleDateFormat iso8601Format = new SimpleDateFormat(isoDatePattern);
+      Date date = iso8601Format.parse(expiresIn);
+      return date.getTime();
+    } catch (ParseException pe) {
+      // log error and maybe throw an error
+      return 0;
+    }
   }
 
   private String getErrorStream(HttpsURLConnection connection) {
--- firebase-ml-modeldownloader/src/test/java/com/google/firebase/ml/modeldownloader/internal/CustomModelDownloadServiceTest.java
+++ firebase-ml-modeldownloader/src/test/java/com/google/firebase/ml/modeldownloader/internal/CustomModelDownloadServiceTest.java
@@ -14,10 +14,28 @@
 
 package com.google.firebase.ml.modeldownloader.internal;
 
-import static com.google.common.truth.Truth.assertThat;
+// import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+// import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
+// import static com.github.tomakehurst.wiremock.client.WireMock.get;
+// import static com.github.tomakehurst.wiremock.client.WireMock.post;
+// import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
+// import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+// import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+// import static com.github.tomakehurst.wiremock.client.WireMock.verify;
 import static com.google.common.truth.Truth.assertWithMessage;
-import static org.junit.Assert.fail;
+import static org.mockito.ArgumentMatchers.anyBoolean;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import androidx.annotation.NonNull;
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import com.google.android.gms.tasks.Tasks;
+import com.google.common.util.concurrent.MoreExecutors;
+import com.google.firebase.installations.FirebaseInstallationsApi;
+import com.google.firebase.installations.InstallationTokenResult;
+import java.util.concurrent.ExecutorService;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
@@ -26,31 +44,105 @@ import org.robolectric.RobolectricTestRunner;
 @RunWith(RobolectricTestRunner.class)
 public class CustomModelDownloadServiceTest {
 
-  private final String TEST_EXPIRATION_TIMESTAMP = "604800s";
-  private final long TEST_EXPIRATION_IN_SECS = 604800;
+  private final String TEST_EXPIRATION_TIMESTAMP = "2020-11-04T17:56:34.215Z";
+  private final long TEST_EXPIRATION_IN_MS = 1604530594215L;
   private final String INCORRECT_EXPIRATION_TIMESTAMP = "2345";
+  private final String PROJECT_ID = "fake-firebase-ml-team";
+  private final String MODEL_NAME = "MyModel";
+
+  private static final String INSTALLATION_TOKEN = "instance_token";
+  private static final InstallationTokenResult INSTALLATION_TOKEN_RESULT =
+      new InstallationTokenResult() {
+        @NonNull
+        @Override
+        public String getToken() {
+          return INSTALLATION_TOKEN;
+        }
+
+        @Override
+        public long getTokenExpirationTimestamp() {
+          return 0;
+        }
+
+        @Override
+        public long getTokenCreationTimestamp() {
+          return 0;
+        }
+
+        @Override
+        public Builder toBuilder() {
+          return null;
+        }
+      };
+
+  @Rule public WireMockRule wireMockRule = new WireMockRule(8999);
+
+  private ExecutorService directExecutor;
+  private FirebaseInstallationsApi installationsApiMock;
+
+  @Before
+  public void setUp() throws Exception {
+    directExecutor = MoreExecutors.newDirectExecutorService();
+    installationsApiMock = mock(FirebaseInstallationsApi.class);
+    when(installationsApiMock.getToken(anyBoolean()))
+        .thenReturn(Tasks.forResult(INSTALLATION_TOKEN_RESULT));
+  }
 
   @Test
   public void parseTokenExpirationTimestamp_successful() {
     long actual =
         CustomModelDownloadService.parseTokenExpirationTimestamp(TEST_EXPIRATION_TIMESTAMP);
 
-    assertWithMessage("Exception status doesn't match")
+    assertWithMessage("Expected time to be properly formatted.")
         .that(actual)
-        .isEqualTo(TEST_EXPIRATION_IN_SECS);
+        .isEqualTo(TEST_EXPIRATION_IN_MS);
   }
 
   @Test
   public void parseTokenExpirationTimestamp_failed() {
-    try {
-      CustomModelDownloadService.parseTokenExpirationTimestamp(INCORRECT_EXPIRATION_TIMESTAMP);
-      fail("Parsing token expiration timestamp failed.");
-    } catch (IllegalArgumentException expected) {
-      assertThat(expected)
-          .hasMessageThat()
-          .isEqualTo(CustomModelDownloadService.PARSING_EXPIRATION_TIME_ERROR_MESSAGE);
-    }
+    long actual =
+        CustomModelDownloadService.parseTokenExpirationTimestamp(INCORRECT_EXPIRATION_TIMESTAMP);
+
+    assertWithMessage("Invalid times should return 0.").that(actual).isEqualTo(0);
   }
 
   // TODO(annz) add url testing using wiremock
+  //
+  //  @Test
+  //  public void testDownloadService_noHashSuccess() throws Exception {
+  //    String downloadPath = String
+  //        .format(CustomModelDownloadService.DOWNLOAD_MODEL_REGEX, PROJECT_ID, MODEL_NAME);
+  //    // this is copied from CctTranspostBackendTest.java
+  //    // it fails with j
+  //    // java.lang.NoSuchMethodError: 'java.lang.CharSequence
+  // org.apache.http.util.Args.containsNoBlanks
+  //    // actually want the get call below.
+  //    stubFor(
+  //        post(urlEqualTo("/api"))
+  //            .willReturn(
+  //                aResponse()
+  //                    .withStatus(200)
+  //                    .withHeader("Content-Type", "application/json;charset=UTF8;hello=world")
+  //                    .withBody("{\"nextRequestWaitMillis\":3}")));
+  //
+  //    stubFor(
+  //        get(urlEqualTo(downloadPath))
+  //            .withHeader(CustomModelDownloadService.INSTALLATIONS_AUTH_TOKEN_HEADER,
+  //                equalTo(INSTALLATION_TOKEN))
+  //            .willReturn(
+  //                aResponse()
+  //                    .withStatus(200)
+  //                    .withHeader("Content-Type", "application/json;charset=UTF8;hello=world")
+  //                    .withBody("Hello world")));
+  //
+  //    CustomModelDownloadService service = new CustomModelDownloadService(installationsApiMock,
+  //        directExecutor);
+  //
+  //    service.getNewDownloadUrlWithExpiry(PROJECT_ID, MODEL_NAME);
+  //
+  //    verify(
+  //        postRequestedFor(urlEqualTo(downloadPath))
+  //            .withHeader(CustomModelDownloadService.INSTALLATIONS_AUTH_TOKEN_HEADER,
+  //                equalTo(INSTALLATION_TOKEN)));
+  //  }
 }
