e0295a860a095e2f8ad419a956e902bde7a00c7a
Yifan Yang <yifayan@gmail.com>
2020-12-08 10:11:12 -0800

    Remove messaging test from smoke test suite.

--- smoke-tests/src/main/java/com/google/firebase/testing/BuildOnlyTest.java
+++ smoke-tests/src/main/java/com/google/firebase/testing/BuildOnlyTest.java
@@ -18,7 +18,7 @@ import static com.google.common.truth.Truth.assertThat;
 
 import com.google.firebase.appindexing.FirebaseAppIndex;
 import com.google.firebase.inappmessaging.FirebaseInAppMessaging;
-import com.google.firebase.messaging.FirebaseMessaging;
+//import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.perf.FirebasePerformance;
 import com.google.firebase.ml.vision.FirebaseVision;
 import org.junit.Test;
@@ -39,10 +39,10 @@ public final class BuildOnlyTest {
     assertThat(FirebaseInAppMessaging.getInstance()).isNotNull();
   }
 
-  @Test
-  public void messaging_IsNotNull() {
-    assertThat(FirebaseMessaging.getInstance()).isNotNull();
-  }
+//  @Test
+//  public void messaging_IsNotNull() {
+//    assertThat(FirebaseMessaging.getInstance()).isNotNull();
+//  }
 
   @Test
   public void performance_IsNotNull() {
