91510a096c2f34c85e36fd49270d1c658b2611c8
Sebastian Schmidt <mrschmidt@google.com>
2021-02-08 09:53:41 -0700

    Revert test runners

--- firebase-database/src/androidTest/java/com/google/firebase/database/IntegrationTestHelpers.java
+++ firebase-database/src/androidTest/java/com/google/firebase/database/IntegrationTestHelpers.java
@@ -14,6 +14,8 @@
 
 package com.google.firebase.database;
 
+import androidx.annotation.NonNull;
+
 import static com.google.firebase.database.core.utilities.Utilities.hardAssert;
 import static com.google.firebase.database.snapshot.NodeUtilities.NodeFromJSON;
 import static org.junit.Assert.assertTrue;
@@ -22,6 +24,7 @@ import static org.junit.Assert.fail;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.internal.InternalAuthProvider;
 import com.google.firebase.database.android.AndroidAuthTokenProvider;
 import com.google.firebase.database.core.CompoundWrite;
 import com.google.firebase.database.core.Context;
@@ -35,6 +38,8 @@ import com.google.firebase.database.core.view.QuerySpec;
 import com.google.firebase.database.future.WriteFuture;
 import com.google.firebase.database.snapshot.ChildKey;
 import com.google.firebase.database.snapshot.Node;
+import com.google.firebase.inject.Deferred;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Method;
@@ -261,7 +266,9 @@ public class IntegrationTestHelpers {
     config.setEventTarget(new TestEventTarget());
     config.setRunLoop(runLoop);
     config.setFirebaseApp(FirebaseApp.getInstance());
-    config.setAuthTokenProvider(AndroidAuthTokenProvider.forUnauthenticatedAccess());
+    config.setAuthTokenProvider(new AndroidAuthTokenProvider(never -> {
+      // Auth is not available in our integration tests
+    }));
     return config;
   }
 
