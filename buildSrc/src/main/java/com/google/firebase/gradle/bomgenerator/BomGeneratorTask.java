fdf38e07d248f37fe0492e7337b482688c99d0d0
David Motsonashvili <davidmotson@google.com>
2021-02-19 11:53:17 -0800

    fix whitespace and gJF

--- buildSrc/src/main/java/com/google/firebase/gradle/bomgenerator/BomGeneratorTask.java
+++ buildSrc/src/main/java/com/google/firebase/gradle/bomgenerator/BomGeneratorTask.java
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
 package com.google.firebase.gradle.bomgenerator;
 
 import static java.util.stream.Collectors.toList;
