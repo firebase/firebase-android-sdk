2efb263a10180ca77d7b936849ab91a115454d9a
Rodrigo Lazo Paz <rlazo@google.com>
2021-02-20 00:23:42 -0500

    Add copyright notice

--- buildSrc/gms_transform.sh
+++ buildSrc/gms_transform.sh
@@ -1,4 +1,17 @@
-#Derived from post processing done in granular releases
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 set -x
 #!/bin/sh -x
 grep -rl 'http://' | xargs sed -i 's|http://|//|g'
@@ -30,7 +43,6 @@ find . -name '*.html' | xargs sed -i 's|/docs/reference/_book.yaml|/android/_boo
 find . -name "_toc.yaml" | xargs sed -i 's|section: |section:|g'
 
 #Delete blank lines at top of file
-find . -name "_toc.yaml" | xargs sed -i '/./,$!d' 
+find . -name "_toc.yaml" | xargs sed -i '/./,$!d'
 
 rm -rf "ktx/"
-
