3acfaeedfeccf08e15651786228278558245fd1c
Vladimir Kryachko <vkryachko@google.com>
2021-03-18 13:23:42 -0400

    Rename and improve lint test that verifies fall positives are gone.

--- tools/lint/src/test/kotlin/ProviderAssignmentDetectorTests.kt
+++ tools/lint/src/test/kotlin/ProviderAssignmentDetectorTests.kt
@@ -88,14 +88,16 @@ class ProviderAssignmentDetectorTests : LintDetectorTest() {
                 .expectClean()
     }
 
-    fun test_assignmentAnssdUseOfProvider_shouldSucceed() {
+    fun test_assignmentOfAPropertyOrMethodOfTheProvidedObject_shouldSucceed() {
         lint().files(java(providerSource()), java("""
             import com.google.firebase.inject.Provider;
 
             class Foo {
               private final int length;
+              private final String s;
               Foo(Provider<String> p) {
                 length = p.get().length;
+                s = p.get().toString();
               }
             }
         """.trimIndent()))
