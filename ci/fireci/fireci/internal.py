57494c8aa1af582825880ce335ee12d00ec34bf3
Vladimir Kryachko <vkryachko@google.com>
2021-04-15 16:25:43 -0400

    Cleanup dead code in fireci, refactor.

--- ci/fireci/fireci/internal.py
+++ ci/fireci/fireci/internal.py
@@ -21,7 +21,6 @@ import logging
 import os
 import shutil
 
-from . import emulator
 
 _logger = logging.getLogger('fireci')
 
@@ -51,16 +50,6 @@ def _artifact_handler(target_directory, artifact_patterns):
         shutil.copyfile(path, target_name)
 
 
-@contextlib.contextmanager
-def _emulator_handler(enabled, *args, **kwargs):
-  if not enabled:
-    yield
-    return
-
-  with emulator.EmulatorHandler(*args, **kwargs):
-    yield
-
-
 class _CommonOptions:
   pass
 
@@ -84,32 +73,6 @@ _pass_options = click.make_pass_decorator(_CommonOptions, ensure=True)
     multiple=True,
     type=str,
 )
-@click.option(
-    '--with-emulator',
-    default=False,
-    help='Specifies whether to start an Android emulator a command executes.',
-    is_flag=True,
-)
-@click.option(
-    '--emulator-name',
-    default='test',
-    help='Specifies the AVD name to launch the emulator with.',
-)
-@click.option(
-    '--emulator-binary',
-    default='emulator',
-    help='Specifies the name/full path to the emulator binary.',
-)
-@click.option(
-    '--adb-binary',
-    default='adb',
-    help='Specifies the name/full path to the adb binary.',
-)
-@click.option(
-    '--enable-metrics',
-    is_flag=True,
-    envvar='FIREBASE_ENABLE_METRICS',
-    help='Enables metrics collection for various build stages.')
 @_pass_options
 def main(options, **kwargs):
   """Main command group.
@@ -141,12 +104,7 @@ def ci_command(name=None):
     def new_func(ctx, options, *args, **kwargs):
       with _artifact_handler(
           options.artifact_target_dir,
-          options.artifact_patterns), _emulator_handler(
-              options.with_emulator,
-              options.artifact_target_dir,
-              name=options.emulator_name,
-              emulator_binary=options.emulator_binary,
-              adb_binary=options.adb_binary):
+          options.artifact_patterns):
         return ctx.invoke(f, *args, **kwargs)
 
     return functools.update_wrapper(new_func, f)
