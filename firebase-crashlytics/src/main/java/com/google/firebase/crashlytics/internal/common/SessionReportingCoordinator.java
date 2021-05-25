7cdb3b81ba268d483f7745289dbac5e9cc35f0ab
Sam Edson <samedson@google.com>
2021-05-25 17:07:25 -0400

    Crashlytics Add Internal Keys feature for Unity Metadata (#2671)

--- firebase-crashlytics/src/main/java/com/google/firebase/crashlytics/internal/common/SessionReportingCoordinator.java
+++ firebase-crashlytics/src/main/java/com/google/firebase/crashlytics/internal/common/SessionReportingCoordinator.java
@@ -220,6 +220,8 @@ public class SessionReportingCoordinator implements CrashlyticsLifecycleEvents {
 
     final List<CustomAttribute> sortedCustomAttributes =
         getSortedCustomAttributes(reportMetadata.getCustomKeys());
+    final List<CustomAttribute> sortedInternalKeys =
+        getSortedCustomAttributes(reportMetadata.getInternalKeys());
 
     if (!sortedCustomAttributes.isEmpty()) {
       eventBuilder.setApp(
@@ -227,6 +229,7 @@ public class SessionReportingCoordinator implements CrashlyticsLifecycleEvents {
               .getApp()
               .toBuilder()
               .setCustomAttributes(ImmutableList.from(sortedCustomAttributes))
+              .setInternalKeys(ImmutableList.from(sortedInternalKeys))
               .build());
     }
 
