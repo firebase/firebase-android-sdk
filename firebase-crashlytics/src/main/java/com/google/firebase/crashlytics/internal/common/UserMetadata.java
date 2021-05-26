ae357812a87c4ae48596d041ffb68055ecdf7b0f
Sam Edson <samedson@google.com>
2021-05-26 15:55:18 -0400

    Revert "Crashlytics Add Internal Keys feature for Unity Metadata (#2671)" (#2697)
    This reverts commit 79c13051737a09f3bfb7630628f490a8f52e7e15.

--- firebase-crashlytics/src/main/java/com/google/firebase/crashlytics/internal/common/UserMetadata.java
+++ firebase-crashlytics/src/main/java/com/google/firebase/crashlytics/internal/common/UserMetadata.java
@@ -16,17 +16,20 @@ package com.google.firebase.crashlytics.internal.common;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import com.google.firebase.crashlytics.internal.Logger;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /** Handles attributes set by the user. */
 public class UserMetadata {
   static final int MAX_ATTRIBUTES = 64;
   static final int MAX_ATTRIBUTE_SIZE = 1024;
-  static final int MAX_INTERNAL_KEY_SIZE = 8192;
 
   private String userId = null;
-  private final KeysMap customKeys = new KeysMap(MAX_ATTRIBUTES, MAX_ATTRIBUTE_SIZE);
-  private final KeysMap internalKeys = new KeysMap(MAX_ATTRIBUTES, MAX_INTERNAL_KEY_SIZE);
+  private final Map<String, String> attributes = new HashMap<>();
 
   public UserMetadata() {}
 
@@ -36,27 +39,76 @@ public class UserMetadata {
   }
 
   public void setUserId(String identifier) {
-    userId = customKeys.sanitizeAttribute(identifier);
+    userId = sanitizeAttribute(identifier);
   }
 
   @NonNull
   public Map<String, String> getCustomKeys() {
-    return customKeys.getKeys();
+    return Collections.unmodifiableMap(attributes);
   }
 
   public void setCustomKey(String key, String value) {
-    customKeys.setKey(key, value);
+    setSyncCustomKeys(
+        new HashMap<String, String>() {
+          {
+            put(sanitizeKey(key), sanitizeAttribute(value));
+          }
+        });
   }
 
   public void setCustomKeys(Map<String, String> keysAndValues) {
-    customKeys.setKeys(keysAndValues);
+    setSyncCustomKeys(keysAndValues);
   }
 
-  public Map<String, String> getInternalKeys() {
-    return internalKeys.getKeys();
+  /** Gatekeeper function for access to attributes */
+  private synchronized void setSyncCustomKeys(Map<String, String> keysAndValues) {
+    // We want all access to the attributes hashmap to be locked so that there is no way to create
+    // a race condition and add more than MAX_ATTRIBUTES keys.
+
+    // Update any existing keys first, then add any additional keys
+    Map<String, String> currentKeys = new HashMap<String, String>();
+    Map<String, String> newKeys = new HashMap<String, String>();
+
+    // Split into current and new keys
+    for (Map.Entry<String, String> entry : keysAndValues.entrySet()) {
+      String key = sanitizeKey(entry.getKey());
+      String value = (entry.getValue() == null) ? "" : sanitizeAttribute(entry.getValue());
+      if (attributes.containsKey(key)) {
+        currentKeys.put(key, value);
+      } else {
+        newKeys.put(key, value);
+      }
+    }
+
+    attributes.putAll(currentKeys);
+
+    // Add new keys if there is space
+    if (attributes.size() + newKeys.size() > MAX_ATTRIBUTES) {
+      int keySlotsLeft = MAX_ATTRIBUTES - attributes.size();
+      Logger.getLogger()
+          .v("Exceeded maximum number of custom attributes (" + MAX_ATTRIBUTES + ").");
+      List<String> newKeyList = new ArrayList<>(newKeys.keySet());
+      newKeys.keySet().retainAll(newKeyList.subList(0, keySlotsLeft));
+    }
+    attributes.putAll(newKeys);
   }
 
-  public void setInternalKey(String key, String value) {
-    internalKeys.setKey(key, value);
+  /** Checks that the key is not null then sanitizes it. */
+  private static String sanitizeKey(String key) {
+    if (key == null) {
+      throw new IllegalArgumentException("Custom attribute key must not be null.");
+    }
+    return sanitizeAttribute(key);
+  }
+
+  /** Trims the string and truncates it to MAX_ATTRIBUTE_SIZE. */
+  private static String sanitizeAttribute(String input) {
+    if (input != null) {
+      input = input.trim();
+      if (input.length() > MAX_ATTRIBUTE_SIZE) {
+        input = input.substring(0, MAX_ATTRIBUTE_SIZE);
+      }
+    }
+    return input;
   }
 }
