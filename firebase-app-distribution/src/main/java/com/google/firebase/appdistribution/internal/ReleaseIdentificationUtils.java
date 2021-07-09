a7c9e2dfc5e5cb7d5455a833657b53fe069a2e28
Rachel Prince <rachelprince@google.com>
2021-07-09 12:22:52 -0400

    Compare IasArtifactId for AAB binaries
    Extracting release identification logic into its own utils class. Also writing tests for AAB signature verification
    
    fixing tests
    
    java format fixes
    
    fixing the build
    
    Fixing some more tests

---
+++ firebase-app-distribution/src/main/java/com/google/firebase/appdistribution/internal/ReleaseIdentificationUtils.java
@@ -0,0 +1,43 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.firebase.appdistribution.internal;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+public final class ReleaseIdentificationUtils {
+  private static final String TAG = "ReleaseIdentification";
+
+  @Nullable
+  public static String extractInternalAppSharingArtifactId(Context appContext) {
+    try {
+      PackageInfo packageInfo =
+          appContext
+              .getPackageManager()
+              .getPackageInfo(appContext.getPackageName(), PackageManager.GET_META_DATA);
+      if (packageInfo.applicationInfo.metaData == null) {
+        return null;
+      }
+      return packageInfo.applicationInfo.metaData.getString("com.android.vending.internal.apk.id");
+    } catch (PackageManager.NameNotFoundException e) {
+      Log.w(TAG, "Could not extract internal app sharing artifact ID");
+      return null;
+    }
+  }
+}
--- firebase-app-distribution/src/test/java/com/google/firebase/appdistribution/FirebaseAppDistributionTest.java
+++ firebase-app-distribution/src/test/java/com/google/firebase/appdistribution/FirebaseAppDistributionTest.java
@@ -20,6 +20,7 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -28,16 +29,22 @@ import static org.robolectric.Shadows.shadowOf;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
 import android.content.pm.ResolveInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import androidx.test.core.app.ApplicationProvider;
+import androidx.test.core.content.pm.ApplicationInfoBuilder;
+import androidx.test.core.content.pm.PackageInfoBuilder;
 import com.google.android.gms.tasks.Task;
 import com.google.android.gms.tasks.Tasks;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import com.google.firebase.appdistribution.internal.AppDistributionReleaseInternal;
 import com.google.firebase.installations.FirebaseInstallationsApi;
 import com.google.firebase.installations.InstallationTokenResult;
+import java.net.ProtocolException;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -55,28 +62,31 @@ public class FirebaseAppDistributionTest {
   private static final String TEST_APP_ID_1 = "1:123456789:android:abcdef";
   private static final String TEST_PROJECT_ID = "777777777777";
   private static final String TEST_FID_1 = "cccccccccccccccccccccc";
-  private static final String TEST_FID_2 = "dddddddddddddddddddddd";
   private static final String TEST_AUTH_TOKEN = "fad.auth.token";
-  private static final String TEST_URL =
+  private static final String TEST_IAS_ARTIFACT_ID = "ias-artifact-id";
+  private static final String IAS_ARTIFACT_ID_KEY = "com.android.vending.internal.apk.id";
+  private static final long INSTALLED_VERSION_CODE = 2;
+
+  public static final String TEST_URL =
       String.format(
           "https://appdistribution.firebase.google.com/pub/testerapps/%s/installations/%s/buildalerts"
               + "?appName=com.google.firebase.appdistribution.test"
               + "&packageName=com.google.firebase.appdistribution.test",
           TEST_APP_ID_1, TEST_FID_1);
-  private static AppDistributionRelease TEST_RELEASE_1 =
-      AppDistributionRelease.builder()
-          .setBinaryType(BinaryType.APK)
+  private static final AppDistributionReleaseInternal TEST_RELEASE_NEWER =
+      AppDistributionReleaseInternal.builder()
           .setBuildVersion("3")
           .setDisplayVersion("3.0")
           .setReleaseNotes("Newer version.")
+          .setBinaryType(BinaryType.APK)
           .build();
 
-  AppDistributionRelease TEST_RELEASE_2 =
-      AppDistributionRelease.builder()
+  private static final AppDistributionReleaseInternal TEST_RELEASE_CURRENT =
+      AppDistributionReleaseInternal.builder()
           .setBinaryType(BinaryType.APK)
-          .setBuildVersion("0")
-          .setDisplayVersion("0.0")
-          .setReleaseNotes("Older version.")
+          .setBuildVersion(Long.toString(INSTALLED_VERSION_CODE))
+          .setDisplayVersion("2.0")
+          .setReleaseNotes("Current version.")
           .build();
 
   private FirebaseApp firebaseApp;
@@ -84,6 +94,7 @@ public class FirebaseAppDistributionTest {
   private TestActivity activity;
   private ShadowActivity shadowActivity;
   private ShadowPackageManager shadowPackageManager;
+  private ApplicationInfo applicationInfo;
 
   @Mock private FirebaseInstallationsApi mockFirebaseInstallations;
   @Mock private FirebaseAppDistributionTesterApiClient mockFirebaseAppDistributionTesterApiClient;
@@ -115,19 +126,31 @@ public class FirebaseAppDistributionTest {
 
     when(mockFirebaseInstallations.getId()).thenReturn(Tasks.forResult(TEST_FID_1));
     when(mockFirebaseInstallations.getToken(false))
-        .thenReturn(Tasks.forResult(mockInstallationTokenResult));
+            .thenReturn(Tasks.forResult(mockInstallationTokenResult));
+
     when(mockInstallationTokenResult.getToken()).thenReturn(TEST_AUTH_TOKEN);
 
     when(mockFirebaseAppDistributionTesterApiClient.fetchLatestRelease(
             TEST_FID_1, TEST_APP_ID_1, TEST_API_KEY, TEST_AUTH_TOKEN))
-        .thenReturn(TEST_RELEASE_1);
-
-    when(mockFirebaseAppDistributionTesterApiClient.fetchLatestRelease(
-            TEST_FID_2, TEST_APP_ID_1, TEST_API_KEY, TEST_AUTH_TOKEN))
-        .thenReturn(TEST_RELEASE_2);
+        .thenReturn(TEST_RELEASE_CURRENT);
 
     shadowPackageManager =
         shadowOf(ApplicationProvider.getApplicationContext().getPackageManager());
+
+    applicationInfo =
+        ApplicationInfoBuilder.newBuilder()
+            .setPackageName(ApplicationProvider.getApplicationContext().getPackageName())
+            .build();
+    applicationInfo.metaData = new Bundle();
+    applicationInfo.metaData.putString(IAS_ARTIFACT_ID_KEY, TEST_IAS_ARTIFACT_ID);
+    PackageInfo packageInfo =
+            PackageInfoBuilder.newBuilder()
+                    .setPackageName(ApplicationProvider.getApplicationContext().getPackageName())
+                    .setApplicationInfo(applicationInfo)
+                    .build();
+    packageInfo.setLongVersionCode(INSTALLED_VERSION_CODE);
+    shadowPackageManager.installPackage(packageInfo);
+
     activity = Robolectric.buildActivity(TestActivity.class).create().get();
     shadowActivity = shadowOf(activity);
   }
@@ -233,21 +256,86 @@ public class FirebaseAppDistributionTest {
   }
 
   @Test
-  public void getLatestReleaseFromClient_whenNotOnLatestBuild_returnsRelease() {
+  public void getLatestReleaseFromClient_whenLatestReleaseIsNewerBuildThanInstalled_returnsRelease()
+      throws FirebaseAppDistributionException, ProtocolException {
+    when(mockFirebaseAppDistributionTesterApiClient.fetchLatestRelease(
+            TEST_FID_1, TEST_APP_ID_1, TEST_API_KEY, TEST_AUTH_TOKEN))
+        .thenReturn(TEST_RELEASE_NEWER);
+
     AppDistributionRelease release =
         firebaseAppDistribution.getLatestReleaseFromClient(
             TEST_FID_1, TEST_APP_ID_1, TEST_API_KEY, TEST_AUTH_TOKEN);
 
     assertNotNull(release);
-    assertEquals(TEST_RELEASE_1.getBuildVersion(), release.getBuildVersion());
+    assertEquals(TEST_RELEASE_NEWER.getBuildVersion(), release.getBuildVersion());
   }
 
   @Test
-  public void getLatestReleaseFromClient_whenOnLatestBuild_returnsNull() {
+  public void getLatestReleaseFromClient_whenLatestReleaseIsOlderBuildThanInstalled_returnsNull()
+          throws FirebaseAppDistributionException, ProtocolException {
+    AppDistributionReleaseInternal olderTestRelease =
+            AppDistributionReleaseInternal.builder()
+                    .setBinaryType(BinaryType.APK)
+                    .setBuildVersion("1")
+                    .setDisplayVersion("1.0")
+                    .setReleaseNotes("Older version.")
+                    .build();
+    when(mockFirebaseAppDistributionTesterApiClient.fetchLatestRelease(
+            TEST_FID_1, TEST_APP_ID_1, TEST_API_KEY, TEST_AUTH_TOKEN))
+            .thenReturn(olderTestRelease);
+
     AppDistributionRelease release =
-        firebaseAppDistribution.getLatestReleaseFromClient(
-            TEST_FID_2, TEST_APP_ID_1, TEST_API_KEY, TEST_AUTH_TOKEN);
+            firebaseAppDistribution.getLatestReleaseFromClient(
+                    TEST_FID_1, TEST_APP_ID_1, TEST_API_KEY, TEST_AUTH_TOKEN);
 
     assertNull(release);
+  }
+
+  @Test
+  public void handleLatestReleaseFromClient_whenNewAabIsAvailable_returnsRelease()
+      throws Exception {
+    firebaseAppDistribution.onActivityResumed(activity);
+    when(mockFirebaseAppDistributionTesterApiClient.fetchLatestRelease(any(), any(), any(), any()))
+        .thenReturn(
+            AppDistributionReleaseInternal.builder()
+                .setBuildVersion(TEST_RELEASE_CURRENT.getBuildVersion())
+                .setDisplayVersion(TEST_RELEASE_CURRENT.getDisplayVersion())
+                .setCodeHash("codehash")
+                .setDownloadUrl("http://fake-download-url")
+                .setIasArtifactId("test-ias-artifact-id-2")
+                .setBinaryType(BinaryType.AAB)
+                .build());
+
+    AppDistributionRelease result =
+        firebaseAppDistribution.getLatestReleaseFromClient(
+            TEST_FID_1, TEST_APP_ID_1, TEST_API_KEY, TEST_AUTH_TOKEN);
+    assertEquals(
+        AppDistributionRelease.builder()
+                .setBuildVersion(TEST_RELEASE_CURRENT.getBuildVersion())
+                .setDisplayVersion(TEST_RELEASE_CURRENT.getDisplayVersion())
+            .setBinaryType(BinaryType.AAB)
+            .build(),
+        result);
+  }
+
+  @Test
+  public void handleLatestReleaseFromClient_whenLatestReleaseIsSameAsInstalledAab_returnsNull()
+      throws FirebaseAppDistributionException, ProtocolException {
+    firebaseAppDistribution.onActivityResumed(activity);
+    when(mockFirebaseAppDistributionTesterApiClient.fetchLatestRelease(any(), any(), any(), any()))
+        .thenReturn(
+            AppDistributionReleaseInternal.builder()
+                .setBuildVersion(TEST_RELEASE_CURRENT.getBuildVersion())
+                .setDisplayVersion(TEST_RELEASE_CURRENT.getDisplayVersion())
+                .setCodeHash("codehash")
+                .setDownloadUrl("http://fake-download-url")
+                .setIasArtifactId(TEST_IAS_ARTIFACT_ID)
+                .setBinaryType(BinaryType.AAB)
+                .build());
+
+    AppDistributionRelease result =
+        firebaseAppDistribution.getLatestReleaseFromClient(
+            TEST_FID_1, TEST_APP_ID_1, TEST_API_KEY, TEST_AUTH_TOKEN);
+    assertNull(result);
   }
 }
