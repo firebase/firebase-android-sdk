d05fe4b683320d53181b55716ad151c3e8fe6403
Andrew Kreisher Bibiloni <bibiloni@google.com>
2021-07-20 12:02:28 -0400

    Implement updateToLatestRelease, add method to set exception for updateApp

--- firebase-app-distribution/src/main/java/com/google/firebase/appdistribution/Constants.java
+++ firebase-app-distribution/src/main/java/com/google/firebase/appdistribution/Constants.java
@@ -32,6 +32,8 @@ class Constants {
 
     public static final String TIMEOUT_ERROR = "Failed to fetch releases due to timeout";
 
+    public static final String UPDATE_CANCELLED = "Update Cancelled.";
+
     public static final String UNKNOWN_ERROR = "Unknown Error";
   }
 }
