bf3ea1eff0ebf63a967ac0d3e7b7411a531a48c0
Eldhose M Babu <eldhosembabu@google.com>
2021-07-22 12:11:35 -0700

    Experimental commit to migrate to Glide from Picasso (#2068)
    Migrate from Picasso to glide for image loading in FIAM
    
    For more context:
    https://docs.google.com/document/d/17jp01IqSZkuzwGST4g5crk-_PHl9LZJu4Dx65eqE8aA/edit#heading=h.ol58d2hrolgk

--- firebase-inappmessaging-display/src/main/java/com/google/firebase/inappmessaging/display/internal/FiamImageLoader.java
+++ firebase-inappmessaging-display/src/main/java/com/google/firebase/inappmessaging/display/internal/FiamImageLoader.java
@@ -14,55 +14,162 @@
 
 package com.google.firebase.inappmessaging.display.internal;
 
+import static com.bumptech.glide.load.DecodeFormat.PREFER_ARGB_8888;
+
+import android.graphics.drawable.Drawable;
+import android.text.TextUtils;
 import android.widget.ImageView;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.VisibleForTesting;
+import com.bumptech.glide.RequestBuilder;
+import com.bumptech.glide.RequestManager;
+import com.bumptech.glide.load.model.GlideUrl;
+import com.bumptech.glide.load.model.LazyHeaders;
+import com.bumptech.glide.request.target.CustomTarget;
+import com.bumptech.glide.request.transition.Transition;
 import com.google.firebase.inappmessaging.display.internal.injection.scopes.FirebaseAppScope;
-import com.squareup.picasso.Callback;
-import com.squareup.picasso.Picasso;
-import com.squareup.picasso.RequestCreator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 import javax.inject.Inject;
 
 /**
- * Image loader abstraction around the Picasso singleton to facilitate testing and injection
+ * Image loader abstraction around the Glide singleton to facilitate testing and injection
  *
  * @hide
  */
 @FirebaseAppScope
 public class FiamImageLoader {
-  private final Picasso picasso;
+
+  private final RequestManager requestManager;
+  private final Map<String, Set<CustomTarget>> tags = new HashMap<>();
 
   @Inject
-  FiamImageLoader(Picasso picasso) {
-    this.picasso = picasso;
+  public FiamImageLoader(RequestManager requestManager) {
+    this.requestManager = requestManager;
   }
 
   public FiamImageRequestCreator load(@Nullable String imageUrl) {
-    return new FiamImageRequestCreator(picasso.load(imageUrl));
+    Logging.logd("Starting Downloading Image : " + imageUrl);
+    GlideUrl glideUrl =
+        new GlideUrl(imageUrl, new LazyHeaders.Builder().addHeader("Accept", "image/*").build());
+    RequestBuilder<Drawable> requestBuilder =
+        requestManager.load(glideUrl).format(PREFER_ARGB_8888);
+    return new FiamImageRequestCreator(requestBuilder);
   }
 
   public void cancelTag(Class c) {
-    picasso.cancelTag(c);
+    String tag = c.getSimpleName();
+    synchronized (tag) {
+      if (tags.containsKey(tag)) {
+        Set<CustomTarget> targets = tags.get(tag);
+        for (CustomTarget target : targets) {
+          if (target != null) {
+            requestManager.clear(target);
+          }
+        }
+      }
+    }
   }
 
-  public static class FiamImageRequestCreator {
-    private final RequestCreator mRequestCreator;
+  @VisibleForTesting
+  boolean containsTag(String tag) {
+    return tags != null
+        && tags.containsKey(tag)
+        && tags.get(tag) != null
+        && tags.get(tag).size() > 0;
+  }
 
-    public FiamImageRequestCreator(RequestCreator requestCreator) {
-      mRequestCreator = requestCreator;
+  public class FiamImageRequestCreator {
+    private final RequestBuilder<Drawable> requestBuilder;
+    private Callback target;
+    private String tag;
+
+    public FiamImageRequestCreator(RequestBuilder<Drawable> requestBuilder) {
+      this.requestBuilder = requestBuilder;
     }
 
     public FiamImageRequestCreator placeholder(int placeholderResId) {
-      mRequestCreator.placeholder(placeholderResId);
+      requestBuilder.placeholder(placeholderResId);
+      Logging.logd("Downloading Image Placeholder : " + placeholderResId);
       return this;
     }
 
     public FiamImageRequestCreator tag(Class c) {
-      mRequestCreator.tag(c);
+      tag = c.getSimpleName();
+      checkAndTag();
       return this;
     }
 
+    private void checkAndTag() {
+      if (target == null || TextUtils.isEmpty(tag)) {
+        return;
+      }
+
+      synchronized (tags) {
+        Set<CustomTarget> set;
+        if (tags.containsKey(tag)) {
+          set = tags.get(tag);
+        } else {
+          set = new HashSet<>();
+          tags.put(tag, set);
+        }
+
+        if (!set.contains(target)) {
+          set.add(target);
+        }
+      }
+    }
+
     public void into(ImageView imageView, Callback callback) {
-      mRequestCreator.into(imageView, callback);
+      Logging.logd("Downloading Image Callback : " + callback);
+      callback.setImageView(imageView);
+      requestBuilder.into(callback);
+      target = callback;
+      checkAndTag();
+    }
+  }
+
+  public abstract static class Callback extends CustomTarget<Drawable> {
+
+    private ImageView imageView;
+
+    @Override
+    public void onLoadFailed(@Nullable Drawable errorDrawable) {
+      Logging.logd("Downloading Image Failed");
+      setImage(errorDrawable);
+      onError(new Exception("Image loading failed!"));
+    }
+
+    private void setImage(Drawable drawable) {
+      if (imageView != null) {
+        imageView.setImageDrawable(drawable);
+      }
+    }
+
+    @Override
+    public void onResourceReady(
+        @NonNull Drawable resource, @Nullable Transition<? super Drawable> transition) {
+      Logging.logd("Downloading Image Success!!!");
+      setImage(resource);
+      onSuccess();
+    }
+
+    @Override
+    public void onLoadCleared(@Nullable Drawable placeholder) {
+      Logging.logd("Downloading Image Cleared");
+      setImage(placeholder);
+      onSuccess();
+    }
+
+    public abstract void onSuccess();
+
+    public abstract void onError(Exception e);
+
+    void setImageView(ImageView imageView) {
+      this.imageView = imageView;
     }
   }
 }
