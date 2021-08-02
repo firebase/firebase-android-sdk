0990c509545d47515e35ff2bd48b4d4fd2ff3d22
andrewbibiloni <bibiloni@google.com>
2021-08-02 14:42:24 -0400

    Implement install activity

--- firebase-app-distribution/src/main/java/com/google/firebase/appdistribution/InstallActivity.java
+++ firebase-app-distribution/src/main/java/com/google/firebase/appdistribution/InstallActivity.java
@@ -1,1 +1,78 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
+package com.google.firebase.appdistribution;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+import com.google.android.gms.tasks.TaskCompletionSource;
+import java.io.File;
+
+public class InstallActivity extends AppCompatActivity {
+
+    private static TaskCompletionSource<Integer> taskCompletionSource;
+    private ActivityResultLauncher<Intent> mStartForResult;
+    private final String APK_MIME_TYPE = "application/vnd.android.package-archive";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.mStartForResult =
+                registerForActivityResult(
+                        new ActivityResultContracts.StartActivityForResult(),
+                        new ActivityResultCallback<ActivityResult>() {
+                            @Override
+                            public void onActivityResult(ActivityResult activityResult) {
+                                int resultCode = activityResult.getResultCode();
+                                Log.v("Result code", Integer.toString(resultCode));
+                                if (resultCode == Activity.RESULT_OK) {
+                                    taskCompletionSource.setResult(resultCode);
+                                } else {
+                                    taskCompletionSource.setException(
+                                            new FirebaseAppDistributionException(
+                                                    "Installation failed with result code: " + activityResult.getResultCode(),
+                                                    FirebaseAppDistributionException.Status.INSTALLATION_FAILURE));
+                                }
+                                finish();
+                            }
+                        });
+        Intent originalIntent = getIntent();
+        String path = originalIntent.getStringExtra("INSTALL_PATH");
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.putExtra(Intent.EXTRA_RETURN_RESULT, true);
+        Log.v("Path: install activity", path);
+        Uri apkUri =
+                FileProvider.getUriForFile(
+                        getApplicationContext(),
+                        getApplicationContext().getPackageName() + ".provider",
+                        new File(path));
+        intent.setDataAndType(apkUri, APK_MIME_TYPE);
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        this.mStartForResult.launch(intent);
+    }
+
+    public static void registerOnCompletionListener(TaskCompletionSource<Integer> taskCompletion) {
+        Log.v("Register", "Listener registered");
+        taskCompletionSource = taskCompletion;
+    }
+}
