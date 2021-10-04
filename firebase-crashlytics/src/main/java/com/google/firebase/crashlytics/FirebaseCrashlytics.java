ae3704f7a44a4ca456cbff76cd8c07a5096afec9
Tejas Deshpande <tdeshpande@google.com>
2021-10-04 22:37:04 +0530

    Update wording

--- firebase-crashlytics/src/main/java/com/google/firebase/crashlytics/FirebaseCrashlytics.java
+++ firebase-crashlytics/src/main/java/com/google/firebase/crashlytics/FirebaseCrashlytics.java
@@ -43,8 +43,8 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 
 /**
- * The Firebase Crashlytics API provides methods to annotate and manage fatal, non-fatal and ANR
- * reports captured and reported to Firebase Crashlytics.
+ * The Firebase Crashlytics API provides methods to annotate and manage fatal crashes, non-fatal
+ * errors, and ANRs captured and reported to Firebase Crashlytics.
  *
  * <p>By default, Firebase Crashlytics is automatically initialized.
  *
