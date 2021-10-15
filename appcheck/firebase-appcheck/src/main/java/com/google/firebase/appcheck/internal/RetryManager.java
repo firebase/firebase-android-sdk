83d516d2cead57216c8202419f44a33a6548e526
Rosalyn Tan <rosalyntan@google.com>
2021-10-14 17:47:56 -0700

    Hook RetryManager up to providers and fix copyright check.

--- appcheck/firebase-appcheck/src/main/java/com/google/firebase/appcheck/internal/RetryManager.java
+++ appcheck/firebase-appcheck/src/main/java/com/google/firebase/appcheck/internal/RetryManager.java
@@ -1,3 +1,17 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package com.google.firebase.appcheck.internal;
 
 import androidx.annotation.IntDef;
@@ -6,7 +20,7 @@ import com.google.firebase.appcheck.internal.util.Clock;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
-/** Class to manage when an App Check token exchange can be retried after a failure. */
+/** Class to manage when an App Check token exchange can be retried after a server error. */
 public class RetryManager {
 
   private static final long MAX_EXPONENTIAL_BACKOFF_MILLIS = 4 * 60 * 60 * 1000; // 4 hours
