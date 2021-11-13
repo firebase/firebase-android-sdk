44696085dc10ae8097d729c8edf1dc4054b61d00
Vladimir Kryachko <vkryachko@google.com>
2021-11-13 09:39:00 -0500

    Fix licenses and javadoc

--- firebase-common/src/main/java/com/google/firebase/monitoring/package-info.java
+++ firebase-common/src/main/java/com/google/firebase/monitoring/package-info.java
@@ -12,23 +12,5 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+/** @hide */
 package com.google.firebase.monitoring;
-
-import java.util.concurrent.atomic.AtomicReference;
-
-public class DelegatingTracer implements Tracer {
-  private final AtomicReference<Tracer> delegate = new AtomicReference<>();
-
-  public void setTracer(Tracer tracer) {
-    delegate.set(tracer);
-  }
-
-  @Override
-  public TraceHandle startTrace(String name) {
-    Tracer tracer = delegate.get();
-    if (tracer == null) {
-      return TraceHandle.NOOP;
-    }
-    return tracer.startTrace(name);
-  }
-}
