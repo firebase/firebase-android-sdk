6b5d9e0f16ac581b0a925f59c9b4a71dbc974c1c
Sebastian Schmidt <mrschmidt@google.com>
2021-11-23 15:42:11 -0700

    Update SQLiteSchema.java

--- firebase-firestore/src/main/java/com/google/firebase/firestore/local/SQLiteSchema.java
+++ firebase-firestore/src/main/java/com/google/firebase/firestore/local/SQLiteSchema.java
@@ -358,7 +358,7 @@ class SQLiteSchema {
    * <p>The `index_configuration` table holds the configuration for all indices. Entries in this
    * table apply for all users. It is not possible to only enable indices for a subset of users.
    *
-   * <p>The `index_state` table holds backfill information on each index to show when it was last
+   * <p>The `index_state` table holds backfill information on each index that stores when it was last
    * updated.
    *
    * <p>The `index_entries` table holds the index values themselves. An index value is created for
