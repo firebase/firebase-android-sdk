ef6f6e56be156693f4f3cb05fb685c0677710f4b
Leo Zhan <zhanl@google.com>
2021-12-01 11:02:09 -0800

    decor view hardware accel

--- firebase-perf/dev-app/src/main/java/com/googletest/firebase/perf/testapp/FragmentActivity.java
+++ firebase-perf/dev-app/src/main/java/com/googletest/firebase/perf/testapp/FragmentActivity.java
@@ -32,8 +32,9 @@ public class FragmentActivity extends AppCompatActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
+    // Listening on FragmentManager's FragmentLifeCycleCallbacks
     registerListeners();
+    super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_fragment);
     BottomNavigationView navView = findViewById(R.id.nav_view);
     // Passing each menu ID as a set of Ids because each
@@ -52,10 +53,29 @@ public class FragmentActivity extends AppCompatActivity {
     NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
     NavigationUI.setupWithNavController(
         (BottomNavigationView) findViewById(R.id.nav_view), navController);
-    // Listening on FragmentManager's FragmentLifeCycleCallbacks
   }
 
-  private boolean decorViewCheck() {
+  @Override
+  protected void onStart() {
+    super.onStart();
+    Log.d(
+        "FragmentActivity",
+        "onStart; View Hardware Accel: "
+            + decorViewAcceleratedFlag()
+            + ", FrameMetricsObservers won't be observing, only queued.");
+  }
+
+  @Override
+  public void onAttachedToWindow() {
+    super.onAttachedToWindow();
+    Log.d(
+        "FragmentActivity",
+        "onAttachedToWindow; View Hardware Accel: "
+            + decorViewAcceleratedFlag()
+            + ", FrameMetricsObservers in queue and any new observers will start observing.");
+  }
+
+  private boolean decorViewAcceleratedFlag() {
     View v = this.getWindow().getDecorView();
     return v.isHardwareAccelerated();
   }
@@ -65,25 +85,12 @@ public class FragmentActivity extends AppCompatActivity {
         .registerFragmentLifecycleCallbacks(
             new FragmentManager.FragmentLifecycleCallbacks() {
               @Override
-              public void onFragmentPreCreated(
-                  @NonNull FragmentManager fm,
-                  @NonNull Fragment f,
-                  @Nullable Bundle savedInstanceState) {
-                super.onFragmentPreCreated(fm, f, savedInstanceState);
-                Log.d(
-                    "FragmentManager",
-                    "Fragment precreated" + f.getClass().getSimpleName() + decorViewCheck());
-              }
-
-              @Override
               public void onFragmentCreated(
                   @NonNull FragmentManager fm,
                   @NonNull Fragment f,
                   @Nullable Bundle savedInstanceState) {
                 super.onFragmentCreated(fm, f, savedInstanceState);
-                Log.d(
-                    "FragmentManager",
-                    "Fragment created " + f.getClass().getSimpleName() + decorViewCheck());
+                Log.d("FragmentManager", "Fragment created " + f.getClass().getSimpleName());
               }
 
               @Override
@@ -93,40 +100,32 @@ public class FragmentActivity extends AppCompatActivity {
                   @NonNull View v,
                   @Nullable Bundle savedInstanceState) {
                 super.onFragmentViewCreated(fm, f, v, savedInstanceState);
-                Log.d(
-                    "FragmentManager",
-                    "View created " + f.getClass().getSimpleName() + decorViewCheck());
+                Log.d("FragmentManager", "View created " + f.getClass().getSimpleName());
               }
 
               @Override
               public void onFragmentStarted(@NonNull FragmentManager fm, @NonNull Fragment f) {
                 super.onFragmentStarted(fm, f);
-                Log.d(
-                    "FragmentManager",
-                    "View started " + f.getClass().getSimpleName() + decorViewCheck());
+                Log.d("FragmentManager", "Fragment started " + f.getClass().getSimpleName());
               }
 
               @Override
               public void onFragmentResumed(@NonNull FragmentManager fm, @NonNull Fragment f) {
                 super.onFragmentResumed(fm, f);
-                Log.d(
-                    "FragmentManager",
-                    "View resumed " + f.getClass().getSimpleName() + decorViewCheck());
+                Log.d("FragmentManager", "Fragment resumed " + f.getClass().getSimpleName());
               }
 
               @Override
               public void onFragmentStopped(@NonNull FragmentManager fm, @NonNull Fragment f) {
                 super.onFragmentStopped(fm, f);
-                Log.d("FragmentManager", "View stopped " + f.getClass().getSimpleName());
+                Log.d("FragmentManager", "Fragment stopped " + f.getClass().getSimpleName());
               }
 
               @Override
               public void onFragmentViewDestroyed(
                   @NonNull FragmentManager fm, @NonNull Fragment f) {
-                Log.d("FragmentManager", "View destroyed " + f.getClass().getSimpleName());
                 super.onFragmentViewDestroyed(fm, f);
-                //                              Log.d("FragmentManager", "View destroyed " +
-                // f.getClass().getSimpleName());
+                Log.d("FragmentManager", "View destroyed " + f.getClass().getSimpleName());
               }
             },
             true);
