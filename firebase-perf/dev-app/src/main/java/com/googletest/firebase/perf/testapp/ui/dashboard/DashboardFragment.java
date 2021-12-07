e87b4c75a317e43c5dcc0e5ad2dd08a38d074565
Leo Zhan <zhanl@google.com>
2021-12-07 15:41:57 -0800

    Fireperf: create an activity with fragments in `dev-app` for testing (#3133)
    * add a new activity with fragments
    
    * copyright
    
    * more listeners
    
    * decor view hardware accel
    
    * 2021 copyright
    
    * adding gifs
    
    * gJF
    
    * fix copyright
    
    * switch statement returns
    
    * pretty toolbar
    
    * add laggy recyclerview to one of the fragments
    
    * gJF

---
+++ firebase-perf/dev-app/src/main/java/com/googletest/firebase/perf/testapp/ui/dashboard/DashboardFragment.java
@@ -0,0 +1,74 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+//
+// You may obtain a copy of the License at
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googletest.firebase.perf.testapp.ui.dashboard;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.googletest.firebase.perf.testapp.R;
+import com.googletest.firebase.perf.testapp.SharedViewModel;
+
+public class DashboardFragment extends Fragment {
+
+  private DashboardViewModel dashboardViewModel;
+  private SharedViewModel sharedViewModel;
+
+  public View onCreateView(
+      @NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    dashboardViewModel =
+        new ViewModelProvider(this, new ViewModelProvider.NewInstanceFactory())
+            .get(DashboardViewModel.class);
+    View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
+    final TextView textView = root.findViewById(R.id.text_dashboard);
+    dashboardViewModel
+        .getText()
+        .observe(
+            getViewLifecycleOwner(),
+            new Observer<String>() {
+              @Override
+              public void onChanged(@Nullable String s) {
+                textView.setText(s);
+              }
+            });
+    // Gif loading for testing
+    sharedViewModel = new ViewModelProvider(requireActivity()).get(SharedViewModel.class);
+    final ImageView imageView = root.findViewById(R.id.img_dashboard);
+    sharedViewModel
+        .getImageSrc()
+        .observe(
+            getViewLifecycleOwner(),
+            new Observer<String>() {
+              @Override
+              public void onChanged(String s) {
+                Glide.with(requireActivity())
+                    .load(s)
+                    .diskCacheStrategy(DiskCacheStrategy.NONE)
+                    .skipMemoryCache(true)
+                    .into(imageView);
+              }
+            });
+    return root;
+  }
+}
