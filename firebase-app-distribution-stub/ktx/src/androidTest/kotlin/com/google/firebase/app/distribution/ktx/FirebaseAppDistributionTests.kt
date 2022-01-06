0ddd49f29efec3e8e04bf75fb378c299ab784935
Lee Kellogg <lkellogg@google.com>
2022-01-06 13:15:07 -0500

    Fix kotlin tests

--- firebase-app-distribution-stub/ktx/src/androidTest/kotlin/com/google/firebase/app/distribution/ktx/FirebaseAppDistributionTests.kt
+++ firebase-app-distribution-stub/ktx/src/androidTest/kotlin/com/google/firebase/app/distribution/ktx/FirebaseAppDistributionTests.kt
@@ -14,71 +14,17 @@
 
 package com.google.firebase.app.distribution.ktx
 
-import androidx.test.core.app.ApplicationProvider
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import com.google.common.truth.Truth
-import com.google.firebase.FirebaseApp
-import com.google.firebase.FirebaseOptions
 import com.google.firebase.appdistribution.FirebaseAppDistribution
 import com.google.firebase.ktx.Firebase
-import com.google.firebase.ktx.app
-import com.google.firebase.ktx.initialize
-import com.google.firebase.platforminfo.UserAgentPublisher
-import org.junit.After
-import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
-const val APP_ID = "APP_ID"
-const val API_KEY = "API_KEY"
-
-const val EXISTING_APP = "existing"
-
 @RunWith(AndroidJUnit4ClassRunner::class)
-abstract class BaseTestCase {
-    @Before
-    fun setUp() {
-        Firebase.initialize(
-                ApplicationProvider.getApplicationContext(),
-                FirebaseOptions.Builder()
-                        .setApplicationId(APP_ID)
-                        .setApiKey(API_KEY)
-                        .setProjectId("123")
-                        .build()
-        )
-
-        Firebase.initialize(
-                ApplicationProvider.getApplicationContext(),
-                FirebaseOptions.Builder()
-                        .setApplicationId(APP_ID)
-                        .setApiKey(API_KEY)
-                        .setProjectId("123")
-                        .build(),
-                EXISTING_APP
-        )
-    }
-
-    @After
-    fun cleanUp() {
-        FirebaseApp.clearInstancesForTest()
-    }
-}
-
-@RunWith(AndroidJUnit4ClassRunner::class)
-class FirebaseAppDistributionTests : BaseTestCase() {
+class FirebaseAppDistributionTests {
     @Test
-    fun appDistribution_default_callsDefaultGetInstance() {
-        Truth.assertThat(Firebase.appDistribution).isSameInstanceAs(FirebaseAppDistribution.getInstance())
-    }
-}
-
-internal const val LIBRARY_NAME: String = "fire-app-distribution-ktx"
-
-@RunWith(AndroidJUnit4ClassRunner::class)
-class LibraryVersionTest : BaseTestCase() {
-    @Test
-    fun libraryRegistrationAtRuntime() {
-        val publisher = Firebase.app.get(UserAgentPublisher::class.java)
-        Truth.assertThat(publisher.userAgent).contains(LIBRARY_NAME)
+    fun appDistribution_default_returnsInstanceOfFirebaseAppDistribution() {
+        Truth.assertThat(Firebase.appDistribution).isInstanceOf(FirebaseAppDistribution::class.java)
     }
 }
