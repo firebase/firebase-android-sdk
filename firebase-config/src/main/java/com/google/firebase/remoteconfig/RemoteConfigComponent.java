9a72cf56d78decc58535854150c229f45947b70f
qdpham <qdpham@google.com>
2022-02-08 00:51:13 +0000

    Replace HTTP/1.1 chunked streaming with HTTP/2 SSE in original Realtime HTTP Client. Also add headers for GFE.

--- firebase-config/src/main/java/com/google/firebase/remoteconfig/RemoteConfigComponent.java
+++ firebase-config/src/main/java/com/google/firebase/remoteconfig/RemoteConfigComponent.java
@@ -261,7 +261,7 @@ public class RemoteConfigComponent {
 
   @VisibleForTesting
   synchronized ConfigRealtimeHTTPClient getRealtimeClient(ConfigFetchHandler configFetchHandler) {
-    return new ConfigRealtimeHTTPClient(configFetchHandler);
+    return new ConfigRealtimeHTTPClient(this.firebaseApp, this.firebaseInstallations, configFetchHandler, this.context, this.executorService);
   }
 
   private ConfigGetParameterHandler getGetHandler(
