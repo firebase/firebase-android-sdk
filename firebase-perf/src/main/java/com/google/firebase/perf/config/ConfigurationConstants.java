611d8eb2774001e8181d2d1b84dc0f1b48b6a657
Viswanathan Munisamy <visumickey@gmail.com>
2022-03-17 11:59:33 -0700

    Update comments to reflect 100%.

--- firebase-perf/src/main/java/com/google/firebase/perf/config/ConfigurationConstants.java
+++ firebase-perf/src/main/java/com/google/firebase/perf/config/ConfigurationConstants.java
@@ -640,7 +640,7 @@ final class ConfigurationConstants {
 
     @Override
     protected Float getDefault() {
-      // Sampling rate range is [0.00f, 1.00f]. By default, sampling rate is 0.20f, which is 20%.
+      // Sampling rate range is [0.00f, 1.00f]. By default, sampling rate is 1.00f, which is 100%.
       // 0.00f means 0%, Fireperf will not capture any event for fragment trace from the device,
       // 1.00f means 100%, Fireperf will capture all events for fragment trace from the device.
       return 1.00f;
