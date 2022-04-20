93ef077f661c10877c79eb695c6d38ed98770dc2
Leo Zhan <zhanl@google.com>
2022-04-20 16:10:05 -0700

    Add tests for `snapshot()`

--- firebase-perf/src/main/java/com/google/firebase/perf/application/FrameMetricsRecorder.java
+++ firebase-perf/src/main/java/com/google/firebase/perf/application/FrameMetricsRecorder.java
@@ -1,56 +1,66 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package com.google.firebase.perf.application;
 
 import android.app.Activity;
 import android.util.SparseIntArray;
-
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.core.app.FrameMetricsAggregator;
-
 import com.google.android.gms.common.util.VisibleForTesting;
 import com.google.firebase.perf.logging.AndroidLogger;
-import com.google.firebase.perf.metrics.FrameMetricsCalculator;
 import com.google.firebase.perf.metrics.FrameMetricsCalculator.PerfFrameMetrics;
 import com.google.firebase.perf.util.Constants;
-
 import java.util.WeakHashMap;
 
 /**
  * Provides FrameMetrics data from an Activity's Window. Encapsulates FrameMetricsAggregator.
- * <p>
- * Note: each recorder holds a reference to an Activity, so it is very important to dereference
+ *
+ * <p>Note: each recorder holds a reference to an Activity, so it is very important to dereference
  * each recorder before the associated Activity is destroyed
- * </p>
- * */
+ */
 public class FrameMetricsRecorder {
   private static final AndroidLogger logger = AndroidLogger.getInstance();
   private final Activity activity;
   private final FrameMetricsAggregator fma;
   private final WeakHashMap<Object, PerfFrameMetrics> subTraceMap;
-  private boolean isRecording;
+  private boolean isRecording = false;
 
   /**
    * Creates a recorder for a specific activity
+   *
    * @param activity the activity that the recorder is collecting data from
    */
   public FrameMetricsRecorder(Activity activity) {
-    this(activity, new FrameMetricsAggregator(), new WeakHashMap<>(), false);
+    this(activity, new FrameMetricsAggregator(), new WeakHashMap<>());
   }
 
   @VisibleForTesting
-  FrameMetricsRecorder(Activity activity, FrameMetricsAggregator frameMetricsAggregator, WeakHashMap<Object, PerfFrameMetrics> subTraceMap, boolean isRecording) {
+  FrameMetricsRecorder(
+      Activity activity,
+      FrameMetricsAggregator frameMetricsAggregator,
+      WeakHashMap<Object, PerfFrameMetrics> subTraceMap) {
     this.activity = activity;
     this.fma = frameMetricsAggregator;
     this.subTraceMap = subTraceMap;
-    this.isRecording = isRecording;
   }
 
-  /**
-   * Starts recording FrameMetrics for the activity window
-   */
+  /** Starts recording FrameMetrics for the activity window */
   public void start() {
     if (isRecording) {
-      logger.error("FrameMetricsAggregator is already recording %s", activity.getClass().getSimpleName());
+      logger.error(
+          "FrameMetricsAggregator is already recording %s", activity.getClass().getSimpleName());
       return;
     }
     fma.add(activity);
@@ -59,6 +69,7 @@ public class FrameMetricsRecorder {
 
   /**
    * Stops recording FrameMetrics for the activity window
+   *
    * @return FrameMetrics accumulated during the current recording
    */
   public PerfFrameMetrics stop() {
@@ -66,7 +77,6 @@ public class FrameMetricsRecorder {
       logger.error("Cannot stop because no recording was started");
       return null;
     }
-    isRecording = false;
     PerfFrameMetrics data = this.snapshot();
     try {
       fma.remove(activity);
@@ -74,11 +84,13 @@ public class FrameMetricsRecorder {
       logger.error("View not hardware accelerated. Unable to collect FrameMetrics.");
     }
     fma.reset();
+    isRecording = false;
     return data;
   }
 
   /**
    * Starts a sub-trace in the current recording
+   *
    * @param key a UI state to associate this sub-trace with. e.g.) fragment
    */
   public void startSubTrace(Object key) {
@@ -87,7 +99,9 @@ public class FrameMetricsRecorder {
       return;
     }
     if (subTraceMap.containsKey(key)) {
-      logger.error("Cannot start sub-trace because one is already ongoing with the key %s", key.getClass().getSimpleName());
+      logger.error(
+          "Cannot start sub-trace because one is already ongoing with the key %s",
+          key.getClass().getSimpleName());
       return;
     }
     subTraceMap.put(key, this.snapshot());
@@ -95,6 +109,7 @@ public class FrameMetricsRecorder {
 
   /**
    * Stops the sub-trace associated with the given UI state
+   *
    * @param key the UI state associated with the sub-trace to be stopped
    * @return FrameMetrics accumulated during this sub-trace
    */
@@ -104,7 +119,9 @@ public class FrameMetricsRecorder {
       return null;
     }
     if (!subTraceMap.containsKey(key)) {
-      logger.error("Sub-trace associated with key %s was not started or does not exist", key.getClass().getSimpleName());
+      logger.error(
+          "Sub-trace associated with key %s was not started or does not exist",
+          key.getClass().getSimpleName());
       return null;
     }
     PerfFrameMetrics snapshotStart = subTraceMap.remove(key);
@@ -119,27 +136,33 @@ public class FrameMetricsRecorder {
    * @return the frame metrics
    */
   protected @NonNull PerfFrameMetrics snapshot() {
+    if (!isRecording) {
+      throw new IllegalStateException("No recording has been started.");
+    }
     SparseIntArray[] arr = this.fma.getMetrics();
+    if (arr == null) {
+      throw new IllegalStateException("FrameMetricsAggregator.mMetrics is uninitialized.");
+    }
+    SparseIntArray frameTimes = arr[FrameMetricsAggregator.TOTAL_INDEX];
+    if (frameTimes == null) {
+      throw new IllegalStateException(
+          "FrameMetricsAggregator.mMetrics[TOTAL_INDEX] is uninitialized.");
+    }
     int totalFrames = 0;
     int slowFrames = 0;
     int frozenFrames = 0;
 
-    if (arr != null) {
-      SparseIntArray frameTimes = arr[FrameMetricsAggregator.TOTAL_INDEX];
-      if (frameTimes != null) {
-        for (int i = 0; i < frameTimes.size(); i++) {
-          int frameTime = frameTimes.keyAt(i);
-          int numFrames = frameTimes.valueAt(i);
-          totalFrames += numFrames;
-          if (frameTime > Constants.FROZEN_FRAME_TIME) {
-            // Frozen frames mean the app appear frozen.  The recommended thresholds is 700ms
-            frozenFrames += numFrames;
-          }
-          if (frameTime > Constants.SLOW_FRAME_TIME) {
-            // Slow frames are anything above 16ms (i.e. 60 frames/second)
-            slowFrames += numFrames;
-          }
-        }
+    for (int i = 0; i < frameTimes.size(); i++) {
+      int frameTime = frameTimes.keyAt(i);
+      int numFrames = frameTimes.valueAt(i);
+      totalFrames += numFrames;
+      if (frameTime > Constants.FROZEN_FRAME_TIME) {
+        // Frozen frames mean the app appear frozen.  The recommended thresholds is 700ms
+        frozenFrames += numFrames;
+      }
+      if (frameTime > Constants.SLOW_FRAME_TIME) {
+        // Slow frames are anything above 16ms (i.e. 60 frames/second)
+        slowFrames += numFrames;
       }
     }
     // Only incrementMetric if corresponding metric is non-zero.
--- firebase-perf/src/test/java/com/google/firebase/perf/application/FrameMetricsRecorderTest.java
+++ firebase-perf/src/test/java/com/google/firebase/perf/application/FrameMetricsRecorderTest.java
@@ -1,8 +1,23 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package com.google.firebase.perf.application;
 
+import static com.google.common.truth.Truth.assertThat;
 import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.ArgumentMatchers.nullable;
-import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doCallRealMethod;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.inOrder;
 import static org.mockito.Mockito.mock;
@@ -12,13 +27,13 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 import android.app.Activity;
+import android.util.SparseIntArray;
 import android.view.WindowManager;
-
 import androidx.core.app.FrameMetricsAggregator;
-
 import com.google.firebase.perf.FirebasePerformanceTestBase;
 import com.google.firebase.perf.metrics.FrameMetricsCalculator.PerfFrameMetrics;
-
+import java.util.List;
+import java.util.WeakHashMap;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -31,9 +46,6 @@ import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.android.controller.ActivityController;
 
-import java.util.List;
-import java.util.WeakHashMap;
-
 /** Unit tests for {@link com.google.firebase.perf.application.FrameMetricsRecorder}. */
 @RunWith(RobolectricTestRunner.class)
 public class FrameMetricsRecorderTest extends FirebasePerformanceTestBase {
@@ -43,22 +55,19 @@ public class FrameMetricsRecorderTest extends FirebasePerformanceTestBase {
   @Mock private FrameMetricsAggregator fma;
   @Mock private PerfFrameMetrics frameMetrics1;
   @Mock private PerfFrameMetrics frameMetrics2;
-  @Mock private PerfFrameMetrics frameMetrics3;
 
-  @Spy
-  private final WeakHashMap<Object, PerfFrameMetrics> subTraceMap = new WeakHashMap<>();
+  @Spy private final WeakHashMap<Object, PerfFrameMetrics> subTraceMap = new WeakHashMap<>();
 
   @Before
   public void setUp() {
     initMocks(this);
     activity = createFakeActivity(true);
-    recorder = spy(new FrameMetricsRecorder(activity, fma, subTraceMap, false));
-    // stop() depends on snapshot(), so we stub snapshot() to test stop() independently
-    doReturn(null).when(recorder).snapshot();
+    recorder = spy(new FrameMetricsRecorder(activity, fma, subTraceMap));
   }
 
   @Test
   public void eachInstance_usesOneAndTheSameActivityInFrameMetricsAggregator_always() {
+    stubSnapshotToDoNothing();
     ArgumentCaptor<Activity> activityCaptor = ArgumentCaptor.forClass(Activity.class);
     recorder.start();
     recorder.stop();
@@ -86,6 +95,7 @@ public class FrameMetricsRecorderTest extends FirebasePerformanceTestBase {
 
   @Test
   public void start_afterPreviousEnded_succeeds() {
+    stubSnapshotToDoNothing();
     recorder.start();
     recorder.stop();
     recorder.start();
@@ -94,6 +104,7 @@ public class FrameMetricsRecorderTest extends FirebasePerformanceTestBase {
 
   @Test
   public void stop_callsFrameMetricsAggregator() {
+    stubSnapshotToDoNothing();
     recorder.start();
     recorder.stop();
     verify(fma, times(1)).remove(nullable(Activity.class));
@@ -102,6 +113,7 @@ public class FrameMetricsRecorderTest extends FirebasePerformanceTestBase {
 
   @Test
   public void stop_whileNotStarted_fails() {
+    stubSnapshotToDoNothing();
     recorder.stop();
     verify(fma, times(0)).remove(nullable(Activity.class));
     verify(fma, times(0)).reset();
@@ -114,18 +126,17 @@ public class FrameMetricsRecorderTest extends FirebasePerformanceTestBase {
 
   @Test
   public void stop_snapshotsFrameMetricsAggregator_beforeReset() {
-    FrameMetricsRecorder recorder = new FrameMetricsRecorder(activity, fma, subTraceMap, true);
-    FrameMetricsRecorder spyRecorder = spy(recorder);
-    // stop() depends on snapshot(), so we stub snapshot() to test stop() independently
-    doReturn(null).when(spyRecorder).snapshot();
-    spyRecorder.stop();
-    InOrder orderVerifier = inOrder(spyRecorder, fma);
-    orderVerifier.verify(spyRecorder).snapshot();
+    stubSnapshotToDoNothing();
+    recorder.start();
+    recorder.stop();
+    InOrder orderVerifier = inOrder(recorder, fma);
+    orderVerifier.verify(recorder).snapshot();
     orderVerifier.verify(fma).reset();
   }
 
   @Test
   public void startSubTrace_whenNotRecording_fails() {
+    stubSnapshotToDoNothing();
     Object uiState = new Object();
     recorder.startSubTrace(uiState);
     verify(subTraceMap, times(0)).put(nullable(Object.class), nullable(PerfFrameMetrics.class));
@@ -146,9 +157,9 @@ public class FrameMetricsRecorderTest extends FirebasePerformanceTestBase {
     recorder.start();
     recorder.startSubTrace(uiState1);
     verify(subTraceMap, times(1)).put(objectCaptor.capture(), nullable(PerfFrameMetrics.class));
-    Assert.assertSame(subTraceMap.get(uiState1), frameMetrics1);
-    Assert.assertSame(objectCaptor.getValue(), uiState1);
-    Assert.assertNotSame(objectCaptor.getValue(), uiState2);
+    Assert.assertSame(frameMetrics1, subTraceMap.get(uiState1));
+    Assert.assertSame(uiState1, objectCaptor.getValue());
+    Assert.assertNotSame(uiState2, objectCaptor.getValue());
 
     recorder.startSubTrace(uiState1);
     verify(subTraceMap, times(1)).put(nullable(Object.class), nullable(PerfFrameMetrics.class));
@@ -161,15 +172,16 @@ public class FrameMetricsRecorderTest extends FirebasePerformanceTestBase {
     Object uiState2 = new Object();
     recorder.start();
     recorder.startSubTrace(uiState1);
-    Assert.assertSame(subTraceMap.get(uiState1), frameMetrics1);
+    Assert.assertSame(frameMetrics1, subTraceMap.get(uiState1));
 
     doReturn(frameMetrics2).when(recorder).snapshot();
     recorder.startSubTrace(uiState2);
-    Assert.assertSame(subTraceMap.get(uiState2), frameMetrics2);
+    Assert.assertSame(frameMetrics2, subTraceMap.get(uiState2));
   }
 
   @Test
   public void stopSubTrace_whenNotRecording_fails() {
+    stubSnapshotToDoNothing();
     Object uiState = new Object();
     subTraceMap.put(uiState, frameMetrics1);
     Assert.assertTrue(subTraceMap.containsKey(uiState));
@@ -222,15 +234,165 @@ public class FrameMetricsRecorderTest extends FirebasePerformanceTestBase {
     Assert.assertSame(difference, result);
   }
 
+  @Test
+  public void snapshot_invokedAfterStartBeforeStop_doesNotThrow() {
+    FrameMetricsAggregator fma = new FrameMetricsAggregator();
+    FrameMetricsRecorder recorder = new FrameMetricsRecorder(activity, fma, subTraceMap);
+    recorder.start();
+    recorder.snapshot();
+  }
+
+  @Test(expected = IllegalStateException.class)
+  public void snapshot_invokedBeforeStart_throwsIllegalStateException() {
+    FrameMetricsAggregator fma = new FrameMetricsAggregator();
+    FrameMetricsRecorder recorder = new FrameMetricsRecorder(activity, fma, subTraceMap);
+    recorder.snapshot();
+  }
+
+  @Test(expected = IllegalStateException.class)
+  public void snapshot_invokedAfterStop_throwsIllegalStateException() {
+    FrameMetricsAggregator fma = new FrameMetricsAggregator();
+    FrameMetricsRecorder recorder = new FrameMetricsRecorder(activity, fma, subTraceMap);
+    try {
+      recorder.start();
+      recorder.stop();
+    } catch (Exception ignored) {
+    }
+    recorder.snapshot();
+  }
+
+  @Test(expected = IllegalStateException.class)
+  public void snapshot_sparseIntArrayIsNull_throwsIllegalStateException() {
+    doReturn(null).when(fma).getMetrics();
+    doCallRealMethod().when(recorder).snapshot();
+    try {
+      recorder.start();
+    } catch (Exception ignored) {
+    }
+    recorder.snapshot();
+  }
+
+  @Test(expected = IllegalStateException.class)
+  public void snapshot_sparseIntArrayTotalIndexIsNull_throwsIllegalStateException() {
+    SparseIntArray[] arr = new SparseIntArray[1];
+    arr[FrameMetricsAggregator.TOTAL_INDEX] = null;
+    doReturn(arr).when(fma).getMetrics();
+    doCallRealMethod().when(recorder).snapshot();
+    try {
+      recorder.start();
+    } catch (Exception ignored) {
+    }
+    recorder.snapshot();
+  }
+
+  @Test
+  public void snapshot_validSparseIntArray_returnsCorrectFrameMetrics() {
+    // Slow frames have duration greater than 16ms and frozen frames have duration greater than
+    // 700ms. The key value pair means (duration, num_of_samples).
+    SparseIntArray sparseIntArray = new SparseIntArray();
+    sparseIntArray.append(5, 3);
+    sparseIntArray.append(20, 2);
+    sparseIntArray.append(800, 5);
+    SparseIntArray[] arr = new SparseIntArray[1];
+    arr[FrameMetricsAggregator.TOTAL_INDEX] = sparseIntArray;
+
+    doReturn(arr).when(fma).getMetrics();
+    doCallRealMethod().when(recorder).snapshot();
+    recorder.start();
+    PerfFrameMetrics metrics = recorder.snapshot();
+
+    // we should expect 3+2+5=10 total frames, 2+5=7 slow frames, and 5 frozen frames.
+    assertThat(metrics.getTotalFrames()).isEqualTo(10);
+    assertThat(metrics.getSlowFrames()).isEqualTo(7);
+    assertThat(metrics.getFrozenFrames()).isEqualTo(5);
+  }
+
+  @Test
+  public void snapshot_validSparseIntArrayWithoutFrozenFrames_returnsCorrectFrameMetrics() {
+    // Slow frames have duration greater than 16ms and frozen frames have duration greater than
+    // 700ms. The key value pair means (duration, num_of_samples).
+    SparseIntArray sparseIntArray = new SparseIntArray();
+    sparseIntArray.append(5, 3);
+    sparseIntArray.append(20, 2);
+    SparseIntArray[] arr = new SparseIntArray[1];
+    arr[FrameMetricsAggregator.TOTAL_INDEX] = sparseIntArray;
+
+    doReturn(arr).when(fma).getMetrics();
+    doCallRealMethod().when(recorder).snapshot();
+    recorder.start();
+    PerfFrameMetrics metrics = recorder.snapshot();
+
+    // we should expect 3+2=5 total frames, 2 slow frames, and 0 frozen frames.
+    assertThat(metrics.getTotalFrames()).isEqualTo(5);
+    assertThat(metrics.getSlowFrames()).isEqualTo(2);
+    assertThat(metrics.getFrozenFrames()).isEqualTo(0);
+  }
+
+  @Test
+  public void snapshot_validSparseIntArrayWithoutSlowFrames_returnsCorrectFrameMetrics() {
+    // Slow frames have duration greater than 16ms and frozen frames have duration greater than
+    // 700ms. The key value pair means (duration, num_of_samples).
+    SparseIntArray sparseIntArray = new SparseIntArray();
+    sparseIntArray.append(5, 3);
+    sparseIntArray.append(701, 2);
+    SparseIntArray[] arr = new SparseIntArray[1];
+    arr[FrameMetricsAggregator.TOTAL_INDEX] = sparseIntArray;
+
+    doReturn(arr).when(fma).getMetrics();
+    doCallRealMethod().when(recorder).snapshot();
+    recorder.start();
+    PerfFrameMetrics metrics = recorder.snapshot();
+
+    // we should expect 3+2=5 total frames, 0+2=2 slow frames, and 2 frozen frames.
+    assertThat(metrics.getTotalFrames()).isEqualTo(5);
+    assertThat(metrics.getSlowFrames()).isEqualTo(2);
+    assertThat(metrics.getFrozenFrames()).isEqualTo(2);
+  }
+
+  @Test
+  public void
+      snapshot_validSparseIntArrayWithoutSlowFramesOrFrozenFrames_returnsCorrectFrameMetrics() {
+    // Slow frames have duration greater than 16ms and frozen frames have duration greater than
+    // 700ms. The key value pair means (duration, num_of_samples).
+    SparseIntArray sparseIntArray = new SparseIntArray();
+    sparseIntArray.append(5, 3);
+    SparseIntArray[] arr = new SparseIntArray[1];
+    arr[FrameMetricsAggregator.TOTAL_INDEX] = sparseIntArray;
+
+    doReturn(arr).when(fma).getMetrics();
+    doCallRealMethod().when(recorder).snapshot();
+    recorder.start();
+    PerfFrameMetrics metrics = recorder.snapshot();
+
+    // we should expect 3 total frames, 0 slow frames, and 0 frozen frames.
+    assertThat(metrics.getTotalFrames()).isEqualTo(3);
+    assertThat(metrics.getSlowFrames()).isEqualTo(0);
+    assertThat(metrics.getFrozenFrames()).isEqualTo(0);
+  }
+
   private static Activity createFakeActivity(boolean isHardwareAccelerated) {
     ActivityController<Activity> fakeActivityController = Robolectric.buildActivity(Activity.class);
 
     if (isHardwareAccelerated) {
-      fakeActivityController.get().getWindow().addFlags(WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED);
+      fakeActivityController
+          .get()
+          .getWindow()
+          .addFlags(WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED);
     } else {
-      fakeActivityController.get().getWindow().clearFlags(WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED);
+      fakeActivityController
+          .get()
+          .getWindow()
+          .clearFlags(WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED);
     }
 
     return fakeActivityController.start().get();
+  }
+
+  /**
+   * stop() and stopSubTrace() depends on snapshot(), so stub snapshot() to test them independently
+   */
+  private void stubSnapshotToDoNothing() {
+    PerfFrameMetrics genericFrameMetricsMock = mock(PerfFrameMetrics.class);
+    doReturn(genericFrameMetricsMock).when(recorder).snapshot();
   }
 }
