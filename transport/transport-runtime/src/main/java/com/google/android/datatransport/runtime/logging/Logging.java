bc18fdeb936cda696e83c9cddfe4d3d504c90782
argzdev <alvinrustan@google.com>
2022-05-16 17:29:03 +0800

    Shortened prefixes for LOG_TAG in classes

--- transport/transport-runtime/src/main/java/com/google/android/datatransport/runtime/logging/Logging.java
+++ transport/transport-runtime/src/main/java/com/google/android/datatransport/runtime/logging/Logging.java
@@ -22,7 +22,7 @@ public final class Logging {
 
   private static String getTag(String tag) {
     // isLoggable has a max limit of 23 chars for versions lower than 7.0
-    return android.os.Build.VERSION.SDK_INT > Build.VERSION_CODES.N ? "TRuntime." + tag : "";
+    return "TRuntime." + tag;
   }
 
   public static void d(String tag, String message) {
