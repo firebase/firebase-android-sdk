50da87e2990c8e82c4e116d69574bfd6b5248465
qdpham <qdpham@google.com>
2022-05-17 17:43:09 +0000

    Format file.

--- firebase-config/src/main/java/com/google/firebase/remoteconfig/internal/ConfigRealtimeHttpClient.java
+++ firebase-config/src/main/java/com/google/firebase/remoteconfig/internal/ConfigRealtimeHttpClient.java
@@ -9,56 +9,54 @@ import java.util.Map;
  * @author Quan Pham
  */
 public class ConfigRealtimeHttpClient {
-    
-    private final Map<Integer, ConfigUpdateListener> listeners;
-    private int listenerCount;
-    
-    public ConfigRealtimeHttpClient() {
-        listeners = new HashMap<>();
-        listenerCount = 1;
+
+  private final Map<Integer, ConfigUpdateListener> listeners;
+  private int listenerCount;
+
+  public ConfigRealtimeHttpClient() {
+    listeners = new HashMap<>();
+    listenerCount = 1;
+  }
+
+  // Kicks off Http stream listening and autofetch. Will implement in later PRs
+  private void beginRealtimeStream() {}
+
+  // Pauses Http stream listening
+  private void pauseRealtimeStream() {}
+
+  public ConfigUpdateListenerRegistration addRealtimeConfigUpdateListener(
+      ConfigUpdateListener configUpdateListener) {
+    listeners.put(listenerCount, configUpdateListener);
+    beginRealtimeStream();
+    return new ConfigUpdateListenerRegistration(this, listenerCount++);
+  }
+
+  public void removeRealtimeConfigUpdateListener(int listenerKey) {
+    listeners.remove(listenerKey);
+    if (listeners.isEmpty()) {
+      pauseRealtimeStream();
+    }
+  }
+
+  public static class ConfigUpdateListenerRegistration {
+    private final ConfigRealtimeHttpClient client;
+    private final int listenerKey;
+
+    public ConfigUpdateListenerRegistration(ConfigRealtimeHttpClient client, int listenerKey) {
+      this.client = client;
+      this.listenerKey = listenerKey;
     }
 
-    // Kicks off Http stream listening and autofetch. Will implement in later PRs
-    private void beginRealtimeStream() {
+    public void remove() {
+      client.removeRealtimeConfigUpdateListener(listenerKey);
     }
+  }
 
-    // Pauses Http stream listening
-    private void pauseRealtimeStream() {
-    }
-    
-    public ConfigUpdateListenerRegistration addRealtimeConfigUpdateListener(ConfigUpdateListener configUpdateListener) {
-        listeners.put(listenerCount, configUpdateListener);
-        beginRealtimeStream();
-        return new ConfigUpdateListenerRegistration(this, listenerCount++);
-    }
-    
-    public void removeRealtimeConfigUpdateListener(int listenerKey) {
-        listeners.remove(listenerKey);
-        if (listeners.isEmpty()) {
-            pauseRealtimeStream();
-        }
-    }
+  // Event Listener interface to be used by developers.
+  public interface ConfigUpdateListener {
+    // Call back for when Realtime fetches.
+    void onEvent();
 
-    public static class ConfigUpdateListenerRegistration {
-        private final ConfigRealtimeHttpClient client;
-        private final int listenerKey;
-
-        public ConfigUpdateListenerRegistration (
-                ConfigRealtimeHttpClient client, int listenerKey) {
-            this.client = client;
-            this.listenerKey = listenerKey;
-        }
-
-        public void remove() {
-            client.removeRealtimeConfigUpdateListener(listenerKey);
-        }
-    }
-
-    // Event Listener interface to be used by developers.
-    public interface ConfigUpdateListener {
-        // Call back for when Realtime fetches.
-        void onEvent();
-
-        void onError(Exception error);
-    }
+    void onError(Exception error);
+  }
 }
