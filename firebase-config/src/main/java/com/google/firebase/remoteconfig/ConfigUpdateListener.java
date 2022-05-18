b5c4a962936d2bd1ffd3dbf4be22af7a17e78041
qdpham <qdpham@google.com>
2022-05-18 18:33:21 +0000

    Add author

--- firebase-config/src/main/java/com/google/firebase/remoteconfig/ConfigUpdateListener.java
+++ firebase-config/src/main/java/com/google/firebase/remoteconfig/ConfigUpdateListener.java
@@ -16,13 +16,14 @@ package com.google.firebase.remoteconfig;
 
 import javax.annotation.Nonnull;
 
-/** Event Listener for Realtime config update callbacks. */
+/** Event Listener for Realtime config update callbacks.
+ *
+ * @author Quan Pham
+ */
 public interface ConfigUpdateListener {
   /**
    * Callback for when a new config has been automatically fetched from the backend. Can be used to
    * activate the new config.
-   *
-   * @author Quan Pham
    */
   void onEvent();
 
