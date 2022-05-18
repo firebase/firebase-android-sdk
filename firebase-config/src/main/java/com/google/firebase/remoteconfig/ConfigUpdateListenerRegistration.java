51bf2447da2c5c6adcab3b03263c9181c274fdec
qdpham <qdpham@google.com>
2022-05-18 18:27:53 +0000

    Change registration to interface.

--- firebase-config/src/main/java/com/google/firebase/remoteconfig/ConfigUpdateListenerRegistration.java
+++ firebase-config/src/main/java/com/google/firebase/remoteconfig/ConfigUpdateListenerRegistration.java
@@ -1,7 +1,18 @@
-package com.google.firebase.remoteconfig;
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+//
+// You may obtain a copy of the License at
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-import com.google.firebase.remoteconfig.internal.ConfigRealtimeHttpClient;
-import javax.annotation.Nonnull;
+package com.google.firebase.remoteconfig;
 
 /**
  * Represents a listener that can be removed by calling remove. This is returned when calling
@@ -10,21 +21,11 @@ import javax.annotation.Nonnull;
  *
  * @author Quan Pham
  */
-public class ConfigUpdateListenerRegistration {
-  private final ConfigRealtimeHttpClient client;
-  private final int listenerKey;
-
-  public ConfigUpdateListenerRegistration(
-      @Nonnull ConfigRealtimeHttpClient client, int listenerKey) {
-    this.client = client;
-    this.listenerKey = listenerKey;
-  }
+public interface ConfigUpdateListenerRegistration {
 
   /**
    * Removes the listener being tracked by this 'ConfigUpdateListenerRegistration`. After the
    * initial call, subsequent calls have no effect.
    */
-  public void remove() {
-    client.removeRealtimeConfigUpdateListener(listenerKey);
-  }
+  public void remove();
 }
--- firebase-config/src/main/java/com/google/firebase/remoteconfig/internal/ConfigRealtimeHttpClient.java
+++ firebase-config/src/main/java/com/google/firebase/remoteconfig/internal/ConfigRealtimeHttpClient.java
@@ -1,3 +1,17 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+//
+// You may obtain a copy of the License at
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package com.google.firebase.remoteconfig.internal;
 
 import com.google.firebase.remoteconfig.ConfigUpdateListener;
@@ -30,13 +44,29 @@ public class ConfigRealtimeHttpClient {
       ConfigUpdateListener configUpdateListener) {
     listeners.put(listenerCount, configUpdateListener);
     beginRealtimeStream();
-    return new ConfigUpdateListenerRegistration(this, listenerCount++);
+    return new ConfigUpdateListenerRegistrationInternal(this, listenerCount++);
   }
 
   public void removeRealtimeConfigUpdateListener(int listenerKey) {
     listeners.remove(listenerKey);
     if (listeners.isEmpty()) {
       pauseRealtimeStream();
+    }
+  }
+
+  public static class ConfigUpdateListenerRegistrationInternal
+      implements ConfigUpdateListenerRegistration {
+    private final ConfigRealtimeHttpClient realtimeHttpClient;
+    private final int listenerKey;
+
+    public ConfigUpdateListenerRegistrationInternal(
+        ConfigRealtimeHttpClient realtimeHttpClient, int listenerKey) {
+      this.realtimeHttpClient = realtimeHttpClient;
+      this.listenerKey = listenerKey;
+    }
+
+    public void remove() {
+      this.realtimeHttpClient.removeRealtimeConfigUpdateListener(this.listenerKey);
     }
   }
 }
