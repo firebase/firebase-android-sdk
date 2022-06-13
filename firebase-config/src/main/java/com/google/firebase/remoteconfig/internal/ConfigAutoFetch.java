bffa8edcad1d957b82bc50237e3131aec29b62ab
Quan Pham <qdpham@google.com>
2022-06-13 12:47:01 -0700

    Rc realtime dev (#3743)
    * Creates Realtime Http client file. Includes ConfigUpdate Listener & Registration. Also exposes methods to be used by public RC file.
    
    * Creates Realtime Http client file. Includes ConfigUpdate Listener & Registration. Also exposes methods to be used by public RC file.
    
    * Update stream name
    
    * Add copyright info to top of file.
    
    * Update comments
    
    * Format file.
    
    * Creates Realtime Http client file. Includes ConfigUpdate Listener & Registration. Also exposes methods to be used by public RC file.
    
    * Update stream name
    
    * Add copyright info to top of file.
    
    * Update comments
    
    * Format file.
    
    * Moved ConfigUpdateListener and ConfigUpdateListenerRegistration to public level.
    
    * Moved ConfigUpdateListener and ConfigUpdateListenerRegistration to public level.
    
    * Format and update comments
    
    * Uodate api.txt
    
    * Add nonnull and nullable to params
    
    * Change registration to interface.
    
    * Add author
    
    * Update api.txt
    
    * Dependabot High severity alerts (#3734)
    
    * Dependency updates for `smoke-tests`
    
    * Do not attempt to parse empty json files in MetaDataStore (#3735)
    
    * Do not attempt to parse empty json files in MetaDataStore
    
    * Safely delete corrupt files in MetaDataStore
    
    * Disable javadoc for undocumented sdks. (#3738)
    
    * Creates Realtime Http client file. Includes ConfigUpdate Listener & Registration. Also exposes methods to be used by public RC file.
    
    * Reupdate files.
    
    * Make changes based on PR comments;
    Make internal ConfigUpdateListenerRegistration non-static to access private listener removal method from outer class.
    Add synchonization to listeners set.
    
    * Remove synchronized set and add explicit synchonized to methods.
    
    * Add http connection logic
    
    * Add autofetch logic
    
    * Add http stream connection and async autofetching. Also add template version number to cache.
    
    * Add message decoding
    
    * add unit tests and Realtime based exceptions
    
    * Update api.txt
    
    * Fix check failures
    
    * Format files
    
    * Update changes based on PR comments
    
    * Add back template version number check
    
    * Alter template version number comment to reflect changes
    
    * Revert template version number extraction
    
    * Update template version number comment
    
    * Add thread safe for autofetch listeners
    
    * Add thread safe for autofetch listeners
    
    * add stream unit tests and thread safe for http connection object
    
    * finish autofetch testing
    
    * Post release version updates (#3752)
    
    * Ignore appdistro-api since it's still in beta. (#3751)
    
    * Upgrade Gradle to 6.9 (#3744)
    
    * Migrate from deprecated api usage in Coverage script
    
    * Fix bugs preventing 6.9 upgrade
    
    * oops, that shouldn't have been there still
    
    * Fixed old usage of Gradle BOM support (or lack-there-of)
    
    * Fixed some minor gradle consistencies, and added firehorn to gitignore
    
    * Added distribution versioning back
    
    * Explicitly depend on appdistribution-api
    
    Co-authored-by: Yifan Yang <yifany@google.com>
    
    * Upgrade kotlin version to 1.6.20 (#3762)
    
    * Upgrade kotlin version to 1.6.20
    
    * Upgrade dagger in protoc-gen-firebase-encoders.
    
    * Filter out only release component.
    
    * Add explanation.
    
    * Remove extra log
    
    Co-authored-by: Daymon <17409137+daymxn@users.noreply.github.com>
    Co-authored-by: Matthew Robertson <mrober@users.noreply.github.com>
    Co-authored-by: Vladimir Kryachko <vkryachko@google.com>
    Co-authored-by: Yifan Yang <yifany@google.com>

---
+++ firebase-config/src/main/java/com/google/firebase/remoteconfig/internal/ConfigAutoFetch.java
@@ -0,0 +1,182 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+//
+// You may obtain a copy of the License at
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.firebase.remoteconfig.internal;
+
+import static com.google.firebase.remoteconfig.FirebaseRemoteConfig.TAG;
+
+import android.util.Log;
+import androidx.annotation.GuardedBy;
+import androidx.annotation.VisibleForTesting;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
+import com.google.firebase.remoteconfig.ConfigUpdateListener;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigException;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigRealtimeUpdateFetchException;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigRealtimeUpdateStreamException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.util.Random;
+import java.util.Set;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class ConfigAutoFetch {
+
+  private static final int FETCH_RETRY = 3;
+
+  @GuardedBy("this")
+  private final Set<ConfigUpdateListener> eventListeners;
+
+  @GuardedBy("this")
+  private final HttpURLConnection httpURLConnection;
+
+  private final ConfigFetchHandler configFetchHandler;
+  private final ConfigUpdateListener retryCallback;
+  private final ScheduledExecutorService scheduledExecutorService;
+  private final Random random;
+  private final Executor executor;
+
+  public ConfigAutoFetch(
+      HttpURLConnection httpURLConnection,
+      ConfigFetchHandler configFetchHandler,
+      Set<ConfigUpdateListener> eventListeners,
+      ConfigUpdateListener retryCallback,
+      Executor executor,
+      ScheduledExecutorService scheduledExecutorService) {
+    this.httpURLConnection = httpURLConnection;
+    this.configFetchHandler = configFetchHandler;
+    this.eventListeners = eventListeners;
+    this.retryCallback = retryCallback;
+    this.scheduledExecutorService = scheduledExecutorService;
+    this.random = new Random();
+    this.executor = executor;
+  }
+
+  public void beginAutoFetch() {
+    executor.execute(
+        new Runnable() {
+          @Override
+          public void run() {
+            listenForNotifications();
+          }
+        });
+  }
+
+  private synchronized void propagateErrors(FirebaseRemoteConfigException exception) {
+    for (ConfigUpdateListener listener : eventListeners) {
+      listener.onError(exception);
+    }
+  }
+
+  private synchronized void executeAllListenerCallbacks() {
+    for (ConfigUpdateListener listener : eventListeners) {
+      listener.onEvent();
+    }
+  }
+
+  // Check connection and establish InputStream
+  @VisibleForTesting
+  public synchronized void listenForNotifications() {
+    if (httpURLConnection != null) {
+      try {
+        int responseCode = httpURLConnection.getResponseCode();
+        if (responseCode == 200) {
+          InputStream inputStream = httpURLConnection.getInputStream();
+          handleNotifications(inputStream);
+          inputStream.close();
+        } else {
+          propagateErrors(
+              new FirebaseRemoteConfigRealtimeUpdateStreamException(
+                  "Http connection responded with error: " + responseCode));
+        }
+      } catch (IOException ex) {
+        propagateErrors(
+            new FirebaseRemoteConfigRealtimeUpdateFetchException(
+                "Error handling stream messages while fetching.", ex.getCause()));
+      }
+    }
+    retryCallback.onEvent();
+  }
+
+  // Auto-fetch new config and execute callbacks on each new message
+  private void handleNotifications(InputStream inputStream) throws IOException {
+    BufferedReader reader = new BufferedReader((new InputStreamReader(inputStream, "utf-8")));
+    String message;
+    while ((message = reader.readLine()) != null) {
+      long targetTemplateVersion = configFetchHandler.getTemplateVersionNumber();
+      try {
+        JSONObject jsonObject = new JSONObject(message);
+        if (jsonObject.has("latestTemplateVersionNumber")) {
+          targetTemplateVersion = jsonObject.getLong("latestTemplateVersionNumber");
+        }
+      } catch (JSONException ex) {
+        Log.i(TAG, "Unable to parse latest config update message.");
+      }
+
+      autoFetch(FETCH_RETRY, targetTemplateVersion);
+    }
+    reader.close();
+  }
+
+  private void autoFetch(int remainingAttempts, long targetVersion) {
+    if (remainingAttempts == 0) {
+      propagateErrors(
+          new FirebaseRemoteConfigRealtimeUpdateFetchException("Unable to fetch latest version."));
+      return;
+    }
+
+    // Needs fetch to occur between 2 - 12 seconds. Randomize to not cause ddos alerts in backend
+    int timeTillFetch = random.nextInt(6) + 1;
+    scheduledExecutorService.schedule(
+        new Runnable() {
+          @Override
+          public void run() {
+            fetchLatestConfig(remainingAttempts, targetVersion);
+          }
+        },
+        timeTillFetch,
+        TimeUnit.SECONDS);
+  }
+
+  @VisibleForTesting
+  public synchronized void fetchLatestConfig(int remainingAttempts, long targetVersion) {
+    Task<ConfigFetchHandler.FetchResponse> fetchTask = configFetchHandler.fetch(0L);
+    fetchTask.onSuccessTask(
+        (fetchResponse) -> {
+          long newTemplateVersion = 0;
+          if (fetchResponse.getFetchedConfigs() != null) {
+            newTemplateVersion = fetchResponse.getFetchedConfigs().getTemplateVersionNumber();
+          }
+
+          if (newTemplateVersion >= targetVersion) {
+            executeAllListenerCallbacks();
+          } else {
+            Log.i(
+                TAG,
+                "Fetched template version is the same as SDK's current version."
+                    + " Retrying fetch.");
+            // Continue fetching until template version number if greater then current.
+            autoFetch(remainingAttempts - 1, targetVersion);
+          }
+          return Tasks.forResult(null);
+        });
+  }
+}
