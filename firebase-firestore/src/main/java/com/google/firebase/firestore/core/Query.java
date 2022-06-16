2bd73f32f993331e18542f61328361c8de16ac43
Ehsan Nasiri <ehsannas@gmail.com>
2022-06-16 15:56:07 -0500

    Attemt at a better comment.

--- firebase-firestore/src/main/java/com/google/firebase/firestore/core/Query.java
+++ firebase-firestore/src/main/java/com/google/firebase/firestore/core/Query.java
@@ -392,11 +392,12 @@ public final class Query {
 
   /** A document must have a value for every ordering clause in order to show up in the results. */
   private boolean matchesOrderBy(Document doc) {
-    // We must use `getOrderBy` here, not `explicitSortOrder`. For queries without disjunctions,
-    // the `matchesFilters` method guarantees the existence of the implicit orderBy field in the
-    // document. With the introduction of OR queries, it is possible that a document is missing the
-    // inequality field, but passes the `matchesFilters` check because it matches another
-    // disjunction term.
+    // We must use `getOrderBy()` to get the list of all orderBys (both implicit and explicit).
+    // Note that for OR queries, orderBy applies to all disjunction terms and implicit orderBys must
+    // be taken into account. For example, the query "a > 1 || b==1" has an implicit "orderBy a" due
+    // to the inequality, and is evaluated as "a > 1 orderBy a || b==1 orderBy a".
+    // A document with content of {b:1} matches the filters, but does not match the orderBy because
+    // it's missing the field 'a'.
     for (OrderBy order : getOrderBy()) {
       // order by key always matches
       if (!order.getField().equals(FieldPath.KEY_PATH) && (doc.getField(order.field) == null)) {
