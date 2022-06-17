0fe047754b39ecb11c257f9f6050180e8afe6d1a
Vladimir Kryachko <vkryachko@google.com>
2022-06-17 14:12:59 -0400

    Cleanup

--- buildSrc/src/main/java/com/google/firebase/gradle/plugins/ci/AffectedProjectFinder.java
+++ buildSrc/src/main/java/com/google/firebase/gradle/plugins/ci/AffectedProjectFinder.java
@@ -68,8 +68,10 @@ public class AffectedProjectFinder {
 
   private static Set<String> changedPaths(File workDir) {
     try {
+      // works on Prow only.
       Process process =
-          Runtime.getRuntime().exec("git diff --name-only --submodule=diff HEAD~1", null, workDir);
+              Runtime.getRuntime()
+                      .exec("git diff --name-only --submodule=diff HEAD@{0} HEAD@{1}", null, workDir);
       try {
         return ImmutableSet.copyOf(
             CharStreams.readLines(new InputStreamReader(process.getInputStream())));
