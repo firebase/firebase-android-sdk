fa2b3ac80ad5ac724286b0fd425d6d8d4933a4b4
ywmei-brt1 <ywmei@google.com>
2022-06-28 14:19:37 +0000

    add copyrights

--- firebase-firestore/ktx/src/main/kotlin/com/google/firebase/firestore/ktx/serialization/MapEncoderKtxImp.kt
+++ firebase-firestore/ktx/src/main/kotlin/com/google/firebase/firestore/ktx/serialization/MapEncoderKtxImp.kt
@@ -1,3 +1,17 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package com.google.firebase.firestore.ktx.serialization
 
 import com.google.firebase.components.Component
@@ -33,7 +47,7 @@ class MapEncoderKtxImp : MapEncoder {
      * Provides the concrete component that implements the [MapEncoder] interface to the component
      * registrar.
      */
-    open fun component(): Component<MapEncoderKtxImp?> {
+    fun component(): Component<MapEncoderKtxImp?> {
         return Component.builder(MapEncoderKtxImp::class.java, MapEncoder::class.java)
             .factory { MapEncoderKtxImp() }
             .build()
