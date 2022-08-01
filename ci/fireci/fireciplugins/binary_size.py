a2899f11b3675f7fdbad36ccd4630da6369aaff6
Yifan Yang <yifany@google.com>
2022-08-01 16:24:15 -0700

    test

--- ci/fireci/fireciplugins/binary_size.py
+++ ci/fireci/fireciplugins/binary_size.py
@@ -54,7 +54,8 @@ def binary_size(pull_request, log, metrics_service_url, access_token):
 
   gradle.run('assembleAllForSmokeTests')
 
-  subprocess.run(["tree", "-L", "10"])
+  subprocess.run(["tree", "-L", "10", "build"])
+  subprocess.run(["cat", "build/m2repository/changed-artifacts.json"])
 
   affected_artifacts, all_artifacts = _parse_artifacts()
   artifacts = affected_artifacts if pull_request else all_artifacts
@@ -101,5 +102,9 @@ def _measure_apk_sizes():
 
 def _parse_artifacts():
   with open('./build/m2repository/changed-artifacts.json') as json_file:
+    print("yifany: json_file")
+    print(json_file.read())
+    print("yifany: artifacts")
     artifacts = json.load(json_file)
+    print(artifacts)
     return artifacts['default'], artifacts['headGit']
