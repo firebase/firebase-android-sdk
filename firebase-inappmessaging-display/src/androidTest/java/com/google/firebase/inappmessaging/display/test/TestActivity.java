e4ceaf576d870f0a8adb9bd3b10c2569663afdac
Vladimir Kryachko <vkryachko@google.com>
2022-08-13 10:19:04 -0400

    Fix fiam tests, fiamd is still failing with missing resources.

--- firebase-inappmessaging-display/src/androidTest/java/com/google/firebase/inappmessaging/display/test/TestActivity.java
+++ firebase-inappmessaging-display/src/androidTest/java/com/google/firebase/inappmessaging/display/test/TestActivity.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.google.firebase.inappmessaging.display.internal.bindingwrappers;
+package com.google.firebase.inappmessaging.display.test;
 
 import android.app.Activity;
 
