be963cf27e1566a85daa846bc9425d6df32792a5
Vladimir Kryachko <vkryachko@google.com>
2022-08-18 09:38:51 -0400

    Prepare fiam for AGP upgrade.
    * Remove redundant duplicate symbols
    * Move TestActivity to proper java package
    * Cleanup manifests and build files

--- firebase-inappmessaging-display/src/androidTest/java/com/google/firebase/inappmessaging/display/test/TestActivity.java
+++ firebase-inappmessaging-display/src/androidTest/java/com/google/firebase/inappmessaging/display/test/TestActivity.java
@@ -11,8 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-package com.google.firebase.inappmessaging.display.internal.bindingwrappers;
+package com.google.firebase.inappmessaging.display.test;
 
 import android.app.Activity;
 
