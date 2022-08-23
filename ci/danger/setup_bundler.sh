9a8d459e8d707562f887b778099c6879ead3ab84
Rodrigo Lazo <rlazo@users.noreply.github.com>
2022-08-22 23:30:41 -0400

    Cleanup files.

--- ci/danger/setup_bundler.sh
+++ ci/danger/setup_bundler.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright 2020 Google LLC
+# Copyright 2022 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,12 +13,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-
-# To test another version of Xcode for all of CI:
-# - Add any edit, like a blank line, to Gemfile.
-# - Uncomment the following line and choose the alternative Xcode version.
-#sudo xcode-select -s /Applications/Xcode_13.0.app/Contents/Developer
 
 bundle update --bundler # ensure bundler version is high enough for Gemfile.lock
 bundle install
