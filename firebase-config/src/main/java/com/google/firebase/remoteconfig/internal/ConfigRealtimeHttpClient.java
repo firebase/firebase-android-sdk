238286e6c6c1173894740695da6bbc5d29adb74d
Dana Silver <danasilver@google.com>
2022-08-29 16:19:22 -0700

    Only reset retries when the realtime connection successfully opens. (#4037)
    Since URL#openConnection() only creates the connection object and doesn't open the connection, we were optimistically resetting the retries remaining before checking if the connection actually succeeded. (connect() or any downstream method like getResponseCode() must be called to open the connection.)
    
    This change:
    * updates beginRealtimeHttpStream() to call connect() before resetting the retries
    * and propagates the IOException thrown by openConnection() rather than checking if the connection object is null so we can collapse the two into one user-visible error about the realtime connection
    
    Comparable iOS change: firebase/firebase-ios-sdk#10143

--- firebase-config/src/main/java/com/google/firebase/remoteconfig/internal/ConfigRealtimeHttpClient.java
+++ firebase-config/src/main/java/com/google/firebase/remoteconfig/internal/ConfigRealtimeHttpClient.java
@@ -236,16 +236,11 @@ public class ConfigRealtimeHttpClient {
     return realtimeURL;
   }
 
-  private HttpURLConnection makeHttpConnection() {
+  private HttpURLConnection createRealtimeConnection() throws IOException {
     URL realtimeUrl = getUrl();
-    HttpURLConnection httpURLConnection = null;
-    try {
-      httpURLConnection = (HttpURLConnection) realtimeUrl.openConnection();
-      setCommonRequestHeaders(httpURLConnection);
-      setRequestParams(httpURLConnection);
-    } catch (IOException ex) {
-      Log.d(TAG, "Can't make connection, will retry after method returns.");
-    }
+    HttpURLConnection httpURLConnection = (HttpURLConnection) realtimeUrl.openConnection();
+    setCommonRequestHeaders(httpURLConnection);
+    setRequestParams(httpURLConnection);
 
     return httpURLConnection;
   }
@@ -298,19 +293,34 @@ public class ConfigRealtimeHttpClient {
     return new ConfigAutoFetch(httpURLConnection, configFetchHandler, listeners, retryCallback);
   }
 
-  // Kicks off Http stream listening and AutoFetching. The AutoFetch method listenForNotifications
-  // will be a blocking call until the Http stream closes, at which point we will try and
-  // reestablish the stream.
+  /**
+   * Open the realtime connection, begin listening for updates, and auto-fetch when an update is
+   * received.
+   *
+   * <p>If the connection is successful, this method will block on its thread while it reads the
+   * chunk-encoded HTTP body. When the connection closes, it attempts to reestablish the stream.
+   */
   @SuppressLint("VisibleForTests")
   synchronized void beginRealtimeHttpStream() {
-    if (canMakeHttpStreamConnection()) {
-      this.httpURLConnection = makeHttpConnection();
-      if (httpURLConnection != null) {
-        resetRetryParameters();
-        ConfigAutoFetch configAutoFetch = startAutoFetch(httpURLConnection);
-        configAutoFetch.listenForNotifications();
-        closeRealtimeHttpStream();
-      }
+    if (!canMakeHttpStreamConnection()) {
+      return;
+    }
+
+    try {
+      // Create the open the connection.
+      httpURLConnection = createRealtimeConnection();
+      httpURLConnection.connect();
+
+      // Reset the retries remaining if we opened the connection without an exception.
+      resetRetryParameters();
+
+      // Start listening for realtime notifications.
+      ConfigAutoFetch configAutoFetch = startAutoFetch(httpURLConnection);
+      configAutoFetch.listenForNotifications();
+    } catch (IOException e) {
+      Log.d(TAG, "Exception connecting to realtime stream. Retrying the connection...");
+    } finally {
+      closeRealtimeHttpStream();
       retryHTTPConnection();
     }
   }
@@ -319,6 +329,13 @@ public class ConfigRealtimeHttpClient {
   synchronized void closeRealtimeHttpStream() {
     if (httpURLConnection != null) {
       this.httpURLConnection.disconnect();
+
+      // Explicitly close the input stream due to a bug in the Android okhttp implementation.
+      // See github.com/firebase/firebase-android-sdk/pull/808.
+      try {
+        this.httpURLConnection.getInputStream().close();
+      } catch (IOException e) {
+      }
       this.httpURLConnection = null;
     }
   }
