d7799e069a8751944d7e1c22afbe960eeb96aa13
Leo Zhan <zhanl@google.com>
2022-08-29 14:57:30 -0700

    revert to store in microseconds due to too many tests being broken if not

--- firebase-perf/src/main/java/com/google/firebase/perf/util/Timer.java
+++ firebase-perf/src/main/java/com/google/firebase/perf/util/Timer.java
@@ -24,54 +24,72 @@ import com.google.android.gms.common.util.VisibleForTesting;
 
 /**
  * A Timer class provides both wall-clock (epoch) time and monotonic time (elapsedRealtime).
- * Milliseconds is the time unit used in private fields and computations, because that's the time
- * unit most widely available in Android APIs. Public methods only return time in microseconds,
- * because Fireperf proto requires microseconds.
+ * Timestamps are captured with millisecond-precision, because that's the time unit most widely
+ * available in Android APIs. However, private fields and public method returns are in microseconds
+ * due to Fireperf proto requirements.
  */
 public class Timer implements Parcelable {
 
   /**
-   * Wall-clock time or epoch time in microseconds. Do NOT use for duration because wall-clock is
+   * Wall-clock time or epoch time in milliseconds. Do NOT use for duration because wall-clock is
    * not guaranteed to be monotonic: it can be set by the user or the phone network, thus it may
    * jump forwards or backwards unpredictably. {@see SystemClock}
    */
-  private long wallClock;
+  private long wallClockMicros;
   /**
    * Monotonic time measured in the {@link SystemClock#elapsedRealtime()} timebase. Only used to
    * compute duration between 2 timestamps in the same timebase. It is NOT wall-clock time.
    */
-  private long elapsedRealtime;
+  private long elapsedRealtimeMicros;
 
   /**
    * Returns a new Timer object as if it was stamped at the given elapsedRealtime. Uses current
    * wall-clock as a reference to extrapolate the wall-clock at the given elapsedRealtime.
    *
-   * @param elapsedRealtime timestamp in the {@link SystemClock#elapsedRealtime()} timebase
+   * @param elapsedRealtime milliseconds timestamp in the {@link SystemClock#elapsedRealtime()}
+   *     timebase
    */
   public static Timer ofElapsedRealtime(long elapsedRealtime) {
-    long wallClockNow = System.currentTimeMillis();
-    long elapsedRealtimeNow = SystemClock.elapsedRealtime();
-    long wallClock = wallClockNow + elapsedRealtime - elapsedRealtimeNow;
+    elapsedRealtime = MILLISECONDS.toMicros(elapsedRealtime);
+    long wallClock = wallClockMicros() + elapsedRealtime - elapsedRealtimeMicros();
     return new Timer(wallClock, elapsedRealtime);
+  }
+
+  /**
+   * Helper to get current wall-clock time from system API.
+   *
+   * @return wall-clock time in microseconds.
+   */
+  private static long wallClockMicros() {
+    return MILLISECONDS.toMicros(System.currentTimeMillis());
+  }
+
+  /**
+   * Helper to get current {@link SystemClock#elapsedRealtime()} from system API.
+   *
+   * @return wall-clock time in microseconds.
+   */
+  private static long elapsedRealtimeMicros() {
+    return MILLISECONDS.toMicros(SystemClock.elapsedRealtime());
   }
 
   // TODO: make all constructors private, use public static factory methods, per Effective Java
   /** Construct Timer object using System clock. */
   public Timer() {
-    this(System.currentTimeMillis(), SystemClock.elapsedRealtime());
+    this(wallClockMicros(), elapsedRealtimeMicros());
   }
 
   /**
    * Construct a Timer object with input wall-clock time and elapsedRealtime.
    *
-   * @param epochTime wall-clock time in milliseconds since epoch
-   * @param elapsedRealtime monotonic time in milliseconds in the {@link
+   * @param epochMicros wall-clock time in milliseconds since epoch
+   * @param elapsedRealtimeMicros monotonic time in microseconds in the {@link
    *     SystemClock#elapsedRealtime()} timebase
    */
   @VisibleForTesting
-  Timer(long epochTime, long elapsedRealtime) {
-    this.wallClock = epochTime;
-    this.elapsedRealtime = elapsedRealtime;
+  Timer(long epochMicros, long elapsedRealtimeMicros) {
+    this.wallClockMicros = epochMicros;
+    this.elapsedRealtimeMicros = elapsedRealtimeMicros;
   }
 
   /**
@@ -89,21 +107,16 @@ public class Timer implements Parcelable {
     this(in.readLong(), in.readLong());
   }
 
-  /** Returns milliseconds duration from this until end. */
-  private long durationUntil(Timer end) {
-    return end.elapsedRealtime - this.elapsedRealtime;
-  }
-
   /** resets the start time */
   public void reset() {
     // TODO: consider removing this method and make Timer immutable thus fully thread-safe
-    wallClock = System.currentTimeMillis();
-    elapsedRealtime = SystemClock.elapsedRealtime();
+    wallClockMicros = wallClockMicros();
+    elapsedRealtimeMicros = elapsedRealtimeMicros();
   }
 
   /** Return wall-clock time in microseconds. */
   public long getMicros() {
-    return MILLISECONDS.toMicros(wallClock);
+    return wallClockMicros;
   }
 
   /**
@@ -114,7 +127,7 @@ public class Timer implements Parcelable {
    * @return duration in microseconds.
    */
   public long getDurationMicros() {
-    return MILLISECONDS.toMicros(durationUntil(new Timer()));
+    return getDurationMicros(new Timer());
   }
 
   /**
@@ -124,7 +137,7 @@ public class Timer implements Parcelable {
    * @return duration in microseconds.
    */
   public long getDurationMicros(@NonNull final Timer end) {
-    return MILLISECONDS.toMicros(durationUntil(end));
+    return end.elapsedRealtimeMicros - this.elapsedRealtimeMicros;
   }
 
   /**
@@ -136,7 +149,7 @@ public class Timer implements Parcelable {
    *     was created.
    */
   public long getCurrentTimestampMicros() {
-    return MILLISECONDS.toMicros(wallClock + durationUntil(new Timer()));
+    return wallClockMicros + getDurationMicros();
   }
 
   /**
@@ -146,7 +159,7 @@ public class Timer implements Parcelable {
    */
   @VisibleForTesting
   public long getElapsedRealtimeMicros() {
-    return MILLISECONDS.toMicros(elapsedRealtime);
+    return elapsedRealtimeMicros;
   }
 
   /**
@@ -157,8 +170,8 @@ public class Timer implements Parcelable {
    * @param flags always will be the value 0.
    */
   public void writeToParcel(Parcel out, int flags) {
-    out.writeLong(wallClock);
-    out.writeLong(elapsedRealtime);
+    out.writeLong(wallClockMicros);
+    out.writeLong(elapsedRealtimeMicros);
   }
 
   /**
