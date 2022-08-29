a50da9f8416ed35df3b1f80f05e5e91c440e6aa1
wu-hui <53845758+wu-hui@users.noreply.github.com>
2022-08-29 09:50:43 -0400

    Online Count with tentative package private api(#3847)

--- firebase-firestore/src/main/java/com/google/firebase/firestore/Query.java
+++ firebase-firestore/src/main/java/com/google/firebase/firestore/Query.java
@@ -1223,6 +1223,17 @@ public class Query {
     }
   }
 
+  /**
+   * Creates an {@link AggregateQuery} counting the number of documents matching this query.
+   *
+   * @return An {@link AggregateQuery} object that can be used to count the number of documents in
+   *     the result set of this query.
+   */
+  @NonNull
+  AggregateQuery count() {
+    return new AggregateQuery(this);
+  }
+
   @Override
   public boolean equals(Object o) {
     if (this == o) {
