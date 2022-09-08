7b1f632848020ae1ad50868a4cb62531584e13b7
Quan Pham <qdpham@google.com>
2022-09-08 15:19:44 -0700

    Realtime RC Store Template Version (#4051)
    * Look for template version in NO_CHANGE response and save template version to disc in metadata
    
    * Fix fetch test
    
    * Remove NO_CHANGE error changes
    
    * Use variable to represent template version to be more consistent with iOS.
    
    * Remove unwanted files
    
    * Add tests for metadata change
    
    * Update based on PR comments
    
    * Remove instance var and retrieve straight from disk

--- firebase-config/src/main/java/com/google/firebase/remoteconfig/internal/ConfigFetchHandler.java
+++ firebase-config/src/main/java/com/google/firebase/remoteconfig/internal/ConfigFetchHandler.java
@@ -319,6 +319,10 @@ public class ConfigFetchHandler {
               getFirstOpenTime(),
               currentTime);
 
+      if (response.getFetchedConfigs() != null) {
+        // Set template version in metadata to be saved on disk.
+        frcMetadata.setLastTemplateVersion(response.getFetchedConfigs().getTemplateVersionNumber());
+      }
       if (response.getLastFetchETag() != null) {
         frcMetadata.setLastFetchETag(response.getLastFetchETag());
       }
@@ -531,10 +535,7 @@ public class ConfigFetchHandler {
   }
 
   public long getTemplateVersionNumber() {
-    if (fetchedConfigsCache.get() != null && fetchedConfigsCache.get().getResult() != null) {
-      return fetchedConfigsCache.get().getResult().getTemplateVersionNumber();
-    }
-    return 1L;
+    return frcMetadata.getLastTemplateVersion();
   }
 
   /** Used to verify that the fetch handler is getting Analytics as expected. */
