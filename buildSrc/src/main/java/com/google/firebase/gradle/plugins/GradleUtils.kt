edc626248f8aa892e8520b96dc098a4ffb77eb10
Daymon <daymxn@google.com>
2022-09-13 14:43:08 -0500

    Add util method for copying directories

---
+++ buildSrc/src/main/java/com/google/firebase/gradle/plugins/GradleUtils.kt
@@ -0,0 +1,10 @@
+package com.google.firebase.gradle.plugins
+
+import java.io.File
+import org.gradle.api.provider.Provider
+import org.gradle.api.tasks.Copy
+
+fun Copy.fromDirectory(directory: Provider<File>) =
+    from(directory) {
+        into(directory.map { it.nameWithoutExtension })
+    }
