0c96b009e2be2c1099aee7737438ae193a4c917b
qdpham <qdpham@google.com>
2022-09-21 21:55:26 +0000

    Change responseCode var to Integer and initialize to null

--- firebase-config/src/main/java/com/google/firebase/remoteconfig/internal/ConfigRealtimeHttpClient.java
+++ firebase-config/src/main/java/com/google/firebase/remoteconfig/internal/ConfigRealtimeHttpClient.java
@@ -329,7 +329,7 @@ public class ConfigRealtimeHttpClient {
       return;
     }
 
-    int responseCode = 0;
+    Integer responseCode = null;
     try {
       // Create the open the connection.
       httpURLConnection = createRealtimeConnection();
@@ -349,8 +349,9 @@ public class ConfigRealtimeHttpClient {
     } finally {
       closeRealtimeHttpStream();
 
-      // If responseCode is 0 then no connection was made to server and the SDK should still retry.
-      if (responseCode == 0
+      // If responseCode is null then no connection was made to server and the SDK should still
+      // retry.
+      if (responseCode == null
           || responseCode == HttpURLConnection.HTTP_OK
           || isStatusCodeRetryable(responseCode)) {
         retryHTTPConnection();
