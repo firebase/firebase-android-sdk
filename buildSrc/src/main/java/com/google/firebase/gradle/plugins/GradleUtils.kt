2c93433f9549ac7c3a808c5a03b66ae70c8309cd
Daymon <daymxn@google.com>
2022-09-27 14:20:56 -0500

    Add util methods for gradle projects

--- buildSrc/src/main/java/com/google/firebase/gradle/plugins/GradleUtils.kt
+++ buildSrc/src/main/java/com/google/firebase/gradle/plugins/GradleUtils.kt
@@ -1,6 +1,7 @@
 package com.google.firebase.gradle.plugins
 
 import java.io.File
+import org.gradle.api.Project
 import org.gradle.api.provider.Provider
 import org.gradle.api.tasks.Copy
 
@@ -8,3 +9,9 @@ fun Copy.fromDirectory(directory: Provider<File>) =
     from(directory) {
         into(directory.map { it.name })
     }
+
+fun Project.fileFromBuildDir(path: String) = file("$buildDir/$path")
+
+fun Project.childFile(provider: Provider<File>, childPath: String) = provider.map {
+    file("${it.path}/$childPath")
+}
