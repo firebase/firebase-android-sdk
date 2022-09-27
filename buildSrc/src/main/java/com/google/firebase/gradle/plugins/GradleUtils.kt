c018f40dc7720af94d5bd5fc12dfa992edc37573
Daymon <daymxn@google.com>
2022-09-27 14:38:30 -0500

    Add documentation for util methods

--- buildSrc/src/main/java/com/google/firebase/gradle/plugins/GradleUtils.kt
+++ buildSrc/src/main/java/com/google/firebase/gradle/plugins/GradleUtils.kt
@@ -10,8 +10,26 @@ fun Copy.fromDirectory(directory: Provider<File>) =
         into(directory.map { it.name })
     }
 
+/**
+ * Creates a file at the buildDir for the given [Project].
+ *
+ * Syntax sugar for:
+ *
+ * ```
+ * project.file("${project.buildDir}/$path)
+ * ```
+ */
 fun Project.fileFromBuildDir(path: String) = file("$buildDir/$path")
 
+/**
+ * Maps a file provider to another file provider as a sub directory.
+ *
+ * Syntax sugar for:
+ *
+ * ```
+ * fileProvider.map { project.file("${it.path}/$path") }
+ * ```
+ */
 fun Project.childFile(provider: Provider<File>, childPath: String) = provider.map {
     file("${it.path}/$childPath")
 }
