3bf2c194a22e96a75c2625d36e2e19bcbebe2cbd
Daymon <17409137+daymxn@users.noreply.github.com>
2022-09-29 17:12:06 -0500

    Specify unique ref tags in Dackka output (#4149)
    * Add util methods for gradle projects
    
    * Disable Javadoc generation on empty projects
    
    * Fix ref path generation in Dackka output
    
    * Add documentation for util methods
    
    * Update the DackkaPlugin docs
    
    * Reduced path to relative from tenant
    
    * Reduced ref head path even more
    
    * Fixed ref tag path to working solution
    
    * Disabled publishJavadoc by default for tests
    
    It should be enabled explicitly when being tested anyhow, and causes issues otherwise. Instead of disabling it for the tests that don't need it- this is much quicker and easier to manager.

--- buildSrc/src/main/java/com/google/firebase/gradle/plugins/FiresiteTransformTask.kt
+++ buildSrc/src/main/java/com/google/firebase/gradle/plugins/FiresiteTransformTask.kt
@@ -4,6 +4,7 @@ import java.io.File
 import org.gradle.api.DefaultTask
 import org.gradle.api.provider.Property
 import org.gradle.api.tasks.CacheableTask
+import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.InputDirectory
 import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.PathSensitive
@@ -21,6 +22,7 @@ import org.gradle.api.tasks.TaskAction
  *  - Appends /docs/ to hyperlinks in html files
  *  - Removes the prefix path from book_path
  *  - Removes the firebase prefix from all links
+ *  - Changes the path for _reference-head-tags at the top of html files
  *
  *  **Please note:**
  *  This task is idempotent- meaning it can safely be ran multiple times on the same set of files.
@@ -30,6 +32,9 @@ abstract class FiresiteTransformTask : DefaultTask() {
     @get:InputDirectory
     @get:PathSensitive(PathSensitivity.RELATIVE)
     abstract val dackkaFiles: Property<File>
+
+    @get:Input
+    abstract val referenceHeadTagsPath: Property<String>
 
     @get:OutputDirectory
     abstract val outputDirectory: Property<File>
@@ -60,7 +65,7 @@ abstract class FiresiteTransformTask : DefaultTask() {
     }
 
     private fun File.fixHTMLFile() {
-        val fixedContent = readText().fixBookPath().fixHyperlinks().removeLeadingFirebaseDomainInLinks()
+        val fixedContent = readText().fixBookPath().fixHyperlinks().removeLeadingFirebaseDomainInLinks().fixReferenceHeadTagsPath()
         writeText(fixedContent)
     }
 
@@ -69,28 +74,33 @@ abstract class FiresiteTransformTask : DefaultTask() {
         writeText(fixedContent)
     }
 
+    // We utilize difference reference head tags between Kotlin and Java docs
+    // TODO(b/248316730): Remove when dackka exposes configuration for this
+    private fun String.fixReferenceHeadTagsPath() =
+        replace(Regex("(?<=include \").*(?=/_reference-head-tags.html\" %})"), referenceHeadTagsPath.get())
+
     // We don't actually upload class or index files,
     // so these headers will throw not found errors if not removed.
-    // TODO(b/243674302): Remove when dackka supports this behavior
+    // TODO(b/243674302): Remove when dackka exposes configuration for this
     private fun String.removeClassHeader() =
         remove(Regex("- title: \"Class Index\"\n {2}path: \".+\"\n\n"))
     private fun String.removeIndexHeader() =
         remove(Regex("- title: \"Package Index\"\n {2}path: \".+\"\n\n"))
 
     // We use a common book for all sdks, wheres dackka expects each sdk to have its own book.
-    // TODO(b/243674303): Remove when dackka supports this behavior
+    // TODO(b/243674303): Remove when dackka exposes configuration for this
     private fun String.fixBookPath() =
         remove(Regex("(?<=setvar book_path ?%})(.+)(?=/_book.yaml\\{% ?endsetvar)"))
 
     // Our documentation lives under /docs/reference/ versus the expected /reference/
-    // TODO(b/243674305): Remove when dackka supports this behavior
+    // TODO(b/243674305): Remove when dackka exposes configuration for this
     private fun String.fixHyperlinks() =
         replace(Regex("(?<=href=\")(/)(?=reference/.*\\.html)"), "/docs/")
 
     // The documentation will work fine without this. This is primarily to make sure that links
     // resolve to their local counter part. Meaning when the docs are staged, they will resolve to
     // staged docs instead of prod docs- and vise versa.
-    // TODO(b/243673063): Remove when dackka supports this behavior
+    // TODO(b/243673063): Remove when dackka exposes configuration for this
     private fun String.removeLeadingFirebaseDomainInLinks() =
         remove(Regex("(?<=\")(https://firebase\\.google\\.com)(?=/docs/reference)"))
 }
