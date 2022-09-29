6090e6d7f0c3da355b35fe5cb9380110712fcfe6
Daymon <daymxn@google.com>
2022-09-29 13:56:18 -0500

    Fixed ref tag path to working solution

--- buildSrc/src/main/java/com/google/firebase/gradle/plugins/DackkaPlugin.kt
+++ buildSrc/src/main/java/com/google/firebase/gradle/plugins/DackkaPlugin.kt
@@ -298,7 +298,7 @@ abstract class DackkaPlugin : Plugin<Project> {
         val transformJavadoc = project.tasks.register<FiresiteTransformTask>("firesiteTransformJavadoc") {
             dependsOnAndMustRunAfter("separateJavadoc")
 
-            referenceHeadTagsPath.set("/android")
+            referenceHeadTagsPath.set("docs/reference/android")
             dackkaFiles.set(project.childFile(separatedFilesDirectory, "android"))
             outputDirectory.set(project.childFile(targetDirectory, "android"))
         }
@@ -306,7 +306,7 @@ abstract class DackkaPlugin : Plugin<Project> {
         val transformKotlindoc = project.tasks.register<FiresiteTransformTask>("firesiteTransformKotlindoc") {
             dependsOnAndMustRunAfter("separateKotlindoc")
 
-            referenceHeadTagsPath.set("/kotlin")
+            referenceHeadTagsPath.set("docs/reference/kotlin")
             dackkaFiles.set(project.childFile(separatedFilesDirectory, "kotlin"))
             outputDirectory.set(project.childFile(targetDirectory, "kotlin"))
         }
