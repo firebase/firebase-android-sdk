039fd5bf790af6f74142737ed28ab7550f377954
utzcoz <utzcoz@outlook.com>
2022-10-01 16:46:53 +0800

    Bump Robolectric to 4.9
    1. Use legacy LooperMode for tests explicitly, because recent
       Robolectric releases switch to use PAUSED mode default. Before these
       tests migrate to PAUSED mode, they use LEGACY mode to pass tests.
    2. Migrate Assert.assertThat to Truth.assertThat to avoid using removed
       APIs.
    3. All build.gradle use the same robolectricVersion except
       transport-backend-cct because Robolectric 4.8+ has compatibility
       problem for TelephonyManager with low compile/targetSdkversion.
       To keep httpclient compatibility, transport-backend-ccts continues to
       use Robolectric 4.3.1.
    4. Remove unused exclude protobuf-java from Robolectric.
    5. Add necessary protobuf-lite dependency on classpath for
       some ktx modules' tests.
    
    Signed-off-by: utzcoz <utzcoz@outlook.com>

--- appcheck/firebase-appcheck/src/test/java/com/google/firebase/appcheck/internal/DefaultFirebaseAppCheckTest.java
+++ appcheck/firebase-appcheck/src/test/java/com/google/firebase/appcheck/internal/DefaultFirebaseAppCheckTest.java
@@ -41,10 +41,12 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
+import org.robolectric.annotation.LooperMode;
 
 /** Tests for {@link DefaultFirebaseAppCheck}. */
 @RunWith(RobolectricTestRunner.class)
 @Config(manifest = Config.NONE)
+@LooperMode(LooperMode.Mode.LEGACY)
 public class DefaultFirebaseAppCheckTest {
 
   private static final String EXCEPTION_TEXT = "exceptionText";
