cde08187f73a4776ece6a97903322554696f0290
Kai Bolay <kbolay@google.com>
2022-10-03 17:03:38 -0400

    Implement notification trigger (#4159)
    * Implement notification trigger.
    * address Lee's feedback
    * pick proper icon
    
    Co-authored-by: Lee Kellogg <lkellogg@google.com>

--- firebase-appdistribution/test-app/src/main/java/com/googletest/firebase/appdistribution/testapp/MainActivity.kt
+++ firebase-appdistribution/test-app/src/main/java/com/googletest/firebase/appdistribution/testapp/MainActivity.kt
@@ -26,11 +26,9 @@ import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
-
-    var firebaseAppDistribution = Firebase.appDistribution
-    var updateTask: Task<Void>? = null
-    var release: AppDistributionRelease? = null
+    val firebaseAppDistribution = Firebase.appDistribution
     val executorService: ExecutorService = Executors.newFixedThreadPool(1)
+
     lateinit var signInButton: AppCompatButton
     lateinit var signOutButton: AppCompatButton
     lateinit var checkForUpdateButton: AppCompatButton
@@ -46,6 +44,9 @@ class MainActivity : AppCompatActivity() {
     lateinit var progressPercent: TextView
     lateinit var progressBar: ProgressBar
     lateinit var feedbackTriggerMenu: TextInputLayout
+
+    var updateTask: Task<Void>? = null
+    var release: AppDistributionRelease? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -71,14 +72,15 @@ class MainActivity : AppCompatActivity() {
         val items = listOf(
             FeedbackTrigger.NONE.label,
             FeedbackTrigger.SHAKE.label,
-            FeedbackTrigger.SCREENSHOT.label
+            FeedbackTrigger.SCREENSHOT.label,
+            FeedbackTrigger.NOTIFICATION.label,
         )
         val adapter = ArrayAdapter(this, R.layout.list_item, items)
         val autoCompleteTextView = feedbackTriggerMenu.editText!! as AutoCompleteTextView
         autoCompleteTextView.setAdapter(adapter)
         // TODO: set it to the actual currently enabled trigger
         autoCompleteTextView.setText(FeedbackTrigger.NONE.label, false)
-        autoCompleteTextView.doOnTextChanged { text, start, before, count ->
+        autoCompleteTextView.doOnTextChanged { text, _, _, _ ->
             when(text.toString()) {
                 FeedbackTrigger.NONE.label -> {
                     disableAllFeedbackTriggers()
@@ -93,6 +95,11 @@ class MainActivity : AppCompatActivity() {
                     Log.i(TAG, "Enabling screenshot detection trigger")
                     ScreenshotDetectionFeedbackTrigger.enable()
                 }
+                FeedbackTrigger.NOTIFICATION.label -> {
+                    disableAllFeedbackTriggers()
+                    Log.i(TAG, "Enabling notification trigger")
+                    NotificationFeedbackTrigger.enable(this)
+                }
             }
         }
     }
@@ -101,6 +108,7 @@ class MainActivity : AppCompatActivity() {
         Log.i(TAG, "Disabling all feedback triggers")
         ShakeForFeedback.disable(application)
         ScreenshotDetectionFeedbackTrigger.disable()
+        NotificationFeedbackTrigger.disable();
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
