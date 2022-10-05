a926becbe7b6ec3c48988d02f917b8c3e749e184
rosariopf <rosariopf@google.com>
2022-10-05 17:15:27 +0100

    create TestService to demonstrate superclass error

---
+++ firebase-messaging/ktx/src/main/kotlin/com/google/firebase/messaging/ktx/TestService.kt
@@ -0,0 +1,8 @@
+package com.google.firebase.messaging.ktx
+
+import com.google.firebase.messaging.FirebaseMessagingService
+
+class TestService : FirebaseMessagingService() {
+
+    // This class is meant to demonstrate the superclass error that is thrown
+}
