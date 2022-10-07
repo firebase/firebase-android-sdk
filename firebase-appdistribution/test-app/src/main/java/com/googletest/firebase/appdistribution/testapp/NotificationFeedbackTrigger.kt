505983455d222a60a7fc3219159f39a9994765ba
Kai Bolay <kbolay@google.com>
2022-10-07 09:35:46 -0400

    Notifications: Ask permission upfront, show only one persistent notification (#4179)
    * Simplify requesting permissions: ask for notification permission upfront. 
    * Keep one notification instead of cancelling and showing a new one as Activities pause/resume.
    * KDoc and misc. cleanup.
    * Rename `currentActivity` to `activityToScreenshot`.

--- firebase-appdistribution/test-app/src/main/java/com/googletest/firebase/appdistribution/testapp/NotificationFeedbackTrigger.kt
+++ firebase-appdistribution/test-app/src/main/java/com/googletest/firebase/appdistribution/testapp/NotificationFeedbackTrigger.kt
@@ -13,33 +13,34 @@ import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.result.ActivityResultCaller
-import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.appcompat.app.AppCompatActivity
+import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import androidx.core.content.ContextCompat
 import com.google.firebase.appdistribution.ktx.appDistribution
 import com.google.firebase.ktx.Firebase
-import com.googletest.firebase.appdistribution.testapp.NotificationFeedbackTrigger.SCREENSHOT_FILE_NAME
-import com.googletest.firebase.appdistribution.testapp.NotificationFeedbackTrigger.takeScreenshot
 import java.io.IOException
-import java.util.*
 
 @SuppressLint("StaticFieldLeak") // Reference to Activity is set to null in onActivityDestroyed
 object NotificationFeedbackTrigger : Application.ActivityLifecycleCallbacks {
   private const val TAG: String = "NotificationFeedbackTrigger"
   private const val FEEBACK_NOTIFICATION_CHANNEL_ID = "InAppFeedbackNotification"
   private const val FEEDBACK_NOTIFICATION_ID = 1
-  const val SCREENSHOT_FILE_NAME = "com.googletest.firebase.appdistribution.testapp.screenshot.png"
 
   private var isEnabled = false
   private var hasRequestedPermission = false
-  private var currentActivity: Activity? = null
-  // TODO(lkellogg): this is getting too complex - simplify it by, for example, only enabling this
-  //   trigger on a per-activity basis instead of app-wide
-  private var requestPermissionLaunchers: WeakHashMap<Activity, ActivityResultLauncher<String>?> = WeakHashMap()
 
+  internal var activityToScreenshot: Activity? = null
+
+  /**
+   * Initialize the notification trigger for this application.
+   *
+   * This should be called during [Application.onCreate].
+   * [enable] should then be called when you want to actually show the notification.
+   *
+   * @param application the [Application] object
+   */
   fun initialize(application: Application) {
     // Create the NotificationChannel, but only on API 26+ because
     // the NotificationChannel class is new and not in the support library
@@ -48,7 +49,7 @@ object NotificationFeedbackTrigger : Application.ActivityLifecycleCallbacks {
         NotificationChannel(
           FEEBACK_NOTIFICATION_CHANNEL_ID,
           application.getString(R.string.feedback_notification_channel_name),
-          NotificationManager.IMPORTANCE_LOW
+          NotificationManager.IMPORTANCE_HIGH
         )
       channel.description =
         application.getString(R.string.feedback_notification_channel_description)
@@ -59,80 +60,103 @@ object NotificationFeedbackTrigger : Application.ActivityLifecycleCallbacks {
     application.registerActivityLifecycleCallbacks(this)
   }
 
-  fun enable(currentActivity: Activity? = null) {
-    this.currentActivity = currentActivity
-    isEnabled = true
-    if (currentActivity != null) {
-      showNotification(currentActivity)
+  /**
+   * Requests permission to show notifications for this application.
+   *
+   * This must be called during [Activity.onCreate].
+   * [enable] should then be called when you want to actually show the notification.
+   *
+   * @param activity the [Activity] object
+   */
+  @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+  fun <T> requestPermission(activity: T) where T : Activity, T : ActivityResultCaller {
+    if (ContextCompat.checkSelfPermission(activity, POST_NOTIFICATIONS) == PERMISSION_GRANTED) {
+      Log.i(TAG, "Already has permission.")
+      return
     }
+
+    if (hasRequestedPermission) {
+      Log.i(TAG, "Already request permission; Not trying again.")
+      return
+    }
+
+    val launcher = activity.registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+      isGranted: Boolean ->
+      if (!isEnabled) {
+        Log.w(TAG, "Trigger disabled after permission check. Abandoning notification.")
+      } else if (isGranted) {
+        showNotification(activity)
+      } else {
+        Log.i(TAG, "Permission not granted")
+        // TODO: Ideally we would show a message indicating the impact of not
+        //   enabling the permission, but there's no way to know if they've
+        //   permanently denied the permission, and we don't want to show them a
+        //   message after each time we try to post a notification.
+      }
+    }
+
+    if (activity.shouldShowRequestPermissionRationale(POST_NOTIFICATIONS)) {
+      Log.i(TAG, "Showing customer rationale for requesting permission.")
+      AlertDialog.Builder(activity)
+        .setMessage(
+          "Using a notification to initiate feedback to the developer. " +
+                  "To enable this feature, allow the app to post notifications."
+        )
+        .setPositiveButton("OK") { _, _ ->
+          Log.i(TAG, "Launching request for permission.")
+          launcher.launch(POST_NOTIFICATIONS)
+        }
+        .show()
+    } else {
+      Log.i(TAG, "Launching request for permission without rationale.")
+      launcher.launch(POST_NOTIFICATIONS)
+    }
+    hasRequestedPermission = true
   }
 
+  /**
+   * Show notifications.
+   *
+   * This could be called during [Activity.onCreate].
+   *
+   * @param activity the [Activity] object
+   */
+  fun enable(activity: Activity) {
+    activityToScreenshot = activity
+    isEnabled = true
+    showNotification(activity)
+  }
+
+  /** Hide notifications. */
   fun disable() {
-    isEnabled = false
-    val activity = currentActivity
-    currentActivity = null
+    val activity = activityToScreenshot
     if (activity != null) {
       cancelNotification(activity)
     }
+    isEnabled = false
+    activityToScreenshot = null
   }
 
-  private fun showNotification(activity: Activity) {
-    if (ContextCompat.checkSelfPermission(activity, POST_NOTIFICATIONS) == PERMISSION_GRANTED) {
-      val intent = Intent(activity, TakeScreenshotAndTriggerFeedbackActivity::class.java)
-      intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
-      val pendingIntent =
-        PendingIntent.getActivity(
-          activity,
-          /* requestCode= */ 0,
-          intent,
-          PendingIntent.FLAG_IMMUTABLE
-        )
-      val builder =
-        NotificationCompat.Builder(activity, FEEBACK_NOTIFICATION_CHANNEL_ID)
-          .setSmallIcon(R.mipmap.ic_launcher)
-          .setContentTitle(activity.getText(R.string.feedback_notification_title))
-          .setContentText(activity.getText(R.string.feedback_notification_text))
-          .setPriority(NotificationCompat.PRIORITY_LOW)
-          .setContentIntent(pendingIntent)
-      val notificationManager = NotificationManagerCompat.from(activity)
-      Log.i(TAG, "Showing notification")
-      notificationManager.notify(FEEDBACK_NOTIFICATION_ID, builder.build())
-    } else {
-      // no permission to post notifications - try to get it
-      if (hasRequestedPermission) {
-        Log.i(
-          TAG,
-          "We've already request permission. Not requesting again for the life of the activity."
-        )
-      } else {
-        requestPermission(activity)
-      }
-    }
-  }
-
-  private fun requestPermission(activity: Activity) {
-    var launcher = requestPermissionLaunchers[activity]
-    if (launcher == null) {
-      Log.i(TAG, "Not requesting permission, because of inability to register for result.")
-    } else {
-      if (activity.shouldShowRequestPermissionRationale(POST_NOTIFICATIONS)) {
-        Log.i(TAG, "Showing customer rationale for requesting permission.")
-        AlertDialog.Builder(activity)
-          .setMessage(
-            "Using a notification to initiate feedback to the developer. " +
-              "To enable this feature, allow the app to post notifications."
-          )
-          .setPositiveButton("OK") { _, _ ->
-            Log.i(TAG, "Launching request for permission.")
-            launcher.launch(POST_NOTIFICATIONS)
-          }
-          .show()
-      } else {
-        Log.i(TAG, "Launching request for permission without rationale.")
-        launcher.launch(POST_NOTIFICATIONS)
-      }
-      hasRequestedPermission = true
-    }
+  private fun showNotification(context: Context) {
+    val intent = Intent(context, TakeScreenshotAndTriggerFeedbackActivity::class.java)
+    intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
+    val pendingIntent =
+      PendingIntent.getActivity(
+        context,
+        /* requestCode = */ 0,
+        intent,
+        PendingIntent.FLAG_IMMUTABLE
+      )
+    val builder =
+      NotificationCompat.Builder(context, FEEBACK_NOTIFICATION_CHANNEL_ID)
+        .setSmallIcon(R.mipmap.ic_launcher)
+        .setContentTitle(context.getText(R.string.feedback_notification_title))
+        .setContentText(context.getText(R.string.feedback_notification_text))
+        .setPriority(NotificationCompat.PRIORITY_HIGH)
+        .setContentIntent(pendingIntent)
+    val notificationManager = NotificationManagerCompat.from(context)
+    Log.i(TAG, "Showing notification")
+    notificationManager.notify(FEEDBACK_NOTIFICATION_ID, builder.build())
   }
 
   private fun cancelNotification(context: Context) {
@@ -145,85 +169,61 @@ object NotificationFeedbackTrigger : Application.ActivityLifecycleCallbacks {
     if (isEnabled) {
       if (activity !is TakeScreenshotAndTriggerFeedbackActivity) {
         Log.d(TAG, "setting current activity")
-        currentActivity = activity
-        showNotification(activity)
+        activityToScreenshot = activity
       }
     }
   }
 
-  override fun onActivityPaused(activity: Activity) {
-    cancelNotification(activity)
-  }
-
   override fun onActivityDestroyed(activity: Activity) {
-    if (activity == currentActivity) {
+    if (activity == activityToScreenshot) {
       Log.d(TAG, "clearing current activity")
-      currentActivity = null
-    }
-    requestPermissionLaunchers[activity] = null
-  }
-
-  override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
-    if (activity is ActivityResultCaller && !hasRequestedPermission) {
-      requestPermissionLaunchers[activity] =
-        activity.registerForActivityResult(ActivityResultContracts.RequestPermission()) {
-          isGranted: Boolean ->
-          if (!isEnabled) {
-            Log.w(TAG, "Trigger disabled after permission check. Abandoning notification.")
-          } else if (isGranted) {
-            showNotification(activity)
-          } else {
-            Log.i(TAG, "Permission not granted")
-            // TODO: Ideally we would show a message indicating the impact of not
-            //   enabling the permission, but there's no way to know if they've
-            //   permanently denied the permission, and we don't want to show them a
-            //   message after each time we try to post a notification.
-          }
-        }
-    } else {
-      Log.w(
-        TAG,
-        "Not showing notification because this activity can't register for permission request results: $activity"
-      )
+      activityToScreenshot = null
     }
   }
 
   // Other lifecycle methods
+  override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {}
   override fun onActivityStarted(activity: Activity) {}
+  override fun onActivityPaused(activity: Activity) {}
   override fun onActivityStopped(activity: Activity) {}
   override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {}
-
-  fun takeScreenshot() {
-    val activity = currentActivity
-    if (activity != null) {
-      val view = activity.window.decorView.rootView
-      val bitmap = Bitmap.createBitmap(view.width, view.height, Bitmap.Config.RGB_565)
-      val canvas = Canvas(bitmap)
-      view.draw(canvas)
-      try {
-        activity.openFileOutput(SCREENSHOT_FILE_NAME, Context.MODE_PRIVATE).use { outputStream ->
-          bitmap.compress(Bitmap.CompressFormat.PNG, /* quality = */ 100, outputStream)
-        }
-        Log.i(TAG, "Wrote screenshot to $SCREENSHOT_FILE_NAME")
-      } catch (e: IOException) {
-        Log.e(TAG, "Can't write $SCREENSHOT_FILE_NAME", e)
-      }
-    } else {
-      Log.e(TAG, "Can't take screenshot because current activity is unknown")
-      return
-    }
-  }
 }
 
-class TakeScreenshotAndTriggerFeedbackActivity : AppCompatActivity() {
+class TakeScreenshotAndTriggerFeedbackActivity : Activity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    takeScreenshot()
+    val activity = NotificationFeedbackTrigger.activityToScreenshot
+    if (activity == null) {
+      Log.e(TAG, "Can't take screenshot because activity is unknown")
+      return
+    }
+    takeScreenshot(activity)
   }
 
   override fun onResume() {
     super.onResume()
     val screenshotUri = Uri.fromFile(getFileStreamPath(SCREENSHOT_FILE_NAME))
     Firebase.appDistribution.startFeedback(R.string.terms_and_conditions, screenshotUri)
+  }
+
+  fun takeScreenshot(activity: Activity) {
+    val view = activity.window.decorView.rootView
+    val bitmap = Bitmap.createBitmap(view.width, view.height, Bitmap.Config.RGB_565)
+    val canvas = Canvas(bitmap)
+    view.draw(canvas)
+    try {
+      activity.openFileOutput(SCREENSHOT_FILE_NAME, Context.MODE_PRIVATE).use { outputStream ->
+        bitmap.compress(Bitmap.CompressFormat.PNG, /* quality = */ 100, outputStream)
+      }
+      Log.i(TAG, "Wrote screenshot to ${SCREENSHOT_FILE_NAME}")
+    } catch (e: IOException) {
+      Log.e(TAG, "Can't write ${SCREENSHOT_FILE_NAME}", e)
+    }
+  }
+
+  companion object {
+    private const val TAG: String = "TakeScreenshotAndTriggerFeedbackActivity"
+    private const val SCREENSHOT_FILE_NAME =
+      "com.googletest.firebase.appdistribution.testapp.screenshot.png"
   }
 }
