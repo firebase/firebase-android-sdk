deb0a70b63f5408d17ff75f49c9ffae7b2b1832e
Lee Kellogg <lfkellogg@users.noreply.github.com>
2022-10-31 11:33:25 -0400

    Simplify custom notification trigger example and move to activity (#4255)
    * Simplify custom notification trigger example and move to activity
    
    * Remove synchronized blocks

--- firebase-appdistribution/test-app/src/main/java/com/googletest/firebase/appdistribution/testapp/CustomNotificationActivity.kt
+++ firebase-appdistribution/test-app/src/main/java/com/googletest/firebase/appdistribution/testapp/CustomNotificationActivity.kt
@@ -1,71 +1,67 @@
 package com.googletest.firebase.appdistribution.testapp
 
+import android.Manifest.permission.POST_NOTIFICATIONS
 import android.app.AlertDialog
 import android.content.pm.PackageManager.PERMISSION_DENIED
 import android.os.Bundle
-import android.os.Handler
-import android.os.HandlerThread
 import android.util.Log
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-import com.googletest.firebase.appdistribution.testapp.ScreenshotDetectionFeedbackTrigger.Companion.screenshotReadPermission
 import java.util.*
 
-class ScreenshotDetectionActivity : AppCompatActivity() {
-  private val handlerThread = HandlerThread("AppDistroFeedbackTrigger").also { it.start() }
-  private var screenshotDetectionFeedbackTrigger =
-    ScreenshotDetectionFeedbackTrigger(
-      this,
-      R.string.feedbackInfoText,
-      Handler(handlerThread.looper)
-    )
+class CustomNotificationActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_screenshot_detection)
+    setContentView(R.layout.activity_custom_notification)
 
     val launcher =
       registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
-        if (!isGranted) {
+        if (isGranted) {
+          Log.i(TAG, "Permission granted, showing notification")
+          CustomNotificationFeedbackTrigger.showNotification(this)
+        } else {
           Log.i(TAG, "Permission not granted")
           AlertDialog.Builder(this)
             .setMessage(
-              "Because storage permission has been denied, screenshots taken will not trigger feedback to the developer."
+              "Because the notification permission has been denied, the app will not show a " +
+                "notification that can be tapped to send feedback to the developer."
             )
             .setPositiveButton("OK") { _, _ -> }
             .show()
         }
       }
 
-    if (ContextCompat.checkSelfPermission(this, screenshotReadPermission) == PERMISSION_DENIED) {
-      if (shouldShowRequestPermissionRationale(screenshotReadPermission)) {
+    if (ContextCompat.checkSelfPermission(this, POST_NOTIFICATIONS) == PERMISSION_DENIED) {
+      if (shouldShowRequestPermissionRationale(POST_NOTIFICATIONS)) {
         Log.i(TAG, "Showing customer rationale for requesting permission.")
         AlertDialog.Builder(this)
           .setMessage(
-            "Taking a screenshot of the app can initiate feedback to the developer. To enable this feature, allow the app access to device storage."
+            "Using a notification to initiate feedback to the developer. " +
+              "To enable this feature, allow the app to post notifications."
           )
           .setPositiveButton("OK") { _, _ ->
             Log.i(TAG, "Launching request for permission.")
-            launcher.launch(screenshotReadPermission)
+            launcher.launch(POST_NOTIFICATIONS)
           }
           .setNegativeButton("No thanks") { _, _ -> Log.i(TAG, "User denied permission request.") }
           .show()
       } else {
         Log.i(TAG, "Launching request for permission without rationale.")
-        launcher.launch(screenshotReadPermission)
+        launcher.launch(POST_NOTIFICATIONS)
       }
+    } else {
+      CustomNotificationFeedbackTrigger.showNotification(this)
     }
-
-    screenshotDetectionFeedbackTrigger.listenForScreenshots()
   }
 
   override fun onDestroy() {
-    screenshotDetectionFeedbackTrigger.stopListeningForScreenshots()
+    CustomNotificationFeedbackTrigger.cancelNotification()
     super.onDestroy()
   }
 
   companion object {
-    private const val TAG = "ScreenshotDetectionActivity"
+    private const val TAG = "CustomNotificationActivity"
   }
 }
