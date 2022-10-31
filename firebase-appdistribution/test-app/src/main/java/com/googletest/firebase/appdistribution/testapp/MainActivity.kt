9d9f9216f2c6c1c616bd9ef123c3aadf52d75d47
Lee Kellogg <lfkellogg@users.noreply.github.com>
2022-10-31 11:33:25 -0400

    Simplify custom notification trigger example and move to activity (#4255)
    * Simplify custom notification trigger example and move to activity
    
    * Remove synchronized blocks

--- firebase-appdistribution/test-app/src/main/java/com/googletest/firebase/appdistribution/testapp/MainActivity.kt
+++ firebase-appdistribution/test-app/src/main/java/com/googletest/firebase/appdistribution/testapp/MainActivity.kt
@@ -69,10 +69,6 @@ class MainActivity : AppCompatActivity() {
         signInStatus = findViewById(R.id.sign_in_status)
         progressBar = findViewById(R.id.progress_bar)
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            CustomNotificationFeedbackTrigger.requestPermission(this)
-        }
-
         // Set up feedback trigger menu
         feedbackTriggerMenu = findViewById(R.id.feedbackTriggerMenu)
         val items = listOf(
@@ -100,8 +96,7 @@ class MainActivity : AppCompatActivity() {
                 }
                 FeedbackTrigger.CUSTOM_NOTIFICATION.label -> {
                     disableAllFeedbackTriggers()
-                    Log.i(TAG, "Enabling notification trigger (custom)")
-                    CustomNotificationFeedbackTrigger.enable(this)
+                    startActivity(Intent(this, CustomNotificationActivity::class.java))
                 }
                 FeedbackTrigger.SHAKE.label -> {
                     disableAllFeedbackTriggers()
@@ -111,9 +106,6 @@ class MainActivity : AppCompatActivity() {
                 FeedbackTrigger.SCREENSHOT.label -> {
                     disableAllFeedbackTriggers()
                     startActivity(Intent(this, ScreenshotDetectionActivity::class.java))
-                    // Set the selection back to None since once we're back to this activity the
-                    // trigger will be disabled
-                    autoCompleteTextView.setText(FeedbackTrigger.NONE.label, false)
                 }
             }
         }
@@ -127,7 +119,6 @@ class MainActivity : AppCompatActivity() {
     private fun disableAllFeedbackTriggers() {
         Log.i(TAG, "Disabling all feedback triggers")
         firebaseAppDistribution.cancelFeedbackNotification()
-        CustomNotificationFeedbackTrigger.disable()
         ShakeDetectionFeedbackTrigger.disable(application)
     }
 
