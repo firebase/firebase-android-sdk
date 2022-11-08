f2c3c1f94c88f45f5ee75fe0375c994bae700e28
Vladimir Kryachko <vkryachko@google.com>
2022-11-08 12:18:03 -0500

    gJF

--- firebase-annotations/src/main/java/com/google/firebase/annotations/concurrent/UiThread.java
+++ firebase-annotations/src/main/java/com/google/firebase/annotations/concurrent/UiThread.java
@@ -16,7 +16,6 @@ package com.google.firebase.annotations.concurrent;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
-
 import javax.inject.Qualifier;
 
 /** An executor/coroutine dispatcher for work that must run on the UI thread. */
