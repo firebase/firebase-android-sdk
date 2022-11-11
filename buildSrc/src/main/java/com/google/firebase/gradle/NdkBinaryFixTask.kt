3aac867a8696f8dee4329aa3152845a3a4bf3b92
Vladimir Kryachko <vkryachko@google.com>
2022-11-11 13:42:01 -0500

    Fix ndk build. (#4311)
    * Fix ndk build.
    
    Due to AGP upgrade the crashlytics-trampoline.so file stopped being
    included in the resulting AAR, this causes issues on devices with api
    29+.
    
    * fix filename

---
+++ buildSrc/src/main/java/com/google/firebase/gradle/NdkBinaryFixTask.kt
@@ -0,0 +1,48 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package com.google.firebase.gradle
+
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.StandardCopyOption
+import org.gradle.api.DefaultTask
+import org.gradle.api.file.RegularFileProperty
+import org.gradle.api.tasks.InputFile
+import org.gradle.api.tasks.Internal
+import org.gradle.api.tasks.OutputFile
+import org.gradle.api.tasks.TaskAction
+
+abstract class NdkBinaryFixTask : DefaultTask() {
+    @get:InputFile
+    abstract val inputFile: RegularFileProperty
+
+    @get:OutputFile
+    val outputFile: File
+        get() = inputFile.get().asFile.let {
+            File(it.parentFile, "lib${it.name}.so")
+        }
+
+    @get:Internal
+    val into: String
+        get() = "jni/${outputFile.parentFile.name}"
+
+    @TaskAction
+    fun run() {
+        Files.copy(
+            inputFile.get().asFile.toPath(),
+            outputFile.toPath(),
+            StandardCopyOption.REPLACE_EXISTING
+        )
+    }
+}
