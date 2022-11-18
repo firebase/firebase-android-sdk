2314d740538152fbca4a9b5f9103f4c0fd08fea7
Lee Kellogg <lkellogg@google.com>
2022-11-18 17:34:24 -0500

    Resolve some changes from the rebase on fad/next

--- firebase-appdistribution/test-app/src/main/java/com/googletest/firebase/appdistribution/testapp/MainActivity.kt
+++ firebase-appdistribution/test-app/src/main/java/com/googletest/firebase/appdistribution/testapp/MainActivity.kt
@@ -43,6 +43,7 @@ import java.util.concurrent.Executors
 class MainActivity : AppCompatActivity() {
 
     var firebaseAppDistribution = Firebase.appDistribution
+    var updateTask: Task<Void>? = null
     val executorService: ExecutorService = Executors.newFixedThreadPool(1)
 
     lateinit var signInButton: AppCompatButton
@@ -60,8 +61,6 @@ class MainActivity : AppCompatActivity() {
     lateinit var progressPercent: TextView
     lateinit var progressBar: ProgressBar
     lateinit var feedbackTriggerMenu: TextInputLayout
-
-    var updateTask: Task<Void>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
