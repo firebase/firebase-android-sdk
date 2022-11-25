33feb6dfc6b299fb12cd2e40f197c62a1b3f1882
Rodrigo Lazo <rlazo@users.noreply.github.com>
2022-11-25 00:53:53 -0500

    Fix typo

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -106,7 +106,7 @@ tasks.withType<Test> {
     val enablePluginTests: String? by rootProject
     enabled = enablePluginTests == "true"
 }
--
+
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
     kotlinOptions {
         jvmTarget = "11"
