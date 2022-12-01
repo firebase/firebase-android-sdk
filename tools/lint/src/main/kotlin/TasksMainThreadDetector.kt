4bbaae4ad56e533e08febc6d1a10e6bbb9b4b0ca
Vladimir Kryachko <vkryachko@google.com>
2022-12-01 12:05:29 -0500

    Fix copyrights

--- tools/lint/src/main/kotlin/TasksMainThreadDetector.kt
+++ tools/lint/src/main/kotlin/TasksMainThreadDetector.kt
@@ -1,3 +1,17 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package com.google.firebase.lint.checks
 
 import com.android.tools.lint.detector.api.Category
@@ -61,8 +75,9 @@ class TasksMainThreadDetector : Detector(), SourceCodeScanner {
         briefDescription = "Use an explicit Executor for Task continuations",
         explanation =
           """
-                    Not providing an executor results in continuations to execute on the main thread \
-                    which is not a good default. Please pass in an executor explicitly.
+                    Not providing an executor results in continuations being executed on the main \
+                    thread, which in most cases is not intended. Please pass in an executor \
+                    explicitly.
                 """,
         category = Category.PERFORMANCE,
         priority = 6,
