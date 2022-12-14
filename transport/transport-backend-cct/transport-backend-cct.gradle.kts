0e10add057a71ad10291aa91c8b5da8f2b6bf447
Vladimir Kryachko <vkryachko@google.com>
2022-12-14 11:39:26 -0400

    fix cct tests

--- transport/transport-backend-cct/transport-backend-cct.gradle.kts
+++ transport/transport-backend-cct/transport-backend-cct.gradle.kts
@@ -1,3 +1,6 @@
+import com.google.protobuf.gradle.builtins
+import com.google.protobuf.gradle.generateProtoTasks
+import com.google.protobuf.gradle.ofTest
 import com.google.protobuf.gradle.protoc
 
 // Copyright 2022 Google LLC
@@ -19,57 +22,65 @@ plugins {
   id("com.google.protobuf")
 }
 
-firebaseLibrary{
+firebaseLibrary {
   publishJavadoc = false
 }
 
 protobuf {
-    // Configure the protoc executable
-    protobuf.protoc {
-        val protocVersion = libs.versions.protoc.get()
-        artifact = "com.google.protobuf:protoc:$protocVersion"
+  // Configure the protoc executable
+  protobuf.protoc {
+    val protocVersion = libs.versions.protoc.get()
+    artifact = "com.google.protobuf:protoc:$protocVersion"
+  }
+
+  protobuf.generateProtoTasks {
+    ofTest().forEach {
+      it.builtins {
+        create("java")
+      }
     }
+  }
 }
 
 android {
-    val targetSdkVersion : Int by rootProject
-    val minSdkVersion : Int by rootProject
-    compileSdk = targetSdkVersion
-    defaultConfig {
-        minSdk = minSdkVersion
-        targetSdk = targetSdkVersion
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("proguard.txt")
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
+  val targetSdkVersion: Int by rootProject
+  val minSdkVersion: Int by rootProject
+  compileSdk = targetSdkVersion
+  defaultConfig {
+    minSdk = minSdkVersion
+    targetSdk = targetSdkVersion
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    consumerProguardFiles("proguard.txt")
+  }
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+  }
 }
 
 dependencies {
-    implementation(project(":transport:transport-api"))
-    implementation(project(":transport:transport-runtime"))
-    implementation(project(":encoders:firebase-encoders"))
-    implementation(project(":encoders:firebase-encoders-json"))
-    implementation(libs.androidx.annotation)
+  implementation(project(":transport:transport-api"))
+  implementation(project(":transport:transport-runtime"))
+  implementation(project(":encoders:firebase-encoders"))
+  implementation(project(":encoders:firebase-encoders-json"))
+  implementation(libs.androidx.annotation)
 
-    compileOnly(libs.autovalue.annotations)
+  compileOnly(libs.autovalue.annotations)
 
-    annotationProcessor(libs.autovalue)
-    annotationProcessor(project(":encoders:firebase-encoders-processor"))
+  annotationProcessor(libs.autovalue)
+  annotationProcessor(project(":encoders:firebase-encoders-processor"))
 
-    testImplementation("com.google.protobuf:protobuf-java-util:3.11.0")
-    testImplementation(libs.androidx.test.core)
-    testImplementation(libs.truth)
-    testImplementation("com.google.truth.extensions:truth-proto-extension:1.0")
-    testImplementation("com.github.tomakehurst:wiremock:2.26.3")
-    //Android compatible version of Apache httpclient.
-    testImplementation("org.apache.httpcomponents:httpclient-android:4.3.5.1")
-    // Keep Robolectric to 4.3.1 for httpclient and TelephonyManager compatibility.
-    testImplementation("org.robolectric:robolectric:4.3.1")
-    testImplementation(libs.junit)
+  testImplementation("com.google.protobuf:protobuf-java-util:3.11.0")
+  testImplementation(libs.androidx.test.core)
+  testImplementation(libs.truth)
+  testImplementation("com.google.truth.extensions:truth-proto-extension:1.0")
+  testImplementation("com.github.tomakehurst:wiremock:2.26.3")
+  //Android compatible version of Apache httpclient.
+  testImplementation("org.apache.httpcomponents:httpclient-android:4.3.5.1")
+  // Keep Robolectric to 4.3.1 for httpclient and TelephonyManager compatibility.
+  testImplementation("org.robolectric:robolectric:4.3.1")
+  testImplementation(libs.junit)
 
-    androidTestImplementation(libs.androidx.test.runner)
-    androidTestImplementation(libs.androidx.test.espresso)
+  androidTestImplementation(libs.androidx.test.runner)
+  androidTestImplementation(libs.androidx.test.espresso)
 }
