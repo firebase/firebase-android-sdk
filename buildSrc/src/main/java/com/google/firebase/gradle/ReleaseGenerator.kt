8da836790d3661c6e59926ad3d4f94883a3f5351
David Motsonashvili <davidmotson@google.com>
2022-12-21 16:37:23 -0800

    ktfmt & new dep

--- buildSrc/src/main/java/com/google/firebase/gradle/ReleaseGenerator.kt
+++ buildSrc/src/main/java/com/google/firebase/gradle/ReleaseGenerator.kt
@@ -12,93 +12,93 @@ import org.gradle.api.tasks.TaskAction
 data class FirebaseLibrary(val moduleNames: List<String>, val directories: List<String>)
 
 open class ReleaseGenerator : DefaultTask() {
-    @TaskAction
-    @Throws(Exception::class)
-    fun generateReleaseConfig() {
-        val currentRelease = project.property("currentRelease").toString()
-        val pastRelease = project.property("pastRelease").toString()
-        val printReleaseConfig = project.property("printOutput").toString().toBoolean()
-        val rootDir = project.rootDir
-        val availableModules = parseSubProjects(rootDir)
-        val firebaseLibraries = extractLibraries(availableModules, rootDir)
+  @TaskAction
+  @Throws(Exception::class)
+  fun generateReleaseConfig() {
+    val currentRelease = project.property("currentRelease").toString()
+    val pastRelease = project.property("pastRelease").toString()
+    val printReleaseConfig = project.property("printOutput").toString().toBoolean()
+    val rootDir = project.rootDir
+    val availableModules = parseSubProjects(rootDir)
+    val firebaseLibraries = extractLibraries(availableModules, rootDir)
 
-        val repo = Git.open(rootDir)
-        val headRef = repo.repository.resolve(Constants.HEAD)
-        val branchRef = getObjectRefForBranchName(repo, pastRelease)
+    val repo = Git.open(rootDir)
+    val headRef = repo.repository.resolve(Constants.HEAD)
+    val branchRef = getObjectRefForBranchName(repo, pastRelease)
 
-        val changedLibraries = getChangedLibraries(repo, branchRef, headRef, firebaseLibraries)
-        writeReleaseConfig(rootDir, changedLibraries, currentRelease)
-        if (printReleaseConfig) {
-            println(changedLibraries.joinToString(",", "LIBRARIES TO RELEASE: "))
-        }
+    val changedLibraries = getChangedLibraries(repo, branchRef, headRef, firebaseLibraries)
+    writeReleaseConfig(rootDir, changedLibraries, currentRelease)
+    if (printReleaseConfig) {
+      println(changedLibraries.joinToString(",", "LIBRARIES TO RELEASE: "))
     }
+  }
 
-    private fun extractLibraries(
-        availableModules: Set<String>,
-        rootDir: File
-    ): List<FirebaseLibrary> {
-        val nonKtxModules = availableModules.filter { !it.endsWith("ktx") }.toSet()
-        return nonKtxModules
-            .map { moduleName ->
-                val ktxModuleName = "$moduleName:ktx"
+  private fun extractLibraries(
+    availableModules: Set<String>,
+    rootDir: File
+  ): List<FirebaseLibrary> {
+    val nonKtxModules = availableModules.filter { !it.endsWith("ktx") }.toSet()
+    return nonKtxModules
+      .map { moduleName ->
+        val ktxModuleName = "$moduleName:ktx"
 
-                val moduleNames = listOf(moduleName, ktxModuleName).filter { availableModules.contains(it) }
-                val directories = moduleNames.map { it.replace(":", "/") }
+        val moduleNames = listOf(moduleName, ktxModuleName).filter { availableModules.contains(it) }
+        val directories = moduleNames.map { it.replace(":", "/") }
 
-                FirebaseLibrary(moduleNames, directories)
-            }
-            .filter { firebaseLibrary ->
-                firebaseLibrary.directories.first().let { File(rootDir, "$it/gradle.properties").exists() }
-            }
-    }
+        FirebaseLibrary(moduleNames, directories)
+      }
+      .filter { firebaseLibrary ->
+        firebaseLibrary.directories.first().let { File(rootDir, "$it/gradle.properties").exists() }
+      }
+  }
 
-    private fun parseSubProjects(rootDir: File) =
-        File(rootDir, "subprojects.cfg")
-            .readLines()
-            .filterNot { it.startsWith("#") || it.isEmpty() }
-            .toSet()
+  private fun parseSubProjects(rootDir: File) =
+    File(rootDir, "subprojects.cfg")
+      .readLines()
+      .filterNot { it.startsWith("#") || it.isEmpty() }
+      .toSet()
 
-    @Throws(GitAPIException::class)
-    private fun getObjectRefForBranchName(repo: Git, branchName: String) =
-        repo
-            .branchList()
-            .setListMode(ListBranchCommand.ListMode.REMOTE)
-            .call()
-            .firstOrNull { it.name == "refs/remotes/origin/$branchName" }
-            ?.objectId
-            ?: throw RuntimeException("Could not find branch named $branchName")
+  @Throws(GitAPIException::class)
+  private fun getObjectRefForBranchName(repo: Git, branchName: String) =
+    repo
+      .branchList()
+      .setListMode(ListBranchCommand.ListMode.REMOTE)
+      .call()
+      .firstOrNull { it.name == "refs/remotes/origin/$branchName" }
+      ?.objectId
+      ?: throw RuntimeException("Could not find branch named $branchName")
 
-    private fun getChangedLibraries(
-        repo: Git,
-        previousReleaseRef: ObjectId,
-        currentReleaseRef: ObjectId,
-        libraries: List<FirebaseLibrary>
-    ) =
-        libraries
-            .filter { library ->
-                library.directories.any { checkDirChanges(repo, previousReleaseRef, currentReleaseRef, it) }
-            }
-            .flatMap { it.moduleNames }
+  private fun getChangedLibraries(
+    repo: Git,
+    previousReleaseRef: ObjectId,
+    currentReleaseRef: ObjectId,
+    libraries: List<FirebaseLibrary>
+  ) =
+    libraries
+      .filter { library ->
+        library.directories.any { checkDirChanges(repo, previousReleaseRef, currentReleaseRef, it) }
+      }
+      .flatMap { it.moduleNames }
 
-    private fun checkDirChanges(
-        repo: Git,
-        previousReleaseRef: ObjectId,
-        currentReleaseRef: ObjectId,
-        directory: String
-    ) =
-        repo
-            .log()
-            .addPath("$directory/")
-            .addRange(previousReleaseRef, currentReleaseRef)
-            .setMaxCount(1)
-            .call()
-            .iterator()
-            .hasNext()
+  private fun checkDirChanges(
+    repo: Git,
+    previousReleaseRef: ObjectId,
+    currentReleaseRef: ObjectId,
+    directory: String
+  ) =
+    repo
+      .log()
+      .addPath("$directory/")
+      .addRange(previousReleaseRef, currentReleaseRef)
+      .setMaxCount(1)
+      .call()
+      .iterator()
+      .hasNext()
 
-    private fun writeReleaseConfig(configPath: File, libraries: List<String>, releaseName: String) {
-        File(configPath, "release.cfg")
-            .writeText(
-                """
+  private fun writeReleaseConfig(configPath: File, libraries: List<String>, releaseName: String) {
+    File(configPath, "release.cfg")
+      .writeText(
+        """
                     [release]
                     name = $releaseName
                     mode = RELEASE
@@ -106,7 +106,7 @@ open class ReleaseGenerator : DefaultTask() {
                     [modules]
                     ${libraries.joinToString("\n".padEnd(21, ' '))}
                 """
-                    .trimIndent()
-            )
-    }
+          .trimIndent()
+      )
+  }
 }
