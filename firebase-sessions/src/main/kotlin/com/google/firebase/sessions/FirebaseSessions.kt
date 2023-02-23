402c686f3f3c61b248c44a0fd82b7a92a310ef57
Matthew Robertson <mrober@users.noreply.github.com>
2023-02-23 13:50:19 -0500

    Initial commit for Firebase Sessions SDK (#4713)
    * Initial commit for Firebase Sessions SDK
    
    * Add copyright notice
    
    * Remove redundant dependency
    
    * Revert 'Remove redundant dependency' to avoid using api
    
    * Generate api.txt file

---
+++ firebase-sessions/src/main/kotlin/com/google/firebase/sessions/FirebaseSessions.kt
@@ -0,0 +1,34 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.firebase.sessions
+
+import androidx.annotation.Discouraged
+import com.google.firebase.FirebaseApp
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.ktx.app
+
+class FirebaseSessions internal constructor() {
+  @Discouraged(message = "This will be replaced with a real API.")
+  fun greeting(): String = "Matt says hi!"
+
+  companion object {
+    @JvmStatic
+    val instance: FirebaseSessions
+      get() = getInstance(Firebase.app)
+
+    @JvmStatic
+    fun getInstance(app: FirebaseApp): FirebaseSessions = app.get(FirebaseSessions::class.java)
+  }
+}
