8589d3d0cd3ccff89cba7e04ae9c98cd3058d596
VinayGuthal <vguthal@google.com>
2023-03-06 13:46:55 -0500

    update

--- buildSrc/src/main/java/com/google/firebase/gradle/plugins/CheckHeadDependencies.kt
+++ buildSrc/src/main/java/com/google/firebase/gradle/plugins/CheckHeadDependencies.kt
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-@file:JvmName("CheckHeadDependency")
-
 package com.google.firebase.gradle.plugins
 
 import org.gradle.api.DefaultTask
