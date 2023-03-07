e8b2870224312894a0a3cf773672ae1816d53798
Daymon <daymxn@google.com>
2023-03-07 14:56:18 -0600

    Test 1.0.5

--- buildSrc/src/main/java/com/google/firebase/gradle/plugins/DackkaGenerationTask.kt
+++ buildSrc/src/main/java/com/google/firebase/gradle/plugins/DackkaGenerationTask.kt
@@ -121,8 +121,6 @@ constructor(private val workerExecutor: WorkerExecutor) : GenerateDocumentationT
       )
     val annotationsNotToDisplayKotlin = listOf("kotlin.ExtensionFunctionType")
 
-    println("SOURCES: ${sources.get().joinToString(" | ")}")
-
     val jsonMap =
       mapOf(
         "moduleName" to "",
@@ -132,8 +130,7 @@ constructor(private val workerExecutor: WorkerExecutor) : GenerateDocumentationT
           listOf(
             mutableMapOf(
               "sourceSetID" to mapOf("scopeId" to "androidx", "sourceSetName" to "main"),
-              "sourceRoots" to
-                sources.get().filterNot { it.path.contains("release") }.map { it.absolutePath },
+              "sourceRoots" to sources.get().filter { it.exists() }.map { it.absolutePath },
               "classpath" to dependencies.get().map { it.absolutePath },
               "documentedVisibilities" to listOf("PUBLIC", "PROTECTED"),
               "skipEmptyPackages" to "true",
