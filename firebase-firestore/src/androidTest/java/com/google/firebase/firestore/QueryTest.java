e38db6408e74422bb61caabb4700da19a14d0fa2
Denver Coneybeare <dconeybe@google.com>
2023-03-10 23:34:42 -0500

    Add missing import of assumeTrue, which was also lost during a bad merge

--- firebase-firestore/src/androidTest/java/com/google/firebase/firestore/QueryTest.java
+++ firebase-firestore/src/androidTest/java/com/google/firebase/firestore/QueryTest.java
@@ -31,6 +31,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assume.assumeTrue;
 
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import com.google.android.gms.tasks.Task;
