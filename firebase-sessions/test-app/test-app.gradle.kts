d14dabe3c843665c70b7271f9113cda44a085822
Matthew Robertson <mrober@users.noreply.github.com>
2023-03-14 12:27:22 -0400

    Add Firebase Sessions Test App (#4775)
    * Add Firebase Sessions Test App
    
    * Remove boilerplate

---
+++ firebase-sessions/test-app/test-app.gradle.kts
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+plugins {
+  id("com.android.application")
+  id("org.jetbrains.kotlin.android")
+  id("com.google.gms.google-services")
+}
+
+android {
+  namespace = "com.google.firebase.testing.sessions"
+  compileSdk = 33
+  defaultConfig {
+    applicationId = "com.google.firebase.testing.sessions"
+    minSdk = 16
+    targetSdk = 33
+    versionCode = 1
+    versionName = "1.0"
+    multiDexEnabled = true
+  }
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+  }
+  kotlinOptions { jvmTarget = "1.8" }
+}
+
+dependencies {
+  // TODO(mrober): Remove when we have configurable deps on Crashlytics and Fireperf.
+  implementation(project(":firebase-sessions"))
+
+  implementation("androidx.appcompat:appcompat:1.6.1")
+  implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+  implementation("androidx.core:core-ktx:1.9.0")
+  implementation("com.google.android.material:material:1.8.0")
+}
