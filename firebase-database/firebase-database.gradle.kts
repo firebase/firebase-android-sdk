e708492471c8a8af5e1b9f90cdc2037ca55ebb9f
Daymon <daymxn@google.com>
2023-03-24 13:12:36 -0500

    Clean up firebase-database

--- firebase-database/firebase-database.gradle.kts
+++ firebase-database/firebase-database.gradle.kts
@@ -1,5 +1,3 @@
-@file:Suppress("UnstableApiUsage")
-
 // Copyright 2023 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -28,8 +26,8 @@ android {
   val minSdkVersion: Int by rootProject
 
   installation.timeOutInMs = 60 * 1000
-
   compileSdk = targetSdkVersion
+
   defaultConfig {
     minSdk = minSdkVersion
     targetSdk = targetSdkVersion
@@ -38,25 +36,25 @@ android {
   }
 
   buildTypes {
-    release { isMinifyEnabled = false }
+    release {
+      isMinifyEnabled = false
+    }
   }
 
   sourceSets {
     named("androidTest") {
-      java.srcDirs("src/testUtil/java")
+      java.srcDir("src/testUtil/java")
     }
     named("test") {
-      java.srcDirs("src/testUtil")
+      java.srcDir("src/testUtil")
     }
+
     compileOptions {
       sourceCompatibility = JavaVersion.VERSION_1_8
       targetCompatibility = JavaVersion.VERSION_1_8
     }
 
-    packagingOptions {
-      resources.excludes.add("META-INF/DEPENDENCIES")
-    }
-
+    packagingOptions.resources.excludes += "META-INF/DEPENDENCIES"
     testOptions.unitTests.isIncludeAndroidResources = true
   }
 }
@@ -75,36 +73,24 @@ dependencies {
     exclude("com.google.firebase", "firebase-common")
   }
 
-  javadocClasspath("com.google.code.findbugs:jsr305:3.0.2")
   javadocClasspath("com.google.auto.value:auto-value-annotations:1.6.6")
 
-  androidTestImplementation("androidx.annotation:annotation:1.1.0")
-  androidTestImplementation("androidx.test:rules:1.2.0")
-  androidTestImplementation("androidx.test:runner:1.2.0")
-  androidTestImplementation("androidx.test.ext:junit:1.1.1")
-  androidTestImplementation(libs.truth)
-  androidTestImplementation("com.fasterxml.jackson.core:jackson-core:2.9.8")
-  androidTestImplementation("com.fasterxml.jackson.core:jackson-databind:2.9.8")
-  androidTestImplementation("junit:junit:4.12")
-  androidTestImplementation("org.hamcrest:hamcrest:2.2")
-  androidTestImplementation("org.hamcrest:hamcrest-library:2.2")
-  androidTestImplementation("net.java:quickcheck:0.6")
-
+  testImplementation(libs.truth)
+  testImplementation(libs.robolectric)
   testImplementation("junit:junit:4.12")
   testImplementation("org.mockito:mockito-core:2.25.0")
-  testImplementation(libs.robolectric)
   testImplementation("com.firebase:firebase-token-generator:2.0.0")
   testImplementation("com.fasterxml.jackson.core:jackson-core:2.9.8")
   testImplementation("com.fasterxml.jackson.core:jackson-databind:2.9.8")
   testImplementation("net.java.quickcheck:quickcheck:0.6")
-  testImplementation(libs.truth) // does androidTestImpl not derive from testImpl?
   testImplementation("androidx.test:core:1.2.0")
   testImplementation("androidx.test:rules:1.2.0")
+
+  androidTestImplementation("androidx.test:runner:1.2.0")
+  androidTestImplementation("androidx.test.ext:junit:1.1.1")
+  androidTestImplementation("org.hamcrest:hamcrest:2.2")
+  androidTestImplementation("org.hamcrest:hamcrest-library:2.2")
 }
 
-// ==========================================================================
-// Copy from here down if you want to use the google-services plugin in your
-// androidTest integration tests.
-// ==========================================================================
 ext["packageName"] = "com.google.firebase.database"
 apply("../gradle/googleServices.gradle")
