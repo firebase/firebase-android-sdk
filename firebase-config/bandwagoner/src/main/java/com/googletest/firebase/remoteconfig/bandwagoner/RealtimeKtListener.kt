c68f2785a002ca7c8cfff3f408e3f3eae3848d3b
Dana Silver <danasilver@google.com>
2023-04-05 10:31:37 -0700

    Format Kotlin in bandwagoner.

--- firebase-config/bandwagoner/src/main/java/com/googletest/firebase/remoteconfig/bandwagoner/RealtimeKtListener.kt
+++ firebase-config/bandwagoner/src/main/java/com/googletest/firebase/remoteconfig/bandwagoner/RealtimeKtListener.kt
@@ -20,28 +20,25 @@ import android.util.Log
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.remoteconfig.ktx.configUpdates
 import com.google.firebase.remoteconfig.ktx.remoteConfig
+import java.util.concurrent.CompletableFuture
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.future.future
-import java.util.concurrent.CompletableFuture
 
 class RealtimeKtListener {
-    companion object {
-        private val TAG = "RealtimeListener"
+  companion object {
+    private val TAG = "RealtimeListener"
 
-        fun listenForUpdatesAsync(): CompletableFuture<Unit> =
-            GlobalScope.future { listenForUpdates() }
+    fun listenForUpdatesAsync(): CompletableFuture<Unit> = GlobalScope.future { listenForUpdates() }
 
-        private suspend fun listenForUpdates() {
-            Firebase.remoteConfig.configUpdates
-                .catch { exception ->
-                    Log.w(TAG, "Error listening for updates!", exception)
-                }
-                .collect { configUpdate ->
-                    if (configUpdate.updatedKeys.contains("welcome_message")) {
-                        Firebase.remoteConfig.activate()
-                    }
-                }
+    private suspend fun listenForUpdates() {
+      Firebase.remoteConfig.configUpdates
+        .catch { exception -> Log.w(TAG, "Error listening for updates!", exception) }
+        .collect { configUpdate ->
+          if (configUpdate.updatedKeys.contains("welcome_message")) {
+            Firebase.remoteConfig.activate()
+          }
         }
     }
+  }
 }
