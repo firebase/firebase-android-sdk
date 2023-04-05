72b01fed7655bce328ac11ec1ff734e5c962ed9b
Viswanathan Munisamy <visumickey@gmail.com>
2023-04-04 18:26:47 -0700

    Setup configuration layer for the library.

---
+++ firebase-sessions/src/main/kotlin/com/google/firebase/sessions/SessionsSettings.kt
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.firebase.sessions
+
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.minutes
+
+/**
+ * [SessionsSettings] manages all the configs that are relevant to the sessions library.
+ *
+ * @hide
+ */
+internal class SessionsSettings {
+  // Setting to qualify if sessions service is enabled.
+  internal val sessionsEnabled: Boolean get() {
+    return true
+  }
+
+  // Setting that provides the sessions sampling rate.
+  internal val samplingRate: Double get() {
+    return 1.0
+  }
+
+  // Background timeout config value before which a new session is generated
+  val sessionRestartTimeout: Duration
+    get() = 30.minutes
+
+  // Update the settings for all the settings providers
+  internal fun updateSettings() {
+    // Placeholder to initiate settings update on different sources
+    // Expected sources: RemoteSettings, ManifestOverrides, SDK Defaults
+  }
+}
---
+++ firebase-sessions/src/test/kotlin/com/google/firebase/sessions/SessionsSettingsTest.kt
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.firebase.sessions
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+import kotlin.time.Duration.Companion.minutes
+
+class SessionsSettingsTest {
+
+  @Test
+  fun sessionSettings_fetchDefaults() {
+    assertThat(SessionsSettings().sessionsEnabled).isTrue()
+    assertThat(SessionsSettings().samplingRate).isEqualTo(1.0)
+    assertThat(SessionsSettings().sessionRestartTimeout).isEqualTo(30.minutes)
+  }
+}
