1b297d6f0e3e4e7ec5eecefd8a136dc0670e86b0
Wu-Hui <wu.hui.github@gmail.com>
2023-04-18 15:53:46 -0400

    Add kotlin

--- firebase-firestore/ktx/src/main/kotlin/com/google/firebase/firestore/ktx/Firestore.kt
+++ firebase-firestore/ktx/src/main/kotlin/com/google/firebase/firestore/ktx/Firestore.kt
@@ -182,6 +182,22 @@ fun memoryCacheSettings(init: MemoryCacheSettings.Builder.() -> Unit): MemoryCac
   return builder.build()
 }
 
+fun memoryEagerGcSettings(
+  init: MemoryEagerGcSettings.Builder.() -> Unit
+): MemoryEagerGcSettings {
+  val builder = MemoryEagerGcSettings.newBuilder()
+  builder.init()
+  return builder.build()
+}
+
+fun memoryLruGcSettings(
+  init: MemoryLruGcSettings.Builder.() -> Unit
+): MemoryLruGcSettings {
+  val builder = MemoryLruGcSettings.newBuilder()
+  builder.init()
+  return builder.build()
+}
+
 fun persistentCacheSettings(
   init: PersistentCacheSettings.Builder.() -> Unit
 ): PersistentCacheSettings {
