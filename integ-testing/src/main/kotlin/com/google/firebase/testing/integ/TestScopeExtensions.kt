d7f06522e6c553351e57fe23957ccd2a66534a24
Daymon <daymxn@google.com>
2023-04-27 14:21:39 -0500

    Add extension methods

---
+++ integ-testing/src/main/kotlin/com/google/firebase/testing/integ/TestScopeExtensions.kt
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+@file:OptIn(ExperimentalCoroutinesApi::class)
+
+package com.google.firebase.testing.integ
+
+import androidx.annotation.RestrictTo
+import com.google.firebase.concurrent.TestOnlyExecutors
+import kotlin.coroutines.CoroutineContext
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.asCoroutineDispatcher
+import kotlinx.coroutines.test.TestScope
+
+data class FirebaseTestExecutors(
+  val blocking: CoroutineContext,
+  val background: CoroutineContext,
+  val lite: CoroutineContext
+)
+
+@get:RestrictTo(RestrictTo.Scope.TESTS)
+val TestScope.firebaseExecutors: FirebaseTestExecutors
+  get() =
+    FirebaseTestExecutors(
+      TestOnlyExecutors.blocking().asCoroutineDispatcher() + coroutineContext,
+      TestOnlyExecutors.background().asCoroutineDispatcher() + coroutineContext,
+      TestOnlyExecutors.lite().asCoroutineDispatcher() + coroutineContext
+    )
