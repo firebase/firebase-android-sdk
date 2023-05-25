62e9e4b38f0cab8b5697239fb122da8ba5af88f1
Matthew Robertson <mrober@users.noreply.github.com>
2023-05-25 17:49:30 -0400

    Fix behaviour with multiple subscribers (#5031)
    * Fix behaviour with multiple subscribers
    
    * Fix format

--- firebase-sessions/src/main/kotlin/com/google/firebase/sessions/FirebaseSessions.kt
+++ firebase-sessions/src/main/kotlin/com/google/firebase/sessions/FirebaseSessions.kt
@@ -78,6 +78,14 @@ internal constructor(
       "Registering Sessions SDK subscriber with name: ${subscriber.sessionSubscriberName}, " +
         "data collection enabled: ${subscriber.isDataCollectionEnabled}"
     )
+
+    // Immediately call the callback if Sessions generated a session before the
+    // subscriber subscribed, otherwise subscribers might miss the first session.
+    if (sessionGenerator.hasGenerateSession) {
+      subscriber.onSessionChanged(
+        SessionSubscriber.SessionDetails(sessionGenerator.currentSession.sessionId)
+      )
+    }
   }
 
   private fun initiateSessionStart() {
