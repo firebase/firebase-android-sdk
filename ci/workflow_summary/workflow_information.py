141e492d0231f01eac0c0885e6d5213b2574adf0
Mou <sunmou@google.com>
2023-06-29 11:20:42 -0700

    test month calculation

--- ci/workflow_summary/workflow_information.py
+++ ci/workflow_summary/workflow_information.py
@@ -75,8 +75,8 @@ def get_workflow_summary(gh, args):
 
   first_day_last_month = datetime.date.today().replace(day=1) - datetime.timedelta(days=1)
   last_day_last_month = first_day_last_month.replace(day=1)
-  first_day_last_month = datetime.combine(first_day_last_month, datetime.time.min)
-  last_day_last_month = datetime.combine(last_day_last_month, datetime.time.max)
+  first_day_last_month = datetime.datetime.combine(first_day_last_month, datetime.time.min)
+  last_day_last_month = datetime.datetime.combine(last_day_last_month, datetime.time.max)
   created = first_day_last_month.strftime('%Y-%m-%dT%H:%M:%SZ') + '..' + last_day_last_month.strftime('%Y-%m-%dT%H:%M:%SZ')
   logging.info('created:' + created)
 
