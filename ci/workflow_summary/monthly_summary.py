4863361545a07a7957b723a26284319e13d9f12d
Mou <sunmou@google.com>
2023-07-05 15:43:06 -0700

    print summary in markdown

--- ci/workflow_summary/monthly_summary.py
+++ ci/workflow_summary/monthly_summary.py
@@ -28,12 +28,16 @@ Usage:
 
 '''
 
+REPO_OWNER = 'firebase'
+REPO_NAME = 'firebase-android-sdk'
+EXCLUDE_JOB_LIST = ['Determine changed modules','Unit Tests (matrix)','Publish Tests Results','Unit Test Results','Instrumentation Tests','Unit Tests']
+
 def main(): 
   logging.getLogger().setLevel(logging.INFO)
 
   args = parse_cmdline_args()
 
-  gh = github.GitHub('firebase', 'firebase-android-sdk')
+  gh = github.GitHub(REPO_OWNER, REPO_NAME)
 
 
   monthly_summary = {}
@@ -78,21 +82,29 @@ def main():
   print("| --- |" + " --- |" * len(dates))
   # For the workflow, generate the failure rate for each month
   workflow_data = []
-  for _, one_month_summary in sorted(monthly_summary.items(), reverse=True):
-    workflow_data.append(f"{one_month_summary['failure_rate']:.2%} ({one_month_summary['failure_count']}/{one_month_summary['total_count']})")
+  workflow_data = [f"{summary['failure_rate']:.2%} ({summary['failure_count']}/{summary['total_count']})" for summary in monthly_summary.values()]
   print(f"| Workflow | {' | '.join(workflow_data)} |")
 
   # List to hold all dates
   print(f"| Job Name | {' | '.join(dates)} |")
   print("| --- |" + " --- |" * len(dates))
-  # Generating a set of all unique job names
-  failure_jobs_this_month = monthly_summary[sorted(monthly_summary.keys(), reverse=True)[0]]['failure_jobs'].keys()
-  # Sorting jobs by last month's failure rate
-  sorted_jobs = sorted(failure_jobs_this_month, key=lambda j: monthly_summary[sorted(monthly_summary.keys(), reverse=True)[0]]['failure_jobs'][j]['failure_rate'], reverse=True)
-  # For each job, generate the failure rate for each month
-  for job_name in sorted_jobs:
+  # Sorted Jobs for the latest month
+  latest_month = next(iter(monthly_summary.values()))
+  sorted_jobs = sorted(latest_month['failure_jobs'], key=lambda job: latest_month['failure_jobs'][job]['failure_rate'], reverse=True)
+  # High Failure Jobs in 6 months
+  all_jobs = {job for summary in list(monthly_summary.values())[:6] for job in summary['failure_jobs']}
+  all_jobs.difference_update(set(sorted_jobs))
+  avg_failure_rates = {job: sum([summary['failure_jobs'][job]['failure_rate'] for summary in list(monthly_summary.values())[:6] if job in summary['failure_jobs']])/6 for job in all_jobs}
+  high_failure_jobs = {job: rate for job, rate in avg_failure_rates.items() if rate >= 0.1}
+  # Combine Last Month Failure Jobs and High Failure Rate Jobs
+  all_jobs = sorted_jobs + sorted(high_failure_jobs, key=high_failure_jobs.get, reverse=True)
+  # Loop through All Jobs
+  for job_name in all_jobs:
+    if job_name in EXCLUDE_JOB_LIST:
+      continue
+
     job_data = []
-    for _, one_month_summary in monthly_summary.items():
+    for _, one_month_summary in sorted(monthly_summary.items(), reverse=True):
       one_month_job_summary = one_month_summary['failure_jobs'].get(job_name)
       if one_month_job_summary:
         job_data.append(f"{one_month_job_summary['failure_rate']:.2%} ({one_month_job_summary['failure_count']}/{one_month_job_summary['total_count']})")
