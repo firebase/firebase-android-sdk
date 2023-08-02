ccb83b9bcb04cf23c12be451b48c3885769b28d8
Vinay Guthal <vguthal@google.com>
2023-08-02 15:16:11 -0400

    update

--- buildSrc/src/main/java/com/google/firebase/gradle/plugins/PackageTransform.kt
+++ buildSrc/src/main/java/com/google/firebase/gradle/plugins/PackageTransform.kt
@@ -14,6 +14,36 @@ abstract class PackageTransform : DefaultTask() {
   @get:Input abstract val groupId: Property<String>
   @get:Input abstract val projectPath: Property<String>
 
+  fun getSymbol(line: String): String {
+    val parts = line.split(" ")
+    if (parts.get(0) == "val" || parts.get(0) == "class") {
+      // Field or class
+      return parts.get(1).replace(":", "")
+    } else if (parts.get(0) == "object") {
+      return parts.get(1)
+    } else if (parts.get(0) == "fun") {
+      // Method
+      var output: String = ""
+      var ignore = false
+      for (c in line) {
+        if (c == ':') {
+          ignore = true
+        }
+        if (c == ')' || c == ',') {
+          ignore = false
+        }
+        if (c == '=') {
+          break
+        }
+        if (!ignore) {
+          output += c
+        }
+      }
+      return output.replace("fun", "")
+    }
+    return ""
+  }
+
   fun copyDir(src: Path, dest: Path) {
     Files.walk(src).forEach {
       if (!Files.isDirectory(it)) {
@@ -22,15 +52,22 @@ abstract class PackageTransform : DefaultTask() {
     }
   }
 
-  fun deprecateKTX(src: String) {
+  fun deprecateKTX(src: String, pkgName: String) {
     File(src).walk().forEach {
       if (it.absolutePath.endsWith(".kt")) {
         val lines = File(it.absolutePath).readLines()
         val output = mutableListOf<String>()
-        for (line in lines) {
-          output.add(line)
-          if (line.contains("*/")) {
-            output.add("@Deprecated")
+        for (i in 0 until lines.size) {
+          output.add(lines[i])
+          if (lines[i].contains("*/")) {
+            var symbol = ""
+            var ctr = i + 1
+            while (symbol.isEmpty()) {
+              symbol = getSymbol(lines[ctr++]).trim()
+            }
+            output.add(
+              """@Deprecated("Use `${pkgName}${symbol}`", ReplaceWith("${pkgName}${symbol}"))"""
+            )
           }
         }
         File(it.absolutePath).writeText(output.joinToString("\n"))
@@ -40,8 +77,25 @@ abstract class PackageTransform : DefaultTask() {
   fun updateKTXReferences(src: String) {
     // Remove all .ktx suffixes essentially.
     File(src).walk().forEach {
-      if (it.absolutePath.endsWith(".kt")) {
+      if (it.absolutePath.endsWith(".kt") && !it.absolutePath.contains("/ktx/")) {
         val lines = File(it.absolutePath).readLines().map { x -> x.replace(".ktx", "") }
+        File(it.absolutePath).delete()
+        val newFile = File(it.absolutePath)
+        newFile.writeText(lines.joinToString("\n"))
+      }
+    }
+  }
+
+  fun updatePlatformLogging(src: String) {
+    File(src).walk().forEach {
+      if (it.absolutePath.endsWith(".kt")) {
+        val lines =
+          File(it.absolutePath).readLines().map { x ->
+            x.replace(
+              """LibraryVersionComponent.create(LIBRARY_NAME, BuildConfig.VERSION_NAME),""",
+              ""
+            )
+          }
         File(it.absolutePath).delete()
         val newFile = File(it.absolutePath)
         newFile.writeText(lines.joinToString("\n"))
@@ -59,6 +113,8 @@ abstract class PackageTransform : DefaultTask() {
     copyDir(File(ktxArtifactPath).toPath(), File(ktxPackagePath).toPath())
     copyDir(File(ktxArtifactPath).toPath(), File(mainPackagePath).toPath())
     updateKTXReferences(mainPackagePath)
-    deprecateKTX(ktxPackagePath)
+    updatePlatformLogging(mainPackagePath)
+    deprecateKTX(ktxPackagePath, packageNamePath.replace("/", "."))
+    project.dependencies.create("com.google.firebase:firebase-functions-ktx:20.3.1")
   }
 }
