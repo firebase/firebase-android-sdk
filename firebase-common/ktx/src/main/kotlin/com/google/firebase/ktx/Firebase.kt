df0b99aed2ebf7e6e64179fcfc9e8e6e4ca7cfa3
Vinay Guthal <vguthal@google.com>
2023-08-08 15:16:32 -0400

    update

--- firebase-common/ktx/src/main/kotlin/com/google/firebase/ktx/Firebase.kt
+++ firebase-common/ktx/src/main/kotlin/com/google/firebase/ktx/Firebase.kt
@@ -1,4 +1,4 @@
-// Copyright 2023 Google LLC
+// Copyright 2019 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -13,17 +13,72 @@
 // limitations under the License.
 package com.google.firebase.ktx
 
+import android.content.Context
 import androidx.annotation.Keep
+import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseOptions
+import com.google.firebase.annotations.concurrent.Background
+import com.google.firebase.annotations.concurrent.Blocking
+import com.google.firebase.annotations.concurrent.Lightweight
+import com.google.firebase.annotations.concurrent.UiThread
 import com.google.firebase.components.Component
 import com.google.firebase.components.ComponentRegistrar
+import com.google.firebase.components.Dependency
+import com.google.firebase.components.Qualified
 import com.google.firebase.platforminfo.LibraryVersionComponent
+import java.util.concurrent.Executor
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.asCoroutineDispatcher
+
+/**
+ * Single access point to all firebase SDKs from Kotlin.
+ *
+ * <p>Acts as a target for extension methods provided by sdks.
+ */
+object Firebase
+
+/** Returns the default firebase app instance. */
+val Firebase.app: FirebaseApp
+  get() = FirebaseApp.getInstance()
+
+/** Returns a named firebase app instance. */
+fun Firebase.app(name: String): FirebaseApp = FirebaseApp.getInstance(name)
+
+/** Initializes and returns a FirebaseApp. */
+fun Firebase.initialize(context: Context): FirebaseApp? = FirebaseApp.initializeApp(context)
+
+/** Initializes and returns a FirebaseApp. */
+fun Firebase.initialize(context: Context, options: FirebaseOptions): FirebaseApp =
+  FirebaseApp.initializeApp(context, options)
+
+/** Initializes and returns a FirebaseApp. */
+fun Firebase.initialize(context: Context, options: FirebaseOptions, name: String): FirebaseApp =
+  FirebaseApp.initializeApp(context, options, name)
+
+/** Returns options of default FirebaseApp */
+val Firebase.options: FirebaseOptions
+  get() = Firebase.app.options
 
 internal const val LIBRARY_NAME: String = "fire-core-ktx"
 
 /** @suppress */
 @Keep
-class FirebaseCommonLoggingRegistrar : ComponentRegistrar {
+class FirebaseCommonKtxRegistrar : ComponentRegistrar {
   override fun getComponents(): List<Component<*>> {
-    return listOf(LibraryVersionComponent.create(LIBRARY_NAME, BuildConfig.VERSION_NAME))
+    return listOf(
+      LibraryVersionComponent.create(LIBRARY_NAME, BuildConfig.VERSION_NAME),
+      coroutineDispatcher<Background>(),
+      coroutineDispatcher<Lightweight>(),
+      coroutineDispatcher<Blocking>(),
+      coroutineDispatcher<UiThread>()
+    )
   }
 }
+
+private inline fun <reified T : Annotation> coroutineDispatcher(): Component<CoroutineDispatcher> =
+  Component.builder(Qualified.qualified(T::class.java, CoroutineDispatcher::class.java))
+    .add(Dependency.required(Qualified.qualified(T::class.java, Executor::class.java)))
+    .factory { c ->
+      c.get(Qualified.qualified(T::class.java, Executor::class.java)).asCoroutineDispatcher()
+    }
+    .build()
