aeef091d4c28554c816311e5e97077e181dc46f9
Vinay Guthal <vguthal@google.com>
2023-08-08 12:30:55 -0400

    update manifest file as well

--- buildSrc/src/main/java/com/google/firebase/gradle/plugins/PackageTransform.kt
+++ buildSrc/src/main/java/com/google/firebase/gradle/plugins/PackageTransform.kt
@@ -4,10 +4,13 @@ import java.io.File
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.StandardCopyOption
+import javax.xml.parsers.DocumentBuilderFactory
 import org.gradle.api.DefaultTask
 import org.gradle.api.provider.Property
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.TaskAction
+import org.gradle.kotlin.dsl.project
+import org.w3c.dom.Element
 
 abstract class PackageTransform : DefaultTask() {
   @get:Input abstract val artifactId: Property<String>
@@ -92,12 +95,10 @@ abstract class PackageTransform : DefaultTask() {
     File(src).walk().forEach {
       if (it.absolutePath.endsWith(".kt")) {
         val lines =
-          File(it.absolutePath).readLines().map { x ->
-            x.replace(
-              """LibraryVersionComponent.create(LIBRARY_NAME, BuildConfig.VERSION_NAME),""",
-              ""
-            )
-          }.filter { x-> !x.contains("contains(LIBRARY_NAME)") }
+          File(it.absolutePath).readLines().filter { x ->
+            !x.contains("contains(LIBRARY_NAME)") &&
+              !x.contains("LibraryVersionComponent.create(LIBRARY_NAME, BuildConfig.VERSION_NAME)")
+          }
         File(it.absolutePath).delete()
         val newFile = File(it.absolutePath)
         newFile.writeText(lines.joinToString("\n"))
@@ -122,6 +123,48 @@ abstract class PackageTransform : DefaultTask() {
       }
     }
     return output.filter { x -> (x.trim().isNotEmpty() && !x.startsWith("//")) }
+  }
+
+  fun copyManifestComponent(source: String, dest: String) {
+    val documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder()
+    val ktxManifest = documentBuilder.parse(File(source))
+    ktxManifest.documentElement.normalize()
+    val nodeList = ktxManifest.getElementsByTagName("*")
+    val metadata =
+      (0..nodeList.length - 1)
+        .toList()
+        .map { x -> nodeList.item(x) }
+        .filter { it.nodeType == Element.ELEMENT_NODE }
+        .map { x -> (x as Element) }
+        .filter { it.nodeName == "meta-data" }
+        .map { x ->
+          "<meta-data android:name=\"${x.getAttribute("android:name").replace(".ktx.",".")}\" android:value=\"${x.getAttribute("android:value")}\"/>"
+        }
+        .joinToString("\n")
+
+    val lines = File(dest).readLines()
+    var output = mutableListOf<String>()
+    var inside = false
+    for (line in lines) {
+      if (
+        line.contains("android:name=\"com.google.firebase.components.ComponentDiscoveryService\"")
+      ) {
+        inside = true
+      }
+      if (inside == true) {
+        if (line.contains("/>")) {
+          output.add(line.replace("/>", "> ${metadata} </service>"))
+          inside = false
+          continue
+        } else if (line.contains(">")) {
+          output.add(line.replace(">", "> ${metadata}"))
+          inside = false
+          continue
+        }
+      }
+      output.add(line)
+    }
+    File(dest).writeText(output.joinToString("\n"))
   }
 
   @TaskAction
@@ -153,6 +196,10 @@ abstract class PackageTransform : DefaultTask() {
     }
     updateKTXReferences(mainPackagePath)
     updatePlatformLogging("${projectPath.get()}/src")
+    copyManifestComponent(
+      "${projectPath.get()}/ktx/src/main/AndroidManifest.xml",
+      "${projectPath.get()}/src/main/AndroidManifest.xml"
+    )
     deprecateKTX(ktxPackagePath, packageNamePath.replace("/", "."))
     var gradlePath = "${projectPath.get()}/${artifactId.get()}.gradle.kts"
     var ktxGradlePath = "${projectPath.get()}/ktx/ktx.gradle.kts"
@@ -163,11 +210,17 @@ abstract class PackageTransform : DefaultTask() {
     val dependencies = readDependencies(gradlePath)
     val ktxDependencies = readDependencies(ktxGradlePath)
     val deps =
-      (dependencies.toSet() + ktxDependencies.toSet()).toList().sorted().map { x -> "    " + x }
+      (dependencies.toSet() + ktxDependencies.toSet())
+        .toList()
+        .sorted()
+        .map { x -> "    " + x }
+        .filter { !it.contains("project(\"${project.path}\")") }
     val lines = File(gradlePath).readLines()
     val output = mutableListOf<String>()
-    println(project.path)
     for (line in lines) {
+      if (line.contains("id(\"kotlin-android\")")) {
+        continue
+      }
       output.add(line)
       if (line.contains("id(\"firebase-library\")")) {
         output.add("    id(\"kotlin-android\")")
