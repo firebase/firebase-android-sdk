19fe81811a2275ab27f1c7aac4e3f299385aaad7
qdpham <qdpham@google.com>
2023-08-10 02:25:49 +0000

    Rename folder to be in config instead of sessions

--- firebase-config/test-app/src/main/kotlin/com/google/firebase/testing/config/MainActivity.kt
+++ firebase-config/test-app/src/main/kotlin/com/google/firebase/testing/config/MainActivity.kt
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.google.firebase.testing.sessions
+package com.google.firebase.testing.config
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
