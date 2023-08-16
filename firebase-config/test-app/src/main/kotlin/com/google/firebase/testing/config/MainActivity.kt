be2c2d28f8fd649bc99a3f7d84b6afe50b106752
qdpham <qdpham@google.com>
2023-08-16 16:18:23 +0000

    Replace `Firebase Sessions` with `Firebase Remote Config`

--- firebase-config/test-app/src/main/kotlin/com/google/firebase/testing/config/MainActivity.kt
+++ firebase-config/test-app/src/main/kotlin/com/google/firebase/testing/config/MainActivity.kt
@@ -17,13 +17,14 @@
 package com.google.firebase.testing.config
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    //    setContentView(R.layout.activity_main)
+    setContentView(R.layout.activity_main)
 
-    //    findViewById<TextView>(R.id.greeting_text).text = getText(R.string.firebase_greetings)
+    findViewById<TextView>(R.id.greeting_text).text = getText(R.string.firebase_greetings)
   }
 }
