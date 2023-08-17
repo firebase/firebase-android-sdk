d0c5e51413238262e9d8520b5c4999f2bee5a5a0
Dana Silver <danasilver@google.com>
2023-08-17 11:19:08 -0700

    Initial commit of firebase-config-interop SDK.

---
+++ firebase-config-interop/firebase-config-interop.gradle.kts
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+plugins {
+    id("firebase-library")
+}
+
+firebaseLibrary {
+    publishSources = true
+    publishJavadoc = false
+}
+
+android {
+    val targetSdkVersion: Int by rootProject
+
+    namespace = "com.google.firebase.remoteconfiginterop"
+    compileSdk = 33
+
+    defaultConfig {
+        minSdk = 16
+        targetSdk = targetSdkVersion
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+    implementation("com.google.firebase:firebase-encoders-json:18.0.1")
+    implementation("com.google.firebase:firebase-encoders:17.0.0")
+
+    compileOnly("com.google.auto.value:auto-value-annotations:1.10.1")
+
+    annotationProcessor("com.google.auto.value:auto-value:1.10.1")
+    annotationProcessor(project(":encoders:firebase-encoders-processor"))
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+
+    testImplementation(libs.junit)
+    testImplementation(libs.truth)
+    testImplementation(libs.robolectric)
+
+    androidTestImplementation(libs.androidx.test.junit)
+    androidTestImplementation(libs.espresso.core)
+
+
+}
