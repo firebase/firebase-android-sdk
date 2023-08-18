0f45c6c29ec6fefa8d12da8b0522adcd5f9b4170
Emily Ploszaj <emilyploszaj@google.com>
2023-08-18 11:22:52 -0500

    Adjust based on comments

--- buildSrc/src/main/java/com/google/firebase/gradle/plugins/GmavenVersionChecker.kt
+++ buildSrc/src/main/java/com/google/firebase/gradle/plugins/GmavenVersionChecker.kt
@@ -1,3 +1,17 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package com.google.firebase.gradle.plugins
 
 import org.gradle.api.DefaultTask
@@ -20,35 +34,43 @@ abstract class GmavenVersionChecker : DefaultTask() {
   fun run() {
     val mavenHelper = GmavenHelper(groupId.get(), artifactId.get())
     val latestMavenVersion = mavenHelper.getLatestReleasedVersion()
+    val info =
+      "\n  latestReleasedVersion in gradle.properties should match the latest release on GMaven (${latestMavenVersion})" +
+        "\n  version in gradle.properties should be a version bump above this, following SemVer, and should not be released on GMaven"
     // Either the Maven metadata does not exist, or the library hasn't been released
     if (latestMavenVersion.isEmpty()) {
       return
     }
+    // b/285892320
+    if (artifactId.get() == "protolite-well-known-types") {
+      return
+    }
     if (version.get() == latestReleasedVersion.get()) {
       throw GradleException(
-        "Current and latest released versions from gradle.properties are the same (${version.get()})" +
-          "\nLatest release should be ${latestMavenVersion}"
+        "version and latestReleasedVersion from gradle.properties are the same (${version.get()})" +
+          info
       )
     }
     if (latestMavenVersion == version.get()) {
       throw GradleException(
-        "Current version from gradle.properties (${version.get()}) is already the latest release on GMaven"
+        "version from gradle.properties (${version.get()}) is already the latest release on GMaven" +
+          info
       )
     } else if (mavenHelper.hasReleasedVersion(version.get())) {
       throw GradleException(
-        "Current version from gradle.properties (${version.get()}) has already been released on GMaven" +
-          "\nThe latest released version on GMaven ${latestMavenVersion}"
+        "version from gradle.properties (${version.get()}) has already been released on GMaven" +
+          info
       )
     }
     if (latestMavenVersion != latestReleasedVersion.get()) {
       if (mavenHelper.hasReleasedVersion(latestReleasedVersion.get())) {
         throw GradleException(
-          "Latest released version from gradle.properties (${latestReleasedVersion.get()}) is not the latest released on GMaven (${latestMavenVersion})"
+          "latestReleasedVersion from gradle.properties (${latestReleasedVersion.get()}) has been released but is not the latest release on GMaven (${latestMavenVersion})"
         )
       } else {
         throw GradleException(
-          "Latest released version from gradle.properties (${latestReleasedVersion.get()}) has not been released on GMaven" +
-            "\nThe latest released version on GMaven ${latestMavenVersion}"
+          "latestReleasedVersion from gradle.properties (${latestReleasedVersion.get()}) has not been released on GMaven" +
+            info
         )
       }
     }
