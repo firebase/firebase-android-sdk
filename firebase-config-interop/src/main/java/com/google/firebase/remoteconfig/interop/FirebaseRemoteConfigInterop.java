d8a892cbe83d501feebcd25dd7cdfdc62b354809
Dana Silver <danasilver@google.com>
2023-08-21 13:46:00 -0700

    Add javadoc for registerRolloutsStateSubscriber

--- firebase-config-interop/src/main/java/com/google/firebase/remoteconfig/interop/FirebaseRemoteConfigInterop.java
+++ firebase-config-interop/src/main/java/com/google/firebase/remoteconfig/interop/FirebaseRemoteConfigInterop.java
@@ -20,6 +20,16 @@ import androidx.annotation.NonNull;
 import com.google.firebase.remoteconfig.interop.rollouts.RolloutsStateSubscriber;
 
 public interface FirebaseRemoteConfigInterop {
+  /**
+   * Register a {@link RolloutsStateSubscriber} {@code subscriber} whose callback {@code
+   * onRolloutsStateChanged} is invoked when the set of assigned rollouts changes for the given
+   * Remote Config {@code namespace}.
+   *
+   * @param namespace The Remote Config namespace for which {@code subscriber} should be registered.
+   *     A Firebase project can have multiple Remote Configs, each belonging to a unique namespace,
+   *     ex. "firebase" for the default configuration.
+   * @param subscriber An implementation of {@link RolloutsStateSubscriber}.
+   */
   void registerRolloutsStateSubscriber(
       @NonNull String namespace, @NonNull RolloutsStateSubscriber subscriber);
 }
