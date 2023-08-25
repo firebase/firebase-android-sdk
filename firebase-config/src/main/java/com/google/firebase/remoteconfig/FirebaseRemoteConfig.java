1f1073d081b9e06db4a4e3d97be1ac596651a477
Dana Silver <danasilver@google.com>
2023-08-25 13:49:15 -0700

    Remove extra def

--- firebase-config/src/main/java/com/google/firebase/remoteconfig/FirebaseRemoteConfig.java
+++ firebase-config/src/main/java/com/google/firebase/remoteconfig/FirebaseRemoteConfig.java
@@ -713,10 +713,6 @@ public class FirebaseRemoteConfig {
     return experimentInfoMaps;
   }
 
-  RolloutsStateSubscriptionsHandler getRolloutsStateSubscriptionsHandler() {
-    return rolloutsStateSubscriptionsHandler;
-  }
-
   /** Returns true if the fetched configs are fresher than the activated configs. */
   private static boolean isFetchedFresh(
       ConfigContainer fetched, @Nullable ConfigContainer activated) {
