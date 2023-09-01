55d7a6ff7737f5d364385868e146824369f288d8
Dana Silver <danasilver@google.com>
2023-09-01 16:55:53 -0700

    Implement RolloutsStateFactory. (#5281)
    PR 3 of 3 implementing the pub/sub pattern in RC.
    
    PRs:
    
    1. [Add dependency with stubbed
    implementation.](https://github.com/firebase/firebase-android-sdk/pull/5277)
    2. [Implement publish and
    subscribe.](https://github.com/firebase/firebase-android-sdk/pull/5280)
    3. **This PR: Implement getActiveRolloutsState.**

--- firebase-config/src/test/java/com/google/firebase/remoteconfig/internal/rollouts/RolloutsStateFactoryTest.java
+++ firebase-config/src/test/java/com/google/firebase/remoteconfig/internal/rollouts/RolloutsStateFactoryTest.java
@@ -16,5 +16,104 @@
 
 package com.google.firebase.remoteconfig.internal.rollouts;
 
-// TODO: Add tests.
-public class RolloutsStateFactoryTest {}
+import static com.google.common.truth.Truth.assertThat;
+import static com.google.firebase.remoteconfig.testutil.Assert.assertThrows;
+import static org.mockito.Mockito.when;
+
+import com.google.common.collect.ImmutableSet;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigClientException;
+import com.google.firebase.remoteconfig.internal.ConfigContainer;
+import com.google.firebase.remoteconfig.internal.ConfigGetParameterHandler;
+import com.google.firebase.remoteconfig.interop.rollouts.RolloutAssignment;
+import com.google.firebase.remoteconfig.interop.rollouts.RolloutsState;
+import org.json.JSONArray;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
+public class RolloutsStateFactoryTest {
+  @Mock ConfigGetParameterHandler mockConfigGetParameterHandler;
+
+  private static final String PARAMETER_KEY = "my_feature";
+  private static final String PARAMETER_VALUE = "true";
+
+  private static final RolloutAssignment rolloutAssignment =
+      RolloutAssignment.builder()
+          .setRolloutId("rollout_1")
+          .setVariantId("control")
+          .setParameterKey(PARAMETER_KEY)
+          .setParameterValue(PARAMETER_VALUE)
+          .setTemplateVersion(1L)
+          .build();
+  private static final RolloutsState rolloutsState =
+      RolloutsState.create(ImmutableSet.of(rolloutAssignment));
+  private static ConfigContainer configContainerWithRollout;
+  private static ConfigContainer configContainerWithoutRollout;
+
+  private RolloutsStateFactory factory;
+
+  @Before
+  public void setUp() throws Exception {
+    MockitoAnnotations.initMocks(this);
+
+    configContainerWithRollout =
+        ConfigContainer.newBuilder()
+            .withRolloutMetadata(
+                new JSONArray(
+                    "["
+                        + "{"
+                        + "\"rollout_id\": \"rollout_1\","
+                        + "\"variant_id\": \"control\","
+                        + "\"affected_parameter_keys\": [\""
+                        + PARAMETER_KEY
+                        + "\"]"
+                        + "}]"))
+            .withTemplateVersionNumber(1L)
+            .build();
+    configContainerWithoutRollout = ConfigContainer.newBuilder().build();
+
+    factory = new RolloutsStateFactory(mockConfigGetParameterHandler);
+  }
+
+  @Test
+  public void getActiveRolloutsState_noRollouts_returnsEmptyState() throws Exception {
+    RolloutsState actual = factory.getActiveRolloutsState(configContainerWithoutRollout);
+
+    assertThat(actual).isEqualTo(RolloutsState.create(ImmutableSet.of()));
+  }
+
+  @Test
+  public void getActiveRolloutsState_hasRolloutsMetadata_populatesRolloutsState() throws Exception {
+    when(mockConfigGetParameterHandler.getString(PARAMETER_KEY)).thenReturn(PARAMETER_VALUE);
+
+    RolloutsState actual = factory.getActiveRolloutsState(configContainerWithRollout);
+
+    assertThat(actual).isEqualTo(rolloutsState);
+  }
+
+  @Test
+  public void getActiveRolloutsState_jsonException_throwsRemoteConfigException() throws Exception {
+    when(mockConfigGetParameterHandler.getString(PARAMETER_KEY)).thenReturn(PARAMETER_VALUE);
+    JSONArray rolloutMetadatamMissingVariantId =
+        new JSONArray(
+            "["
+                + "{"
+                + "\"rollout_id\": \"rollout_1\","
+                + "\"affected_parameter_keys\": [\""
+                + PARAMETER_KEY
+                + "\"]"
+                + "}]");
+
+    assertThrows(
+        FirebaseRemoteConfigClientException.class,
+        () ->
+            factory.getActiveRolloutsState(
+                ConfigContainer.newBuilder()
+                    .withRolloutMetadata(rolloutMetadatamMissingVariantId)
+                    .build()));
+  }
+}
