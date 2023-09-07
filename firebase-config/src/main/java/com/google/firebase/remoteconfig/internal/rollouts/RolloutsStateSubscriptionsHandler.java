10d374b4af86629fae69609940bb6eaffd75f34d
Dana Silver <danasilver@google.com>
2023-09-07 09:06:25 -0700

    Check for null ConfigContainer before constructing RolloutsState. (#5304)
    It's possible the ConfigContainerTask returns null while the
    ConfigContainer is being instantiated from the disk cache (ex. at app
    startup when Crashlytics registers a handler).

--- firebase-config/src/main/java/com/google/firebase/remoteconfig/internal/rollouts/RolloutsStateSubscriptionsHandler.java
+++ firebase-config/src/main/java/com/google/firebase/remoteconfig/internal/rollouts/RolloutsStateSubscriptionsHandler.java
@@ -20,6 +20,7 @@ import static com.google.firebase.remoteconfig.FirebaseRemoteConfig.TAG;
 
 import android.util.Log;
 import androidx.annotation.NonNull;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.remoteconfig.FirebaseRemoteConfigException;
 import com.google.firebase.remoteconfig.internal.ConfigCacheClient;
 import com.google.firebase.remoteconfig.internal.ConfigContainer;
@@ -52,22 +53,29 @@ public class RolloutsStateSubscriptionsHandler {
   public void registerRolloutsStateSubscriber(@NonNull RolloutsStateSubscriber subscriber) {
     subscribers.add(subscriber);
 
-    activatedConfigsCache
-        .get()
-        .addOnSuccessListener(
-            executor,
-            configContainer -> {
-              try {
-                RolloutsState rolloutsState =
-                    rolloutsStateFactory.getActiveRolloutsState(configContainer);
-                executor.execute(() -> subscriber.onRolloutsStateChanged(rolloutsState));
-              } catch (FirebaseRemoteConfigException e) {
-                Log.w(
-                    TAG,
-                    "Exception publishing RolloutsState to subscriber. Continuing to listen for changes.",
-                    e);
-              }
-            });
+    Task<ConfigContainer> activatedConfigsCacheTask = activatedConfigsCache.get();
+
+    activatedConfigsCacheTask.addOnSuccessListener(
+        executor,
+        unused -> {
+          try {
+            ConfigContainer activatedConfigsCache = activatedConfigsCacheTask.getResult();
+
+            // If we try to read the cache before the Remote Config component has started up,
+            // the cache will return null. We don't need to publish any rollouts since none are
+            // assigned yet.
+            if (activatedConfigsCache != null) {
+              RolloutsState rolloutsState =
+                  rolloutsStateFactory.getActiveRolloutsState(activatedConfigsCache);
+              executor.execute(() -> subscriber.onRolloutsStateChanged(rolloutsState));
+            }
+          } catch (FirebaseRemoteConfigException e) {
+            Log.w(
+                TAG,
+                "Exception publishing RolloutsState to subscriber. Continuing to listen for changes.",
+                e);
+          }
+        });
   }
 
   public void publishActiveRolloutsState(@NonNull ConfigContainer configContainer) {
