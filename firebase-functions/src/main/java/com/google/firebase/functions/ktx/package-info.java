7b5ade446a3526cd4d81cba615e805ff459033be
Vinay Guthal <vguthal@google.com>
2023-09-14 15:59:50 -0400

    update package info?

---
+++ firebase-functions/src/main/java/com/google/firebase/functions/ktx/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * We are merging our firebase-functions-ktx into firebase-functions
+ * under the com.google.firebase.functions package.Therefore, kindly revise your
+ * Gradle dependencies to rely on com.google.firebase:firebase-functions rather than
+ * com.google.firebase:firebase-functions-ktx. Additionally, update your code to replace
+ * all occurrences of firebase-functions-ktx APIs with the relocated APIs in
+ * firebase-functions under the com.google.firebase.functions package."
+ */
+package com.google.firebase.functions.ktx;
