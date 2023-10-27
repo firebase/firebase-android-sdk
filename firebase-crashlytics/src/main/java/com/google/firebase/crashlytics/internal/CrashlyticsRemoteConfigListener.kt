71e5434c32871e0d34d6f851d1df1c0eb41d14dc
Themis wang <themisw@google.com>
2023-10-27 11:04:00 -0400

    refactor code

--- firebase-crashlytics/src/main/java/com/google/firebase/crashlytics/internal/CrashlyticsRemoteConfigListener.kt
+++ firebase-crashlytics/src/main/java/com/google/firebase/crashlytics/internal/CrashlyticsRemoteConfigListener.kt
@@ -1,16 +1,18 @@
-// Copyright 2023 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.google.firebase.crashlytics.internal
 
 import com.google.firebase.crashlytics.internal.metadata.RolloutAssignment
@@ -21,23 +23,17 @@ import com.google.firebase.remoteconfig.interop.rollouts.RolloutsStateSubscriber
 class CrashlyticsRemoteConfigListener(private val userMetadata: UserMetadata) :
   RolloutsStateSubscriber {
   override fun onRolloutsStateChanged(rolloutsState: RolloutsState) {
-    val rolloutAssignmentList = rolloutsState.rolloutAssignments
-
-    val crashlyticsRolloutAssignmentList = mutableListOf<RolloutAssignment>()
-
-    rolloutAssignmentList.map { assignment ->
-      val crashlyticsRolloutAssignment =
+    userMetadata.updateRolloutsState(
+      rolloutsState.rolloutAssignments.map { it ->
         RolloutAssignment.create(
-          assignment.rolloutId,
-          assignment.parameterKey,
-          assignment.parameterValue,
-          assignment.variantId,
-          assignment.templateVersion
+          it.rolloutId,
+          it.parameterKey,
+          it.parameterValue,
+          it.variantId,
+          it.templateVersion
         )
-      crashlyticsRolloutAssignmentList.add(crashlyticsRolloutAssignment)
-    }
-
-    userMetadata.updateRolloutsState(crashlyticsRolloutAssignmentList)
+      }
+    )
     Logger.getLogger().d("Updated Crashlytics Rollout State")
   }
 }
