1476a77669d19cb0662998ec8bf346ec0c72b9f0
Rodrigo Lazo Paz <rlazo@google.com>
2023-10-27 13:00:35 -0400

    Compliance should only be set at Event level

--- transport/transport-backend-cct/src/main/java/com/google/android/datatransport/cct/internal/LogRequest.java
+++ transport/transport-backend-cct/src/main/java/com/google/android/datatransport/cct/internal/LogRequest.java
@@ -48,9 +48,6 @@ public abstract class LogRequest {
   @Nullable
   public abstract QosTier getQosTier();
 
-  @Nullable
-  public abstract ComplianceData getComplianceData();
-
   @NonNull
   public static Builder builder() {
     return new AutoValue_LogRequest.Builder();
@@ -89,9 +86,6 @@ public abstract class LogRequest {
 
     @NonNull
     public abstract Builder setQosTier(@Nullable QosTier value);
-
-    @NonNull
-    public abstract Builder setComplianceData(@Nullable ComplianceData value);
 
     @NonNull
     public abstract LogRequest build();
