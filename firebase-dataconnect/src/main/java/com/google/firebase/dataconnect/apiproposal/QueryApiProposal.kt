33b4e7708a657f73a0dd4e60bd3c87896204d090
Denver Coneybeare <dconeybe@google.com>
2023-11-03 14:14:39 -0400

    QueryApiProposal.kt updated (#510)

--- firebase-dataconnect/src/main/java/com/google/firebase/dataconnect/apiproposal/QueryApiProposal.kt
+++ firebase-dataconnect/src/main/java/com/google/firebase/dataconnect/apiproposal/QueryApiProposal.kt
@@ -2,8 +2,6 @@ package com.google.firebase.dataconnect.apiproposal
 
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
@@ -11,46 +9,60 @@ import kotlinx.coroutines.runBlocking
 // CORE SDK
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-interface FirebaseDataConnect {
-
-  suspend fun executeQuery(
-    operationName: String,
-    operationSet: String,
-    revision: String,
-    variables: Map<String, Any?>
-  ): Map<String, Any?>
-
-  fun subscribe(
-    operationName: String,
-    operationSet: String,
-    revision: String,
-    variables: Map<String, Any?>
-  ): QuerySubscription
+class FirebaseDataConnect {
 
   // NOTE: This interface and val are only needed if the alternative about adding a
   // getPost() method by the generated SDK below is accepted.
-  interface Queries {
-    val dataConnect: FirebaseDataConnect
+  class Queries internal constructor(val dataConnect: FirebaseDataConnect)
+  val queries: Queries = TODO()
+}
+
+open class DataConnectException(message: String) : Exception(message)
+open class ResultDecodeError(message: String) : DataConnectException(message)
+
+abstract class BaseRef<VariablesType, ResultType>(
+  val dataConnect: FirebaseDataConnect,
+  operationName: String,
+  operationSet: String,
+  revision: String,
+  variables: VariablesType
+) {
+  val variables: VariablesType = TODO()
+
+  abstract suspend fun execute(): ResultType
+
+  protected open fun onUpdate() {}
+  
+  protected interface Codec<VariablesType, ResultType> {
+    fun encodeVariables(variables: VariablesType): Map<String, Any?>
+    fun decodeResult(map: Map<String, Any?>): ResultType
   }
-  val queries: Queries
+
+  protected abstract val codec: Codec<VariablesType, ResultType>
 }
 
-interface QuerySubscription {
-  fun reload()
-  fun subscribe(): Flow<Result<Map<String, Any?>>>
-}
+abstract class QueryRef<VariablesType, ResultType>(
+  dataConnect: FirebaseDataConnect,
+  operationName: String,
+  operationSet: String,
+  revision: String,
+  variables: VariablesType
+) :
+  BaseRef<VariablesType, ResultType>(
+    dataConnect = dataConnect,
+    operationName = operationName,
+    operationSet = operationSet,
+    revision = revision,
+    variables = variables
+  ) {
 
-// Implemented by the generated SDK for each `query` defined in graphql
-// Alternative: Delete this interface altogether, because it's only real value is so that
-// customers could have a List<QueryRef<*>> if, say, they wanted to reload() every one.
-interface QueryRef<T> {
   // QUESTION: Should the result of execute() be delivered to the listeners that are subscribed?
   // The current implementation does _not_ because it's difficult to know which is the "latest"
   // result from concurrent calls to execute(). One workaround would be to serialize the calls to
   // execute, but that could introduce unacceptable lag for the invocations at the end of the queue.
-  suspend fun execute(): T
+  override suspend fun execute(): ResultType = TODO()
 
-  fun subscribe(): Flow<Result<T>>
+  fun subscribe(): Flow<Result<ResultType>> = TODO()
 
   // Alternative considered: Return `Deferred<Result<T>>` so that customer knows when the reload
   // completes. For example, suppose a UI has a "Reload" button and when the customer clicks it they
@@ -58,74 +70,27 @@ interface QueryRef<T> {
   // check mark or red "X". Note that simply waiting for a result to be delivered to a Flow isn't
   // sufficient because it's not clear that the result was from the specific call to reload() or
   // some previous call to reload() by some other unrelated operation.
-  fun reload()
+  fun reload(): Unit = TODO()
 
   // Alternative considered: add `lastResult`. The problem is, what do we do with this value if the
   // variables are changed? Do we clear it? Or do we leave it there even though it came from a
   // request with different variables?
-  val lastResult: Result<T>?
-}
-
-// Alternative considered: Make QueryRef and abstract class
-abstract class QueryRefAlt<T>(
-  // Alternative considered: Make `dataConnect` public for convenience
-  private val dataConnect: FirebaseDataConnect,
-  private val operationName: String,
-  private val operationSet: String,
-  private val revision: String,
-  variables: Map<String, Any?>
-) {
-
-  private val variables = variables.toMap()
-  private val subscription: QuerySubscription by lazy {
-    dataConnect.subscribe(
-      operationName = operationName,
-      operationSet = operationSet,
-      revision = revision,
-      variables = variables
-    )
-  }
-
-  fun reload() = subscription.reload()
-
-  protected suspend fun execute(): T =
-    parseResult(
-      dataConnect.executeQuery(
-        operationName = operationName,
-        operationSet = operationSet,
-        revision = revision,
-        variables = variables
-      )
-    )
-
-  fun subscribe(): Flow<Result<T>> = subscription.subscribe().map { it.map(::parseResult) }
-
-  protected abstract fun parseResult(map: Map<String, Any?>): T
+  val lastResult: Result<ResultType>? get() = TODO()
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 // GENERATED SDK
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-interface GetPostQuery : QueryRef<GetPostQuery.Result> {
-  data class Variables(val id: String)
-
-  data class Result(val post: Post) {
-    data class Post(val content: String, val comments: List<Comment>) {
-      data class Comment(val id: String, val content: String)
-    }
-  }
-}
-
-class GetPostQueryAlt
-internal constructor(dataConnect: FirebaseDataConnect, variables: Map<String, Any?>) :
-  QueryRefAlt<GetPostQueryAlt.Result>(
+class GetPostQuery(dataConnect: FirebaseDataConnect, variables: Variables) :
+  QueryRef<GetPostQuery.Variables, GetPostQuery.Result>(
     dataConnect = dataConnect,
     operationName = "getPost",
     operationSet = "crud",
-    revision = "abc123",
-    variables = variables
+    revision = "1234567890abcdef",
+    variables = variables,
   ) {
+
   data class Variables(val id: String)
 
   data class Result(val post: Post) {
@@ -134,10 +99,21 @@ internal constructor(dataConnect: FirebaseDataConnect, variables: Map<String, An
     }
   }
 
-  override fun parseResult(map: Map<String, Any?>): Result = TODO()
+  override val codec = object : Codec<Variables, Result> {
+    override fun encodeVariables(variables: Variables) = TODO()
+    override fun decodeResult(map: Map<String, Any?>) = TODO()
+  }
 }
 
-fun FirebaseDataConnect.query(variables: GetPostQuery.Variables): GetPostQuery = TODO()
+fun FirebaseDataConnect.query(variables: GetPostQuery.Variables): GetPostQuery =
+  GetPostQuery(dataConnect = this, variables = variables)
+
+fun FirebaseDataConnect.Queries.getPost(id: String): GetPostQuery =
+  dataConnect.query(GetPostQuery.Variables(id = id))
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// CUSTOMER CODE
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
 fun demo(dataConnect: FirebaseDataConnect) = runBlocking {
   val query = dataConnect.query(GetPostQuery.Variables(id = "abc"))
@@ -152,14 +128,4 @@ fun demo(dataConnect: FirebaseDataConnect) = runBlocking {
   delay(5000)
   subscribeJob1.cancel()
   subscribeJob2.cancel()
-}
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-// Alternatives Considered
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
-fun FirebaseDataConnect.Queries.getPost(id: String): GetPostQuery = TODO()
-
-suspend fun demoAlt(dataConnect: FirebaseDataConnect) {
-  dataConnect.queries.getPost(id = "abc").execute()
 }
