68a169a075ad825bf2ed6ad27d3e7cfa9fe11bfa
Matthew Robertson <mrober@users.noreply.github.com>
2023-11-06 10:57:50 -0500

    Use new ProcessDetailsProvider to get process name

--- firebase-sessions/src/main/kotlin/com/google/firebase/sessions/SessionDataStoreConfigs.kt
+++ firebase-sessions/src/main/kotlin/com/google/firebase/sessions/SessionDataStoreConfigs.kt
@@ -16,11 +16,7 @@
 
 package com.google.firebase.sessions
 
-import android.app.Application
-import android.os.Build
-import android.os.Process
 import android.util.Base64
-import com.google.android.gms.common.util.ProcessUtils
 import com.google.firebase.sessions.settings.SessionsSettings
 
 /**
@@ -32,7 +28,7 @@ object SessionDataStoreConfigs {
   /** Sanitized process name to use in config filenames. */
   private val PROCESS_NAME =
     Base64.encodeToString(
-      getProcessName().encodeToByteArray(),
+      ProcessDetailsProvider.getProcessName().encodeToByteArray(),
       Base64.NO_WRAP or Base64.URL_SAFE, // URL safe is also filename safe.
     )
 
@@ -41,25 +37,4 @@ object SessionDataStoreConfigs {
 
   /** Config name for [SessionsSettings] */
   val SETTINGS_CONFIGS_NAME = "firebase_session_${PROCESS_NAME}_settings"
-
-  /** Returns the current process name or an empty string if it could not be found. */
-  private fun getProcessName(): String {
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-      return Process.myProcessName()
-    }
-
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-      Application.getProcessName()?.let {
-        return it
-      }
-    }
-
-    // GMS core has different ways to get the process name on old api levels.
-    ProcessUtils.getMyProcessName()?.let {
-      return it
-    }
-
-    // Default to an empty string if nothing works.
-    return ""
-  }
 }
