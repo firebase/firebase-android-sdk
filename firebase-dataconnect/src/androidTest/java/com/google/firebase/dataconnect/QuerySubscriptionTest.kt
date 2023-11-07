2fdf53d001ea291f93182485c0a18fd93d863626
Denver Coneybeare <dconeybe@google.com>
2023-11-06 21:39:07 -0500

    QuerySubscriptionTest.kt: test cases added

--- firebase-dataconnect/src/androidTest/java/com/google/firebase/dataconnect/QuerySubscriptionTest.kt
+++ firebase-dataconnect/src/androidTest/java/com/google/firebase/dataconnect/QuerySubscriptionTest.kt
@@ -19,10 +19,12 @@ package com.google.firebase.dataconnect
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.google.common.truth.Subject
 import com.google.common.truth.Truth.assertThat
+import com.google.common.truth.Truth.assertWithMessage
 import com.google.firebase.dataconnect.testutil.DataConnectLogLevelRule
 import com.google.firebase.dataconnect.testutil.TestDataConnectFactory
 import com.google.firebase.dataconnect.testutil.schemas.*
 import java.util.concurrent.Executors
+import kotlin.time.Duration
 import kotlin.time.Duration.Companion.seconds
 import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.*
@@ -61,7 +63,7 @@ class QuerySubscriptionTest {
   }
 
   @Test
-  fun flow_collect_should_trigger_reload() = runBlocking {
+  fun reload_should_notify_collecting_flows() = runBlocking {
     schema.createPerson.execute(id = "TestId12345", name = "Name0", age = 10000)
     val querySubscription = schema.getPerson.subscribe(id = "TestId12345")
 
@@ -86,7 +88,79 @@ class QuerySubscriptionTest {
   }
 
   @Test
-  fun calling_reload_many_times_concurrently_does_not_cause_issues() = runBlocking {
+  fun flow_collect_should_get_immediately_invoked_with_last_result() = runBlocking {
+    schema.createPerson.execute(id = "TestId12345", name = "TestName", age = 10000)
+    val querySubscription = schema.getPerson.subscribe(id = "TestId12345")
+
+    withTimeout(2.seconds) {
+      val result1 = querySubscription.flow.first().getOrThrow()
+      assertThat(result1).isEqualToGetPersonQueryResult(name = "TestName", age = 10000)
+
+      schema.updatePerson.execute(id = "TestId12345", name = "TestName2", age = 10002)
+
+      val result2 = querySubscription.flow.first().getOrThrow()
+      assertThat(result2).isEqualToGetPersonQueryResult(name = "TestName", age = 10000)
+    }
+  }
+
+  @Test
+  fun slow_flows_do_not_block_fast_flows() = runBlocking {
+    schema.createPerson.execute(id = "TestId12345", name = "TestName", age = 10000)
+    val querySubscription = schema.getPerson.subscribe(id = "TestId12345")
+
+    withTimeout(2.seconds) {
+      val slowFlowJob = launch {
+        querySubscription.flow.collect { delay(Integer.MAX_VALUE.seconds) }
+      }
+
+      repeat(5) {
+        assertWithMessage("fast flow retrieval iteration $it")
+          .that(querySubscription.flow.first().getOrThrow())
+          .isEqualToGetPersonQueryResult(name = "TestName", age = 10000)
+      }
+
+      slowFlowJob.cancel()
+    }
+  }
+
+  @Test
+  fun reload_delivers_result_to_all_registered_flows() = runBlocking {
+    schema.createPerson.execute(id = "TestId12345", name = "TestName0", age = 10000)
+    val querySubscription = schema.getPerson.subscribe(id = "TestId12345")
+
+    withTimeout(2.seconds) {
+      val resultsChannel1 =
+        Channel<PersonSchema.GetPersonQueryRef.Result?>(capacity = Channel.UNLIMITED)
+      val flowJob1 = launch {
+        querySubscription.flow.collect { resultsChannel1.send(it.getOrThrow()) }
+      }
+      val resultsChannel2 =
+        Channel<PersonSchema.GetPersonQueryRef.Result?>(capacity = Channel.UNLIMITED)
+      val flowJob2 = launch {
+        querySubscription.flow.collect { resultsChannel2.send(it.getOrThrow()) }
+      }
+      resultsChannel1.purge(0.25.seconds).forEach {
+        assertThat(it).isEqualToGetPersonQueryResult(name = "TestName0", age = 10000)
+      }
+      resultsChannel2.purge(0.25.seconds).forEach {
+        assertThat(it).isEqualToGetPersonQueryResult(name = "TestName0", age = 10000)
+      }
+
+      schema.updatePerson.execute(id = "TestId12345", name = "TestName1", age = 10001)
+      querySubscription.reload()
+
+      assertThat(resultsChannel1.receive())
+        .isEqualToGetPersonQueryResult(name = "TestName1", age = 10001)
+      assertThat(resultsChannel2.receive())
+        .isEqualToGetPersonQueryResult(name = "TestName1", age = 10001)
+
+      flowJob1.cancel()
+      flowJob2.cancel()
+    }
+  }
+
+  @Test
+  fun reload_concurrent_invocations_get_conflated() = runBlocking {
     schema.createPerson.execute(id = "TestId12345", name = "Name", age = 10000)
     val querySubscription = schema.getPerson.subscribe(id = "TestId12345")
 
@@ -116,5 +190,19 @@ class QuerySubscriptionTest {
   }
 }
 
-fun Subject.isEqualToGetPersonQueryResult(name: String, age: Int?) =
+private fun Subject.isEqualToGetPersonQueryResult(name: String, age: Int?) =
   isEqualTo(PersonSchema.GetPersonQueryRef.Result(name = name, age = age))
+
+private suspend fun <T> ReceiveChannel<T>.purge(timeout: Duration): List<T> = coroutineScope {
+  mutableListOf<T>()
+    .also {
+      while (true) {
+        try {
+          withTimeout(timeout) { it.add(receive()) }
+        } catch (e: TimeoutCancellationException) {
+          break
+        }
+      }
+    }
+    .toList()
+}
