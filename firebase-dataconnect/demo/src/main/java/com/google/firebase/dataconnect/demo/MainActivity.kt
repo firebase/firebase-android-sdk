df831bc5f7eb41e08d82362414fb9ae6b7d054ed
Denver Coneybeare <dconeybe@google.com>
2024-01-25 15:49:09 -0500

    demo: get jetpack compose preview working

--- firebase-dataconnect/demo/src/main/java/com/google/firebase/dataconnect/demo/MainActivity.kt
+++ firebase-dataconnect/demo/src/main/java/com/google/firebase/dataconnect/demo/MainActivity.kt
@@ -4,12 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContent {
-      Text("Hello world!")
+      MessageCard(name = "Denver")
     }
   }
+}
+
+@Composable
+fun MessageCard(name: String) {
+  Text(text = "Hello $name")
+}
+
+@Preview
+@Composable
+fun PreviewMessageCard() {
+  MessageCard(name = "Preview")
 }
