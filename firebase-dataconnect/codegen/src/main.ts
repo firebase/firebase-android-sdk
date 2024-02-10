5db89ebbbf835c39be024771194f7f350b1fd72d
Denver Coneybeare <dconeybe@google.com>
2024-02-10 00:20:23 -0500

    codegen: prepare for writing output file

--- firebase-dataconnect/codegen/src/main.ts
+++ firebase-dataconnect/codegen/src/main.ts
@@ -24,10 +24,38 @@ const GRAPHQL_SCHEMA_FILE =
 const GRAPHQL_OPS_FILE =
   '/home/dconeybe/dev/firebase/android/firebase-dataconnect/src/androidTest' +
   '/assets/testing_graphql_schemas/person/ops.gql';
+const OUTPUT_BASE_DIR =
+  '/home/dconeybe/dev/firebase/android/firebase-dataconnect/src/main/' +
+  'kotlin/com/google/firebase/dataconnect/connectors';
+const CONNECTOR_NAME = 'crud';
+const KOTLIN_BASE_PACKAGE = 'com.google.firebase.dataconnect.connectors';
 
 function main() {
   const types = parseGraphQLTypes();
-  parseGraphQLOperations();
+  const operations = parseGraphQLOperations();
+  generateOperationsKtSources(operations, types);
+}
+
+function generateOperationsKtSources(
+  operations: Map<string, graphql.OperationDefinitionNode>,
+  types: Map<string, graphql.ObjectTypeDefinitionNode>
+): void {
+  const operationNamesSorted = Array.from(operations.keys()).sort();
+  for (const operationName of operationNamesSorted) {
+    const operation = operations.get(operationName)!;
+    generateOperationKtSource(operationName, operation, types);
+  }
+}
+
+function generateOperationKtSource(
+  operationName: string,
+  operation: graphql.OperationDefinitionNode,
+  types: Map<string, graphql.ObjectTypeDefinitionNode>
+): void {
+  const outputDir = `${OUTPUT_BASE_DIR}/${CONNECTOR_NAME}`;
+  const outputFile = `${outputDir}/${operationName}.kt`;
+  console.log(`Creating ${outputFile}`);
+  fs.mkdirSync(outputDir, { recursive: true });
 }
 
 function parseGraphQLTypes(): Map<string, graphql.ObjectTypeDefinitionNode> {
@@ -57,13 +85,26 @@ function parseGraphQLTypes(): Map<string, graphql.ObjectTypeDefinitionNode> {
   return types;
 }
 
-function parseGraphQLOperations() {
+function parseGraphQLOperations(): Map<
+  string,
+  graphql.OperationDefinitionNode
+> {
   const parsedFile = parseGraphQLFile(GRAPHQL_OPS_FILE);
 
-  const types = new Map<string, graphql.ObjectTypeDefinitionNode>();
+  const operations = new Map<string, graphql.OperationDefinitionNode>();
 
   for (const definition of parsedFile.definitions) {
     if (definition.kind === graphql.Kind.OPERATION_DEFINITION) {
+      const operationName = definition.name?.value;
+      if (!operationName) {
+        continue;
+      }
+      if (operations.has(operationName)) {
+        throw new DuplicateGraphQLOperationDefinitionError(
+          `operation defined more than once: ${operationName}`
+        );
+      }
+      operations.set(operationName, definition);
     } else {
       throw new UnsupportedGraphQLDefinitionKindError(
         `unsupported GraphQL definition kind ` +
@@ -72,11 +113,11 @@ function parseGraphQLOperations() {
     }
   }
 
-  return types;
+  return operations;
 }
 
 function parseGraphQLFile(path: string): graphql.DocumentNode {
-  console.log('Parsing file:', path);
+  console.log(`Parsing ${path}`);
   const body = fs.readFileSync(path, { encoding: 'utf-8' });
   const source = new graphql.Source(body, path);
   return graphql.parse(source);
@@ -113,6 +154,12 @@ class UnsupportedGraphQLDefinitionKindError extends Error {
 }
 
 class DuplicateGraphQLTypeDefinitionError extends Error {
+  constructor(message: string) {
+    super(message);
+  }
+}
+
+class DuplicateGraphQLOperationDefinitionError extends Error {
   constructor(message: string) {
     super(message);
   }
