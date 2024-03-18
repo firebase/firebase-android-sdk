0cae73e3e99cbcb82314a241e390e920e9c6119e
Denver Coneybeare <dconeybe@google.com>
2024-03-18 15:08:16 -0400

    TurbineUtils.kt: add +@CheckReturnValue to ReceiveTurbine<T>.skipItemsWhere()

--- firebase-dataconnect/androidTestutil/src/main/kotlin/com/google/firebase/dataconnect/testutil/TurbineUtils.kt
+++ firebase-dataconnect/androidTestutil/src/main/kotlin/com/google/firebase/dataconnect/testutil/TurbineUtils.kt
@@ -1,7 +1,9 @@
 package com.google.firebase.dataconnect.testutil
 
 import app.cash.turbine.ReceiveTurbine
+import javax.annotation.CheckReturnValue
 
+@CheckReturnValue
 suspend fun <T> ReceiveTurbine<T>.skipItemsWhere(predicate: (T) -> Boolean): T {
   while (true) {
     val item = awaitItem()
