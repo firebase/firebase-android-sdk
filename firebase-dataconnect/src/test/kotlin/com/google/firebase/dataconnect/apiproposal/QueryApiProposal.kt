1446408aa2fab8c957a91f5acfa5ffc65a83815d
cherylEnkidu <cheryllin@google.com>
2024-03-19 11:45:28 -0400

    Fix format

--- firebase-dataconnect/src/test/kotlin/com/google/firebase/dataconnect/apiproposal/QueryApiProposal.kt
+++ firebase-dataconnect/src/test/kotlin/com/google/firebase/dataconnect/apiproposal/QueryApiProposal.kt
@@ -1,6 +1,6 @@
 @file:Suppress(
   "LocalVariableName",
-  "MemberVisibilityCanBePrivate"
+  "MemberVisibilityCanBePrivate",
   "RedundantSuspendModifier",
   "RemoveSetterParameterType",
   "unused",
