e8df506a7d446a3c2aac09ca11e19f24eebd1b87
Rodrigo Lazo Paz <rlazo@google.com>
2024-04-10 10:13:18 -0400

    Update vertexai's common dependency
    Bump to 0.2.0

--- firebase-vertexai/firebase-vertexai.gradle.kts
+++ firebase-vertexai/firebase-vertexai.gradle.kts
@@ -52,7 +52,7 @@ dependencies {
   implementation("com.google.firebase:firebase-components:17.1.5")
   implementation("com.google.firebase:firebase-annotations:16.2.0")
   implementation("com.google.firebase:firebase-appcheck-interop:17.1.0")
-  implementation("com.google.ai.client.generativeai:common:0.1.0")
+  implementation("com.google.ai.client.generativeai:common:0.2.0")
   implementation(libs.androidx.annotation)
   implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
   implementation("androidx.core:core-ktx:1.12.0")
