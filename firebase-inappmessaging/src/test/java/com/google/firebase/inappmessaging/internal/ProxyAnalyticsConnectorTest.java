cc4b74b0082fd0e6f6926ca49e7ede7f2d26b437
David Motsonashvili <davidmotson@google.com>
2024-04-16 11:17:41 -0700

    upgrade mockito in inappmessaging

--- firebase-inappmessaging/src/test/java/com/google/firebase/inappmessaging/internal/ProxyAnalyticsConnectorTest.java
+++ firebase-inappmessaging/src/test/java/com/google/firebase/inappmessaging/internal/ProxyAnalyticsConnectorTest.java
@@ -15,7 +15,7 @@
 package com.google.firebase.inappmessaging.internal;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Matchers.anyString;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
