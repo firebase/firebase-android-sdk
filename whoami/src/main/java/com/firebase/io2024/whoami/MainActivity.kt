f3dfb83166af580d57d7f7bfcbb831261055fbbb
Viswanathan Munisamy <visumickey@gmail.com>
2024-05-02 03:15:13 -0700

    Make the visual appearance better with different buttons for fetch and parse.

--- whoami/src/main/java/com/firebase/io2024/whoami/MainActivity.kt
+++ whoami/src/main/java/com/firebase/io2024/whoami/MainActivity.kt
@@ -2,7 +2,6 @@ package com.firebase.io2024.whoami
 
 import android.os.Build
 import android.os.Bundle
-import android.provider.Settings
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,7 +9,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -20,6 +18,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
@@ -30,40 +30,49 @@ import com.firebase.io2024.whoami.ui.theme.WhoAmITheme
 import com.google.firebase.Firebase
 import com.google.firebase.vertexai.type.generationConfig
 import com.google.firebase.vertexai.vertexAI
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 import org.json.JSONObject
 
 
 class MainActivity : ComponentActivity() {
+  private val deviceInfo = MutableStateFlow("Device details information here...!!")
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
 
     setContent {
       WhoAmITheme {
+        val info by deviceInfo.collectAsState()
         // A surface container using the 'background' color from the theme
         Surface(modifier = Modifier.fillMaxWidth(), color = MaterialTheme.colorScheme.background) {
           Column(Modifier.fillMaxHeight(), horizontalAlignment = Alignment.CenterHorizontally) {
             Spacer(modifier = Modifier.height(32.dp)) // Add space between button and text
-            Button(onClick = { promptAi("") }) {
+            Button(onClick = {
+              promptAi { newInfo ->
+                deviceInfo.value = newInfo.toString()
+              }
+            }) {
               Text("Current Device", modifier = Modifier.width(IntrinsicSize.Max), textAlign = TextAlign.Center)
             }
 
             Spacer(modifier = Modifier.height(32.dp)) // Add space between button and text
-            Text(text = "Device Info will be displayed here", modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp))
+            Text(text = info, modifier = Modifier.fillMaxWidth().fillMaxHeight(0.8f).padding(horizontal = 16.dp))
+            Spacer(modifier = Modifier.height(32.dp)) // Add space between button and text
+            Button(onClick = { parseContent() }) {
+              Text("Parse Content", modifier = Modifier.width(IntrinsicSize.Max), textAlign = TextAlign.Center)
+            }
           }
-//          Button(
-//            onClick = {
-//              promptAi("")
-//            }
-//          ) {
-//            Text(text = "Current Device", modifier = Modifier.fillMaxWidth())
-//          }
         }
       }
     }
   }
 
-  fun promptAi(query: String) =
+  private fun parseContent() {
+    val jsonObject = JSONObject(deviceInfo.value)
+    jsonObject.getString("name")
+  }
+
+  private fun promptAi(callback: (Any?) -> Unit) =
     lifecycleScope.launch {
       val generativeModel =
         Firebase.vertexAI.generativeModel(
@@ -71,15 +80,15 @@ class MainActivity : ComponentActivity() {
           generationConfig = generationConfig { temperature = 0.7f },
         )
 
+      deviceInfo.value = "Fetching device details"
       val manufacturer = Build.MANUFACTURER
       val deviceName = Build.MODEL// Settings.Global.getString(contentResolver, Settings.Global.DEVICE_NAME)
-      val deviceDetailsPrompt = "Give me details about the phone ${manufacturer} ${deviceName} in JSON format without adding any extra details outside of json. Include information about the features that are available on the device including camera, bluetooth, size, weight and battery"
+      val deviceDetailsPrompt = "Give me details about the phone ${manufacturer} ${deviceName} in JSON format without adding any extra details outside of json. Include information about the features that are available on the device including camera, bluetooth, size, weight and battery. Respond as just a plain json string without any markups."
       val result = generativeModel.generateContent(deviceDetailsPrompt)
 
       Log.i("TAG", "find me Prompt: ${deviceDetailsPrompt}")
       Log.i("TAG", "find me Response: ${result.text}")
-      val jsonObject = JSONObject(result.text)
-      jsonObject.getString("name")
+      callback(result.text.toString())
     }
 }
 
