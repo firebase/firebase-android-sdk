e25efeb010662cc2e0060e09338c112db0cf0515
Denver Coneybeare <dconeybe@google.com>
2024-05-03 14:14:34 -0400

    emulator_noauth.sh: add back -local_connection_string since running the cli directly does not appear to pick up the value from .firebaserc

--- firebase-dataconnect/emulator/emulator_noauth.sh
+++ firebase-dataconnect/emulator/emulator_noauth.sh
@@ -10,6 +10,7 @@ readonly CLI_ARGS=(
   dev
   -disable_sdk_generation=true
   -config_dir=dataconnect
+  -local_connection_string='postgresql://postgres:postgres@localhost:5432?sslmode=disable'
 )
 
 echo "[$0] Running command: ${CLI_ARGS[*]}"
