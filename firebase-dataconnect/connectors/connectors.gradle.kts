22b111d1712108c084a6ffcc00d220486fbda463
Denver Coneybeare <dconeybe@google.com>
2024-05-06 15:57:29 -0400

    generate the code into the 'build' directory

--- firebase-dataconnect/connectors/connectors.gradle.kts
+++ firebase-dataconnect/connectors/connectors.gradle.kts
@@ -34,6 +34,11 @@ android {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
   }
+  sourceSets {
+    getByName("main") {
+      kotlin.srcDirs("build/generated/source/dataconnect")
+    }
+  }
   kotlinOptions { jvmTarget = "1.8" }
 }
 
