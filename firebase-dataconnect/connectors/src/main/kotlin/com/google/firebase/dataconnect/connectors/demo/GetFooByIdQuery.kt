4c1940aa8a4de65a3590ef54fd8e23a280bf3a43
Denver Coneybeare <dconeybe@google.com>
2024-05-06 14:22:23 -0400

    Add copyright notice to generated code (b/336814936) (#561)

--- firebase-dataconnect/connectors/src/main/kotlin/com/google/firebase/dataconnect/connectors/demo/GetFooByIdQuery.kt
+++ firebase-dataconnect/connectors/src/main/kotlin/com/google/firebase/dataconnect/connectors/demo/GetFooByIdQuery.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 @file:Suppress(
   "KotlinRedundantDiagnosticSuppress",
   "LocalVariableName",
@@ -69,10 +85,12 @@ public fun GetFooByIdQuery.flow(
     .subscribe()
     .flow
 
+
 // The lines below are used by the code generator to ensure that this file is deleted if it is no
 // longer needed. Any files in this directory that contain the lines below will be deleted by the
 // code generator if the file is no longer needed. If, for some reason, you do _not_ want the code
 // generator to delete this file, then remove the line below (and this comment too, if you want).
+
 
 // FIREBASE_DATA_CONNECT_GENERATED_FILE MARKER 42da5e14-69b3-401b-a9f1-e407bee89a78
 // FIREBASE_DATA_CONNECT_GENERATED_FILE CONNECTOR demo
