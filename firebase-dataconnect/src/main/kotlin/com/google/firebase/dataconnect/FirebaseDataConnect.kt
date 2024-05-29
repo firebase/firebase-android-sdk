58e1ebd3ac7e243878e5ddba6bea02d6788059ff
Denver Coneybeare <dconeybe@google.com>
2024-05-28 21:46:55 -0400

    FirebaseDataConnect.kt: set release date for 16.0.0-alpha04 to May 29, 2024

--- firebase-dataconnect/src/main/kotlin/com/google/firebase/dataconnect/FirebaseDataConnect.kt
+++ firebase-dataconnect/src/main/kotlin/com/google/firebase/dataconnect/FirebaseDataConnect.kt
@@ -85,7 +85,7 @@ import kotlinx.serialization.SerializationStrategy
  * into the `master` branch of https://github.com/firebase/firebase-android-sdk, at which point the
  * release notes will become part of the regular Android SDK releases.
  *
- * ##### 16.0.0-alpha04 (not yet released)
+ * ##### 16.0.0-alpha04 (May 29, 2024)
  * - Fixed time zone issues when serializing java.util.Date objects (
  * [#5976](https://github.com/firebase/firebase-android-sdk/pull/5976))
  * - Changed default port of useEmulator() to 9399 (was 9510); this goes with a change to the Data
