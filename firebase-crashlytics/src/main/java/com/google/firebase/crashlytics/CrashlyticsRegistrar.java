7849d5618c8902d754829e3603f0de8bfa1ee9d0
Themis wang <themisw@google.com>
2024-06-06 14:37:35 -0400

    remove unused imports

--- firebase-crashlytics/src/main/java/com/google/firebase/crashlytics/CrashlyticsRegistrar.java
+++ firebase-crashlytics/src/main/java/com/google/firebase/crashlytics/CrashlyticsRegistrar.java
@@ -32,7 +32,6 @@ import com.google.firebase.sessions.api.FirebaseSessionsDependencies;
 import com.google.firebase.sessions.api.SessionSubscriber;
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 
 /** @hide */
