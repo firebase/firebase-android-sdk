8148b58290076cc6a9c00c009bb8d6092c472785
Denver Coneybeare <dconeybe@google.com>
2024-06-12 15:34:53 +0000

    Move DataConnectAuthImpl.kt into DataConnectAuth.kt

--- firebase-dataconnect/src/main/kotlin/com/google/firebase/dataconnect/core/DataConnectAuth.kt
+++ firebase-dataconnect/src/main/kotlin/com/google/firebase/dataconnect/core/DataConnectAuth.kt
@@ -16,13 +16,143 @@
 
 package com.google.firebase.dataconnect.core
 
-import kotlinx.coroutines.*
-import kotlinx.coroutines.sync.*
-import kotlinx.coroutines.tasks.*
+import com.google.firebase.auth.internal.IdTokenListener
+import com.google.firebase.auth.internal.InternalAuthProvider
+import com.google.firebase.dataconnect.util.nextSequenceNumber
+import com.google.firebase.internal.InternalTokenResult
+import com.google.firebase.internal.api.FirebaseNoSignedInUserException
+import java.util.concurrent.Executor
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+import kotlinx.coroutines.tasks.await
 
 internal interface DataConnectAuth {
 
   suspend fun getAccessToken(requestId: String): String?
+}
+
+internal class DataConnectAuthImpl(
+  deferredAuthProvider: com.google.firebase.inject.Deferred<InternalAuthProvider>,
+  blockingExecutor: Executor,
+  parentLogger: Logger,
+) : DataConnectAuth {
+  private val logger =
+    Logger("DataConnectAuthImpl").apply { debug { "Created by ${parentLogger.nameWithId}" } }
+
+  private val idTokenListener = IdTokenListener { runBlocking { onIdTokenChanged(it) } }
+
+  private val mutex = Mutex()
+  private var closed = false
+  private var authProvider: InternalAuthProvider? = null
+  private var tokenSequenceNumber = nextSequenceNumber()
+
+  init {
+    // Run `whenAvailable()` on a background thread because it accesses SharedPreferences, which
+    // performs disk i/o, violating the StrictMode policy `android.os.strictmode.DiskReadViolation`.
+    blockingExecutor.execute {
+      deferredAuthProvider.whenAvailable {
+        runBlocking { onInternalAuthProviderAvailable(it.get()) }
+      }
+    }
+  }
+
+  suspend fun close() {
+    logger.debug { "close()" }
+    mutex.withLock {
+      closed = true
+      authProvider?.removeIdTokenListener(idTokenListener)
+      authProvider = null
+    }
+  }
+
+  override suspend fun getAccessToken(requestId: String): String? {
+    while (true) {
+      val (capturedAuthProvider, capturedTokenSequenceNumber) =
+        mutex.withLock {
+          val capturedAuthProvider = authProvider
+          if (closed) {
+            logger.debug {
+              "[rid=$requestId] DataConnectAuth is closed; returning null access token"
+            }
+            return null
+          } else if (capturedAuthProvider === null) {
+            logger.debug {
+              "[rid=$requestId] FirebaseAuth is not (yet?) available; " +
+                "returning null access token"
+            }
+            return null
+          }
+          Pair(capturedAuthProvider, tokenSequenceNumber)
+        }
+
+      val accessTokenResult = capturedAuthProvider.runCatching { getAccessToken(false).await() }
+
+      val tokenSequenceNumberChanged =
+        mutex.withLock { capturedTokenSequenceNumber != tokenSequenceNumber }
+      if (tokenSequenceNumberChanged) {
+        logger.debug { "[rid=$requestId] token sequence number changed during fetch; re-fetching" }
+        continue
+      }
+
+      if (accessTokenResult.isSuccess) {
+        val accessToken = accessTokenResult.getOrNull()?.token
+        if (accessToken === null) {
+          logger.debug {
+            "[rid=$requestId] returning null access token as provided by FirebaseAuth"
+          }
+          return null
+        }
+        logger.debug { "[rid=$requestId] got access token: ${accessToken.toScrubbedAccessToken()}" }
+        return accessToken
+      }
+
+      val exception = accessTokenResult.exceptionOrNull()!!
+      if (exception is FirebaseNoSignedInUserException) {
+        logger.debug {
+          "[rid=$requestId] FirebaseAuth has no signed-in user; returning null access token"
+        }
+        return null
+      }
+
+      logger.warn(exception) {
+        "[rid=$requestId] getting access token failed for an unknown reason"
+      }
+      throw exception
+    }
+  }
+
+  private suspend fun onInternalAuthProviderAvailable(newAuthProvider: InternalAuthProvider) =
+    mutex.withLock {
+      val newId = System.identityHashCode(newAuthProvider)
+
+      if (closed) {
+        return
+      }
+      if (authProvider === newAuthProvider) {
+        return
+      }
+      if (authProvider !== null) {
+        val oldId = System.identityHashCode(authProvider)
+        logger.warn {
+          "WARNING: Deferred<InternalAuthProvider>.whenAvailable() callback " +
+            "was invoked multiple times, first with $oldId and then with $newId; " +
+            "this is unexpected; ignoring $newId and continuing to use $oldId."
+        }
+        return
+      }
+
+      logger.debug { "onInternalAuthProviderAvailable($newId) setting InternalAuthProvider" }
+      authProvider = newAuthProvider
+      newAuthProvider.addIdTokenListener(idTokenListener)
+    }
+
+  private suspend fun onIdTokenChanged(result: InternalTokenResult) {
+    logger.debug {
+      "onIdTokenChanged(): token=${result.token?.uppercase()?.toScrubbedAccessToken()}"
+    }
+    mutex.withLock { tokenSequenceNumber = nextSequenceNumber() }
+  }
 }
 
 /**
