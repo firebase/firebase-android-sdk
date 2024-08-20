873d08cd1c789d600012a64ba6cadbb9ee806a4c
Themis wang <themisw@google.com>
2024-08-20 16:54:18 -0400

    add internal

--- firebase-crashlytics/src/main/java/com/google/firebase/crashlytics/internal/model/InternalTracingMetrics.kt
+++ firebase-crashlytics/src/main/java/com/google/firebase/crashlytics/internal/model/InternalTracingMetrics.kt
@@ -1,6 +1,6 @@
 package com.google.firebase.crashlytics.internal.model
 
-class InternalTracingMetrics {
+internal class InternalTracingMetrics {
   companion object InternalMetrics {
     var metrics = mutableMapOf<String, String>()
     fun put(key: String, value: String) {
