5e08c71e29d61846c24330487b61993c35bc36f5
Rodrigo Lazo <rlazo@users.noreply.github.com>
2024-09-25 11:39:20 -0400

    Update firebase-vertexai/src/main/kotlin/com/google/firebase/vertexai/common/server/Types.kt
    Co-authored-by: Daymon <17409137+daymxn@users.noreply.github.com>

--- firebase-vertexai/src/main/kotlin/com/google/firebase/vertexai/common/server/Types.kt
+++ firebase-vertexai/src/main/kotlin/com/google/firebase/vertexai/common/server/Types.kt
@@ -82,7 +82,9 @@ internal data class Date(
   * Year of the date. Must be between 1 and 9999, or 0 for no year.
   */
   val year: Int? = null,
-  // 1-based index for month. Must be from 1 to 12, or 0 to specify a year without a month.
+  /**
+   * 1-based index for month. Must be from 1 to 12, or 0 to specify a year without a month.
+   */
   val month: Int? = null,
   // Day of a month. Must be from 1 to 31 and valid for the year and month, or 0
   // to specify a year by itself or a year and month where the day isn't
