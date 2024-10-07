7993a4002b035d19f919d9bb1f99323c9d93845f
Denver Coneybeare <dconeybe@google.com>
2024-10-07 15:20:52 +0000

    gradleplugin/plugin/build.gradle.kts: remove unnecssary whitespace-only change

--- firebase-dataconnect/gradleplugin/plugin/build.gradle.kts
+++ firebase-dataconnect/gradleplugin/plugin/build.gradle.kts
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 plugins {
   `java-gradle-plugin`
   alias(firebaseLibs.plugins.kotlin.jvm)
