8e5e9551463215bc95d8c5addb00f36c022a47d3
Tejas Deshpande <tdeshpande@google.com>
2024-12-05 16:47:12 -0500

    Add a kotlin method for recording an exception with custom keys (#6546)

--- firebase-crashlytics/src/main/java/com/google/firebase/crashlytics/FirebaseCrashlytics.kt
+++ firebase-crashlytics/src/main/java/com/google/firebase/crashlytics/FirebaseCrashlytics.kt
@@ -30,6 +30,10 @@ val Firebase.crashlytics: FirebaseCrashlytics
 fun FirebaseCrashlytics.setCustomKeys(init: KeyValueBuilder.() -> Unit) =
   setCustomKeys(KeyValueBuilder().apply(init).build())
 
+/** Records a non-fatal report to send to Crashlytics with additional custom keys */
+fun FirebaseCrashlytics.recordException(throwable: Throwable, init: KeyValueBuilder.() -> Unit) =
+  recordException(throwable, KeyValueBuilder().apply(init).build())
+
 /** @suppress */
 @Keep
 internal class FirebaseCrashlyticsKtxRegistrar : ComponentRegistrar {
