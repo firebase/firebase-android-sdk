577a44f731857ba84857db702bd2bff49d41e419
Daymon <daymxn@google.com>
2025-01-21 10:34:46 -0600

    Add context receivers to plugins

--- plugins/build.gradle.kts
+++ plugins/build.gradle.kts
@@ -40,6 +40,8 @@ spotless {
   }
 }
 
+kotlin { compilerOptions { freeCompilerArgs.add("-Xcontext-receivers") } }
+
 // Refer latest "perf-plugin" released version on
 // https://maven.google.com/web/index.html?q=perf-plugin#com.google.firebase:perf-plugin
 // The System property allows us to integrate with an unreleased version from https://bityl.co/3oYt.
