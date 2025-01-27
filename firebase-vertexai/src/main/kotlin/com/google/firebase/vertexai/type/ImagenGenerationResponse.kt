a554fbae1609e20d5db3a9f1a958d9fcbb933e4b
David Motsonashvili <davidmotson@google.com>
2025-01-27 12:08:02 -0800

    hide constructor for ImagenGenerationResponse

--- firebase-vertexai/src/main/kotlin/com/google/firebase/vertexai/type/ImagenGenerationResponse.kt
+++ firebase-vertexai/src/main/kotlin/com/google/firebase/vertexai/type/ImagenGenerationResponse.kt
@@ -1,6 +1,4 @@
 package com.google.firebase.vertexai.type
 
-public class ImagenGenerationResponse<T>(
-  public val images: List<T>,
-  public val filteredReason: String?,
-) {}
+public class ImagenGenerationResponse<T>
+internal constructor(public val images: List<T>, public val filteredReason: String?) {}
