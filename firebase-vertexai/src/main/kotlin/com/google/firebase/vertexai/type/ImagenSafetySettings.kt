0ee91ac7e66e1d1ce8d7529ec9db0fd32941c836
David Motsonashvili <davidmotson@google.com>
2025-01-28 09:48:34 -0800

    remove internal constructor on ImagenSafetySettings

--- firebase-vertexai/src/main/kotlin/com/google/firebase/vertexai/type/ImagenSafetySettings.kt
+++ firebase-vertexai/src/main/kotlin/com/google/firebase/vertexai/type/ImagenSafetySettings.kt
@@ -1,7 +1,6 @@
 package com.google.firebase.vertexai.type
 
-public class ImagenSafetySettings
-internal constructor(
+public class ImagenSafetySettings(
   internal val safetyFilterLevel: ImagenSafetyFilterLevel,
   internal val personFilterLevel: ImagenPersonFilterLevel,
 ) {}
