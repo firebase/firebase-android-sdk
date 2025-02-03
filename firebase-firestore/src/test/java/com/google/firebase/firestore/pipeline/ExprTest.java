7f95bb5b907d41a1b35dd7ba39c391c5e0ded344
Tom Andersen <tom-andersen@users.noreply.github.com>
2025-02-03 11:34:59 -0500

    Test

---
+++ firebase-firestore/src/test/java/com/google/firebase/firestore/pipeline/ExprTest.java
@@ -0,0 +1,16 @@
+package com.google.firebase.firestore.pipeline;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(manifest = Config.NONE)
+public class ExprTest {
+
+  @Test
+  public void test() {
+
+  }
+}
---
+++ firebase-firestore/src/test/java/com/google/firebase/firestore/pipeline/StageTest.java
@@ -0,0 +1,16 @@
+package com.google.firebase.firestore.pipeline;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(manifest = Config.NONE)
+public class StageTest {
+
+  @Test
+  public void test() {
+
+  }
+}
