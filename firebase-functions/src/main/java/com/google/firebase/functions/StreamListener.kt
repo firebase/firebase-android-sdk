895ee023d481e4a268a120e3dc4782b3897bb294
mustafajadid <jaditmustafa@gmail.com>
2025-02-03 23:01:33 -0800

    Remove StreamListener.kt and StreamFunctionsTask.kt

--- firebase-functions/src/main/java/com/google/firebase/functions/StreamListener.kt
+++
@@ -1,8 +0,0 @@
-package com.google.firebase.functions
-
-/** Listener for events from a Server-Sent Events stream. */
-public interface StreamListener {
-
-  /** Called when a new event is received. */
-  public fun onNext(message: Any)
-}
