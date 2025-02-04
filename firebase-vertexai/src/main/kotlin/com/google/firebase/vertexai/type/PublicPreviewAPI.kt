ef4dbf2152ba3404ead81dff500c8ba618f28910
David Motsonashvili <davidmotson@gmail.com>
2025-02-04 13:52:42 -0800

    Update firebase-vertexai/src/main/kotlin/com/google/firebase/vertexai/type/PublicPreviewAPI.kt
    Co-authored-by: Daymon <17409137+daymxn@users.noreply.github.com>

--- firebase-vertexai/src/main/kotlin/com/google/firebase/vertexai/type/PublicPreviewAPI.kt
+++ firebase-vertexai/src/main/kotlin/com/google/firebase/vertexai/type/PublicPreviewAPI.kt
@@ -20,7 +20,7 @@ package com.google.firebase.vertexai.type
 @RequiresOptIn(
   level = RequiresOptIn.Level.ERROR,
   message =
-    "This API is currently experimental and in public preview and may change in behavior in " +
+    "This API is part of an experimental public preview and may change in " +
       "backwards-incompatible ways without notice.",
 )
 public annotation class PublicPreviewAPI()
