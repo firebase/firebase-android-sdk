339cd2bb04776725c153697d8a8ac5a414519d35
Tejas Deshpande <tdeshpande@google.com>
2025-02-07 14:58:37 -0500

    Remove the logging of GaugeMetadata to allow using AQS (#6678)
    Based on the behaviour of AQS w/ Fireperf, an AQS session isn't
    available when (currently) logging gauge metadata.
    
    Changes:
    - Remove the current logging of gauge metadata - will be re-introduced
    in a future PR.
    - Switch Gauge collection from `scheduleAtFixedRate` to
    `scheduleWithFixedDelay`. As
    [documented](https://stackoverflow.com/a/78405653), this *should*
    prevent a potentially large amounts of gauge collection if a process is
    cached, and then restored during a verbose session - which *should* make
    it work better w/ AQS.
    - Remove API restricted behaviour which is no longer relevant.

--- firebase-perf/src/main/java/com/google/firebase/perf/session/gauges/GaugeMetadataManager.java
+++ firebase-perf/src/main/java/com/google/firebase/perf/session/gauges/GaugeMetadataManager.java
@@ -17,18 +17,11 @@ package com.google.firebase.perf.session.gauges;
 import android.app.ActivityManager;
 import android.app.ActivityManager.MemoryInfo;
 import android.content.Context;
-import android.os.Build.VERSION;
-import android.os.Build.VERSION_CODES;
 import androidx.annotation.VisibleForTesting;
 import com.google.firebase.perf.logging.AndroidLogger;
 import com.google.firebase.perf.util.StorageUnit;
 import com.google.firebase.perf.util.Utils;
 import com.google.firebase.perf.v1.GaugeMetadata;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * The {@code GaugeMetadataManager} class is responsible for collecting {@link GaugeMetadata}
@@ -41,7 +34,6 @@ class GaugeMetadataManager {
   private final Runtime runtime;
   private final ActivityManager activityManager;
   private final MemoryInfo memoryInfo;
-  private final Context appContext;
 
   GaugeMetadataManager(Context appContext) {
     this(Runtime.getRuntime(), appContext);
@@ -50,7 +42,6 @@ class GaugeMetadataManager {
   @VisibleForTesting
   GaugeMetadataManager(Runtime runtime, Context appContext) {
     this.runtime = runtime;
-    this.appContext = appContext;
     this.activityManager = (ActivityManager) appContext.getSystemService(Context.ACTIVITY_SERVICE);
     memoryInfo = new ActivityManager.MemoryInfo();
     activityManager.getMemoryInfo(memoryInfo);
@@ -75,29 +66,6 @@ class GaugeMetadataManager {
 
   /** Returns the total memory (in kilobytes) accessible by the kernel (called the RAM size). */
   public int getDeviceRamSizeKb() {
-    if (VERSION.SDK_INT >= VERSION_CODES.JELLY_BEAN) {
-      return Utils.saturatedIntCast(StorageUnit.BYTES.toKilobytes(memoryInfo.totalMem));
-    }
-
-    return readTotalRAM(/* procFileName= */ "/proc/meminfo");
-  }
-
-  /** Returns the total ram size of the device (in kilobytes) by reading the "proc/meminfo" file. */
-  @VisibleForTesting
-  int readTotalRAM(String procFileName) {
-    try (BufferedReader br = new BufferedReader(new FileReader(procFileName))) {
-      for (String s = br.readLine(); s != null; s = br.readLine()) {
-        if (s.startsWith("MemTotal")) {
-          Matcher m = Pattern.compile("\\d+").matcher(s);
-          return m.find() ? Integer.parseInt(m.group()) : 0;
-        }
-      }
-    } catch (IOException ioe) {
-      logger.warn("Unable to read '" + procFileName + "' file: " + ioe.getMessage());
-    } catch (NumberFormatException nfe) {
-      logger.warn("Unable to parse '" + procFileName + "' file: " + nfe.getMessage());
-    }
-
-    return 0;
+    return Utils.saturatedIntCast(StorageUnit.BYTES.toKilobytes(memoryInfo.totalMem));
   }
 }
