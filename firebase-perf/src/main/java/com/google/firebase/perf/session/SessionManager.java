b259d9ead9add42d5a0490fb4cf7ea46d2f47a00
Tejas Deshpande <tdeshpande@google.com>
2025-02-11 13:06:38 -0500

    Implement a SessionSubscriber for Firebase Performance (#6683)
    This PR doesn't change the use of session ID to AQS - except in
    GaugeMetadata. I've added TODOs to identify the missing locations.

--- firebase-perf/src/main/java/com/google/firebase/perf/session/SessionManager.java
+++ firebase-perf/src/main/java/com/google/firebase/perf/session/SessionManager.java
@@ -19,7 +19,6 @@ import android.content.Context;
 import androidx.annotation.Keep;
 import androidx.annotation.VisibleForTesting;
 import com.google.firebase.perf.application.AppStateMonitor;
-import com.google.firebase.perf.application.AppStateUpdateHandler;
 import com.google.firebase.perf.session.gauges.GaugeManager;
 import com.google.firebase.perf.v1.ApplicationProcessState;
 import com.google.firebase.perf.v1.GaugeMetadata;
@@ -27,15 +26,13 @@ import com.google.firebase.perf.v1.GaugeMetric;
 import java.lang.ref.WeakReference;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Objects;
 import java.util.Set;
 import java.util.UUID;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
 /** Session manager to generate sessionIDs and broadcast to the application. */
 @Keep // Needed because of b/117526359.
-public class SessionManager extends AppStateUpdateHandler {
+public class SessionManager {
 
   @SuppressLint("StaticFieldLeak")
   private static final SessionManager instance = new SessionManager();
@@ -45,7 +42,6 @@ public class SessionManager extends AppStateUpdateHandler {
   private final Set<WeakReference<SessionAwareObject>> clients = new HashSet<>();
 
   private PerfSession perfSession;
-  private Future syncInitFuture;
 
   /** Returns the singleton instance of SessionManager. */
   public static SessionManager getInstance() {
@@ -71,7 +67,6 @@ public class SessionManager extends AppStateUpdateHandler {
     this.gaugeManager = gaugeManager;
     this.perfSession = perfSession;
     this.appStateMonitor = appStateMonitor;
-    registerForAppState();
   }
 
   /**
@@ -79,42 +74,7 @@ public class SessionManager extends AppStateUpdateHandler {
    * (currently that is before onResume finishes) to ensure gauge collection starts on time.
    */
   public void setApplicationContext(final Context appContext) {
-    // TODO(b/258263016): Migrate to go/firebase-android-executors
-    @SuppressLint("ThreadPoolCreation")
-    ExecutorService executorService = Executors.newSingleThreadExecutor();
-    syncInitFuture =
-        executorService.submit(
-            () -> {
-              gaugeManager.initializeGaugeMetadataManager(appContext);
-            });
-  }
-
-  @Override
-  public void onUpdateAppState(ApplicationProcessState newAppState) {
-    super.onUpdateAppState(newAppState);
-
-    if (appStateMonitor.isColdStart()) {
-      // We want the Session to remain unchanged if this is a cold start of the app since we already
-      // update the PerfSession in FirebasePerfProvider#onAttachInfo().
-      return;
-    }
-
-    if (newAppState == ApplicationProcessState.FOREGROUND) {
-      // A new foregrounding of app will force a new sessionID generation.
-      PerfSession session = PerfSession.createWithId(UUID.randomUUID().toString());
-      updatePerfSession(session);
-    } else {
-      // If the session is running for too long, generate a new session and collect gauges as
-      // necessary.
-      if (perfSession.isSessionRunningTooLong()) {
-        PerfSession session = PerfSession.createWithId(UUID.randomUUID().toString());
-        updatePerfSession(session);
-      } else {
-        // For any other state change of the application, modify gauge collection state as
-        // necessary.
-        startOrStopCollectingGauges(newAppState);
-      }
-    }
+    gaugeManager.initializeGaugeMetadataManager(appContext);
   }
 
   /**
@@ -138,7 +98,7 @@ public class SessionManager extends AppStateUpdateHandler {
    */
   public void updatePerfSession(PerfSession perfSession) {
     // Do not update the perf session if it is the exact same sessionId.
-    if (perfSession.sessionId() == this.perfSession.sessionId()) {
+    if (Objects.equals(perfSession.sessionId(), this.perfSession.sessionId())) {
       return;
     }
 
@@ -206,10 +166,5 @@ public class SessionManager extends AppStateUpdateHandler {
   @VisibleForTesting
   public void setPerfSession(PerfSession perfSession) {
     this.perfSession = perfSession;
-  }
-
-  @VisibleForTesting
-  public Future getSyncInitFuture() {
-    return this.syncInitFuture;
   }
 }
