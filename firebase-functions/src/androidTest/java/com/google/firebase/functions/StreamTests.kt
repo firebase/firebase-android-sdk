ca17db7f793090c369f8e650e920281699ea3aa9
mustafajadid <jaditmustafa@gmail.com>
2025-02-19 06:58:49 -0800

    Add Copyright to StreamTests.

--- firebase-functions/src/androidTest/java/com/google/firebase/functions/StreamTests.kt
+++ firebase-functions/src/androidTest/java/com/google/firebase/functions/StreamTests.kt
@@ -1,14 +1,27 @@
-package com.google.firebase.functions.ktx
+/*
+ * Copyright 2025 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.firebase.functions
 
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.google.common.truth.Truth.assertThat
 import com.google.firebase.Firebase
-import com.google.firebase.functions.FirebaseFunctions
-import com.google.firebase.functions.StreamResponse
 import com.google.firebase.functions.StreamResponse.Message
 import com.google.firebase.functions.StreamResponse.Result
-import com.google.firebase.functions.functions
 import com.google.firebase.initialize
 import java.util.concurrent.TimeUnit
 import kotlinx.coroutines.reactive.asFlow
