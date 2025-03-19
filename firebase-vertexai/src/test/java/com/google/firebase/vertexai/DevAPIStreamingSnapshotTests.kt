fa68048d1ac388cfb6b4f8180b0980f5c1883df7
David Motsonashvili <davidmotson@google.com>
2025-03-19 14:11:04 -0700

    add first devapi tests

---
+++ firebase-vertexai/src/test/java/com/google/firebase/vertexai/DevAPIStreamingSnapshotTests.kt
@@ -0,0 +1,115 @@
+/*
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.firebase.vertexai
+
+import com.google.firebase.vertexai.type.BlockReason
+import com.google.firebase.vertexai.type.FinishReason
+import com.google.firebase.vertexai.type.HarmCategory
+import com.google.firebase.vertexai.type.InvalidAPIKeyException
+import com.google.firebase.vertexai.type.PromptBlockedException
+import com.google.firebase.vertexai.type.ResponseStoppedException
+import com.google.firebase.vertexai.type.SerializationException
+import com.google.firebase.vertexai.type.ServerException
+import com.google.firebase.vertexai.type.TextPart
+import com.google.firebase.vertexai.util.goldenDevAPIStreamingFile
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.matchers.nulls.shouldNotBeNull
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.shouldContain
+import io.ktor.http.HttpStatusCode
+import kotlin.time.Duration.Companion.seconds
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.withTimeout
+import org.junit.Test
+
+internal class DevAPIStreamingSnapshotTests {
+  private val testTimeout = 5.seconds
+
+  @Test
+  fun `short reply`() =
+    goldenDevAPIStreamingFile("streaming-success-basic-reply-short.txt") {
+      val responses = model.generateContentStream("prompt")
+
+      withTimeout(testTimeout) {
+        val responseList = responses.toList()
+        responseList.isEmpty() shouldBe false
+        responseList.first().candidates.first().finishReason shouldBe FinishReason.STOP
+        responseList.first().candidates.first().content.parts.isEmpty() shouldBe false
+        responseList.first().candidates.first().safetyRatings.isEmpty() shouldBe false
+      }
+    }
+
+  @Test
+  fun `long reply`() =
+    goldenDevAPIStreamingFile("streaming-success-basic-reply-long.txt") {
+      val responses = model.generateContentStream("prompt")
+
+      withTimeout(testTimeout) {
+        val responseList = responses.toList()
+        responseList.isEmpty() shouldBe false
+        responseList.forEach {
+          it.candidates.first().finishReason shouldBe FinishReason.STOP
+          it.candidates.first().content.parts.isEmpty() shouldBe false
+          it.candidates.first().safetyRatings.isEmpty() shouldBe false
+        }
+      }
+    }
+
+  @Test
+  fun `prompt blocked for safety`() =
+    goldenDevAPIStreamingFile("streaming-failure-prompt-blocked-safety.txt") {
+      val responses = model.generateContentStream("prompt")
+
+      withTimeout(testTimeout) {
+        val exception = shouldThrow<PromptBlockedException> { responses.collect() }
+        exception.response?.promptFeedback?.blockReason shouldBe BlockReason.SAFETY
+      }
+    }
+
+  @Test
+  fun `citation parsed correctly`() =
+    goldenDevAPIStreamingFile("streaming-success-citations.txt") {
+      val responses = model.generateContentStream("prompt")
+
+      withTimeout(testTimeout) {
+        val responseList = responses.toList()
+        responseList.any {
+          it.candidates.any { it.citationMetadata?.citations?.isNotEmpty() ?: false }
+        } shouldBe true
+      }
+    }
+
+  @Test
+  fun `stopped for recitation`() =
+    goldenDevAPIStreamingFile("streaming-failure-recitation-no-content.txt") {
+      val responses = model.generateContentStream("prompt")
+
+      withTimeout(testTimeout) {
+        val exception = shouldThrow<ResponseStoppedException> { responses.collect() }
+        exception.response.candidates.first().finishReason shouldBe FinishReason.RECITATION
+      }
+    }
+
+  @Test
+  fun `image rejected`() =
+    goldenDevAPIStreamingFile("streaming-failure-image-rejected.txt", HttpStatusCode.BadRequest) {
+      val responses = model.generateContentStream("prompt")
+
+      withTimeout(testTimeout) { shouldThrow<ServerException> { responses.collect() } }
+    }
+}
