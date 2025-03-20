5fdcaeee0443ecfc13416d0cac61a20f1d4b14ce
Matthew Robertson <mrober@users.noreply.github.com>
2025-03-20 10:28:52 -0600

    Use multi-process DataStore instead of Preferences DataStore (#6781)
    Use multi-process DataStore instead of Preferences DataStore.
    
    This change allows multiple processes to share the same datastore file
    safely. This reduces settings fetch to one per app run, not one per
    process.
    
    Also updated the TimeProvider to provide an object with explicit time
    units. This will make time less error prone. Removed all instances of
    `System.currentTimeMillis()` from tests, making them deterministic.

---
+++ firebase-sessions/src/test/kotlin/com/google/firebase/sessions/testing/FakeSettingsCache.kt
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2025 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.firebase.sessions.testing
+
+import com.google.firebase.sessions.TimeProvider
+import com.google.firebase.sessions.settings.SessionConfigs
+import com.google.firebase.sessions.settings.SessionConfigsSerializer
+import com.google.firebase.sessions.settings.SettingsCache
+
+/** Fake implementation of [SettingsCache]. */
+internal class FakeSettingsCache(
+  private val timeProvider: TimeProvider = FakeTimeProvider(),
+  private var sessionConfigs: SessionConfigs = SessionConfigsSerializer.defaultValue,
+) : SettingsCache {
+  override fun hasCacheExpired(): Boolean {
+    val cacheUpdatedTimeMs = sessionConfigs.cacheUpdatedTimeMs
+    val cacheDurationSeconds = sessionConfigs.cacheDurationSeconds
+
+    if (cacheUpdatedTimeMs != null && cacheDurationSeconds != null) {
+      val timeDifferenceSeconds = (timeProvider.currentTime().ms - cacheUpdatedTimeMs) / 1000
+      if (timeDifferenceSeconds < cacheDurationSeconds) {
+        return false
+      }
+    }
+
+    return true
+  }
+
+  override fun sessionsEnabled(): Boolean? = sessionConfigs.sessionsEnabled
+
+  override fun sessionSamplingRate(): Double? = sessionConfigs.sessionSamplingRate
+
+  override fun sessionRestartTimeout(): Int? = sessionConfigs.sessionTimeoutSeconds
+
+  override suspend fun updateConfigs(sessionConfigs: SessionConfigs) {
+    this.sessionConfigs = sessionConfigs
+  }
+}
