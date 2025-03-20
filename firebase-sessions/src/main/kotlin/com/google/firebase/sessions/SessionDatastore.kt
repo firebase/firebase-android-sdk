7ab5d3d8847b3cff732fffdad826229a7fecf402
Matthew Robertson <mrober@users.noreply.github.com>
2025-03-20 10:28:52 -0600

    Use multi-process DataStore instead of Preferences DataStore (#6781)
    Use multi-process DataStore instead of Preferences DataStore.
    
    This change allows multiple processes to share the same datastore file
    safely. This reduces settings fetch to one per app run, not one per
    process.
    
    Also updated the TimeProvider to provide an object with explicit time
    units. This will make time less error prone. Removed all instances of
    `System.currentTimeMillis()` from tests, making them deterministic.

--- firebase-sessions/src/main/kotlin/com/google/firebase/sessions/SessionDatastore.kt
+++ firebase-sessions/src/main/kotlin/com/google/firebase/sessions/SessionDatastore.kt
@@ -17,15 +17,15 @@
 package com.google.firebase.sessions
 
 import android.util.Log
+import androidx.datastore.core.CorruptionException
 import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.core.edit
-import androidx.datastore.preferences.core.emptyPreferences
-import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.core.Serializer
 import com.google.firebase.Firebase
 import com.google.firebase.annotations.concurrent.Background
 import com.google.firebase.app
 import java.io.IOException
+import java.io.InputStream
+import java.io.OutputStream
 import java.util.concurrent.atomic.AtomicReference
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -33,11 +33,29 @@ import kotlin.coroutines.CoroutineContext
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
 
-/** Datastore for sessions information */
-internal data class FirebaseSessionsData(val sessionId: String?)
+/** Data for sessions information */
+@Serializable internal data class SessionData(val sessionId: String?)
+
+/** DataStore json [Serializer] for [SessionData]. */
+internal object SessionDataSerializer : Serializer<SessionData> {
+  override val defaultValue = SessionData(sessionId = null)
+
+  override suspend fun readFrom(input: InputStream): SessionData =
+    try {
+      Json.decodeFromString<SessionData>(input.readBytes().decodeToString())
+    } catch (ex: Exception) {
+      throw CorruptionException("Cannot parse session data", ex)
+    }
+
+  override suspend fun writeTo(t: SessionData, output: OutputStream) {
+    @Suppress("BlockingMethodInNonBlockingContext") // blockingDispatcher is safe for blocking calls
+    output.write(Json.encodeToString(SessionData.serializer(), t).encodeToByteArray())
+  }
+}
 
 /** Handles reading to and writing from the [DataStore]. */
 internal interface SessionDatastore {
@@ -61,23 +79,17 @@ internal class SessionDatastoreImpl
 @Inject
 constructor(
   @Background private val backgroundDispatcher: CoroutineContext,
-  @SessionDetailsDataStore private val dataStore: DataStore<Preferences>,
+  private val sessionDataStore: DataStore<SessionData>,
 ) : SessionDatastore {
 
   /** Most recent session from datastore is updated asynchronously whenever it changes */
-  private val currentSessionFromDatastore = AtomicReference<FirebaseSessionsData>()
+  private val currentSessionFromDatastore = AtomicReference<SessionData>()
 
-  private object FirebaseSessionDataKeys {
-    val SESSION_ID = stringPreferencesKey("session_id")
-  }
-
-  private val firebaseSessionDataFlow: Flow<FirebaseSessionsData> =
-    dataStore.data
-      .catch { exception ->
-        Log.e(TAG, "Error reading stored session data.", exception)
-        emit(emptyPreferences())
-      }
-      .map { preferences -> mapSessionsData(preferences) }
+  private val firebaseSessionDataFlow: Flow<SessionData> =
+    sessionDataStore.data.catch { ex ->
+      Log.e(TAG, "Error reading stored session data.", ex)
+      emit(SessionDataSerializer.defaultValue)
+    }
 
   init {
     CoroutineScope(backgroundDispatcher).launch {
@@ -88,19 +100,14 @@ constructor(
   override fun updateSessionId(sessionId: String) {
     CoroutineScope(backgroundDispatcher).launch {
       try {
-        dataStore.edit { preferences ->
-          preferences[FirebaseSessionDataKeys.SESSION_ID] = sessionId
-        }
-      } catch (e: IOException) {
-        Log.w(TAG, "Failed to update session Id: $e")
+        sessionDataStore.updateData { SessionData(sessionId) }
+      } catch (ex: IOException) {
+        Log.w(TAG, "Failed to update session Id", ex)
       }
     }
   }
 
-  override fun getCurrentSessionId() = currentSessionFromDatastore.get()?.sessionId
-
-  private fun mapSessionsData(preferences: Preferences): FirebaseSessionsData =
-    FirebaseSessionsData(preferences[FirebaseSessionDataKeys.SESSION_ID])
+  override fun getCurrentSessionId(): String? = currentSessionFromDatastore.get()?.sessionId
 
   private companion object {
     private const val TAG = "FirebaseSessionsRepo"
