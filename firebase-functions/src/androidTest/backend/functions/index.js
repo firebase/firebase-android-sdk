firebase/firebase-android-sdk

commit 0554f0df8b8c8972c9497bf16aa861a2e42e5dc7

fix(functions): use notifyError() instead of throwing (#6773)
Throwing inside the `PublisherStream` causes a Runtime exception that
can't be caught in the call site.
Instead, we should use `notifyError()` so that the error can be caught
in the `Subscriber#onError()` function.

I tried to catch the exception using both of the code snippets below,
but none of them worked. (they both work with the changes in this PR):

```kotlin
functions.getHttpsCallable("nonExistentFunction")
    .stream().asFlow()
    .catch {
        // Handle error for a 404 function
    }
    .collect {
        // ...
    }
```

```kotlin
try {
    functions.getHttpsCallable("nonExistentFunction")
        .stream().asFlow()
        .collect {
            // ...
        }
} catch(e: Exception) {
    // Handle error for a 404 function
}
```

Runtime exception thrown:

```
FATAL EXCEPTION: OkHttp Dispatcher
    Process: com.google.samples.quickstart.functions, PID: 13321
    com.google.firebase.functions.FirebaseFunctionsException: Value <html><head> of type java.lang.String cannot be converted to JSONObject Unexpected Response:
                                                                             
    <html><head>
    <meta http-equiv="content-type" content="text/html;charset=utf-8">
    <title>404 Page not found</title>
    </head>
        <body text=#000000 bgcolor=#ffffff>
            <h1>Error: Page not found</h1>
            <h2>The requested URL was not found on this server.</h2>
            <h2></h2>
        </body></html>
                                                                              
    at com.google.firebase.functions.PublisherStream.validateResponse(PublisherStream.kt:316)
    at com.google.firebase.functions.PublisherStream.access$validateResponse(PublisherStream.kt:41)
    at com.google.firebase.functions.PublisherStream$startStreaming$1$4.onResponse(PublisherStream.kt:161)
    at okhttp3.RealCall$AsyncCall.execute(RealCall.java:203)

firebase-functions/src/main/java/com/google/firebase/functions/PublisherStream.kt
@@ -300,10 +300,12 @@ internal class PublisherStream(
     val errorMessage: String
     if (response.code() == 404 && response.header("Content-Type") == htmlContentType) {
       errorMessage = """URL not found. Raw response: ${response.body()?.string()}""".trimMargin()
-      throw FirebaseFunctionsException(
-        errorMessage,
-        FirebaseFunctionsException.Code.fromHttpStatus(response.code()),
-        null
+      notifyError(
+        FirebaseFunctionsException(
+          errorMessage,
+          FirebaseFunctionsException.Code.fromHttpStatus(response.code()),
+          null
+        )
       )
     }
 
@@ -313,16 +315,17 @@ internal class PublisherStream(
       val json = JSONObject(text)
       error = serializer.decode(json.opt("error"))
     } catch (e: Throwable) {
-      throw FirebaseFunctionsException(
-        "${e.message} Unexpected Response:\n$text ",
-        FirebaseFunctionsException.Code.INTERNAL,
-        e
+      notifyError(
+        FirebaseFunctionsException(
+          "${e.message} Unexpected Response:\n$text ",
+          FirebaseFunctionsException.Code.INTERNAL,
+          e
+        )
       )
+      return
     }
-    throw FirebaseFunctionsException(
-      error.toString(),
-      FirebaseFunctionsException.Code.INTERNAL,
-      error
+    notifyError(
+      FirebaseFunctionsException(error.toString(), FirebaseFunctionsException.Code.INTERNAL, error)
     )
   }
 }

firebase-functions/src/androidTest/java/com/google/firebase/functions/StreamTests.kt
@@ -144,6 +144,26 @@ class StreamTests {
   }
 
   @Test
+  fun nonExistentFunction_receivesError() = runBlocking {
+    val function =
+      functions.getHttpsCallable("nonexistentFunction").withTimeout(2000, TimeUnit.MILLISECONDS)
+    val subscriber = StreamSubscriber()
+
+    function.stream().subscribe(subscriber)
+
+    withTimeout(2000) {
+      while (subscriber.throwable == null) {
+        delay(100)
+      }
+    }
+
+    assertThat(subscriber.throwable).isNotNull()
+    assertThat(subscriber.throwable).isInstanceOf(FirebaseFunctionsException::class.java)
+    assertThat((subscriber.throwable as FirebaseFunctionsException).code)
+      .isEqualTo(FirebaseFunctionsException.Code.NOT_FOUND)
+  }
+
+  @Test
   fun genStreamWeather_receivesWeatherForecasts() = runBlocking {
     val inputData = listOf(mapOf("name" to "Toronto"), mapOf("name" to "London"))
     val input = mapOf("data" to inputData)

commit 615352d68db776bdde40fff1a1aeea851ee8d6bc

[Functions] Several functions improvements (#6796)
These improvements are both in the SDK itself, and in the code used for
the actual functions running in the cloud during integration testing:

A summary of the changes is:
- [Backend functions] Update the dependencies and node runtime version
- [Backend functions] Add logging in case the client does not support
streaming
- [SDK] Use a more robust handle of the media type
- [SDK] Fix issue introduced in #6773 that missed a return in case of
error
- [SDK] Other minor code fixes

b/404814020

firebase-functions/src/androidTest/backend/functions/index.js
@@ -154,6 +154,9 @@ exports.genStream = functionsV2.https.onCall(async (request, response) => {
       response.sendChunk(chunk);
     }
   }
+  else {
+      console.log("CLIENT DOES NOT SUPPORT STEAMING");
+  }
   return streamData.join(' ');
 });
 
@@ -225,6 +228,8 @@ exports.genStreamLargeData = functionsV2.https.onCall(
         response.sendChunk(chunk);
         await sleep(100);
       }
+    } else {
+      console.log("CLIENT DOES NOT SUPPORT STEAMING")
     }
     return "Stream Completed";
   }

firebase-functions/src/main/java/com/google/firebase/functions/PublisherStream.kt
@@ -136,12 +136,20 @@ internal class PublisherStream(
           MediaType.parse("application/json"),
           JSONObject(mapOf("data" to serializer.encode(data))).toString()
         )
-      val requestBuilder =
-        Request.Builder().url(url).post(requestBody).header("Accept", "text/event-stream")
-      context?.authToken?.let { requestBuilder.header("Authorization", "Bearer $it") }
-      context?.instanceIdToken?.let { requestBuilder.header("Firebase-Instance-ID-Token", it) }
-      context?.appCheckToken?.let { requestBuilder.header("X-Firebase-AppCheck", it) }
-      val request = requestBuilder.build()
+      val request =
+        Request.Builder()
+          .url(url)
+          .post(requestBody)
+          .apply {
+            header("Accept", "text/event-stream")
+            header("Content-Type", "application/json")
+            context?.apply {
+              authToken?.let { header("Authorization", "Bearer $it") }
+              instanceIdToken?.let { header("Firebase-Instance-ID-Token", it) }
+              appCheckToken?.let { header("X-Firebase-AppCheck", it) }
+            }
+          }
+          .build()
       val call = configuredClient.newCall(request)
       activeCall = call
 
@@ -205,6 +213,9 @@ internal class PublisherStream(
               }
             eventBuffer.append(dataChunk.trim()).append("\n")
           }
+        }
+        if (eventBuffer.isNotEmpty()) {
+          processEvent(eventBuffer.toString())
         }
       } catch (e: Exception) {
         notifyError(
@@ -296,9 +307,11 @@ internal class PublisherStream(
   private fun validateResponse(response: Response) {
     if (response.isSuccessful) return
 
-    val htmlContentType = "text/html; charset=utf-8"
     val errorMessage: String
-    if (response.code() == 404 && response.header("Content-Type") == htmlContentType) {
+    if (
+      response.code() == 404 &&
+        MediaType.parse(response.header("Content-Type") ?: "")?.subtype() == "html"
+    ) {
       errorMessage = """URL not found. Raw response: ${response.body()?.string()}""".trimMargin()
       notifyError(
         FirebaseFunctionsException(
@@ -307,6 +320,7 @@ internal class PublisherStream(
           null
         )
       )
+      return
     }
 
     val text = response.body()?.string() ?: ""

firebase-functions/src/androidTest/java/com/google/firebase/functions/StreamTests.kt
@@ -116,11 +116,11 @@ class StreamTests {
       throwable = e
     }
 
+    assertThat(throwable).isNull()
     assertThat(messages.map { it.message.data.toString() })
       .containsExactly("hello", "world", "this", "is", "cool")
     assertThat(result).isNotNull()
     assertThat(result!!.result.data.toString()).isEqualTo("hello world this is cool")
-    assertThat(throwable).isNull()
     assertThat(isComplete).isTrue()
   }
 
@@ -196,6 +196,7 @@ class StreamTests {
     function.stream(mapOf("data" to "test")).subscribe(subscriber)
 
     withTimeout(2000) { delay(500) }
+    assertThat(subscriber.throwable).isNull()
     assertThat(subscriber.messages).isEmpty()
     assertThat(subscriber.result).isNull()
   }