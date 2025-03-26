8459026b58f7f2f47312480619994c72c5929c6d
Daymon <daymxn@google.com>
2025-03-26 14:17:54 -0500

    Make provider lazy

--- firebase-common/src/main/java/com/google/firebase/heartbeatinfo/DefaultHeartBeatController.java
+++ firebase-common/src/main/java/com/google/firebase/heartbeatinfo/DefaultHeartBeatController.java
@@ -27,6 +27,7 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.annotations.concurrent.Background;
 import com.google.firebase.components.Component;
 import com.google.firebase.components.Dependency;
+import com.google.firebase.components.Lazy;
 import com.google.firebase.components.Qualified;
 import com.google.firebase.inject.Provider;
 import com.google.firebase.platforminfo.UserAgentPublisher;
@@ -117,7 +118,7 @@ public class DefaultHeartBeatController implements HeartBeatController, HeartBea
       Provider<UserAgentPublisher> userAgentProvider,
       Executor backgroundExecutor) {
     this(
-        () -> new HeartBeatInfoStorage(context, persistenceKey),
+        new Lazy<>(() -> new HeartBeatInfoStorage(context, persistenceKey)),
         consumers,
         backgroundExecutor,
         userAgentProvider,
