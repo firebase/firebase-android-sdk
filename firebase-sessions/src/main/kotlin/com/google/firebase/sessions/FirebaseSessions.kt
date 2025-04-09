2182ce7da2fc30b4c390168a81fa0bcf0c2e02db
Matthew Robertson <mrober@users.noreply.github.com>
2025-04-09 08:43:36 -0600

    Create SharedSessionRepository and remove bound service (#6844)
    This removes all the bound service implementation, and starts the shared
    sessions repository implementation. The shared repository is responsible
    for sharing session data between processes in a simple way.
    
    This does not detect cold starts yet. The `SharedSessionRepository`
    class might be getting too much, we will consider splitting it up as
    implementation goes further.

--- firebase-sessions/src/main/kotlin/com/google/firebase/sessions/FirebaseSessions.kt
+++ firebase-sessions/src/main/kotlin/com/google/firebase/sessions/FirebaseSessions.kt
@@ -38,14 +38,14 @@ constructor(
   private val firebaseApp: FirebaseApp,
   private val settings: SessionsSettings,
   @Background backgroundDispatcher: CoroutineContext,
-  lifecycleServiceBinder: SessionLifecycleServiceBinder,
+  sessionsActivityLifecycleCallbacks: SessionsActivityLifecycleCallbacks,
 ) {
 
   init {
     Log.d(TAG, "Initializing Firebase Sessions SDK.")
     val appContext = firebaseApp.applicationContext.applicationContext
     if (appContext is Application) {
-      appContext.registerActivityLifecycleCallbacks(SessionsActivityLifecycleCallbacks)
+      appContext.registerActivityLifecycleCallbacks(sessionsActivityLifecycleCallbacks)
 
       CoroutineScope(backgroundDispatcher).launch {
         val subscribers = FirebaseSessionsDependencies.getRegisteredSubscribers()
@@ -56,16 +56,12 @@ constructor(
           if (!settings.sessionsEnabled) {
             Log.d(TAG, "Sessions SDK disabled. Not listening to lifecycle events.")
           } else {
-            val lifecycleClient = SessionLifecycleClient(backgroundDispatcher)
-            lifecycleClient.bindToService(lifecycleServiceBinder)
-            SessionsActivityLifecycleCallbacks.lifecycleClient = lifecycleClient
-
             firebaseApp.addLifecycleEventListener { _, _ ->
-              Log.w(
-                TAG,
-                "FirebaseApp instance deleted. Sessions library will stop collecting data.",
-              )
-              SessionsActivityLifecycleCallbacks.lifecycleClient = null
+              // Log.w(
+              //   TAG,
+              //   "FirebaseApp instance deleted. Sessions library will stop collecting data.",
+              // )
+              // TODO(mrober): Clean up on firebase app delete
             }
           }
         }
