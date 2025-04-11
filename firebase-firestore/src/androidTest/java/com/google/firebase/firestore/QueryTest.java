edcea54ea570af828f31cf5f54e952ad6c32ff61
Mila <107142260+milaGGL@users.noreply.github.com>
2025-04-11 11:16:06 -0400

    fix: remove null value inclusion from `whereNotEqualTo` and `whereNotIn` filter results  (#6859)

--- firebase-firestore/src/androidTest/java/com/google/firebase/firestore/QueryTest.java
+++ firebase-firestore/src/androidTest/java/com/google/firebase/firestore/QueryTest.java
@@ -1647,4 +1647,53 @@ public class QueryTest {
     Query query2 = collection.where(inArray("a", asList(2, 3))).orderBy("a");
     checkOnlineAndOfflineResultsMatch(collection, query2, "doc6", "doc3");
   }
+
+  @Test
+  public void testSDKUsesNotEqualFiltersSameAsServer() {
+    Map<String, Map<String, Object>> testDocs =
+        map(
+            "a", map("zip", Double.NaN),
+            "b", map("zip", 91102L),
+            "c", map("zip", 98101L),
+            "d", map("zip", "98101"),
+            "e", map("zip", asList(98101L)),
+            "f", map("zip", asList(98101L, 98102L)),
+            "g", map("zip", asList("98101", map("zip", 98101L))),
+            "h", map("zip", map("code", 500L)),
+            "i", map("zip", null),
+            "j", map("code", 500L));
+    CollectionReference collection = testCollectionWithDocs(testDocs);
+
+    Query query = collection.whereNotEqualTo("zip", 98101L);
+    checkOnlineAndOfflineResultsMatch(collection, query, "a", "b", "d", "e", "f", "g", "h");
+
+    query = collection.whereNotEqualTo("zip", Double.NaN);
+    checkOnlineAndOfflineResultsMatch(collection, query, "b", "c", "d", "e", "f", "g", "h");
+
+    query = collection.whereNotEqualTo("zip", null);
+    checkOnlineAndOfflineResultsMatch(collection, query, "a", "b", "c", "d", "e", "f", "g", "h");
+  }
+
+  @Test
+  public void testSDKUsesNotInFiltersSameAsServer() {
+    Map<String, Map<String, Object>> testDocs =
+        map(
+            "a", map("zip", Double.NaN),
+            "b", map("zip", 91102L),
+            "c", map("zip", 98101L),
+            "d", map("zip", "98101"),
+            "e", map("zip", asList(98101L)),
+            "f", map("zip", asList(98101L, 98102L)),
+            "g", map("zip", asList("98101", map("zip", 98101L))),
+            "h", map("zip", map("code", 500L)),
+            "i", map("zip", null),
+            "j", map("code", 500L));
+    CollectionReference collection = testCollectionWithDocs(testDocs);
+
+    Query query = collection.whereNotIn("zip", asList(98101L, 98103L, asList(98101L, 98102L)));
+    checkOnlineAndOfflineResultsMatch(collection, query, "a", "b", "d", "e", "g", "h");
+
+    query = collection.whereNotIn("zip", nullList());
+    checkOnlineAndOfflineResultsMatch(collection, query);
+  }
 }
