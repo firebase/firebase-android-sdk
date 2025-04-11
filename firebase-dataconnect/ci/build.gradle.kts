574e987fe384742697e869ecb1d5b506a7ff94c0
Denver Coneybeare <dconeybe@google.com>
2025-04-11 15:05:29 -0400

    finish installFirebaseTools task

--- firebase-dataconnect/ci/build.gradle.kts
+++ firebase-dataconnect/ci/build.gradle.kts
@@ -16,12 +16,21 @@ abstract class InstallFirebaseToolsTask : DefaultTask() {
 
   @get:OutputDirectory abstract val destDir: DirectoryProperty
 
+  @get:OutputFile abstract val firebaseExecutable: RegularFileProperty
+
   @get:Inject abstract val execOperations: ExecOperations
+
+  fun initializeProperties(version: Provider<String>, destDir: Provider<Directory>) {
+    this.version.set(version)
+    this.destDir.set(destDir)
+    this.firebaseExecutable.set(destDir.map { it.dir("node_modules").dir(".bin").file("firebase") })
+  }
 
   @TaskAction
   fun execute() {
     val destDir: File = destDir.get().asFile
     val version: String = version.get()
+    val firebaseExecutable: File = firebaseExecutable.get().asFile
 
     val packageJsonFile = File(destDir, "package.json")
     logger.lifecycle("Creating $packageJsonFile")
@@ -40,26 +49,35 @@ abstract class InstallFirebaseToolsTask : DefaultTask() {
       )
       logger.lifecycle("Running command in directory $workingDir: ${commandLine.joinToString(" ")}")
     }
+
+    execOperations.exec {
+      setCommandLine(firebaseExecutable.path, "--version")
+      logger.lifecycle(
+        "Running command to verify successful installation: ${commandLine.joinToString(" ")}"
+      )
+    }
   }
 }
 
-val installFirebaseToolsTask = tasks.register<InstallFirebaseToolsTask>("installFirebaseTools") {
-  group = "Data Connect CI"
-  description = "Install the firebase-tools npm package"
-  destDir.set(layout.buildDirectory.dir("firebase-tools"))
-  version.set(providers.requiredGradleProperty("firebaseToolsVersion"))
-}
+val installFirebaseToolsTask =
+  tasks.register<InstallFirebaseToolsTask>("installFirebaseTools") {
+    group = "Data Connect CI"
+    description = "Install the firebase-tools npm package"
+    initializeProperties(
+      version = providers.requiredGradleProperty("firebaseToolsVersion"),
+      destDir = layout.buildDirectory.dir("firebase-tools"),
+    )
+  }
 
 fun ProviderFactory.requiredGradleProperty(propertyName: String) =
   gradleProperty(propertyName)
     .orElse(
       providers.provider<Nothing> {
         throw RequiredPropertyMissing(
-          "zzyzx=${project.property(propertyName)} Project property \"$propertyName\" was not set, " +
-            "but is required. " +
-            "Consider setting this project property by specifying " +
-            "-P$propertyName=<value> on the Gradle command line, " +
-            "or by setting the ORG_GRADLE_PROJECT_installFirebaseTools environment variable."
+          "Required project property \"$propertyName\" was not set; " +
+            "consider setting it " +
+            "by specifying -P$propertyName=<value> on the Gradle command line " +
+            "or by setting the environment variable ORG_GRADLE_PROJECT_$propertyName"
         )
       }
     )
