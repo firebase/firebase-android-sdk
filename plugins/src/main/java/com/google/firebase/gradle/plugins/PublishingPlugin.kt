9b0ef56148f732a7f399229016e3a2c10b40311d
Daymon <daymxn@google.com>
2025-04-29 11:37:44 -0500

    Update PublishingPlugin.kt

--- plugins/src/main/java/com/google/firebase/gradle/plugins/PublishingPlugin.kt
+++ plugins/src/main/java/com/google/firebase/gradle/plugins/PublishingPlugin.kt
@@ -418,6 +418,7 @@ abstract class PublishingPlugin : Plugin<Project> {
           "com.google.firebase:firebase-crashlytics",
           "com.google.firebase:firebase-perf",
           "com.google.firebase:firebase-vertexai",
+          "com.google.firebase:firebase-ai",
           "com.google.firebase:firebase-messaging",
           "com.google.firebase:firebase-auth",
           "com.google.firebase:firebase-database",
@@ -816,6 +817,7 @@ abstract class PublishingPlugin : Plugin<Project> {
         "com.google.firebase:firebase-storage",
         "com.google.firebase:firebase-storage-ktx",
         "com.google.firebase:firebase-vertexai",
+        "com.google.firebase:firebase-ai"
       )
 
     /** Artifacts that we use in the tutorial bundle, but _not_ in the bom. */
