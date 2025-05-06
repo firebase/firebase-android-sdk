1f290441e21662fae3c0f1a6040437fecca71944
Tejas Deshpande <tdeshpande@google.com>
2025-05-06 16:10:23 -0400

    Update PerfSession

--- firebase-perf/src/main/java/com/google/firebase/perf/session/PerfSession.java
+++ firebase-perf/src/main/java/com/google/firebase/perf/session/PerfSession.java
@@ -40,9 +40,7 @@ public class PerfSession implements Parcelable {
     if (sessionId == null) {
       sessionId = FirebaseSessionsHelperKt.createLegacySessionId();
     }
-    PerfSession session = new PerfSession(sessionId, new Clock());
-    session.setGaugeAndEventCollectionEnabled(session.shouldCollectGaugesAndEvents());
-    return session;
+    return new PerfSession(sessionId, new Clock());
   }
 
   /** Creates a PerfSession with the provided {@code sessionId} and {@code clock}. */
@@ -50,6 +48,7 @@ public class PerfSession implements Parcelable {
   public PerfSession(String sessionId, Clock clock) {
     this.sessionId = sessionId;
     creationTime = clock.getTime();
+    isGaugeAndEventCollectionEnabled = shouldCollectGaugesAndEvents();
   }
 
   private PerfSession(@NonNull Parcel in) {
@@ -70,14 +69,6 @@ public class PerfSession implements Parcelable {
    */
   public Timer getTimer() {
     return creationTime;
-  }
-
-  /*
-   * Enables/Disables the gauge and event collection for the system.
-   */
-  public void setGaugeAndEventCollectionEnabled(boolean enabled) {
-    // TODO(b/394127311): Explore deleting this method.
-    isGaugeAndEventCollectionEnabled = enabled;
   }
 
   /** Returns if the current session is verbose or not. */
@@ -146,11 +137,20 @@ public class PerfSession implements Parcelable {
   }
 
   /** If true, Session Gauge collection is enabled. */
+  @VisibleForTesting
   public boolean shouldCollectGaugesAndEvents() {
     ConfigResolver configResolver = ConfigResolver.getInstance();
     return configResolver.isPerformanceMonitoringEnabled()
         && (Math.abs(this.sessionId.hashCode() % 100)
             < configResolver.getSessionsSamplingRate() * 100);
+  }
+
+  /*
+   * Enables/Disables whether the session is verbose or not.
+   */
+  @VisibleForTesting
+  public void setGaugeAndEventCollectionEnabled(boolean enabled) {
+    isGaugeAndEventCollectionEnabled = enabled;
   }
 
   /**
