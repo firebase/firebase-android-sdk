248fd927ddda5c6106ac98f1576fb6a1221b69d9
Matthew Robertson <mrober@users.noreply.github.com>
2025-05-15 10:31:51 -0700

    Implement ProcessDataManager (#6961)
    Implement ProcessDataManager in the way we designed in
    https://docs.google.com/document/d/1yEd8dCIzwNwLhRRwkDtzWFx9vxV5d-eQU-P47wDL40k/edit?usp=sharing
    
    Tested manually in the sessions test app, and with unit tests

--- firebase-sessions/src/main/kotlin/com/google/firebase/sessions/ProcessDataManager.kt
+++ firebase-sessions/src/main/kotlin/com/google/firebase/sessions/ProcessDataManager.kt
@@ -23,11 +23,20 @@ import javax.inject.Singleton
 
 /** Manage process data, used for detecting cold app starts. */
 internal interface ProcessDataManager {
-  /** An in-memory uuid to uniquely identify this instance of this process. */
+  /** This process's name. */
+  val myProcessName: String
+
+  /** This process's pid. */
+  val myPid: Int
+
+  /** An in-memory uuid to uniquely identify this instance of this process, not the uid. */
   val myUuid: String
 
   /** Checks if this is a cold app start, meaning all processes in the mapping table are stale. */
   fun isColdStart(processDataMap: Map<String, ProcessData>): Boolean
+
+  /** Checks if this process is stale. */
+  fun isMyProcessStale(processDataMap: Map<String, ProcessData>): Boolean
 
   /** Call to notify the process data manager that a session has been generated. */
   fun onSessionGenerated()
@@ -35,20 +44,27 @@ internal interface ProcessDataManager {
   /** Update the mapping of the current processes with data about this process. */
   fun updateProcessDataMap(processDataMap: Map<String, ProcessData>?): Map<String, ProcessData>
 
-  /** Generate a new mapping of process data with the current process only. */
-  fun generateProcessDataMap() = updateProcessDataMap(mapOf())
+  /** Generate a new mapping of process data about this process only. */
+  fun generateProcessDataMap(): Map<String, ProcessData> = updateProcessDataMap(emptyMap())
 }
 
-/** Manage process data, used for detecting cold app starts. */
 @Singleton
 internal class ProcessDataManagerImpl
 @Inject
-constructor(private val appContext: Context, private val uuidGenerator: UuidGenerator) :
-  ProcessDataManager {
+constructor(private val appContext: Context, uuidGenerator: UuidGenerator) : ProcessDataManager {
+  /**
+   * This process's name.
+   *
+   * This value is cached, so will not reflect changes to the process name during runtime.
+   */
+  override val myProcessName: String by lazy { myProcessDetails.processName }
+
+  override val myPid = Process.myPid()
+
   override val myUuid: String by lazy { uuidGenerator.next().toString() }
 
-  private val myProcessName: String by lazy {
-    ProcessDetailsProvider.getCurrentProcessDetails(appContext).processName
+  private val myProcessDetails by lazy {
+    ProcessDetailsProvider.getCurrentProcessDetails(appContext)
   }
 
   private var hasGeneratedSession: Boolean = false
@@ -59,13 +75,19 @@ constructor(private val appContext: Context, private val uuidGenerator: UuidGene
       return false
     }
 
-    return ProcessDetailsProvider.getAppProcessDetails(appContext)
+    // A cold start is when all app processes are stale
+    return getAppProcessDetails()
       .mapNotNull { processDetails ->
         processDataMap[processDetails.processName]?.let { processData ->
           Pair(processDetails, processData)
         }
       }
       .all { (processDetails, processData) -> isProcessStale(processDetails, processData) }
+  }
+
+  override fun isMyProcessStale(processDataMap: Map<String, ProcessData>): Boolean {
+    val myProcessData = processDataMap[myProcessName] ?: return true
+    return myProcessData.pid != myPid || myProcessData.uuid != myUuid
   }
 
   override fun onSessionGenerated() {
@@ -81,17 +103,22 @@ constructor(private val appContext: Context, private val uuidGenerator: UuidGene
       ?.toMap()
       ?: mapOf(myProcessName to ProcessData(Process.myPid(), myUuid))
 
+  /** Gets the current details for all of the app's running processes. */
+  private fun getAppProcessDetails() = ProcessDetailsProvider.getAppProcessDetails(appContext)
+
   /**
    * Returns true if the process is stale, meaning the persisted process data does not match the
    * running process details.
+   *
+   * The [processDetails] is the running process details, and [processData] is the persisted data.
    */
-  private fun isProcessStale(
-    runningProcessDetails: ProcessDetails,
-    persistedProcessData: ProcessData,
-  ): Boolean =
-    if (myProcessName == runningProcessDetails.processName) {
-      runningProcessDetails.pid != persistedProcessData.pid || myUuid != persistedProcessData.uuid
+  private fun isProcessStale(processDetails: ProcessDetails, processData: ProcessData): Boolean =
+    if (myProcessName == processDetails.processName) {
+      // For this process, check pid and uuid
+      processDetails.pid != processData.pid || myUuid != processData.uuid
     } else {
-      runningProcessDetails.pid != persistedProcessData.pid
+      // For other processes, only check pid to avoid inter-process communication
+      // It is very unlikely for there to be a pid collision
+      processDetails.pid != processData.pid
     }
 }
