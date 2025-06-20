f48eda6f557bbcd76530ae4eea0a7ee55e68e801
Denver Coneybeare <dconeybe@google.com>
2025-06-20 17:14:13 +0000

    dataconnect: demo: build.gradle.kts: update dependencies (#7031)

--- firebase-dataconnect/demo/build.gradle.kts
+++ firebase-dataconnect/demo/build.gradle.kts
@@ -19,10 +19,10 @@ import java.nio.charset.StandardCharsets
 
 plugins {
   // Use whichever versions of these dependencies suit your application.
-  // The versions shown here were the latest versions as of May 09, 2025.
+  // The versions shown here were the latest versions as of June 10, 2025.
   // Note, however, that the version of kotlin("plugin.serialization") _must_,
   // in general, match the version of kotlin("android").
-  id("com.android.application") version "8.9.2"
+  id("com.android.application") version "8.10.1"
   id("com.google.gms.google-services") version "4.4.2"
   val kotlinVersion = "2.1.10"
   kotlin("android") version kotlinVersion
@@ -35,14 +35,17 @@ plugins {
 
 dependencies {
   // Use whichever versions of these dependencies suit your application.
-  // The versions shown here were the latest versions as of May 09, 2025.
-  implementation("com.google.firebase:firebase-dataconnect:16.0.1")
+  // The versions shown here were the latest versions as of June 10, 2025.
+
+  // Data Connect
+  implementation(platform("com.google.firebase:firebase-bom:33.15.0"))
+  implementation("com.google.firebase:firebase-dataconnect")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.1")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.1")
-  implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.8.0")
-  implementation("androidx.appcompat:appcompat:1.7.0")
+  implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.8.1")
+  implementation("androidx.appcompat:appcompat:1.7.1")
   implementation("androidx.activity:activity-ktx:1.10.1")
-  implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.0")
+  implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.1")
   implementation("com.google.android.material:material:1.12.0")
 
   // The following code in this "dependencies" block can be omitted from customer
