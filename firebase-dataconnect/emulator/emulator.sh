e75d42ee226992acd4011abe4936518b60caa62c
Denver Coneybeare <dconeybe@google.com>
2025-07-08 16:11:20 +0000

    emulator.sh: set DATACONNECT_EMULATOR_BINARY_PATH and remove auth emulator for now

--- firebase-dataconnect/emulator/emulator.sh
+++ firebase-dataconnect/emulator/emulator.sh
@@ -17,6 +17,7 @@
 set -euo pipefail
 
 export FIREBASE_DATACONNECT_POSTGRESQL_STRING='postgresql://postgres:postgres@localhost:5432?sslmode=disable'
+export DATACONNECT_EMULATOR_BINARY_PATH='/google/src/cloud/dconeybe/codegen/google3/blaze-bin/third_party/firebase/dataconnect/emulator/cli/cli'
 export DATA_CONNECT_PREVIEW=user_enum
 echo "[$0] export FIREBASE_DATACONNECT_POSTGRESQL_STRING='$FIREBASE_DATACONNECT_POSTGRESQL_STRING'"
 
@@ -24,7 +25,7 @@ readonly FIREBASE_ARGS=(
   firebase
   --debug
   emulators:start
-  --only auth,dataconnect
+  --only dataconnect
 )
 
 echo "[$0] Running command: ${FIREBASE_ARGS[*]}"
