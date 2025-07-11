85ac01fdd20de90db0ab3ae116b37443a1879064
Denver Coneybeare <dconeybe@google.com>
2025-07-11 04:19:22 +0000

    EnumValue.kt and EnumValueSerializer.kt added

---
+++ firebase-dataconnect/src/main/kotlin/com/google/firebase/dataconnect/EnumValue.kt
@@ -0,0 +1,101 @@
+/*
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.firebase.dataconnect
+
+/**
+ * Stores the value of an `enum` or a string if the string does not correspond to one of the enum's
+ * values.
+ */
+public sealed interface EnumValue<out T : Enum<out T>> {
+
+  /**
+   * The string value of the enum, either the [Enum.name] in the case of [Known] or the string whose
+   * corresponding enum values was _not_ known, as in the case of [Unknown].
+   */
+  public val stringValue: String
+
+  /**
+   * Represents an unknown enum value.
+   *
+   * This could happen, for example, if an enum gained a new value but this code was compiled for
+   * the older version that lacked the new enum value. Instead of failing, the unknown enum value
+   * will be gracefully mapped to [Unknown].
+   */
+  public class Unknown(public override val stringValue: String) : EnumValue<Nothing> {
+
+    /**
+     * Compares this object with another object for equality.
+     *
+     * @param other The object to compare to this for equality.
+     * @return true if, and only if, the other object is an instance of [Unknown] whose
+     * [stringValue] compares equal to this object's [stringValue] using the `==` operator.
+     */
+    override fun equals(other: Any?): Boolean = other is Unknown && stringValue == other.stringValue
+
+    /** Returns the hash code of [stringValue]. */
+    override fun hashCode(): Int = stringValue.hashCode()
+
+    /**
+     * Returns a string representation of this object, useful for debugging.
+     *
+     * The string representation is _not_ guaranteed to be stable and may change without notice at
+     * any time. Therefore, the only recommended usage of the returned string is debugging and/or
+     * logging. Namely, parsing the returned string or storing the returned string in non-volatile
+     * storage should generally be avoided in order to be robust in case that the string
+     * representation changes.
+     *
+     * @return [stringValue]
+     */
+    override fun toString(): String = stringValue
+  }
+
+  /**
+   * Represents a known enum value.
+   *
+   * @param value The enum value.
+   */
+  public class Known<T : Enum<T>>(public val value: T) : EnumValue<T> {
+
+    override val stringValue: String
+      get() = value.name
+
+    /**
+     * Compares this object with another object for equality.
+     *
+     * @param other The object to compare to this for equality.
+     * @return true if, and only if, the other object is an instance of [Known] whose [value]
+     * compares equal to this object's [value] using the `==` operator.
+     */
+    override fun equals(other: Any?): Boolean = other is Known<*> && value == other.value
+
+    /** Returns the hash code of [value]. */
+    override fun hashCode(): Int = stringValue.hashCode()
+
+    /**
+     * Returns a string representation of this object, useful for debugging.
+     *
+     * The string representation is _not_ guaranteed to be stable and may change without notice at
+     * any time. Therefore, the only recommended usage of the returned string is debugging and/or
+     * logging. Namely, parsing the returned string or storing the returned string in non-volatile
+     * storage should generally be avoided in order to be robust in case that the string
+     * representation changes.
+     *
+     * @return the [Enum.name] of [value].
+     */
+    override fun toString(): String = value.name
+  }
+}
