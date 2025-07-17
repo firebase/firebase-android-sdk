24056ce9110704b8066546f0461c6f87081258ee
hiteshmaurya <hiteshmaurya@google.com>
2025-07-17 05:05:01 +0000

    Resolves comments

--- appcheck/firebase-appcheck-recaptchaenterprise/src/test/java/com/google/firebase/appcheck/recaptchaenterprise/FirebaseAppCheckRecaptchaEnterpriseRegistrarTest.java
+++ appcheck/firebase-appcheck-recaptchaenterprise/src/test/java/com/google/firebase/appcheck/recaptchaenterprise/FirebaseAppCheckRecaptchaEnterpriseRegistrarTest.java
@@ -16,9 +16,7 @@ package com.google.firebase.appcheck.recaptchaenterprise;
 
 import static com.google.common.truth.Truth.assertThat;
 
-import android.content.Context;
 import com.google.firebase.components.Component;
-import com.google.firebase.components.Dependency;
 import java.util.List;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -33,10 +31,8 @@ public class FirebaseAppCheckRecaptchaEnterpriseRegistrarTest {
         new FirebaseAppCheckRecaptchaEnterpriseRegistrar();
     List<Component<?>> components = registrar.getComponents();
     assertThat(components).isNotEmpty();
-    assertThat(components).hasSize(3);
-    Component<?> applicationComponent = components.get(0);
-    assertThat(applicationComponent.getDependencies())
-        .containsExactly(Dependency.required(Context.class));
-    assertThat(applicationComponent.isLazy()).isTrue();
+    assertThat(components).hasSize(2);
+    Component<?> firebaseExecutorsComponent = components.get(0);
+    assertThat(firebaseExecutorsComponent.isLazy()).isTrue();
   }
 }
