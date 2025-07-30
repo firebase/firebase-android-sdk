887ee4e0c328442c49786938636633eebe1fdec4
David Motsonashvili <davidmotson@google.com>
2025-07-30 10:56:35 -0700

    format

--- plugins/src/main/java/com/google/firebase/gradle/plugins/PublishingPlugin.kt
+++ plugins/src/main/java/com/google/firebase/gradle/plugins/PublishingPlugin.kt
@@ -815,7 +815,7 @@ abstract class PublishingPlugin : Plugin<Project> {
         "com.google.firebase:firebase-ml-modeldownloader",
         "com.google.firebase:firebase-perf",
         "com.google.firebase:firebase-storage",
-        "com.google.firebase:firebase-ai"
+        "com.google.firebase:firebase-ai",
       )
 
     /** Artifacts that we use in the tutorial bundle, but _not_ in the bom. */
