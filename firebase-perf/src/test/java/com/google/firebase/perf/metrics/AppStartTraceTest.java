c187fa7dc59636a164ad23fc1e69be6be71308eb
Tejas Deshpande <tdeshpande@google.com>
2025-08-19 17:52:21 -0400

    Additional unit test

--- firebase-perf/src/test/java/com/google/firebase/perf/metrics/AppStartTraceTest.java
+++ firebase-perf/src/test/java/com/google/firebase/perf/metrics/AppStartTraceTest.java
@@ -252,6 +252,7 @@ public class AppStartTraceTest extends FirebasePerformanceTestBase {
     ++currentTime;
     trace.onActivityResumed(activity1);
     Assert.assertNull(trace.getOnResumeTime());
+    fakeExecutorService.runAll();
     // There should be no trace sent.
     verify(transportManager, times(0))
         .log(
@@ -278,10 +279,37 @@ public class AppStartTraceTest extends FirebasePerformanceTestBase {
     ++currentTime;
     trace.onActivityResumed(activity1);
     Assert.assertNotNull(trace.getOnResumeTime());
+    fakeExecutorService.runAll();
+    // There should be no trace sent.
+    verify(transportManager, times(1))
+        .log(
+            traceArgumentCaptor.capture(),
+            ArgumentMatchers.nullable(ApplicationProcessState.class));
+  }
+
+  @Test
+  public void testStartFromBackground_delayedInvertedOrder() {
+    FakeScheduledExecutorService fakeExecutorService = new FakeScheduledExecutorService();
+    Timer fakeTimer = spy(new Timer(currentTime));
+    AppStartTrace trace =
+        new AppStartTrace(transportManager, clock, configResolver, fakeExecutorService);
+    trace.registerActivityLifecycleCallbacks(appContext);
+    trace.setIsStartFromBackground();
+    trace.setMainThreadRunnableTime(fakeTimer);
+
+    when(fakeTimer.getDurationMicros()).thenReturn(TimeUnit.MILLISECONDS.toMicros(100) + 1);
+    trace.onActivityCreated(activity1, bundle);
+    Assert.assertNull(trace.getOnCreateTime());
+    ++currentTime;
+    trace.onActivityStarted(activity1);
+    Assert.assertNull(trace.getOnStartTime());
+    ++currentTime;
+    trace.onActivityResumed(activity1);
+    Assert.assertNull(trace.getOnResumeTime());
     // There should be a trace sent since the delay between the main thread and onActivityCreated
     // is limited.
     fakeExecutorService.runAll();
-    verify(transportManager, times(1))
+    verify(transportManager, times(0))
         .log(
             traceArgumentCaptor.capture(),
             ArgumentMatchers.nullable(ApplicationProcessState.class));
