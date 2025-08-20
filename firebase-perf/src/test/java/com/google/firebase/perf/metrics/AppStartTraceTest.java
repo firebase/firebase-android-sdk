ecb83f4e996f060329eaa834dfa0a2570052c0cd
Tejas Deshpande <tdeshpande@google.com>
2025-08-20 17:12:52 -0400

    Revert "Make a change to prevent false positive background app starts." (#7278)
    Reverts firebase/firebase-android-sdk#7274
    
    Reverting this if the current change is going to be included in M169. 
    
    An additional change is needed to verify that the
    `mainThreadRunnableTime` should be set only if no activity's onCreate
    has been called - otherwise this can cause a regression of *genuine*
    background starts.

--- firebase-perf/src/test/java/com/google/firebase/perf/metrics/AppStartTraceTest.java
+++ firebase-perf/src/test/java/com/google/firebase/perf/metrics/AppStartTraceTest.java
@@ -18,7 +18,6 @@ import static com.google.common.truth.Truth.assertThat;
 import static org.mockito.ArgumentMatchers.isA;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -239,42 +238,11 @@ public class AppStartTraceTest extends FirebasePerformanceTestBase {
   }
 
   @Test
-  public void testStartFromBackground_within100ms() {
+  public void testStartFromBackground() {
     FakeScheduledExecutorService fakeExecutorService = new FakeScheduledExecutorService();
-    Timer fakeTimer = spy(new Timer(currentTime));
     AppStartTrace trace =
         new AppStartTrace(transportManager, clock, configResolver, fakeExecutorService);
-    trace.registerActivityLifecycleCallbacks(appContext);
-    trace.setMainThreadRunnableTime(fakeTimer);
-
-    when(fakeTimer.getDurationMicros()).thenReturn(99L);
-    trace.onActivityCreated(activity1, bundle);
-    Assert.assertNotNull(trace.getOnCreateTime());
-    ++currentTime;
-    trace.onActivityStarted(activity1);
-    Assert.assertNotNull(trace.getOnStartTime());
-    ++currentTime;
-    trace.onActivityResumed(activity1);
-    Assert.assertNotNull(trace.getOnResumeTime());
-    fakeExecutorService.runAll();
-    // There should be a trace sent since the delay between the main thread and onActivityCreated
-    // is limited.
-    verify(transportManager, times(1))
-        .log(
-            traceArgumentCaptor.capture(),
-            ArgumentMatchers.nullable(ApplicationProcessState.class));
-  }
-
-  @Test
-  public void testStartFromBackground_moreThan100ms() {
-    FakeScheduledExecutorService fakeExecutorService = new FakeScheduledExecutorService();
-    Timer fakeTimer = spy(new Timer(currentTime));
-    AppStartTrace trace =
-        new AppStartTrace(transportManager, clock, configResolver, fakeExecutorService);
-    trace.registerActivityLifecycleCallbacks(appContext);
-    trace.setMainThreadRunnableTime(fakeTimer);
-
-    when(fakeTimer.getDurationMicros()).thenReturn(TimeUnit.MILLISECONDS.toMicros(100) + 1);
+    trace.setIsStartFromBackground();
     trace.onActivityCreated(activity1, bundle);
     Assert.assertNull(trace.getOnCreateTime());
     ++currentTime;
@@ -284,7 +252,6 @@ public class AppStartTraceTest extends FirebasePerformanceTestBase {
     trace.onActivityResumed(activity1);
     Assert.assertNull(trace.getOnResumeTime());
     // There should be no trace sent.
-    fakeExecutorService.runAll();
     verify(transportManager, times(0))
         .log(
             traceArgumentCaptor.capture(),
