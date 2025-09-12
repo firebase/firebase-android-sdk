26863ad03ac7ff31a4c2267a759a957c73f40d0d
Denver Coneybeare <dconeybe@google.com>
2025-09-12 16:07:09 -0400

    ObjectValue.java: buildProto() optimized to memoize applied overlays (like it was always supposed to).
    Flame chart: https://screenshot.googleplex.com/7rnpS8eKJzY6qGv

--- firebase-firestore/src/main/java/com/google/firebase/firestore/model/ObjectValue.java
+++ firebase-firestore/src/main/java/com/google/firebase/firestore/model/ObjectValue.java
@@ -16,6 +16,7 @@ package com.google.firebase.firestore.model;
 
 import static com.google.firebase.firestore.util.Assert.hardAssert;
 
+import androidx.annotation.GuardedBy;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import com.google.firebase.firestore.model.mutation.FieldMask;
@@ -28,10 +29,14 @@ import java.util.Set;
 
 /** A structured object value stored in Firestore. */
 public final class ObjectValue implements Cloneable {
+
+  private final Object lock = new Object();
+
   /**
    * The immutable Value proto for this object. Local mutations are stored in `overlayMap` and only
    * applied when {@link #buildProto()} is invoked.
    */
+  @GuardedBy("lock")
   private Value partialValue;
 
   /**
@@ -39,6 +44,7 @@ public final class ObjectValue implements Cloneable {
    * #partialValue}. Values can either be {@link Value} protos, {@code Map<String, Object>} values
    * (to represent additional nesting) or {@code null} (to represent field deletes).
    */
+  @GuardedBy("lock")
   private final Map<String, Object> overlayMap = new HashMap<>();
 
   public static ObjectValue fromMap(Map<String, Value> value) {
@@ -103,7 +109,7 @@ public final class ObjectValue implements Cloneable {
   }
 
   @Nullable
-  private Value extractNestedValue(Value value, FieldPath fieldPath) {
+  private static Value extractNestedValue(Value value, FieldPath fieldPath) {
     if (fieldPath.isEmpty()) {
       return value;
     } else {
@@ -124,14 +130,20 @@ public final class ObjectValue implements Cloneable {
    * invocations are based on this memoized result.
    */
   private Value buildProto() {
-    synchronized (overlayMap) {
-      MapValue mergedResult = applyOverlay(FieldPath.EMPTY_PATH, overlayMap);
-      if (mergedResult != null) {
-        partialValue = Value.newBuilder().setMapValue(mergedResult).build();
-        overlayMap.clear();
+    synchronized (lock) {
+      if (overlayMap.isEmpty()) {
+        return partialValue;
       }
+
+      MapValue mergedResult = applyOverlay(partialValue, FieldPath.EMPTY_PATH, overlayMap);
+      if (mergedResult == null) {
+        return partialValue;
+      }
+
+      overlayMap.clear();
+      partialValue = Value.newBuilder().setMapValue(mergedResult).build();
+      return partialValue;
     }
-    return partialValue;
   }
 
   /**
@@ -171,6 +183,13 @@ public final class ObjectValue implements Cloneable {
    * Adds {@code value} to the overlay map at {@code path}. Creates nested map entries if needed.
    */
   private void setOverlay(FieldPath path, @Nullable Value value) {
+    synchronized (lock) {
+      setOverlay(overlayMap, path, value);
+    }
+  }
+
+  private static void setOverlay(
+      Map<String, Object> overlayMap, FieldPath path, @Nullable Value value) {
     Map<String, Object> currentLevel = overlayMap;
 
     for (int i = 0; i < path.length() - 1; ++i) {
@@ -208,8 +227,8 @@ public final class ObjectValue implements Cloneable {
    *     overlayMap}.
    * @return The merged data at `currentPath` or null if no modifications were applied.
    */
-  private @Nullable MapValue applyOverlay(
-      FieldPath currentPath, Map<String, Object> currentOverlays) {
+  private static @Nullable MapValue applyOverlay(
+      Value partialValue, FieldPath currentPath, Map<String, Object> currentOverlays) {
     boolean modified = false;
 
     @Nullable Value existingValue = extractNestedValue(partialValue, currentPath);
@@ -227,7 +246,8 @@ public final class ObjectValue implements Cloneable {
       if (value instanceof Map) {
         @Nullable
         MapValue nested =
-            applyOverlay(currentPath.append(pathSegment), (Map<String, Object>) value);
+            applyOverlay(
+                partialValue, currentPath.append(pathSegment), (Map<String, Object>) value);
         if (nested != null) {
           resultAtPath.putFields(pathSegment, Value.newBuilder().setMapValue(nested).build());
           modified = true;
