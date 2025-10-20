44785fa83ef3868678d7eb6105d2e241378e75d6
Denver Coneybeare <dconeybe@google.com>
2025-10-20 11:53:38 -0400

    DataConnectCacheDatabaseUnitTest.kt: created, including a few tests for initialize()

---
+++ firebase-dataconnect/src/test/kotlin/com/google/firebase/dataconnect/sqlite2/DataConnectCacheDatabaseUnitTest.kt
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2025 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.firebase.dataconnect.sqlite2
+
+import android.database.sqlite.SQLiteDatabase
+import com.google.firebase.dataconnect.core.Logger
+import com.google.firebase.dataconnect.testutil.DataConnectLogLevelRule
+import com.google.firebase.dataconnect.testutil.RandomSeedTestRule
+import com.google.firebase.dataconnect.testutil.shouldContainWithNonAbuttingText
+import com.google.firebase.dataconnect.testutil.shouldContainWithNonAbuttingTextIgnoringCase
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.property.RandomSource
+import io.kotest.property.arbitrary.next
+import io.mockk.mockk
+import java.io.File
+import kotlinx.coroutines.test.runTest
+import org.junit.After
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TemporaryFolder
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+class DataConnectCacheDatabaseUnitTest {
+
+  @get:Rule val temporaryFolder = TemporaryFolder()
+
+  @get:Rule val randomSeedTestRule = RandomSeedTestRule()
+  private val rs: RandomSource by randomSeedTestRule.rs
+
+  @get:Rule val dataConnectLogLevelRule = DataConnectLogLevelRule()
+
+  private val lazyDataConnectCacheDatabase = lazy {
+    val dbFile = File(temporaryFolder.newFolder(), "db.sqlite")
+    val mockLogger: Logger = mockk(relaxed = true)
+    DataConnectCacheDatabase(dbFile, mockLogger)
+  }
+
+  private val dataConnectCacheDatabase: DataConnectCacheDatabase by
+    lazyDataConnectCacheDatabase::value
+
+  @After
+  fun closeLazyDataConnectCacheDatabase() {
+    if (lazyDataConnectCacheDatabase.isInitialized()) {
+      lazyDataConnectCacheDatabase.value.close()
+    }
+  }
+
+  @Test
+  fun `initialize() should create the database at the file given to the constructor`() = runTest {
+    val dbFile = File(temporaryFolder.newFolder(), "db.sqlite")
+    val mockLogger: Logger = mockk(relaxed = true)
+    val dataConnectCacheDatabase = DataConnectCacheDatabase(dbFile, mockLogger)
+
+    dataConnectCacheDatabase.initialize()
+
+    try {
+      SQLiteDatabase.openDatabase(dbFile.absolutePath, null, 0).use { sqliteDatabase ->
+        // Run a query that would fail if initialization had not occurred.
+        sqliteDatabase.rawQuery("SELECT * FROM metadata", null).close()
+      }
+    } finally {
+      dataConnectCacheDatabase.close()
+    }
+  }
+
+  @Test
+  fun `initialize() should create an in-memory database if a null file given to the constructor`() =
+    runTest {
+      val mockLogger: Logger = mockk(relaxed = true)
+      val dataConnectCacheDatabase = DataConnectCacheDatabase(null, mockLogger)
+
+      dataConnectCacheDatabase.initialize()
+
+      // There's nothing really to "verify" here, except that no exception is thrown.
+      dataConnectCacheDatabase.close()
+    }
+
+  @Test
+  fun `initialize() should throw if it has already completed successfully`() = runTest {
+    dataConnectCacheDatabase.initialize()
+
+    val exception = shouldThrow<IllegalStateException> { dataConnectCacheDatabase.initialize() }
+
+    exception.message shouldContainWithNonAbuttingText "initialize()"
+    exception.message shouldContainWithNonAbuttingTextIgnoringCase "already been called"
+  }
+}
