e50f38f8fb7518f2d5e3d5c5c6c9d7a8a5f7aa27
Denver Coneybeare <dconeybe@google.com>
2025-11-19 20:27:13 -0500

    after hours: upgrade dependencies and clean up ui

--- firebase-dataconnect/demo/build.gradle.kts
+++ firebase-dataconnect/demo/build.gradle.kts
@@ -14,18 +14,16 @@
  * limitations under the License.
  */
 
-import com.android.build.api.variant.ApplicationAndroidComponentsExtension
-import java.nio.charset.StandardCharsets
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
   // Use whichever versions of these dependencies suit your application.
-  // The versions shown here were the latest versions as of July 17, 2025.
+  // The versions shown here were the latest versions as of Nov 19, 2025.
   // Note, however, that the version of kotlin("plugin.serialization") _must_,
   // in general, match the version of kotlin("android").
-  id("com.android.application") version "8.11.1"
-  id("com.google.gms.google-services") version "4.4.3"
-  val kotlinVersion = "2.1.10"
+  id("com.android.application") version "8.13.1"
+  id("com.google.gms.google-services") version "4.4.4"
+  val kotlinVersion = "2.1.21"
   kotlin("android") version kotlinVersion
   kotlin("plugin.serialization") version kotlinVersion
 
@@ -33,32 +31,30 @@ plugins {
 
   // The following code in this "plugins" block can be omitted from customer
   // facing documentation as it is an implementation detail of this application.
-  id("com.diffplug.spotless") version "7.1.0"
-
-  id("org.jetbrains.dokka") version "2.0.0"
+  id("com.diffplug.spotless") version "8.1.0"
 }
 
 dependencies {
   // Use whichever versions of these dependencies suit your application.
-  // The versions shown here were the latest versions as of July 17, 2025.
+  // The versions shown here were the latest versions as of Nov 19, 2025.
 
   // Data Connect
-  implementation(platform("com.google.firebase:firebase-bom:33.16.0"))
+  implementation(platform("com.google.firebase:firebase-bom:34.6.0"))
   implementation("com.google.firebase:firebase-dataconnect")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
   implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.9.0")
-  implementation("androidx.core:core-ktx:1.16.0")
-  implementation("com.google.android.material:material:1.12.0")
+  implementation("androidx.core:core-ktx:1.17.0")
+  implementation("com.google.android.material:material:1.13.0")
   implementation("androidx.appcompat:appcompat:1.7.1")
-  implementation("androidx.activity:activity-ktx:1.10.1")
-  implementation("androidx.activity:activity-compose:1.10.1")
-  implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.2")
-  implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.2")
-  implementation("com.google.android.material:material:1.12.0")
+  implementation("androidx.activity:activity-ktx:1.12.0")
+  implementation("androidx.activity:activity-compose:1.12.0")
+  implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.10.0")
+  implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.10.0")
+  implementation("com.google.android.material:material:1.13.0")
 
   // Jetpack compose dependencies.
-  implementation(platform("androidx.compose:compose-bom:2025.05.00"))
+  implementation(platform("androidx.compose:compose-bom:2025.11.01"))
   implementation("androidx.compose.ui:ui-graphics")
   implementation("androidx.compose.material3:material3")
   debugImplementation("androidx.compose.ui:ui-tooling")
@@ -68,13 +64,6 @@ dependencies {
   coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")
   implementation("io.kotest:kotest-property:5.9.1")
   implementation("io.kotest.extensions:kotest-property-arbs:2.1.2")
-}
-
-dokka {
-  moduleName.set("Data Connect Demo")
-  dokkaSourceSets.main {
-    sourceRoots.from(layout.buildDirectory.dir("dataConnect/generatedSources/").get())
-  }
 }
 
 // The remaining code in this file can be omitted from customer facing
@@ -138,231 +127,5 @@ spotless {
     trimTrailingWhitespace()
     leadingTabsToSpaces(2)
     endWithNewline()
-  }
-}
-
-@CacheableTask
-abstract class DataConnectGenerateSourcesTask : DefaultTask() {
-
-  @get:InputDirectory
-  @get:PathSensitive(PathSensitivity.RELATIVE)
-  abstract val inputDirectory: Property<DirectoryTree>
-
-  @get:Input abstract val firebaseToolsVersion: Property<String>
-
-  @get:Input abstract val firebaseCommand: Property<String>
-
-  @get:Input @get:Optional abstract val nodeExecutableDirectory: Property<String>
-
-  @get:InputFile
-  @get:Optional
-  @get:PathSensitive(PathSensitivity.ABSOLUTE)
-  abstract val dataConnectEmulatorExecutable: RegularFileProperty
-
-  @get:Input @get:Optional abstract val dataConnectPreviewFlags: Property<String>
-
-  @get:OutputDirectory abstract val outputDirectory: DirectoryProperty
-
-  @get:Internal abstract val workDirectory: DirectoryProperty
-
-  @get:Inject protected abstract val execOperations: ExecOperations
-
-  @get:Inject protected abstract val providerFactory: ProviderFactory
-
-  @get:Inject protected abstract val fileSystemOperations: FileSystemOperations
-
-  @TaskAction
-  fun run() {
-    val inputDirectory: File = inputDirectory.get().dir
-    val firebaseToolsVersion: String = firebaseToolsVersion.get()
-    val firebaseCommand: String = firebaseCommand.get()
-    val nodeExecutableDirectory: String? = nodeExecutableDirectory.orNull
-    val dataConnectEmulatorExecutable: File? = dataConnectEmulatorExecutable.orNull?.asFile
-    val dataConnectPreviewFlags: String? = dataConnectPreviewFlags.orNull
-    val outputDirectory: File = outputDirectory.get().asFile
-    val workDirectory: File = workDirectory.get().asFile
-
-    logger.info("inputDirectory: {}", inputDirectory.absolutePath)
-    logger.info("firebaseToolsVersion: {}", firebaseToolsVersion)
-    logger.info("firebaseCommand: {}", firebaseCommand)
-    logger.info("nodeExecutableDirectory: {}", nodeExecutableDirectory)
-    logger.info("dataConnectEmulatorExecutable: {}", dataConnectEmulatorExecutable)
-    logger.info("dataConnectPreviewFlags: {}", dataConnectPreviewFlags)
-    logger.info("outputDirectory: {}", outputDirectory.absolutePath)
-    logger.info("workDirectory: {}", workDirectory.absolutePath)
-
-    outputDirectory.deleteRecursively()
-    outputDirectory.mkdirs()
-    workDirectory.deleteRecursively()
-    workDirectory.mkdirs()
-
-    val logFile =
-      if (logger.isInfoEnabled) {
-        null
-      } else {
-        File(workDirectory, "dataconnect.sdk.generate.log.txt")
-      }
-
-    val execResult =
-      logFile?.outputStream().use { logStream ->
-        execOperations.runCatching {
-          exec {
-            configureFirebaseCommand(
-              this,
-              firebaseCommand = firebaseCommand,
-              nodeExecutableDirectory = nodeExecutableDirectory,
-              dataConnectEmulatorExecutable = dataConnectEmulatorExecutable,
-              dataConnectPreviewFlags = dataConnectPreviewFlags,
-              path = providerFactory.environmentVariable("PATH").orNull,
-            )
-            args("--debug", "dataconnect:sdk:generate")
-            workingDir(inputDirectory)
-            isIgnoreExitValue = false
-            logStream?.let {
-              standardOutput = it
-              errorOutput = it
-            }
-          }
-        }
-      }
-
-    execResult.onFailure { exception ->
-      logFile?.readText(StandardCharsets.UTF_8)?.lines()?.forEach { line ->
-        logger.warn("{}", line.trimEnd())
-      }
-      throw exception
-    }
-  }
-
-  companion object {
-
-    fun configureFirebaseCommand(
-      execSpec: ExecSpec,
-      firebaseCommand: String,
-      nodeExecutableDirectory: String?,
-      dataConnectEmulatorExecutable: File?,
-      dataConnectPreviewFlags: String?,
-      path: String?,
-    ) {
-      execSpec.setCommandLine(firebaseCommand)
-
-      nodeExecutableDirectory?.let {
-        val newPath = if (path === null) it else (it + File.pathSeparator + path)
-        execSpec.environment("PATH", newPath)
-      }
-
-      dataConnectEmulatorExecutable?.let {
-        execSpec.environment("DATACONNECT_EMULATOR_BINARY_PATH", it.absolutePath)
-      }
-
-      dataConnectPreviewFlags?.let { execSpec.environment("DATA_CONNECT_PREVIEW", it) }
-    }
-  }
-}
-
-@DisableCachingByDefault(
-  because = "Copying files is not worth caching, just like org.gradle.api.tasks.Copy"
-)
-abstract class CopyDirectoryTask : DefaultTask() {
-
-  @get:InputDirectory abstract val srcDirectory: Property<DirectoryTree>
-
-  @get:OutputDirectory abstract val destDirectory: DirectoryProperty
-
-  @get:Inject protected abstract val fileSystemOperations: FileSystemOperations
-
-  @TaskAction
-  fun run() {
-    val srcDirectoryTree: DirectoryTree = srcDirectory.get()
-    val destDirectory: File = destDirectory.get().asFile
-
-    logger.info("srcDirectory: {}", srcDirectoryTree.dir.absolutePath)
-    logger.info("destDirectory: {}", destDirectory.absolutePath)
-
-    destDirectory.deleteRecursively()
-    destDirectory.mkdirs()
-
-    fileSystemOperations.copy {
-      from(srcDirectoryTree)
-      into(destDirectory)
-    }
-  }
-}
-
-run {
-  val dataConnectTaskGroupName = "Firebase Data Connect Minimal App"
-  val projectDirectory = layout.projectDirectory
-
-  val generateSourcesTask =
-    tasks.register<DataConnectGenerateSourcesTask>("dataConnectGenerateSources") {
-      group = dataConnectTaskGroupName
-      description =
-        "Run firebase dataconnect:sdk:generate to generate the Data Connect Kotlin SDK sources"
-
-      inputDirectory =
-        fileTree(layout.projectDirectory.dir("firebase")).apply { exclude("**/*.log") }
-
-      outputDirectory = layout.buildDirectory.dir("dataConnect/generatedSources")
-
-      firebaseCommand =
-        project.providers.gradleProperty("dataConnect.demo.firebaseCommand").orElse("firebase")
-
-      nodeExecutableDirectory =
-        project.providers.gradleProperty("dataConnect.demo.nodeExecutableDirectory").map {
-          projectDirectory.dir(it).asFile.absolutePath
-        }
-
-      dataConnectEmulatorExecutable =
-        project.providers.gradleProperty("dataConnect.demo.dataConnectEmulatorExecutable").map {
-          projectDirectory.file(it)
-        }
-
-      dataConnectPreviewFlags =
-        project.providers.gradleProperty("dataConnect.demo.dataConnectPreviewFlags")
-
-      val path = providers.environmentVariable("PATH")
-      firebaseToolsVersion =
-        providers
-          .exec {
-            DataConnectGenerateSourcesTask.configureFirebaseCommand(
-              this,
-              firebaseCommand = firebaseCommand.get(),
-              nodeExecutableDirectory = nodeExecutableDirectory.orNull,
-              dataConnectEmulatorExecutable = dataConnectEmulatorExecutable.orNull?.asFile,
-              dataConnectPreviewFlags = dataConnectPreviewFlags.orNull,
-              path = path.orNull,
-            )
-            args("--version")
-          }
-          .standardOutput
-          .asText
-          .map { it.trim() }
-
-      workDirectory = layout.buildDirectory.dir(name)
-    }
-
-  val androidComponents = extensions.getByType<ApplicationAndroidComponentsExtension>()
-  androidComponents.onVariants { variant ->
-    val variantNameTitleCase = variant.name[0].uppercase() + variant.name.substring(1)
-    val copyTaskName = "dataConnectCopy${variantNameTitleCase}GeneratedSources"
-    val objectFactory = objects
-    val copyTask =
-      tasks.register<CopyDirectoryTask>(copyTaskName) {
-        group = dataConnectTaskGroupName
-        description =
-          "Copy the generated Data Connect Kotlin SDK sources into the " +
-            "generated code directory for the \"${variant.name}\" variant."
-        srcDirectory =
-          generateSourcesTask.flatMap {
-            it.outputDirectory.map { outputDirectory ->
-              objectFactory.fileTree().apply {
-                setDir(outputDirectory)
-                exclude("**/*.log")
-              }
-            }
-          }
-      }
-
-    variant.sources.java!!.addGeneratedSourceDirectory(copyTask, CopyDirectoryTask::destDirectory)
   }
 }
--- firebase-dataconnect/demo/src/main/kotlin/com/google/firebase/dataconnect/minimaldemo/MainComposeActivity.kt
+++ firebase-dataconnect/demo/src/main/kotlin/com/google/firebase/dataconnect/minimaldemo/MainComposeActivity.kt
@@ -21,25 +21,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 
 class MainComposeActivity : ComponentActivity() {
 
@@ -49,16 +40,7 @@ class MainComposeActivity : ComponentActivity() {
     setContent {
       MaterialTheme {
         Scaffold(
-          topBar = {
-            TopAppBar(
-              title = { Text("Data Connect Minimal Demo") },
-              navigationIcon = {
-                IconButton(onClick = { /* "Open nav drawer" */ }) {
-                  Icon(Icons.Filled.Menu, contentDescription = "Hamburger Menu")
-                }
-              },
-            )
-          },
+          topBar = { TopAppBar(title = { Text("After Hours Data Connect") }) },
           modifier = Modifier.fillMaxSize(),
         ) { innerPadding ->
           Greeting(Modifier.padding(innerPadding).fillMaxSize().wrapContentSize())
@@ -68,20 +50,4 @@ class MainComposeActivity : ComponentActivity() {
   }
 }
 
-@Composable
-fun Greeting(modifier: Modifier = Modifier) {
-  Column(verticalArrangement = Arrangement.Center, modifier = modifier) {
-    Text(
-      "foobar 1 2 3 4 5",
-      fontSize = 100.sp,
-      lineHeight = 110.sp,
-      modifier = Modifier.padding(8.dp),
-    )
-  }
-}
-
-@Preview
-@Composable
-fun GreetingPreview() {
-  MaterialTheme { Greeting() }
-}
+@Composable fun Greeting(modifier: Modifier = Modifier) {}
