74bb36441a258ad3d44f8a7c4ed05930bb4471b0
David Motsonashvili <davidmotson@google.com>
2026-01-06 13:21:28 -0800

    fix jvm/kotlin incompatability

--- firebase-ai-ksp-processor/build.gradle.kts
+++ firebase-ai-ksp-processor/build.gradle.kts
@@ -31,8 +31,8 @@ tasks.test { useJUnitPlatform() }
 kotlin { jvmToolchain(17) }
 
 java {
-  sourceCompatibility = JavaVersion.VERSION_1_9
-  targetCompatibility = JavaVersion.VERSION_1_9
+  sourceCompatibility = JavaVersion.VERSION_17
+  targetCompatibility = JavaVersion.VERSION_17
 }
 
 publishing {
