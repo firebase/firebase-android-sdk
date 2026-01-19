19f32b0118f1bd67e088fb7426ca5f8a98fa4756
Rodrigo Lazo Paz <rlazo@google.com>
2026-01-19 17:41:58 -0500

    [AI] Add ondevice interop SDK
    This SDK is meant to be used only as a dep by other SDKs, no publicly
    used. It exposes the API to interact with on-device models provided by
    the upcoming ondevice SDK.

---
+++ firebase-ai-ondevice-interop/firebase-ai-ondevice-api.gradle.kts
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2026 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+@file:Suppress("UnstableApiUsage")
+
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+  id("firebase-library")
+  id("kotlin-android")
+}
+
+firebaseLibrary {
+  testLab.enabled = false
+  publishJavadoc = false
+}
+
+android {
+  val targetSdkVersion: Int by rootProject
+  val minSdkVersion: Int by rootProject
+
+  namespace = "com.google.firebase.ai.ondevice.api"
+  compileSdk = 34
+  defaultConfig {
+    minSdk = minSdkVersion
+    multiDexEnabled = true
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+  }
+  buildTypes { release { isMinifyEnabled = false } }
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+  }
+  testOptions {
+    targetSdk = targetSdkVersion
+    unitTests {
+      isIncludeAndroidResources = true
+      isReturnDefaultValues = true
+    }
+  }
+  lint {
+    targetSdk = targetSdkVersion
+    baseline = file("lint-baseline.xml")
+  }
+  sourceSets { getByName("test").java.srcDirs("src/testUtil") }
+}
+
+kotlin {
+  compilerOptions { jvmTarget = JvmTarget.JVM_1_8 }
+  explicitApi()
+}
+
+dependencies {
+  api(libs.firebase.common)
+  implementation(libs.firebase.components)
+  implementation(libs.firebase.annotations)
+  implementation(libs.androidx.annotation)
+  implementation(libs.androidx.core.ktx)
+  implementation(libs.slf4j.nop)
+  implementation(libs.kotlinx.coroutines.android)
+}
