832cc820a3a950d0f453377306589530a816410c
Denver Coneybeare <dconeybe@google.com>
2026-02-05 12:39:59 -0500

    QueryResultRehydratorUnitTest.kt: add test: `rehydrateQueryResult() should throw on missing entity field`

--- firebase-dataconnect/src/test/kotlin/com/google/firebase/dataconnect/sqlite/QueryResultRehydratorUnitTest.kt
+++ firebase-dataconnect/src/test/kotlin/com/google/firebase/dataconnect/sqlite/QueryResultRehydratorUnitTest.kt
@@ -37,6 +37,7 @@ import io.kotest.property.Arb
 import io.kotest.property.EdgeConfig
 import io.kotest.property.PropTestConfig
 import io.kotest.property.ShrinkingMode
+import io.kotest.property.arbitrary.filter
 import io.kotest.property.arbitrary.of
 import io.kotest.property.checkAll
 import kotlinx.coroutines.test.runTest
@@ -100,6 +101,49 @@ class QueryResultRehydratorUnitTest {
         exception.message shouldContainWithNonAbuttingText missingEntityId
         exception.message shouldContainWithNonAbuttingTextIgnoringCase "not found"
         exception.message should containPathOfMissingEntityIdMatcher
+      }
+    }
+  }
+
+  @Test
+  fun `rehydrateQueryResult() should throw on missing entity field`() = runTest {
+    val queryResultWithAtLeastOneNonEmptyEntityArb =
+      QueryResultArb(entityCountRange = 1..5).filter { sample ->
+        sample.entityStructById.values.any { it.fieldsCount > 0 }
+      }
+
+    checkAll(propTestConfig, queryResultWithAtLeastOneNonEmptyEntityArb) { sample ->
+      check(sample.queryResultProto.entitiesCount > 0)
+      val (entityIdWithMissingField, entityStruct) =
+        Arb.of(sample.entityByPath.values.filter { it.struct.fieldsCount > 0 }).bind()
+      val missingEntityField = Arb.of(entityStruct.fieldsMap.keys.toList()).bind()
+      val entityStructByIdWithMissingField =
+        sample.entityStructById.toMutableMap().run {
+          val entityStruct = checkNotNull(get(entityIdWithMissingField))
+          val entityStructWithMissingField =
+            entityStruct.toBuilder().removeFields(missingEntityField).build()
+          check(entityStruct != entityStructWithMissingField)
+          put(entityIdWithMissingField, entityStructWithMissingField)
+          toMap()
+        }
+
+      val exception =
+        shouldThrow<EntityMissingFieldsException> {
+          rehydrateQueryResult(sample.queryResultProto, entityStructByIdWithMissingField)
+        }
+
+      val containPathOfEntityMissingFieldMatcher =
+        sample.entityByPath.entries
+          .filter { it.value.entityId == entityIdWithMissingField }
+          .map { containWithNonAbuttingText(it.key.toPathString()) }
+          .reduce { acc, matcher -> acc or matcher }
+      assertSoftly {
+        // "entity with ID j4VJ for path=vjlu[0] is missing 1 of 5 fields: JSaU (got 4 fields: cypS,
+        // nokE, nvUB, ny2z) [nrtmqzdfy3]"
+        exception.message shouldContainWithNonAbuttingText "nrtmqzdfy3"
+        exception.message shouldContainWithNonAbuttingText entityIdWithMissingField
+        exception.message shouldContainWithNonAbuttingTextIgnoringCase "missing"
+        exception.message should containPathOfEntityMissingFieldMatcher
       }
     }
   }
