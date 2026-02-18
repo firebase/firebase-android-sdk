37809180742a3a7ba2f2877e9040f16d097086ca
Denver Coneybeare <dconeybe@google.com>
2026-02-18 01:40:46 -0500

    DataConnectGrpcRPCsUnitTest.kt: improvements

--- firebase-dataconnect/src/test/kotlin/com/google/firebase/dataconnect/core/DataConnectGrpcRPCsUnitTest.kt
+++ firebase-dataconnect/src/test/kotlin/com/google/firebase/dataconnect/core/DataConnectGrpcRPCsUnitTest.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-@file:OptIn(ExperimentalKotest::class)
+@file:OptIn(ExperimentalKotest::class, DelicateKotest::class)
 
 package com.google.firebase.dataconnect.core
 
@@ -21,6 +21,7 @@ import com.google.firebase.dataconnect.FirebaseDataConnect.CallerSdkType
 import com.google.firebase.dataconnect.core.DataConnectGrpcRPCs.CacheSettings
 import com.google.firebase.dataconnect.core.DataConnectGrpcRPCs.ExecuteQueryResult
 import com.google.firebase.dataconnect.sqlite.QueryResultArb
+import com.google.firebase.dataconnect.sqlite.QueryResultArb.EntityRepeatPolicy.INTER_SAMPLE
 import com.google.firebase.dataconnect.testutil.CleanupsRule
 import com.google.firebase.dataconnect.testutil.DataConnectLogLevelRule
 import com.google.firebase.dataconnect.testutil.newMockLogger
@@ -29,6 +30,7 @@ import com.google.firebase.dataconnect.testutil.property.arbitrary.dataConnect
 import com.google.firebase.dataconnect.testutil.property.arbitrary.dataConnectGrpcMetadata
 import com.google.firebase.dataconnect.testutil.property.arbitrary.proto
 import com.google.firebase.dataconnect.testutil.property.arbitrary.struct
+import com.google.firebase.dataconnect.testutil.registerDataConnectKotestPrinters
 import com.google.firebase.dataconnect.testutil.shouldBe
 import google.firebase.dataconnect.proto.ConnectorServiceGrpc
 import google.firebase.dataconnect.proto.ExecuteQueryRequest
@@ -38,6 +40,7 @@ import io.grpc.Server
 import io.grpc.okhttp.OkHttpServerBuilder
 import io.grpc.stub.StreamObserver
 import io.kotest.assertions.withClue
+import io.kotest.common.DelicateKotest
 import io.kotest.common.ExperimentalKotest
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.types.shouldBeInstanceOf
@@ -46,12 +49,15 @@ import io.kotest.property.EdgeConfig
 import io.kotest.property.PropTestConfig
 import io.kotest.property.PropertyContext
 import io.kotest.property.arbitrary.bind
+import io.kotest.property.arbitrary.distinct
 import io.kotest.property.arbitrary.enum
 import io.kotest.property.checkAll
 import java.io.File
 import java.util.concurrent.atomic.AtomicInteger
+import java.util.concurrent.atomic.AtomicReference
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.test.runTest
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.TemporaryFolder
@@ -72,15 +78,21 @@ class DataConnectGrpcRPCsClientUnitTest {
   private val callerSdkTypeArb = Arb.enum<CallerSdkType>()
   private val grpcMetadataArb = Arb.dataConnect.dataConnectGrpcMetadata()
 
+  @Before
+  fun registerPrinters() {
+    registerDataConnectKotestPrinters()
+  }
+
   @Test
-  fun `executeQuery() returns query results from cache`() = runTest {
+  fun `executeQuery() returns non-normalized query results from cache`() = runTest {
     checkAll(propTestConfig, QueryResultArb(entityCountRange = 0..5)) { sample ->
-      val response =
-        ExecuteQueryResponse.newBuilder().let { responseBuilder ->
-          responseBuilder.setData(sample.hydratedStruct)
-          responseBuilder.build()
-        }
-      startServer(response).use { server ->
+      startServer().use { server ->
+        val response =
+          ExecuteQueryResponse.newBuilder().let { responseBuilder ->
+            responseBuilder.setData(sample.hydratedStruct)
+            responseBuilder.build()
+          }
+        server.nextResponse = response
         val dataConnectGrpcRPCs = newDataConnectGrpcRPCs(server)
         val request = executeQueryRequestArb.bind()
 
@@ -110,6 +122,66 @@ class DataConnectGrpcRPCsClientUnitTest {
     }
   }
 
+  @Test
+  fun `executeQuery() returns normalized query results from cache`() = runTest {
+    checkAll(propTestConfig, callerSdkTypeArb) { callerSdkType ->
+      startServer().use { server ->
+        val queryResultArb =
+          QueryResultArb(entityCountRange = 0..5, entityRepeatPolicy = INTER_SAMPLE)
+        val sample1 = queryResultArb.bind()
+        val sample2 = queryResultArb.bind()
+        val response1 =
+          ExecuteQueryResponse.newBuilder().let { responseBuilder ->
+            responseBuilder.setData(sample1.hydratedStruct)
+            responseBuilder.build()
+          }
+        val response2 =
+          ExecuteQueryResponse.newBuilder().let { responseBuilder ->
+            responseBuilder.setData(sample2.hydratedStruct)
+            responseBuilder.build()
+          }
+        val dataConnectGrpcRPCs = newDataConnectGrpcRPCs(server)
+        val distinctExecuteQueryRequestArb = executeQueryRequestArb.distinct()
+        val request1 = distinctExecuteQueryRequestArb.bind()
+        val request2 = distinctExecuteQueryRequestArb.bind()
+
+        server.nextResponse = response1
+        dataConnectGrpcRPCs.executeQuery(
+          requestIdArb.bind(),
+          request1,
+          callerSdkType,
+        )
+        server.nextResponse = response2
+        dataConnectGrpcRPCs.executeQuery(
+          requestIdArb.bind(),
+          request2,
+          callerSdkType,
+        )
+        val result1 =
+          dataConnectGrpcRPCs.executeQuery(
+            requestIdArb.bind(),
+            request1,
+            callerSdkType,
+          )
+        val result2 =
+          dataConnectGrpcRPCs.executeQuery(
+            requestIdArb.bind(),
+            request2,
+            callerSdkType,
+          )
+
+        withClue("result1") {
+          val response1 = result1.shouldBeInstanceOf<ExecuteQueryResult.FromCache>().data
+          response1 shouldBe sample1.hydratedStruct
+        }
+        withClue("result2") {
+          val response2 = result2.shouldBeInstanceOf<ExecuteQueryResult.FromCache>().data
+          response2 shouldBe sample2.hydratedStruct
+        }
+      }
+    }
+  }
+
   private fun newDbFile() = File(temporaryFolder.newFolder(), "db.sqlite")
 
   private class StartServerResult(
@@ -122,13 +194,19 @@ class DataConnectGrpcRPCsClientUnitTest {
     val executeQueryInvocationCount: Int
       get() = connectorServiceImpl.executeQueryInvocationCount.get()
 
+    var nextResponse: ExecuteQueryResponse
+      get() = connectorServiceImpl.nextResponse.get()
+      set(value) {
+        connectorServiceImpl.nextResponse.set(value)
+      }
+
     override fun close() {
       grpcServer.shutdown()
     }
   }
 
-  private fun startServer(response: ExecuteQueryResponse): StartServerResult {
-    val connectorServiceImpl = ConnectorServiceImpl(response)
+  private fun startServer(): StartServerResult {
+    val connectorServiceImpl = ConnectorServiceImpl()
     val grpcServer =
       OkHttpServerBuilder.forPort(0, InsecureServerCredentials.create())
         .addService(connectorServiceImpl)
@@ -141,17 +219,17 @@ class DataConnectGrpcRPCsClientUnitTest {
     return StartServerResult(grpcServer, connectorServiceImpl)
   }
 
-  private class ConnectorServiceImpl(private val response: ExecuteQueryResponse) :
-    ConnectorServiceGrpc.ConnectorServiceImplBase() {
+  private class ConnectorServiceImpl : ConnectorServiceGrpc.ConnectorServiceImplBase() {
 
     val executeQueryInvocationCount = AtomicInteger(0)
+    val nextResponse = AtomicReference(ExecuteQueryResponse.getDefaultInstance())
 
     override fun executeQuery(
       request: ExecuteQueryRequest,
       responseObserver: StreamObserver<ExecuteQueryResponse>,
     ) {
       executeQueryInvocationCount.incrementAndGet()
-      responseObserver.onNext(response)
+      responseObserver.onNext(nextResponse.get())
       responseObserver.onCompleted()
     }
   }
