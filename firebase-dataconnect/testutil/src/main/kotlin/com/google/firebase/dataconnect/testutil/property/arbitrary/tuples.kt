5a3456628a0fb70fc85313d9f731c4f3529e5a81
Denver Coneybeare <dconeybe@google.com>
2026-02-20 16:39:07 -0500

    testutil/property/arbitrary/tuples.kt: add toList(), sorted(), and toTwoValues(), toThreeValues(), etc.

--- firebase-dataconnect/testutil/src/main/kotlin/com/google/firebase/dataconnect/testutil/property/arbitrary/tuples.kt
+++ firebase-dataconnect/testutil/src/main/kotlin/com/google/firebase/dataconnect/testutil/property/arbitrary/tuples.kt
@@ -23,7 +23,17 @@ import io.kotest.property.arbitrary.bind
 data class TwoValues<T>(val value1: T, val value2: T) {
   override fun toString() =
     "TwoValues(" + "value1=${value1.print().value}, " + "value2=${value1.print().value})"
+
+  fun toList(): List<T> = listOf(value1, value2)
 }
+
+fun <T> List<T>.toTwoValues(): TwoValues<T> {
+  require(size == 2) { "size is $size, but it must be exactly 2" }
+  return TwoValues(get(0), get(1))
+}
+
+fun <T : Comparable<T>> TwoValues<T>.sorted(): TwoValues<T> =
+  if (value1 < value2) this else TwoValues(value2, value1)
 
 data class ThreeValues<T>(val value1: T, val value2: T, val value3: T) {
   override fun toString() =
@@ -31,6 +41,22 @@ data class ThreeValues<T>(val value1: T, val value2: T, val value3: T) {
       "value1=${value1.print().value}, " +
       "value2=${value2.print().value}, " +
       "value3=${value3.print().value})"
+
+  fun toList(): List<T> = listOf(value1, value2, value3)
+}
+
+fun <T : Comparable<T>> ThreeValues<T>.sorted(): ThreeValues<T> {
+  if (value1 < value2 && value2 < value3) {
+    return this
+  }
+  val values = mutableListOf(value1, value2, value3)
+  values.sort()
+  return ThreeValues(values[0], values[1], values[2])
+}
+
+fun <T> List<T>.toThreeValues(): ThreeValues<T> {
+  require(size == 3) { "size is $size, but it must be exactly 3" }
+  return ThreeValues(get(0), get(1), get(2))
 }
 
 data class FourValues<T>(val value1: T, val value2: T, val value3: T, val value4: T) {
@@ -40,6 +66,22 @@ data class FourValues<T>(val value1: T, val value2: T, val value3: T, val value4
       "value2=${value2.print().value}, " +
       "value3=${value3.print().value}, " +
       "value4=${value4.print().value})"
+
+  fun toList(): List<T> = listOf(value1, value2, value3, value4)
+}
+
+fun <T> List<T>.toFourValues(): FourValues<T> {
+  require(size == 4) { "size is $size, but it must be exactly 4" }
+  return FourValues(get(0), get(1), get(2), get(3))
+}
+
+fun <T : Comparable<T>> FourValues<T>.sorted(): FourValues<T> {
+  if (value1 <= value2 && value2 <= value3 && value3 <= value4) {
+    return this
+  }
+  val values = mutableListOf(value1, value2, value3, value4)
+  values.sort()
+  return FourValues(values[0], values[1], values[2], values[3])
 }
 
 data class FiveValues<T>(
@@ -56,6 +98,22 @@ data class FiveValues<T>(
       "value3=${value3.print().value}, " +
       "value4=${value4.print().value}, " +
       "value5=${value5.print().value})"
+
+  fun toList(): List<T> = listOf(value1, value2, value3, value4, value5)
+}
+
+fun <T> List<T>.toFiveValues(): FiveValues<T> {
+  require(size == 5) { "size is $size, but it must be exactly 5" }
+  return FiveValues(get(0), get(1), get(2), get(3), get(4))
+}
+
+fun <T : Comparable<T>> FiveValues<T>.sorted(): FiveValues<T> {
+  if (value1 <= value2 && value2 <= value3 && value3 <= value4 && value4 < value5) {
+    return this
+  }
+  val values = mutableListOf(value1, value2, value3, value4, value5)
+  values.sort()
+  return FiveValues(values[0], values[1], values[2], values[3], values[4])
 }
 
 fun <T> Arb.Companion.twoValues(arb: Arb<T>): Arb<TwoValues<T>> =
