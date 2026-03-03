c27fd43ae9650d755efb5e329d36043e2715605e
Denver Coneybeare <dconeybe@google.com>
2026-03-03 18:12:28 -0500

    dataconnect(test): QueryCachingIntegrationTest.kt updated with normalized caching tests for string lists and Any scalars (#7863)

--- firebase-dataconnect/testutil/src/main/kotlin/com/google/firebase/dataconnect/testutil/AnyScalarTestUtils.kt
+++ firebase-dataconnect/testutil/src/main/kotlin/com/google/firebase/dataconnect/testutil/AnyScalarTestUtils.kt
@@ -16,17 +16,46 @@
 
 package com.google.firebase.dataconnect.testutil
 
+import com.google.protobuf.Value as ValueProto
+
 @JvmName("expectedAnyScalarRoundTripValueOrNull")
 fun expectedAnyScalarRoundTripValue(value: Any?): Any? =
   if (value === null) null else expectedAnyScalarRoundTripValue(value)
 
 fun expectedAnyScalarRoundTripValue(value: Any): Any =
   when (value) {
-    -0.0 -> 0.0
-    Double.NaN -> "NaN"
-    Double.POSITIVE_INFINITY -> "Infinity"
-    Double.NEGATIVE_INFINITY -> "-Infinity"
+    is Double -> expectedAnyScalarDoubleRoundTripValue(value).value
     is List<*> -> value.map { expectedAnyScalarRoundTripValue(it) }
     is Map<*, *> -> value.mapValues { expectedAnyScalarRoundTripValue(it.value) }
     else -> value
+  }
+
+sealed interface DoubleOrString<T : Any> {
+
+  val value: T
+
+  fun toValueProto(): ValueProto
+
+  @JvmInline
+  value class String(override val value: kotlin.String) : DoubleOrString<kotlin.String> {
+    override fun toValueProto(): ValueProto = value.toValueProto()
+  }
+
+  @JvmInline
+  value class Double(override val value: kotlin.Double) : DoubleOrString<kotlin.Double> {
+    override fun toValueProto(): ValueProto = value.toValueProto()
+  }
+}
+
+fun expectedAnyScalarDoubleRoundTripValue(value: Double): DoubleOrString<*> =
+  when (value) {
+    -0.0 -> DoubleOrString.Double(0.0)
+    Double.POSITIVE_INFINITY -> DoubleOrString.String("Infinity")
+    Double.NEGATIVE_INFINITY -> DoubleOrString.String("-Infinity")
+    else ->
+      if (value.isNaN()) {
+        DoubleOrString.String("NaN")
+      } else {
+        DoubleOrString.Double(value)
+      }
   }
