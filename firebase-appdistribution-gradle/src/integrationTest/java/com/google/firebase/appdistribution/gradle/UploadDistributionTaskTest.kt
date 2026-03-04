47e0220292ed96100bfe2965aaee2416a20e343d
elenadoty <elenadoty@google.com>
2026-03-04 12:57:52 -0500

    Update UploadDistributionTaskTest.kt

--- firebase-appdistribution-gradle/src/integrationTest/java/com/google/firebase/appdistribution/gradle/UploadDistributionTaskTest.kt
+++ firebase-appdistribution-gradle/src/integrationTest/java/com/google/firebase/appdistribution/gradle/UploadDistributionTaskTest.kt
@@ -724,7 +724,7 @@ class UploadDistributionTaskTest {
     // Also remember to update the latest AGP/gradle versions in BeePlusGradleProject.java.
     // firebase-appdistribution-gradle/src/prodTest/java/com/google/firebase/appdistribution/gradle/BeePlusGradleProject.java#L59-L60
     private const val LATEST_GRADLE_VERSION = "9.4.0"
-    private const val LATEST_AGP_VERSION = "9.1.0-alpha07"
+    private const val LATEST_AGP_VERSION = "9.2.0-alpha02"
     private const val LATEST_GOOGLE_SERVICES_VERSION = "4.4.4"
     // For tests against Gradle 9, we get the error:
     // "In order to compile Java 9+ source, please set compileSdkVersion to 30 or above"
