5829f8961ab6f41f1c804e06721f57d433d8afc6
Rodrigo Lazo Paz <rlazo@google.com>
2026-03-09 16:25:33 -0400

    Add missing import

--- ai-logic/firebase-ai/src/main/kotlin/com/google/firebase/ai/TemplateImagenModel.kt
+++ ai-logic/firebase-ai/src/main/kotlin/com/google/firebase/ai/TemplateImagenModel.kt
@@ -19,6 +19,7 @@ package com.google.firebase.ai
 import com.google.firebase.FirebaseApp
 import com.google.firebase.ai.common.APIController
 import com.google.firebase.ai.common.AppCheckHeaderProvider
+import com.google.firebase.ai.common.JSON
 import com.google.firebase.ai.common.TemplateGenerateImageRequest
 import com.google.firebase.ai.type.FirebaseAIException
 import com.google.firebase.ai.type.ImagenGenerationResponse
