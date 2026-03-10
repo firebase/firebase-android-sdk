23177673c367706ddf7695a06143bc0407818065
Mila <107142260+milaGGL@users.noreply.github.com>
2026-03-10 16:17:50 -0400

    fix(firestore): update the test case for mapGet

--- firebase-firestore/src/androidTest/java/com/google/firebase/firestore/PipelineTest.java
+++ firebase-firestore/src/androidTest/java/com/google/firebase/firestore/PipelineTest.java
@@ -1690,7 +1690,7 @@ public class PipelineTest {
         .containsExactly(
             mapOfEntries(
                 entry("title", "The Hitchhiker's Guide to the Galaxy"), entry("nested", true)),
-            mapOfEntries(entry("title", "Dune")));
+            mapOfEntries(entry("title", "Dune"), entry("nested", null)));
   }
 
   @Test
