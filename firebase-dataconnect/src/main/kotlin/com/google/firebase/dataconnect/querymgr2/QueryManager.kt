42d83515a5ce5f586abdacf048d0381b71b426b0
Denver Coneybeare <dconeybe@google.com>
2026-04-07 15:54:31 -0400

    QueryManager.kt: added with start() and close()

---
+++ firebase-dataconnect/src/main/kotlin/com/google/firebase/dataconnect/querymgr2/QueryManager.kt
@@ -0,0 +1,191 @@
+/*
+ * Copyright 2026 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.firebase.dataconnect.querymgr2
+
+import com.google.firebase.dataconnect.core.DataConnectAppCheck
+import com.google.firebase.dataconnect.core.DataConnectAuth
+import com.google.firebase.dataconnect.core.DataConnectGrpcRPCs
+import com.google.firebase.dataconnect.core.Logger
+import com.google.firebase.dataconnect.core.LoggerGlobals.Logger
+import com.google.firebase.dataconnect.core.LoggerGlobals.debug
+import com.google.firebase.dataconnect.core.LoggerGlobals.warn
+import com.google.firebase.dataconnect.sqlite.DataConnectCacheDatabase
+import com.google.firebase.dataconnect.util.RequestIdGenerator
+import java.io.File
+import kotlin.time.Duration
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+
+internal class QueryManager(
+  private val requestName: String,
+  dataConnectGrpcRPCs: DataConnectGrpcRPCs,
+  dataConnectAuth: DataConnectAuth,
+  dataConnectAppCheck: DataConnectAppCheck,
+  ioDispatcher: CoroutineDispatcher,
+  cpuDispatcher: CoroutineDispatcher,
+  requestIdGenerator: RequestIdGenerator,
+  cacheSettings: CacheSettings?,
+  currentTimeMillis: () -> Long,
+  private val logger: Logger,
+) {
+
+  suspend fun start() {
+    val newState: State.New =
+      mutex.withLock {
+        val newState: State.New =
+          when (val currentState = this.state) {
+            is State.New -> currentState
+            is State.Started,
+            State.Starting ->
+              throw IllegalStateException(
+                "start() has already been called (state=$currentState) [bn64pmgp98]"
+              )
+            State.Closed ->
+              throw IllegalStateException("start() cannot be called after close() [n7pc2n7xcd]")
+          }
+
+        this.state = State.Starting
+        newState
+      }
+
+    val coroutineScope =
+      CoroutineScope(
+        SupervisorJob() +
+          CoroutineName(logger.nameWithId) +
+          CoroutineExceptionHandler { context, throwable ->
+            logger.warn(throwable) {
+              "uncaught exception from a coroutine named ${context[CoroutineName]}: " +
+                "$throwable [ekx3ehgakw]"
+            }
+          }
+      )
+
+    val cacheDb: DataConnectCacheDatabase? =
+      newState.cacheSettings?.run {
+        val dbLogger = Logger("DataConnectCacheDatabase")
+        dbLogger.debug { "created by ${logger.nameWithId}" }
+        DataConnectCacheDatabase(dbFile, dbLogger)
+      }
+
+    val startedState =
+      newState.run {
+        State.Started(
+          coroutineScope = coroutineScope,
+          dataConnectGrpcRPCs = dataConnectGrpcRPCs,
+          dataConnectAuth = dataConnectAuth,
+          dataConnectAppCheck = dataConnectAppCheck,
+          ioDispatcher = ioDispatcher,
+          cpuDispatcher = cpuDispatcher,
+          requestIdGenerator = requestIdGenerator,
+          cacheDb = cacheDb,
+          currentTimeMillis = currentTimeMillis,
+        )
+      }
+
+    val oldState =
+      mutex.withLock {
+        val currentState = this.state
+        if (currentState == State.Starting) {
+          this.state = startedState
+        }
+        currentState
+      }
+
+    when (oldState) {
+      State.Starting -> {}
+      is State.Closed -> startedState.close()
+      is State.New,
+      is State.Started ->
+        throw IllegalStateException(
+          "internal error z5snhkxnf2: " + "unexpected value for this.state: $oldState"
+        )
+    }
+  }
+
+  suspend fun close() {
+    val currentState = mutex.withLock { this.state }
+
+    if (currentState is State.Started) {
+      currentState.close()
+    }
+
+    mutex.withLock { this.state = State.Closed }
+  }
+
+  private suspend fun State.Started.close() {
+    coroutineScope.cancel("close() called")
+    cacheDb?.close()
+  }
+
+  private val mutex = Mutex()
+  private var state: State =
+    State.New(
+      dataConnectGrpcRPCs = dataConnectGrpcRPCs,
+      dataConnectAuth = dataConnectAuth,
+      dataConnectAppCheck = dataConnectAppCheck,
+      ioDispatcher = ioDispatcher,
+      cpuDispatcher = cpuDispatcher,
+      requestIdGenerator = requestIdGenerator,
+      cacheSettings = cacheSettings,
+      currentTimeMillis = currentTimeMillis,
+    )
+
+  private sealed interface State {
+    data class New(
+      val dataConnectGrpcRPCs: DataConnectGrpcRPCs,
+      val dataConnectAuth: DataConnectAuth,
+      val dataConnectAppCheck: DataConnectAppCheck,
+      val ioDispatcher: CoroutineDispatcher,
+      val cpuDispatcher: CoroutineDispatcher,
+      val requestIdGenerator: RequestIdGenerator,
+      val cacheSettings: CacheSettings?,
+      val currentTimeMillis: () -> Long,
+    ) : State {
+      override fun toString() = "QueryManager.State.New"
+    }
+
+    data object Starting : State {
+      override fun toString() = "QueryManager.State.Starting"
+    }
+
+    data class Started(
+      val coroutineScope: CoroutineScope,
+      val dataConnectGrpcRPCs: DataConnectGrpcRPCs,
+      val dataConnectAuth: DataConnectAuth,
+      val dataConnectAppCheck: DataConnectAppCheck,
+      val ioDispatcher: CoroutineDispatcher,
+      val cpuDispatcher: CoroutineDispatcher,
+      val requestIdGenerator: RequestIdGenerator,
+      val cacheDb: DataConnectCacheDatabase?,
+      val currentTimeMillis: () -> Long
+    ) : State {
+      override fun toString() = "QueryManager.State.Started"
+    }
+
+    data object Closed : State {
+      override fun toString() = "QueryManager.State.Closed"
+    }
+  }
+
+  data class CacheSettings(val dbFile: File?, val maxAge: Duration)
+}
