748f984eec2731a4342c1609e40a3d2e84f8785f
Denver Coneybeare <dconeybe@google.com>
2026-04-23 15:17:47 -0400

    LaterValueUnitTest.kt added

---
+++ firebase-dataconnect/src/test/kotlin/com/google/firebase/dataconnect/util/LaterValueUnitTest.kt
@@ -0,0 +1,190 @@
+/*
+ * Copyright 2026 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.firebase.dataconnect.util
+
+import com.google.firebase.dataconnect.testutil.property.arbitrary.triple
+import io.kotest.assertions.withClue
+import io.kotest.common.ExperimentalKotest
+import io.kotest.matchers.result.shouldBeFailure
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.types.shouldBeSameInstanceAs
+import io.kotest.property.Arb
+import io.kotest.property.EdgeConfig
+import io.kotest.property.PropTestConfig
+import io.kotest.property.PropertyContext
+import io.kotest.property.ShrinkingMode
+import io.kotest.property.arbitrary.map
+import io.kotest.property.arbitrary.orNull
+import io.kotest.property.arbitrary.string
+import io.kotest.property.checkAll
+import io.mockk.confirmVerified
+import io.mockk.mockk
+import io.mockk.verify
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+
+class LaterValueUnitTest {
+
+  @Test
+  fun `isSet returns false initially`() = runTest {
+    val laterValue = LaterValue<TestValue>()
+
+    laterValue.isSet shouldBe false
+  }
+
+  @Test
+  fun `isSet returns true after set()`() = testWithNullableTestValues { value ->
+    val laterValue = LaterValue<TestValue?>()
+    laterValue.set(value)
+
+    laterValue.isSet shouldBe true
+  }
+
+  @Test
+  fun `set() throws after first invocation`() = runTest {
+    checkAll(propTestConfig, testValueArb().orNull(nullProbability = 0.33).triple()) {
+      (value1, value2, value3) ->
+      val laterValue = LaterValue<TestValue?>()
+      laterValue.set(value1)
+
+      val result1 = laterValue.runCatching { set(value2) }
+      val result2 = laterValue.runCatching { set(value3) }
+
+      val expectedFailure = IllegalStateException("set() has already been called")
+      withClue("result1") { result1.shouldBeFailure(expectedFailure) }
+      withClue("result2") { result2.shouldBeFailure(expectedFailure) }
+    }
+  }
+
+  @Test
+  fun `getOrNull() returns null initially`() = runTest {
+    val laterValue = LaterValue<TestValue>()
+
+    laterValue.getOrNull() shouldBe null
+  }
+
+  @Test fun `getOrNull() returns the set value`() = testGetReturnsSetValue { getOrNull() }
+
+  @Test
+  fun `getOrThrow() throws initially`() = runTest {
+    val laterValue = LaterValue<TestValue>()
+
+    val result = laterValue.runCatching { getOrThrow() }
+
+    result.shouldBeFailure(IllegalStateException("set() has not yet been called"))
+  }
+
+  @Test fun `getOrThrow() returns the set value`() = testGetReturnsSetValue { getOrThrow() }
+
+  @Test
+  fun `toString() before set() is called`() = runTest {
+    val laterValue = LaterValue<TestValue>()
+    laterValue.toString() shouldBe "<unset>"
+  }
+
+  @Test
+  fun `toString() after set() is called`() = testWithNullableTestValues { value ->
+    val laterValue = LaterValue<TestValue?>()
+
+    laterValue.set(value)
+
+    laterValue.toString() shouldBe value.toString()
+  }
+
+  @Test
+  fun `onValue() before set() does not call block`() = runTest {
+    val laterValue = LaterValue<TestValue>()
+    val block: (TestValue) -> Unit = mockk(relaxed = true)
+
+    laterValue.onValue(block)
+
+    confirmVerified(block)
+  }
+
+  @Test
+  fun `onValue() after set() calls block`() = testWithNullableTestValues { value ->
+    val laterValue = LaterValue<TestValue?>()
+    laterValue.set(value)
+    val block: (TestValue?) -> Unit = mockk(relaxed = true)
+
+    laterValue.onValue(block)
+
+    verify(exactly = 1) { block(value) }
+    confirmVerified(block)
+  }
+
+  @Test
+  fun `onValue() before set() returns the receiver`() = runTest {
+    val laterValue = LaterValue<TestValue>()
+    laterValue.onValue(mockk(relaxed = true)) shouldBeSameInstanceAs laterValue
+  }
+
+  @Test
+  fun `onValue() after set() returns the receiver`() = testWithNullableTestValues { value ->
+    val laterValue = LaterValue<TestValue?>()
+    laterValue.set(value)
+
+    laterValue.onValue(mockk(relaxed = true)) shouldBeSameInstanceAs laterValue
+  }
+}
+
+/** The configuration for property-based tests in this file. */
+@OptIn(ExperimentalKotest::class)
+private val propTestConfig =
+  PropTestConfig(
+    iterations = 100,
+    edgeConfig = EdgeConfig(edgecasesGenerationProbability = 0.2),
+    shrinkingMode = ShrinkingMode.Off,
+  )
+
+/**
+ * A simple data class used as a test value to verify referential integrity and basic operations.
+ */
+private data class TestValue(val value: String)
+
+/**
+ * Creates and returns an [Arb] that generates [TestValue] instances.
+ *
+ * @param string The [Arb] used to generate the underlying string values.
+ */
+private fun testValueArb(string: Arb<String> = Arb.string()): Arb<TestValue> =
+  string.map(::TestValue)
+
+/**
+ * Runs a property-based test with nullable [TestValue] instances.
+ *
+ * @param property The property test logic to execute.
+ */
+private fun testWithNullableTestValues(property: suspend PropertyContext.(TestValue?) -> Unit) =
+  runTest {
+    checkAll(propTestConfig, testValueArb().orNull(nullProbability = 0.2), property)
+  }
+
+/**
+ * Verifies that the given [block] correctly returns the value that was set on the [LaterValue].
+ *
+ * @param T The type of the value.
+ * @param block A lambda that retrieves the value from the [LaterValue].
+ */
+private fun <T : TestValue?> testGetReturnsSetValue(block: LaterValue<TestValue?>.() -> T) =
+  testWithNullableTestValues { value ->
+    val laterValue = LaterValue<TestValue?>()
+
+    laterValue.set(value)
+
+    block(laterValue) shouldBeSameInstanceAs value
+  }
