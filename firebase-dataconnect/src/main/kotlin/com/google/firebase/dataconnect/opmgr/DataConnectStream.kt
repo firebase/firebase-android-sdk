c7ddebb28b696481edfa0b5e788654fffbee82f5
Denver Coneybeare <dconeybe@google.com>
2026-04-29 02:40:28 -0400

    ObjectLifecycleManager.kt work

--- firebase-dataconnect/src/main/kotlin/com/google/firebase/dataconnect/opmgr/DataConnectStream.kt
+++ firebase-dataconnect/src/main/kotlin/com/google/firebase/dataconnect/opmgr/DataConnectStream.kt
@@ -18,30 +18,23 @@ package com.google.firebase.dataconnect.opmgr
 
 import com.google.firebase.dataconnect.FirebaseDataConnect
 import com.google.firebase.dataconnect.core.DataConnectAppCheck
-import com.google.firebase.dataconnect.core.DataConnectAppCheck.GetAppCheckTokenResult
 import com.google.firebase.dataconnect.core.DataConnectAuth
-import com.google.firebase.dataconnect.core.DataConnectAuth.GetAuthTokenResult
 import com.google.firebase.dataconnect.core.DataConnectGrpcRPCs
 import com.google.firebase.dataconnect.core.Logger
 import com.google.firebase.dataconnect.core.LoggerGlobals.debug
-import com.google.firebase.dataconnect.util.CoroutineUtils
 import com.google.firebase.dataconnect.util.CoroutineUtils.createSupervisorCoroutineScope
 import com.google.firebase.dataconnect.util.RequestIdGenerator
-import google.firebase.dataconnect.proto.StreamRequest as StreamRequestProto
 import google.firebase.dataconnect.proto.StreamResponse as StreamResponseProto
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.CoroutineStart
-import kotlinx.coroutines.job
 import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.cancel
-import kotlinx.coroutines.channels.SendChannel
 import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.job
 
 internal class DataConnectStream(
   dataConnectGrpcRPCs: DataConnectGrpcRPCs,
@@ -51,23 +44,27 @@ internal class DataConnectStream(
   private val cpuDispatcher: CoroutineDispatcher,
   private val logger: Logger,
 ) {
-  private val state = MutableStateFlow<State>(State.Settings(dataConnectGrpcRPCs, dataConnectAuth, dataConnectAppCheck, requestIdGenerator))
+  private val state =
+    MutableStateFlow<State>(
+      State.Settings(dataConnectGrpcRPCs, dataConnectAuth, dataConnectAppCheck, requestIdGenerator)
+    )
 
   suspend fun close() {
     while (true) {
       val currentState = state.value
 
-      val newState = when (currentState) {
-        is State.Settings -> State.Closed
-        is State.Connecting -> State.Closing(currentState)
-        is State.Connected -> State.Closing(currentState)
-        is State.Closing -> {
-          currentState.coroutineScope.cancel("DataConnectStream.close() called")
-          currentState.coroutineScope.coroutineContext.job.join()
-          State.Closed
+      val newState =
+        when (currentState) {
+          is State.Settings -> State.Closed
+          is State.Connecting -> State.Closing(currentState)
+          is State.Connected -> State.Closing(currentState)
+          is State.Closing -> {
+            currentState.coroutineScope.cancel("DataConnectStream.close() called")
+            currentState.coroutineScope.coroutineContext.job.join()
+            State.Closed
+          }
+          State.Closed -> return
         }
-        State.Closed -> return
-      }
 
       state.compareAndSet(currentState, newState)
     }
@@ -77,21 +74,25 @@ internal class DataConnectStream(
     while (true) {
       val currentState = state.value
 
-      val newState = when (currentState) {
-        is State.Settings -> currentState.run {
-          val coroutineScope = createSupervisorCoroutineScope(cpuDispatcher, logger)
-          val job = coroutineScope.async(start = CoroutineStart.LAZY) {
-            connect(currentState, requestId)
+      val newState =
+        when (currentState) {
+          is State.Settings ->
+            currentState.run {
+              val coroutineScope = createSupervisorCoroutineScope(cpuDispatcher, logger)
+              val job =
+                coroutineScope.async(start = CoroutineStart.LAZY) {
+                  connect(currentState, requestId)
+                }
+              State.Connecting(currentState, coroutineScope, job)
+            }
+          is State.Connecting -> {
+            currentState.job.join()
+            State.Connected(currentState)
           }
-          State.Connecting(currentState, coroutineScope, job)
+          is State.Connected -> return currentState
+          is State.Closing,
+          State.Closed -> error("close() has been called")
         }
-        is State.Connecting -> {
-          currentState.job.join()
-          State.Connected(currentState)
-        }
-        is State.Connected -> return currentState
-        is State.Closing, State.Closed -> error("close() has been called")
-      }
 
       state.compareAndSet(currentState, newState)
     }
@@ -101,27 +102,28 @@ internal class DataConnectStream(
     settings: State.Settings,
     requestId: String,
     callerSdkType: FirebaseDataConnect.CallerSdkType,
-  ): DataConnectGrpcRPCs.ConnectResult = settings.run {
-    val streamId = requestIdGenerator.nextStreamId()
-    logger.debug { "[rid=requestId, sid=$streamId] Connecting to Data Connect backend" }
+  ): DataConnectGrpcRPCs.ConnectResult =
+    settings.run {
+      val streamId = requestIdGenerator.nextStreamId()
+      logger.debug { "[rid=requestId, sid=$streamId] Connecting to Data Connect backend" }
 
-    val authTokenJob = async { dataConnectAuth.getToken(requestId)}
-    val appCheckTokenJob = async { dataConnectAppCheck.getToken(requestId)}
-    awaitAll(authTokenJob, appCheckTokenJob)
-    val authToken = authTokenJob.await()
-    val appCheckToken = appCheckTokenJob.await()
+      val authTokenJob = async { dataConnectAuth.getToken(requestId) }
+      val appCheckTokenJob = async { dataConnectAppCheck.getToken(requestId) }
+      awaitAll(authTokenJob, appCheckTokenJob)
+      val authToken = authTokenJob.await()
+      val appCheckToken = appCheckTokenJob.await()
 
-    dataConnectGrpcRPCs.connect2(
-      streamId=streamId,
-      authToken=authToken,
-      appCheckToken=appCheckToken,
-      callerSdkType=callerSdkType,
-    )
-  }
+      dataConnectGrpcRPCs.connect2(
+        streamId = streamId,
+        authToken = authToken,
+        appCheckToken = appCheckToken,
+        callerSdkType = callerSdkType,
+      )
+    }
 
   private sealed interface StreamResponse {
-    class Success(val response: StreamResponseProto): StreamResponse
-    class Error(val response: StreamResponseProto): StreamResponse
+    class Success(val response: StreamResponseProto) : StreamResponse
+    class Error(val response: StreamResponseProto) : StreamResponse
   }
 
   private sealed interface State {
@@ -131,31 +133,30 @@ internal class DataConnectStream(
       val dataConnectAuth: DataConnectAuth,
       val dataConnectAppCheck: DataConnectAppCheck,
       val requestIdGenerator: RequestIdGenerator,
-    ): State
+    ) : State
 
     class Connecting(
       val settings: Settings,
       val coroutineScope: CoroutineScope,
       val job: Deferred<Unit>,
-    ): State
+    ) : State
 
     class Connected(
       val settings: Settings,
       val authUid: String?,
       val coroutineScope: CoroutineScope,
-    ): State {
-      constructor(state: Connecting, authUid: String?) : this(state.settings, authUid, state.coroutineScope)
+    ) : State {
+      constructor(
+        state: Connecting,
+        authUid: String?
+      ) : this(state.settings, authUid, state.coroutineScope)
     }
 
-    class Closing(val coroutineScope: CoroutineScope): State {
+    class Closing(val coroutineScope: CoroutineScope) : State {
       constructor(state: Connecting) : this(state.coroutineScope)
       constructor(state: Connected) : this(state.coroutineScope)
     }
 
     object Closed : State
-
   }
-
-
 }
-
--- firebase-dataconnect/src/main/kotlin/com/google/firebase/dataconnect/opmgr/RemoteQueries.kt
+++ firebase-dataconnect/src/main/kotlin/com/google/firebase/dataconnect/opmgr/RemoteQueries.kt
@@ -19,109 +19,68 @@ package com.google.firebase.dataconnect.opmgr
 import com.google.firebase.dataconnect.core.Logger
 import com.google.firebase.dataconnect.core.LoggerGlobals.debug
 import com.google.firebase.dataconnect.util.ImmutableByteArray
+import com.google.firebase.dataconnect.util.ObjectLifecycleManager
 import com.google.firebase.dataconnect.util.SuspendingWeakValueHashMap
 import com.google.firebase.dataconnect.util.getOrPut
 import com.google.protobuf.Struct
 import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.flow.MutableStateFlow
 
 internal class RemoteQueries(
   executeFunction: RemoteQueryExecuteFunction,
   cacheManager: CacheManager?,
-  cpuDispatcher: CoroutineDispatcher,
+  private val cpuDispatcher: CoroutineDispatcher,
+  ioDispatcher: CoroutineDispatcher,
   private val logger: Logger,
-) : AutoCloseable {
+) {
 
-  private val state =
-    MutableStateFlow<State>(State.Settings(executeFunction, cacheManager, cpuDispatcher))
+  private val lifecycleManager =
+    ObjectLifecycleManager<State>(
+      open = { dest ->
+        logger.debug { "opening" }
+        dest.set(
+          State(
+            map = SuspendingWeakValueHashMap(ioDispatcher),
+            executeFunction = executeFunction,
+            cacheManager = cacheManager,
+          )
+        )
+      },
+      close = { it.map.close() },
+      cpuDispatcher,
+      logger
+    )
 
-  override fun close() {
-    logger.debug { "close() called" }
-
-    while (true) {
-      val currentState = this.state.value
-
-      val newState =
-        when (currentState) {
-          is State.Settings -> State.Closed
-          is State.Open -> State.Closing(currentState)
-          is State.Closing -> {
-            currentState.map.close()
-            State.Closed
-          }
-          State.Closed -> return
-        }
-
-      state.compareAndSet(currentState, newState)
-    }
+  suspend fun close() {
+    logger.debug { "close()" }
+    lifecycleManager.close()
   }
 
   suspend fun get(
     key: Key,
     operationName: String,
     variables: Struct,
-  ): RemoteQuery =
-    ensureOpen().run {
-      map.getOrPut(key) {
-        RemoteQuery(
-          queryId = key.queryId,
-          operationName = operationName,
-          variables = variables,
-          executeFunction = this.executeFunction,
-          cacheManager = this.cacheManager,
-          cpuDispatcher = cpuDispatcher,
-          logger = logger,
-        )
-      }
-    }
-
-  private fun ensureOpen(): State.Open {
-    while (true) {
-      val currentState = state.value
-
-      val newState =
-        when (currentState) {
-          is State.Settings -> State.Open(currentState)
-          is State.Open -> return currentState
-          is State.Closing,
-          State.Closed -> error("close() has been called")
-        }
-
-      state.compareAndSet(currentState, newState)
+  ): RemoteQuery {
+    val state = lifecycleManager.open()
+    return state.map.getOrPut(key) {
+      RemoteQuery(
+        queryId = key.queryId,
+        operationName = operationName,
+        variables = variables,
+        executeFunction = state.executeFunction,
+        cacheManager = state.cacheManager,
+        cpuDispatcher = cpuDispatcher,
+        logger = logger,
+      )
     }
   }
+
+  private class State(
+    val map: SuspendingWeakValueHashMap<Key, RemoteQuery>,
+    val executeFunction: RemoteQueryExecuteFunction,
+    val cacheManager: CacheManager?,
+  )
 
   data class Key(
     val queryId: ImmutableByteArray,
   )
-
-  private sealed interface State {
-    class Settings(
-      val executeFunction: RemoteQueryExecuteFunction,
-      val cacheManager: CacheManager?,
-      val cpuDispatcher: CoroutineDispatcher,
-    ) : State
-
-    class Open(
-      val map: SuspendingWeakValueHashMap<Key, RemoteQuery>,
-      val executeFunction: RemoteQueryExecuteFunction,
-      val cacheManager: CacheManager?,
-      val cpuDispatcher: CoroutineDispatcher,
-    ) : State {
-      constructor(
-        state: Settings
-      ) : this(
-        SuspendingWeakValueHashMap(state.cpuDispatcher),
-        state.executeFunction,
-        state.cacheManager,
-        state.cpuDispatcher,
-      )
-    }
-
-    class Closing(val map: SuspendingWeakValueHashMap<Key, RemoteQuery>) : State {
-      constructor(state: Open) : this(state.map)
-    }
-
-    object Closed : State
-  }
 }
