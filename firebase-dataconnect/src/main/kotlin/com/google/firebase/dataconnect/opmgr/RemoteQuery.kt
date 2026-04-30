82911c6cd131b388ffe239a8339f555cdf766d1b
Denver Coneybeare <dconeybe@google.com>
2026-04-30 02:20:37 -0400

    RemoteQuery.kt: rewritten

--- firebase-dataconnect/src/main/kotlin/com/google/firebase/dataconnect/opmgr/RemoteQuery.kt
+++ firebase-dataconnect/src/main/kotlin/com/google/firebase/dataconnect/opmgr/RemoteQuery.kt
@@ -19,70 +19,72 @@ package com.google.firebase.dataconnect.opmgr
 import com.google.firebase.dataconnect.FirebaseDataConnect
 import com.google.firebase.dataconnect.core.Logger
 import com.google.firebase.dataconnect.core.LoggerGlobals.warn
-import com.google.firebase.dataconnect.util.CoroutineUtils.createSupervisorCoroutineScope
 import com.google.firebase.dataconnect.util.ImmutableByteArray
+import com.google.firebase.dataconnect.util.ObjectLifecycleManager
 import com.google.firebase.dataconnect.util.SequenceNumberConflatedJobQueue
 import com.google.firebase.dataconnect.util.SequencedReference
-import com.google.protobuf.Struct
+import com.google.firebase.dataconnect.util.open
+import google.firebase.dataconnect.proto.ExecuteRequest
 import kotlinx.coroutines.CoroutineDispatcher
-
-internal typealias RemoteQueryExecuteFunction =
-  suspend (
-    requestId: String,
-    operationName: String,
-    variables: Struct,
-  ) -> ExecuteResponse
+import kotlinx.coroutines.flow.first
 
 internal class RemoteQuery(
   private val queryId: ImmutableByteArray,
-  private val operationName: String,
-  private val variables: Struct,
-  private val executeFunction: RemoteQueryExecuteFunction,
+  private val executeRequest: ExecuteRequest,
+  private val dataConnectStream: DataConnectStream,
   private val cacheManager: CacheManager?,
   cpuDispatcher: CoroutineDispatcher,
   private val logger: Logger,
 ) {
 
-  suspend fun execute(
-    requestId: String,
-    sequenceNumber: Long,
-    callerSdkType: FirebaseDataConnect.CallerSdkType,
-  ): SequencedReference<ExecuteResponse> =
-    jobQueue.execute(
-      sequenceNumber,
-      ExecuteParams(requestId = requestId, callerSdkType = callerSdkType),
-    )
-
-  private suspend fun execute(params: SequencedReference<ExecuteParams>): ExecuteResponse =
-    execute(params.ref)
-
-  private suspend fun execute(params: ExecuteParams): ExecuteResponse =
-    params.run {
-      val response = executeFunction(requestId, operationName, variables)
-
-      cacheManager
-        ?.runCatching {
-          insertQueryResult(
-            params.requestId,
-            response.authUid,
-            queryId,
-            response.data,
-            response.extensions
-          )
-        }
-        ?.onFailure { logger.warn(it) { "[rid=$requestId] failed to update cache" } }
-
-      response
+  private val lifecycle =
+    ObjectLifecycleManager<_, Unit>(cpuDispatcher, logger) {
+      SequenceNumberConflatedJobQueue<ExecuteParams, ExecuteResponse>(lifetimeScope) {
+        executeFromJobQueue(it.ref.requestId, it.ref.callerSdkType)
+      }
     }
 
-  private val jobQueue =
-    SequenceNumberConflatedJobQueue<ExecuteParams, ExecuteResponse>(
-      createSupervisorCoroutineScope(cpuDispatcher, logger),
-      ::execute
-    )
+  suspend fun close() {
+    lifecycle.close()
+  }
+
+  suspend fun execute(
+    sequenceNumber: Long,
+    requestId: String,
+    callerSdkType: FirebaseDataConnect.CallerSdkType,
+  ): SequencedReference<ExecuteResponse> =
+    lifecycle.open().enqueue(sequenceNumber, requestId, callerSdkType)
+
+  private suspend fun executeFromJobQueue(
+    requestId: String,
+    callerSdkType: FirebaseDataConnect.CallerSdkType,
+  ): ExecuteResponse {
+    val response = dataConnectStream.execute(requestId, executeRequest, callerSdkType).first()
+
+    cacheManager
+      ?.runCatching {
+        insertQueryResult(requestId, response.authUid, queryId, response.data, response.extensions)
+      }
+      ?.onFailure { logger.warn(it) { "[rid=$requestId] failed to update cache" } }
+
+    return response
+  }
 
   private class ExecuteParams(
     val requestId: String,
     val callerSdkType: FirebaseDataConnect.CallerSdkType,
   )
+
+  companion object {
+
+    private suspend fun SequenceNumberConflatedJobQueue<ExecuteParams, ExecuteResponse>.enqueue(
+      sequenceNumber: Long,
+      requestId: String,
+      callerSdkType: FirebaseDataConnect.CallerSdkType,
+    ) =
+      execute(
+        sequenceNumber,
+        ExecuteParams(requestId = requestId, callerSdkType = callerSdkType),
+      )
+  }
 }
