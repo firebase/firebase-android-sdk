45b3ed1abdfc42458cb9d193d88a8b443d805641
Denver Coneybeare <dconeybe@google.com>
2026-05-01 17:53:53 -0400

    convert bash shell scripts to zsh

--- firebase-dataconnect/scripts/compile_kotlin.zsh
+++ firebase-dataconnect/scripts/compile_kotlin.zsh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env zsh
 
-# Copyright 2024 Google LLC
+# Copyright 2026 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,12 +14,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -euo pipefail
+setopt errexit nounset pipefail
 
-PROJECT_ROOT_DIR="$(dirname "$0")/../.."
-readonly PROJECT_ROOT_DIR
+source "${0:A:h}/util/say.zsh"
 
-readonly TARGETS=(
+typeset -r project_root_dir="${0:A:h:h:h}"
+
+typeset -r targets=(
   ":firebase-dataconnect:compileDebugKotlin"
   ":firebase-dataconnect:compileDebugUnitTestKotlin"
   ":firebase-dataconnect:compileDebugAndroidTestKotlin"
@@ -34,14 +35,14 @@ readonly TARGETS=(
   ":firebase-dataconnect:testutil:compileDebugAndroidTestKotlin"
 )
 
-readonly args=(
-  "${PROJECT_ROOT_DIR}/gradlew"
+typeset -r args=(
+  "$project_root_dir/gradlew"
   "-p"
-  "${PROJECT_ROOT_DIR}"
+  "$project_root_dir"
   "--configure-on-demand"
   "$@"
-  "${TARGETS[@]}"
+  "${targets[@]}"
 )
 
-echo "${args[*]}"
-exec "${args[@]}"
+say_args "${args[@]}"
+exec "${args[@]}" # zshellcheck disable=ZC1909
