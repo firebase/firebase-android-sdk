bd10a823e6126d57166140b198d6126ac465878e
Denver Coneybeare <dconeybe@google.com>
2026-05-21 15:04:24 -0400

    dataconnect(docs): QueryRef.kt/QuerySubscription.kt: kdoc improvements, especially adding BoM version in addition to SDK version (see cl/918573071)

--- firebase-dataconnect/src/main/kotlin/com/google/firebase/dataconnect/QueryRef.kt
+++ firebase-dataconnect/src/main/kotlin/com/google/firebase/dataconnect/QueryRef.kt
@@ -76,11 +76,11 @@ public interface QueryRef<Data, Variables> : OperationRef<Data, Variables> {
    *
    * ### Realtime updates (May 2026)
    *
-   * Starting with SDK version 17.3.0 (May 2026) query subscriptions gained support for realtime
-   * updates. See
+   * Starting with SDK version 17.3.0 (Firebase BoM 34.13.0, May 2026) query subscriptions gained
+   * support for realtime updates. See
    * [Get real-time updates from SQL Connect](https://firebase.google.com/docs/sql-connect/realtime)
    * for details about this feature in the entire product, and [QuerySubscription] for details
-   * specified to Android.
+   * specific to Android.
    *
    * Prior to SDK version 17.3.0, updates were _not_ realtime, and were _not_ pushed from the
    * server. Instead, the notifications were sent whenever the query was explicitly executed by
