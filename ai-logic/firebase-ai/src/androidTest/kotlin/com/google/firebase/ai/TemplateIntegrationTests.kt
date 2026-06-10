8379086bcfa0ddca971acf4d8fd08b172ed67d91
Emily Ploszaj <emilyploszaj@google.com>
2026-06-10 14:54:07 -0500

    Ignore flakey daily AI Logic test

--- ai-logic/firebase-ai/src/androidTest/kotlin/com/google/firebase/ai/TemplateIntegrationTests.kt
+++ ai-logic/firebase-ai/src/androidTest/kotlin/com/google/firebase/ai/TemplateIntegrationTests.kt
@@ -29,6 +29,7 @@ import io.kotest.matchers.string.shouldNotContain
 import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.runBlocking
 import org.junit.Before
+import org.junit.Ignore
 import org.junit.Test
 
 @OptIn(PublicPreviewAPI::class)
@@ -111,6 +112,7 @@ class TemplateIntegrationTests {
   }
 
   @Test
+  @Ignore("Model is prone to return specifically London instead of New York")
   fun testTemplateGroundingCity_googleAI(): Unit = runBlocking {
     val model =
       FirebaseAI.getInstance(AIModels.app(), GenerativeBackend.googleAI())
