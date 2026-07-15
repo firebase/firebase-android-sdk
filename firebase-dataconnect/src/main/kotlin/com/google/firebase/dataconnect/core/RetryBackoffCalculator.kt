21c1b79cd3ccc49f2f337a9908d57a886d8ad40b
Denver Coneybeare <dconeybe@google.com>
2026-07-15 19:58:03 -0400

    RetryBackoffCalculator.kt: add jitter support

--- firebase-dataconnect/src/main/kotlin/com/google/firebase/dataconnect/core/RetryBackoffCalculator.kt
+++ firebase-dataconnect/src/main/kotlin/com/google/firebase/dataconnect/core/RetryBackoffCalculator.kt
@@ -23,8 +23,12 @@ import kotlin.math.roundToLong
  * Calculates exponential backoff durations for connection retries.
  *
  * This class is thread-safe.
+ *
+ * @param getRandomJitter A function that returns a pseudorandom Double value in the range [-0.5,
+ * 0.5) to introduce random jitter into backoff durations. This function must be thread-safe (safe
+ * to call concurrently).
  */
-internal class RetryBackoffCalculator {
+internal class RetryBackoffCalculator(private val getRandomJitter: () -> Double = { 0.0 }) {
   private val nextBackoffMs = AtomicLong(INITIAL_BACKOFF_MS)
 
   /** Resets the backoff duration to the initial backoff value. */
@@ -43,7 +47,8 @@ internal class RetryBackoffCalculator {
       val current = nextBackoffMs.get()
       val next = calculateNextBackoffMs(current)
       if (nextBackoffMs.compareAndSet(current, next)) {
-        return current
+        val jitter = (current * getRandomJitter()).roundToLong()
+        return current + jitter
       }
     }
   }
