0af727ea200ff88cbd643b7658bc00e285abf1a8
AustinBenoit <austinbenoit@google.com>
2026-08-04 21:31:34 -0400

    test(app_check): Update debug token log unit tests to match new message format

--- appcheck/firebase-appcheck-debug/src/test/java/com/google/firebase/appcheck/debug/internal/DebugAppCheckProviderTest.java
+++ appcheck/firebase-appcheck-debug/src/test/java/com/google/firebase/appcheck/debug/internal/DebugAppCheckProviderTest.java
@@ -141,12 +141,24 @@ public class DebugAppCheckProviderTest {
             .anyMatch(
                 log ->
                     log.msg.equals(
-                        "Enter this debug secret ("
-                            + debugSecret
-                            + ") into the allow list in the Firebase Console for your project: "
-                            + String.format(
+                        String.format(
+                            "Firebase App Check debug token: %s%n%n"
+                                + "To use this token for app debugging, register it with your project.%n%n"
+                                + "You can do so in the Firebase Console: %n"
+                                + "%s%n%n"
+                                + "Or using the Firebase CLI: %n"
+                                + "firebase appcheck:debugtokens:create %s --app %s%n%n"
+                                + "Note: To keep your project secure, please revoke and delete this token using the %n"
+                                + "Firebase Console or the CLI (`firebase appcheck:debugtokens:delete`) when you finish debugging.%n%n"
+                                + "Warning: This debug token is a secret and should not be shared or uploaded to source code.%n%n"
+                                + "Debug Token Guide: https://firebase.google.com/docs/app-check/ios/debug-provider%n"
+                                + "Firebase CLI install instructions: https://firebase.google.com/docs/cli",
+                            debugSecret,
+                            String.format(
                                 "https://console.firebase.google.com/project/%s/appcheck/apps?selectedAppId=%s",
-                                PROJECT_ID, APP_ID)));
+                                PROJECT_ID, APP_ID),
+                            debugSecret,
+                            APP_ID)));
     assertThat(foundLog).isTrue();
   }
 
@@ -162,12 +174,24 @@ public class DebugAppCheckProviderTest {
             .anyMatch(
                 log ->
                     log.msg.equals(
-                        "Enter this debug secret ("
-                            + DEBUG_SECRET
-                            + ") into the allow list in the Firebase Console for your project: "
-                            + String.format(
+                        String.format(
+                            "Firebase App Check debug token: %s%n%n"
+                                + "To use this token for app debugging, register it with your project.%n%n"
+                                + "You can do so in the Firebase Console: %n"
+                                + "%s%n%n"
+                                + "Or using the Firebase CLI: %n"
+                                + "firebase appcheck:debugtokens:create %s --app %s%n%n"
+                                + "Note: To keep your project secure, please revoke and delete this token using the %n"
+                                + "Firebase Console or the CLI (`firebase appcheck:debugtokens:delete`) when you finish debugging.%n%n"
+                                + "Warning: This debug token is a secret and should not be shared or uploaded to source code.%n%n"
+                                + "Debug Token Guide: https://firebase.google.com/docs/app-check/ios/debug-provider%n"
+                                + "Firebase CLI install instructions: https://firebase.google.com/docs/cli",
+                            DEBUG_SECRET,
+                            String.format(
                                 "https://console.firebase.google.com/project/%s/appcheck/apps?selectedAppId=%s",
-                                PROJECT_ID, APP_ID)));
+                                PROJECT_ID, APP_ID),
+                            DEBUG_SECRET,
+                            APP_ID)));
     assertThat(foundLog).isTrue();
   }
 
