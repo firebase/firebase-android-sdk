41b9fd58bc229119e9ae1ef1b75dc484f11d6d6f
Eldhose M Babu <eldhosembabu@google.com>
2026-08-10 12:57:36 -0700

    FCM Copybara import fix. (#8523)
    FCM Copybara import fix.

--- firebase-messaging/src/test/java/com/google/firebase/messaging/GmsRegistrationClientRoboTest.java
+++ firebase-messaging/src/test/java/com/google/firebase/messaging/GmsRegistrationClientRoboTest.java
@@ -280,6 +280,7 @@ public class GmsRegistrationClientRoboTest {
   }
 
   @Test
+  @SuppressWarnings("DoNotMock") // Required while importing to G3
   public void testRegister_v1Flow_withSupport_fidAlreadyUsed_retriesAndFails() throws Exception {
     setV1RegistrationEnabled(true);
     when(metadata.getGmsVersionCode()).thenReturn(261200000); // support V1
@@ -306,6 +307,7 @@ public class GmsRegistrationClientRoboTest {
   }
 
   @Test
+  @SuppressWarnings("DoNotMock") // Required while importing to G3
   public void testRegister_v1Flow_withSupport_otherException_doesNotRetry() throws Exception {
     setV1RegistrationEnabled(true);
     when(metadata.getGmsVersionCode()).thenReturn(261200000); // support V1
