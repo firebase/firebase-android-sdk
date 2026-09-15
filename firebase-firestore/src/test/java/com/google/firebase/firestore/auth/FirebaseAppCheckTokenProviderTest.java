57bdbbe18f5fbb0912e5f23a75805e67a877e18d
Rodrigo Lazo Paz <rlazo@google.com>
2026-09-15 18:36:46 -0400

    [Infra] Update dependencies and add transitive bump checker script
    Updated all declared versions in the version catalog and individual
    Gradle files to match their actual resolved versions. Previously, due
    to transitive dependencies, some declared versions differed from the
    ones actually used by Gradle. This change aligns them for better
    clarity on our project's true dependencies.
    
    Work was done by the new `check_transitive_bumps.py` script. It to
    inspect and audit Gradle dependency version bumps caused by transitive
    dependencies across subprojects.
    
    Finally, Implement the `getLimitedUseToken()` method in
    FirebaseAppCheckTokenProviderTest to comply with updated
    InteropAppCheckTokenProvider interface.

--- firebase-firestore/src/test/java/com/google/firebase/firestore/auth/FirebaseAppCheckTokenProviderTest.java
+++ firebase-firestore/src/test/java/com/google/firebase/firestore/auth/FirebaseAppCheckTokenProviderTest.java
@@ -52,6 +52,12 @@ class TestListenersInteropAppCheckTokenProvider implements InteropAppCheckTokenP
     return null;
   }
 
+  @NonNull
+  @Override
+  public Task<AppCheckTokenResult> getLimitedUseToken() {
+    return null;
+  }
+
   @Override
   public void addAppCheckTokenListener(@NonNull AppCheckTokenListener listener) {
     listeners.add(listener);
