1080b15062a96721ab43da434e892d1c2d2309bd
Rodrigo Lazo Paz <rlazo@google.com>
2026-09-23 11:29:03 -0400

    Ensure dependencies are correct by converting them to project
    This will be temporary, only during the release window

--- firebase-dataconnect/firebase-dataconnect.gradle.kts
+++ firebase-dataconnect/firebase-dataconnect.gradle.kts
@@ -107,7 +107,8 @@ dependencies {
   api(libs.firebase.common)
 
   api(project(":firebase-annotations"))
-  implementation("com.google.firebase:firebase-appcheck-interop:17.1.0")
+  // TODO(m187-repin): restore pin on com.google.firebase:firebase-appcheck-interop.
+  implementation(project(":appcheck:firebase-appcheck-interop"))
   implementation("com.google.firebase:firebase-auth-interop:20.0.0")
   implementation(libs.firebase.components)
 
@@ -140,7 +141,8 @@ dependencies {
   androidTestImplementation(project(":firebase-dataconnect:androidTestutil"))
   androidTestImplementation(project(":firebase-dataconnect:connectors"))
   androidTestImplementation(project(":firebase-dataconnect:testutil"))
-  androidTestImplementation("com.google.firebase:firebase-appcheck:18.0.0")
+  // TODO(m187-repin): restore pin on com.google.firebase:firebase-appcheck.
+  androidTestImplementation(project(":appcheck:firebase-appcheck"))
   androidTestImplementation("com.google.firebase:firebase-auth:22.3.1")
   androidTestImplementation(libs.androidx.test.core)
   androidTestImplementation(libs.androidx.test.junit)
